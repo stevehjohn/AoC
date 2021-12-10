2f397127c2d5a0c345c00aff63d4d72e4a80ee91
Stevö <stevehuwjohn@gmail.com>
2021-12-10 22:43:40 +0000

    WIP

---
+++ AoC.Tests/Solutions/TestAllSolutions.cs
@@ -0,0 +1,23 @@
+﻿using AoC.Solutions.Infrastructure;
+using AoC.Solutions.Solutions._2019._01;
+using Xunit;
+
+namespace AoC.Tests.Solutions;
+
+public class TestAllSolutions
+{
+    [Fact]
+    public void RunTests()
+    {
+        var solutions = typeof(Part1).Assembly
+                                     .GetTypes()
+                                     .Where(t => t.IsSubclassOf(typeof(Solution)) && ! t.IsAbstract)
+                                     .OrderBy(t => t.Namespace)
+                                     .ThenBy(t => t.Name);
+
+        foreach (var solution in solutions)
+        {
+            var instance = Activator.CreateInstance(solution) as Solution;
+        }
+    }
+}
