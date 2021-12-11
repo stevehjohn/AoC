b9b50f55ef2dea39a3ca0f51eae8ee337552f6eb
Steve John <steve.john@just-eat.com>
2021-12-11 13:54:40 +0000

    2021.11.1

--- AoC.Solutions/Solutions/2021/11/Part1.cs
+++ AoC.Solutions/Solutions/2021/11/Part1.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2021._11;
@@ -35,8 +36,6 @@ public class Part1 : Solution
             IncrementGrid();
 
             flashes += DoFlashes();
-            
-            ResetFlashers();
         }
 
         return flashes.ToString();
@@ -57,7 +56,9 @@ public class Part1 : Solution
     {
         var flashed = true;
 
-        var flashes = 0;
+        //Dump();
+
+        var pointsFlashed = new List<Point>();
 
         while (flashed)
         {
@@ -67,84 +68,91 @@ public class Part1 : Solution
             {
                 for (var x = 0; x < _width; x++)
                 {
-                    if (_grid[x, y] == 10)
+                    if (_grid[x, y] != 10)
                     {
-                        _grid[x, y]++;
-
-                        flashes++;
-
-                        flashed = true;
-
-                        Flash(x, y);
-
-                        break;
+                        continue;
                     }
 
-                    if (flashed)
+                    if (pointsFlashed.Any(p => p.X == x && p.Y == y))
                     {
-                        break;
+                        continue;
+                    }
+
+                    pointsFlashed.Add(new Point(x, y));
+
+                    var neighbors = GetNeighbors(new Point(x, y));
+
+                    foreach (var neighbor in neighbors)
+                    {
+                        if (_grid[neighbor.X, neighbor.Y] < 10)
+                        {
+                            _grid[neighbor.X, neighbor.Y]++;
+
+                            flashed = true;
+                        }
                     }
                 }
             }
         }
 
-        return flashes;
-    }
+        //Dump();
 
-    private void Flash(int x, int y)
-    {
-        if (y > 0)
-        {
-            if (x > 0)
-            {
-                _grid[x - 1, y - 1]++;
-            }
+        //Console.ReadKey();
 
-            _grid[x, y - 1]++;
+        var flashers = 0;
 
-            if (x < _width - 1)
-            {
-                _grid[x + 1, y - 1]++;
-            }
-        }
-
-        if (x > 0)
-        {
-            _grid[x - 1, y]++;
-        }
-
-        if (x < _width - 1)
-        {
-            _grid[x + 1, y]++;
-        }
-
-        if (y < _height - 1)
-        {
-            if (x > 0)
-            {
-                _grid[x - 1, y + 1]++;
-            }
-
-            _grid[x, y + 1]++;
-
-            if (x < _width - 1)
-            {
-                _grid[x + 1, y + 1]++;
-            }
-        }
-    }
-
-    private void ResetFlashers()
-    {
         for (var y = 0; y < _height; y++)
         {
             for (var x = 0; x < _width; x++)
             {
-                if (_grid[x, y] > 9)
+                if (_grid[x, y] == 10)
                 {
+                    flashers++;
+
                     _grid[x, y] = 0;
                 }
             }
         }
+
+        return flashers;
+    }
+
+    private void Dump()
+    {
+        Console.WriteLine();
+
+        for (var y = 0; y < _height; y++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                Console.ForegroundColor = _grid[x, y] > 9
+                    ? ConsoleColor.White
+                    : ConsoleColor.DarkGray;
+
+                Console.Write("{0,2}", _grid[x, y]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    private List<Point> GetNeighbors(Point point)
+    {
+        var neighbors = new List<Point>();
+
+        for (var x = Math.Max(point.X - 1, 0); x <= Math.Min(point.X + 1, _width - 1); x++)
+        {
+            for (var y = Math.Max(point.Y - 1, 0); y <= Math.Min(point.Y + 1, _height - 1); y++)
+            {
+                if (x == point.X && y == point.Y)
+                {
+                    continue;
+                }
+
+                neighbors.Add(new Point(x, y));
+            }
+        }
+
+        return neighbors;
     }
 }
