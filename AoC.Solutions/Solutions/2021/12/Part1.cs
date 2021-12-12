274a851f6f8137557f7dddcff4b2efffa9fb8109
Stevö <stevehuwjohn@gmail.com>
2021-12-12 13:34:01 +0000

    2021.12.2 WIP

--- AoC.Solutions/Solutions/2021/12/Part1.cs
+++ AoC.Solutions/Solutions/2021/12/Part1.cs
@@ -1,77 +1,45 @@
-﻿using AoC.Solutions.Infrastructure;
-using JetBrains.Annotations;
+﻿//using JetBrains.Annotations;
 
-namespace AoC.Solutions.Solutions._2021._12;
+//namespace AoC.Solutions.Solutions._2021._12;
 
-[UsedImplicitly]
-public class Part1 : Solution
-{
-    private readonly List<Node> _nodes = new();
+//[UsedImplicitly]
+//public class Part1 : Base
+//{
+//    public override string GetAnswer()
+//    {
+//        LoadMap();
 
-    private Node _start;
+//        var pathCount = Visit(Start, new List<Node>());
 
-    public override string GetAnswer()
-    {
-        foreach (var line in Input)
-        {
-            var caves = line.Split('-');
+//        return pathCount.ToString();
+//    }
 
-            var node = _nodes.FirstOrDefault(n => n.Name == caves[0]);
+//    private static int Visit(Node node, List<Node> visited)
+//    {
+//        if (node.IsEnd)
+//        {
+//            return 1;
+//        }
 
-            if (node == null)
-            {
-                node = new Node(caves[0]);
+//        if (! node.IsBig && visited.Contains(node))
+//        {
+//            return 0;
+//        }
 
-                _nodes.Add(node);
-            }
+//        visited.Add(node);
 
-            var connection = _nodes.FirstOrDefault(c => c.Name == caves[1]);
+//        var total = 0;
 
-            if (connection == null)
-            {
-                connection = new Node(caves[1]);
+//        foreach (var connection in node.Connections)
+//        {
+//            if (connection.IsStart)
+//            {
+//                continue;
+//            }
 
-                _nodes.Add(connection);
-            }
+//            total += Visit(connection, visited.ToList());
+//        }
 
-            node.Connections.Add(connection);
-            
-            connection.Connections.Add(node);
-        }
-
-        _start = _nodes.Single(n => n.IsStart);
-
-        var pathCount = Visit(_start, new List<Node>());
-
-        return pathCount.ToString();
-    }
-
-    private static int Visit(Node node, List<Node> visited)
-    {
-        if (node.IsEnd)
-        {
-            return 1;
-        }
-
-        if (! node.IsBig && visited.Contains(node))
-        {
-            return 0;
-        }
-
-        visited.Add(node);
-
-        var total = 0;
-
-        foreach (var connection in node.Connections)
-        {
-            if (connection.IsStart)
-            {
-                continue;
-            }
-
-            total += Visit(connection, visited.ToList());
-        }
-
-        return total;
-    }
-}
+//        return total;
+//    }
+//}
---
+++ AoC.Solutions/Solutions/2021/12/Part2.cs
@@ -0,0 +1,45 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2021._12;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        LoadMap();
+
+        var pathCount = Visit(Start, new List<Node>());
+
+        return pathCount.ToString();
+    }
+
+    private static int Visit(Node node, List<Node> visited)
+    {
+        if (node.IsEnd)
+        {
+            return 1;
+        }
+
+        if (! node.IsBig && visited.Contains(node))
+        {
+            return 0;
+        }
+
+        visited.Add(node);
+
+        var total = 0;
+
+        foreach (var connection in node.Connections)
+        {
+            if (connection.IsStart)
+            {
+                continue;
+            }
+
+            total += Visit(connection, visited.ToList());
+        }
+
+        return total;
+    }
+}
