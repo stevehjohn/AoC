7d537418e0c295f674657a21c87c70662b0225d2
Stevö <stevehuwjohn@gmail.com>
2021-12-12 22:25:23 +0000

    2019.14.1 WIP

---
+++ AoC.Solutions/Solutions/2019/14/Matter.cs
@@ -0,0 +1,8 @@
+﻿namespace AoC.Solutions.Solutions._2019._14;
+
+public class Matter
+{
+    public string Name { get; set; }
+
+    public int Mass { get; set; }
+}
---
+++ AoC.Solutions/Solutions/2019/14/Part1.cs
@@ -0,0 +1,69 @@
+﻿using AoC.Solutions.Infrastructure;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2019._14;
+
+[UsedImplicitly]
+public class Part1 : Solution
+{
+    private readonly List<Reaction> _reactions = new();
+
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        var fuel = _reactions.Single(r => r.Result.Name == "FUEL");
+
+        var total = GetOre(fuel);
+
+        return "TEST";
+    }
+
+    private void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            var io = line.Split("=>", StringSplitOptions.TrimEntries);
+
+            var reaction = new Reaction
+                           {
+                               Result = ParseMatter(io[1])
+                           };
+
+            var input = io[0].Split(',', StringSplitOptions.TrimEntries);
+
+            foreach (var matter in input)
+            {
+                reaction.Materials.Add(ParseMatter(matter));
+            }
+
+            _reactions.Add(reaction);
+        }
+    }
+
+    private Matter ParseMatter(string input)
+    {
+        var parts = input.Split(' ', StringSplitOptions.TrimEntries);
+
+        return new Matter
+               {
+                   Mass = int.Parse(parts[0]),
+                   Name = parts[1]
+               };
+    }
+
+    private int GetOre(Reaction node, int sum = 0)
+    {
+        foreach (var material in node.Materials)
+        {
+            if (material.Name == "ORE")
+            {
+                continue;
+            }
+
+            sum += GetOre(_reactions.Single(r => r.Result.Name == material.Name), sum);
+        }
+
+        return sum;
+    }
+}
