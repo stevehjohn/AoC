dd98ea8788520d5660cf6f345bd4bf43ae1a0f3f
Steve John <steve.john@just-eat.com>
2021-12-15 05:04:22 +0000

    2021.15 WIP

---
+++ AoC.Solutions/Solutions/2021/15/Part1.cs
@@ -0,0 +1,15 @@
+﻿using AoC.Solutions.Infrastructure;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2021._15;
+
+[UsedImplicitly]
+public class Part1 : Solution
+{
+    public override string Description => "";
+
+    public override string GetAnswer()
+    {
+        throw new NotImplementedException();
+    }
+}
