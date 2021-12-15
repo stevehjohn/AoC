e215b48ecd9708a9e6edca308704931861bd0ef5
Stevö <stevehuwjohn@gmail.com>
2021-12-15 17:27:51 +0000

    2021.15.1 WIP

--- AoC.Solutions/Solutions/2021/15/Part1.cs
+++ AoC.Solutions/Solutions/2021/15/Part1.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2021._15;
@@ -6,10 +7,95 @@ namespace AoC.Solutions.Solutions._2021._15;
 [UsedImplicitly]
 public class Part1 : Solution
 {
-    public override string Description => "";
+    public override string Description => "Risky business";
+
+    private int _width;
+
+    private int _height;
+
+    private int[,] _map;
 
     public override string GetAnswer()
     {
-        throw new NotImplementedException();
+        ParseInput();
+
+        var cost = Solve(new Point(0, 0), new List<Point>());
+
+        return "TEST";
+    }
+
+    private void ParseInput()
+    {
+        _width = Input[0].Length;
+
+        _height = Input.Length;
+        
+        _map = new int[_width, _height];
+
+        for (var y = 0; y < _height; y++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                _map[x, y] = (byte) (Input[y][x] - '0');
+            }
+        }
+    }
+    
+    // TODO: Exit if exceeding lowest cost so far.
+    private int Solve(Point position, List<Point> visited, int cost = 0)
+    {
+        var neighbors = GetNeighbors(position);
+
+        visited.Add(position);
+
+        Console.Write($"-> ({position.X}, {position.Y}) ");
+
+        foreach (var neighbor in neighbors)
+        {
+            if (visited.Any(v => v.X == neighbor.X && v.Y == neighbor.Y))
+            {
+                continue;
+            }
+
+            if (neighbor.X == _width - 1 && neighbor.Y == _height - 1)
+            {
+                cost += _map[neighbor.X, neighbor.Y];
+
+                Console.WriteLine($"-> {cost}");
+
+                return cost;
+            }
+
+            Solve(neighbor, visited.ToList(), cost + _map[neighbor.X, neighbor.Y]);
+        }
+
+        return int.MaxValue;
+    }
+
+    private List<Point> GetNeighbors(Point position)
+    {
+        var neighbors = new List<Point>();
+
+        if (position.X < _width - 1)
+        {
+            neighbors.Add(new Point(position.X + 1, position.Y));
+        }
+
+        if (position.Y < _height - 1)
+        {
+            neighbors.Add(new Point(position.X, position.Y + 1));
+        }
+
+        if (position.X > 1)
+        {
+            neighbors.Add(new Point(position.X - 1, position.Y));
+        }
+
+        if (position.Y > 1)
+        {
+            neighbors.Add(new Point(position.X, position.Y - 1));
+        }
+
+        return neighbors;
     }
 }
