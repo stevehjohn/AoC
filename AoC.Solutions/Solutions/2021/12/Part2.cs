be922c3a29ae5fe6c9e02ec0bb12df0669ec4917
Stevö <stevehuwjohn@gmail.com>
2021-12-17 19:37:23 +0000

    Optimising...

--- AoC.Solutions/Solutions/2021/12/Part2.cs
+++ AoC.Solutions/Solutions/2021/12/Part2.cs
@@ -9,29 +9,43 @@ public class Part2 : Base
     {
         LoadMap();
 
-        var pathCount = Visit(Start, new Dictionary<Node, int>());
+        var pathCount = Visit(Start, new List<Node>());
 
         return pathCount.ToString();
     }
 
-    private static int Visit(Node node, Dictionary<Node, int> visited)
+    private static int Visit(Node node, List<Node> visited, Node specialNode = null, int specialNodeCount = 0)
     {
         if (node.IsEnd)
         {
             return 1;
         }
 
-        if (! node.IsBig && visited.ContainsKey(node))
+        if (! node.IsBig && visited.Contains(node))
         {
-            if (visited.Any(v => v.Value > 0))
+            if (specialNode != null && node != specialNode)
             {
                 return 0;
             }
 
-            visited[node]++;
+            if (specialNode == null)
+            {
+                specialNode = node;
+
+                specialNodeCount = 1;
+            }
+            else
+            {
+                specialNodeCount++;
+
+                if (specialNodeCount == 2)
+                {
+                    return 0;
+                }
+            }
         }
 
-        visited.TryAdd(node, 0);
+        visited.Add(node);
 
         var total = 0;
 
@@ -42,7 +56,7 @@ public class Part2 : Base
                 continue;
             }
 
-            total += Visit(connection, visited.ToDictionary(v => v.Key, v => v.Value));
+            total += Visit(connection, visited.ToList(), specialNode, specialNodeCount);
         }
 
         return total;
