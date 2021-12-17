1c3cbd7ce876e084c5c5eee81dcb217113b3ac74
Stevö <stevehuwjohn@gmail.com>
2021-12-17 17:13:52 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/12/Base.cs
@@ -0,0 +1,34 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2019._12;
+
+public abstract class Base : Solution
+{
+    protected readonly List<Moon> Moons = new();
+
+    protected void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            var components = line[1..^1].Split(',', StringSplitOptions.TrimEntries);
+
+            var moon = new Moon
+                       {
+                           Position =
+                           {
+                               X = ParseComponent(components[0]),
+                               Y = ParseComponent(components[1]),
+                               Z = ParseComponent(components[2])
+                           }
+                       };
+
+            Moons.Add(moon);
+        }
+    }
+
+    private static int ParseComponent(string component)
+    {
+        return int.Parse(component.Split('=', StringSplitOptions.TrimEntries)[1]);
+
+    }
+}
---
+++ AoC.Solutions/Solutions/2019/12/Part2.cs
@@ -0,0 +1,14 @@
+﻿//using JetBrains.Annotations;
+
+//namespace AoC.Solutions.Solutions._2019._12;
+
+//[UsedImplicitly]
+//public class Part2 : Base
+//{
+//    public override string Description => "Orbital simulation";
+
+//    public override string GetAnswer()
+//    {
+//        return "TEST";
+//    }
+//}
