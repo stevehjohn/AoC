aa4a56d95407b4508311fc234526f2e897f076fd
Stevö <stevehuwjohn@gmail.com>
2021-12-17 18:39:58 +0000

    Optimising

--- AoC.Solutions/Solutions/2021/15/PriorityQueue.cs
+++ AoC.Solutions/Solutions/2021/15/PriorityQueue.cs
@@ -1,29 +1,29 @@
-﻿namespace AoC.Solutions.Solutions._2021._15;
+﻿//namespace AoC.Solutions.Solutions._2021._15;
 
-public class PriorityQueue<T>
-{
-    private readonly List<(int Priority, T Entry)> _entries;
+//public class PriorityQueue<T>
+//{
+//    private readonly List<(int Priority, T Entry)> _entries;
 
-    public bool IsEmpty => _entries.Count == 0;
+//    public bool IsEmpty => _entries.Count == 0;
 
-    public PriorityQueue()
-    {
-        _entries = new();
-    }
+//    public PriorityQueue()
+//    {
+//        _entries = new();
+//    }
 
-    public void Push(int priority, T item)
-    {
-        _entries.Add((priority, item));
-    }
+//    public void Push(int priority, T item)
+//    {
+//        _entries.Add((priority, item));
+//    }
 
-    public T Pop()
-    {
-        var min = _entries.Min(e => e.Priority);
+//    public T Pop()
+//    {
+//        var min = _entries.Min(e => e.Priority);
 
-        var entry = _entries.First(e => e.Priority == min);
+//        var entry = _entries.First(e => e.Priority == min);
 
-        _entries.Remove(entry);
+//        _entries.Remove(entry);
 
-        return entry.Entry;
-    }
-}
+//        return entry.Entry;
+//    }
+//}
