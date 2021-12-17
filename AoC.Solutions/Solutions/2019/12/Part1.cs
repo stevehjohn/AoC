e0813a5cf7bb711b17e15b857e12780dcd3413ac
Stevö <stevehuwjohn@gmail.com>
2021-12-17 21:29:19 +0000

    WIP

--- AoC.Solutions/Solutions/2019/12/Part1.cs
+++ AoC.Solutions/Solutions/2019/12/Part1.cs
@@ -11,22 +11,7 @@ public class Part1 : Base
 
         for (var step = 0; step < 1000; step++)
         {
-            for (var comparer = 0; comparer < Moons.Count; comparer++)
-            {
-                for (var comparee = comparer + 1; comparee < Moons.Count; comparee++)
-                {
-                    ApplyGravity(Moons[comparer], Moons[comparee]);
-                }
-            }
-
-            for (var i = 0; i < Moons.Count; i++)
-            {
-                Moons[i].Position.X += Moons[i].Velocity.X;
-
-                Moons[i].Position.Y += Moons[i].Velocity.Y;
-                
-                Moons[i].Position.Z += Moons[i].Velocity.Z;
-            }
+            RunCycle();
         }
 
         var energy = Moons.Sum(m => m.Energy);
