f6cf358a558d8d9830598ee1a9fcd44c5bbe091a
Stevö <stevehuwjohn@gmail.com>
2021-12-17 02:38:43 +0000

    Added priority queue

---
+++ AoC.Solutions/Common/PriorityQueue.cs
@@ -0,0 +1,23 @@
+﻿namespace AoC.Solutions.Common;
+
+public class PriorityQueue<T>
+{
+    private readonly List<(int Priority, T Entry)> _entries;
+
+    public PriorityQueue()
+    {
+        _entries = new();
+    }
+
+    public void Push(int priority, T item)
+    {
+        _entries.Add((priority, item));
+    }
+
+    public T Pop()
+    {
+        var min = _entries.Min(e => e.Priority);
+
+        return _entries.First(e => e.Priority == min).Entry;
+    }
+}
