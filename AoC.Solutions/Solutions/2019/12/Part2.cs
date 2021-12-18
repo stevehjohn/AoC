6634ba70595c80f27fbdb8ebb6864411e5d90459
Stevö <stevehuwjohn@gmail.com>
2021-12-18 21:00:34 +0000

    WIP

--- AoC.Solutions/Solutions/2019/12/Part2.cs
+++ AoC.Solutions/Solutions/2019/12/Part2.cs
@@ -1,127 +1,127 @@
-﻿using JetBrains.Annotations;
+﻿//using JetBrains.Annotations;
 
-namespace AoC.Solutions.Solutions._2019._12;
+//namespace AoC.Solutions.Solutions._2019._12;
 
-[UsedImplicitly]
-public class Part2 : Base
-{
-    public override string GetAnswer()
-    {
-        ParseInput();
+//[UsedImplicitly]
+//public class Part2 : Base
+//{
+//    public override string GetAnswer()
+//    {
+//        ParseInput();
 
-        var previousStates = new List<Moon>[Moons.Count];
+//        var previousStates = new List<Moon>[Moons.Count];
 
-        var cycleCounts = new int[Moons.Count, 3];
+//        var cycleCounts = new int[Moons.Count, 3];
 
-        for (var i = 0; i < Moons.Count; i++)
-        {
-            previousStates[i] = new List<Moon>
-                                {
-                                    new(Moons[i])
-                                };
-        }
+//        for (var i = 0; i < Moons.Count; i++)
+//        {
+//            previousStates[i] = new List<Moon>
+//                                {
+//                                    new(Moons[i])
+//                                };
+//        }
 
-        var cycle = -1;
+//        var cycle = -1;
 
-        var found = 0;
+//        var found = 0;
 
-        while (true)
-        {
-            RunCycle();
+//        while (true)
+//        {
+//            RunCycle();
 
-            cycle++;
+//            cycle++;
 
-            for (var i = 0; i < Moons.Count; i++)
-            {
-                if (previousStates[i].Any(m => m.Position.X == Moons[i].Position.X && m.Velocity.X == Moons[i].Velocity.X))
-                {
-                    if (cycleCounts[i, 0] == 0 && cycle > 0)
-                    {
-                        cycleCounts[i, 0] = cycle;
+//            for (var i = 0; i < Moons.Count; i++)
+//            {
+//                if (previousStates[i].Any(m => m.Position.X == Moons[i].Position.X && m.Velocity.X == Moons[i].Velocity.X))
+//                {
+//                    if (cycleCounts[i, 0] == 0 && cycle > 0)
+//                    {
+//                        cycleCounts[i, 0] = cycle;
 
-                        found++;
-                    }
-                }
+//                        found++;
+//                    }
+//                }
 
-                if (previousStates[i].Any(m => m.Position.Y == Moons[i].Position.Y && m.Velocity.Y == Moons[i].Velocity.Y))
-                {
-                    if (cycleCounts[i, 1] == 0 && cycle > 0)
-                    {
-                        cycleCounts[i, 1] = cycle;
+//                if (previousStates[i].Any(m => m.Position.Y == Moons[i].Position.Y && m.Velocity.Y == Moons[i].Velocity.Y))
+//                {
+//                    if (cycleCounts[i, 1] == 0 && cycle > 0)
+//                    {
+//                        cycleCounts[i, 1] = cycle;
 
-                        found++;
-                    }
-                }
+//                        found++;
+//                    }
+//                }
 
-                if (previousStates[i].Any(m => m.Position.Z == Moons[i].Position.Z && m.Velocity.Z == Moons[i].Velocity.Z))
-                {
-                    if (cycleCounts[i, 2] == 0 && cycle > 0)
-                    {
-                        cycleCounts[i, 2] = cycle;
+//                if (previousStates[i].Any(m => m.Position.Z == Moons[i].Position.Z && m.Velocity.Z == Moons[i].Velocity.Z))
+//                {
+//                    if (cycleCounts[i, 2] == 0 && cycle > 0)
+//                    {
+//                        cycleCounts[i, 2] = cycle;
 
-                        found++;
-                    }
-                }
+//                        found++;
+//                    }
+//                }
 
-                previousStates[i].Add(new Moon(Moons[i]));
-            }
+//                previousStates[i].Add(new Moon(Moons[i]));
+//            }
 
-            if (found == Moons.Count * 3)
-            {
-                break;
-            }
-        }
+//            if (found == Moons.Count * 3)
+//            {
+//                break;
+//            }
+//        }
 
-        var values = new List<long>();
+//        var values = new List<long>();
 
-        for (var i = 0; i < Moons.Count; i++)
-        {
-            values.Add(cycleCounts[i, 0]);
+//        for (var i = 0; i < Moons.Count; i++)
+//        {
+//            values.Add(cycleCounts[i, 0]);
 
-            values.Add(cycleCounts[i, 1]);
+//            values.Add(cycleCounts[i, 1]);
             
-            values.Add(cycleCounts[i, 2]);
-        }
+//            values.Add(cycleCounts[i, 2]);
+//        }
 
-        var lowestCommonMultiple = LowestCommonMultiple(values);
+//        var lowestCommonMultiple = LowestCommonMultiple(values);
 
-        return "TESTING";
-    }
+//        return "TESTING";
+//    }
 
-    private static long LowestCommonMultiple(List<long> input)
-    {
-        if (input.Count == 2)
-        {
-            var left = input[0];
+//    private static long LowestCommonMultiple(List<long> input)
+//    {
+//        if (input.Count == 2)
+//        {
+//            var left = input[0];
 
-            var right = input[1];
+//            var right = input[1];
 
-            return left * right / GreatestCommonFactor(left, right);
-        }
+//            return left * right / GreatestCommonFactor(left, right);
+//        }
 
-        var lowestCommonMultiple = LowestCommonMultiple(input.Take(2).ToList());
+//        var lowestCommonMultiple = LowestCommonMultiple(input.Take(2).ToList());
 
-        var remaining = input.Skip(2).ToList();
+//        var remaining = input.Skip(2).ToList();
 
-        remaining.Add(lowestCommonMultiple);
+//        remaining.Add(lowestCommonMultiple);
 
-        return LowestCommonMultiple(remaining);
-    }
+//        return LowestCommonMultiple(remaining);
+//    }
 
-    private static long GreatestCommonFactor(long left, long right)
-    {
-        while (left != right)
-        {
-            if (left > right)
-            {
-                left -= right;
-            }
-            else
-            {
-                right -= left;
-            }
-        }
+//    private static long GreatestCommonFactor(long left, long right)
+//    {
+//        while (left != right)
+//        {
+//            if (left > right)
+//            {
+//                left -= right;
+//            }
+//            else
+//            {
+//                right -= left;
+//            }
+//        }
 
-        return left;
-    }
-}
+//        return left;
+//    }
+//}
