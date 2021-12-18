40a14e95bdbdc177b190e00d8f12eaa5dcc4796b
Stevö <stevehuwjohn@gmail.com>
2021-12-18 00:44:36 +0000

    WIP

--- AoC.Solutions/Solutions/2021/15/Base.cs
+++ AoC.Solutions/Solutions/2021/15/Base.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Solutions.Common;
+﻿#define DUMP
+using AoC.Solutions.Common;
 using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2021._15;
@@ -93,9 +94,23 @@ public abstract class Base : Solution
                         { 0, 0 }
                     };
 
+#if DUMP && DEBUG
+        Console.Clear();
+
+        Console.CursorVisible = false;
+#endif
+
         while (queue.Count > 0)
         {
             var node = queue.Dequeue();
+
+#if DUMP && DEBUG
+            Console.CursorLeft = node.Position.X;
+
+            Console.CursorTop = node.Position.Y;
+
+            Console.Write('█');
+#endif
 
             if (node.Position.X == _width - 1 && node.Position.Y == _height - 1)
             {
@@ -104,6 +119,14 @@ public abstract class Base : Solution
 
             foreach (var neighbor in node.Neighbors)
             {
+#if DUMP && DEBUG
+                Console.CursorLeft = neighbor.Position.X;
+
+                Console.CursorTop = neighbor.Position.Y;
+
+                Console.Write('█');
+#endif
+
                 var cost = costs[node.Position.X + node.Position.Y * _width] + neighbor.Value;
 
                 if (! costs.TryGetValue(neighbor.Position.X + neighbor.Position.Y * _width, out var nextCost) || cost < nextCost)
@@ -115,6 +138,78 @@ public abstract class Base : Solution
             }
         }
 
+        DrawPath(costs);
+
+        Console.CursorLeft = 0;
+
+        Console.CursorTop = _height + 2;
+
         return costs[_width - 1 + (_height - 1) * _width];
     }
+
+#if DUMP && DEBUG
+    private void DrawPath(Dictionary<int, int> costs)
+    {
+        var position = new Point(_width - 1, _height - 1);
+
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+        while (position.X != 0 || position.Y != 0)
+        {
+            Console.CursorLeft = position.Y;
+
+            Console.CursorTop = position.X;
+
+            Console.Write('█');
+
+            var lowest = int.MaxValue;
+
+            var lowestPosition = new Point();
+
+            for (var x = position.X - 1; x < position.X + 2; x += 2)
+            {
+                if (x < 0 || x >= _width)
+                {
+                    continue;
+                }
+
+                if (costs[x + position.Y * _width] < lowest)
+                {
+                    lowestPosition.X = x;
+
+                    lowestPosition.Y = position.Y;
+
+                    lowest = costs[x + position.Y * _width];
+                }
+            }
+
+            for (var y = position.Y - 1; y < position.Y + 2; y += 2)
+            {
+                if (y < 0 || y >= _width)
+                {
+                    continue;
+                }
+
+                if (costs[position.X + y * _width] < lowest)
+                {
+                    lowestPosition.X = position.X;
+
+                    lowestPosition.Y = y;
+
+                    lowest = costs[position.X + y * _width];
+                }
+            }
+
+            position = lowestPosition;
+        }
+
+        Console.CursorLeft = 0;
+
+        Console.CursorTop = 0;
+
+        Console.Write('█');
+
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+#endif
 }
