dc2a99dc6fe3832b36e2301b68d531793191f7e7
Stevö <stevehuwjohn@gmail.com>
2021-12-18 07:04:00 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2021/18/Number.cs
@@ -0,0 +1,45 @@
+﻿namespace AoC.Solutions.Solutions._2021._18;
+
+public class Number
+{
+    public int? Value { get; set; }
+
+    public Number Left { get; set; }
+
+    public Number Right { get; set; }
+
+    public static Number Parse(string input)
+    {
+        var openings = 0;
+
+        var left = string.Empty;
+        
+        var right = string.Empty;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '[')
+            {
+                openings++;
+
+                continue;
+            }
+
+            if (input[i] == ']')
+            {
+                openings--;
+
+                continue;
+            }
+
+            if (input[i] == ',' && openings == 1)
+            {
+                left = input[1..i];
+
+                right = input[(i + 1)..^1];
+            }
+        }
+
+        return null;
+    }
+}
