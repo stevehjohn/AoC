5241376493b904058a78e49672126e35ef99e468
Stevö <stevehuwjohn@gmail.com>
2021-12-19 02:03:39 +0000

    WIP

--- AoC.Solutions/Solutions/2019/14/Part1.cs
+++ AoC.Solutions/Solutions/2019/14/Part1.cs
@@ -5,22 +5,64 @@ namespace AoC.Solutions.Solutions._2019._14;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private List<Reaction> _reactions = new();
+    private readonly List<Reaction> _reactions = new();
 
-    private Dictionary<string, int> _stock = new();
+    private readonly Dictionary<string, int> _stock = new();
+
+    private const string EndMaterial = "FUEL";
+
+    private const string BaseMaterial = "ORE";
 
     public override string GetAnswer()
     {
         ParseInput();
 
-        throw new NotImplementedException();
+        var endMaterial = _reactions.Single(r => r.Name == EndMaterial);
+
+        var ore = GetRequiredOre(endMaterial, endMaterial.AmountCreated);
+
+        return "TESTING";
+    }
+
+    private int GetRequiredOre(Reaction reaction, int requiredAmount)
+    {
+        if (_stock[reaction.Name] >= requiredAmount)
+        {
+            _stock[reaction.Name] -= requiredAmount;
+
+            return 0;
+        }
+
+        var total = 0;
+
+        foreach (var component in reaction.ComponentsRequired)
+        {
+            if (component.Key == BaseMaterial)
+            {
+                total += component.Value;
+
+                _stock[reaction.Name] += reaction.AmountCreated;
+
+                continue;
+            }
+
+            var requirement = _reactions.Single(r => r.Name == component.Key);
+
+            total += GetRequiredOre(requirement, requiredAmount * requirement.AmountCreated);
+        }
+
+        return total;
     }
 
     private void ParseInput()
     {
         foreach (var line in Input)
         {
-            _reactions.Add(ParseLine(line));
+            var reaction = ParseLine(line);
+
+            _reactions.Add(reaction);
+
+            _stock.Add(reaction.Name, 0);
         }
     }
 
