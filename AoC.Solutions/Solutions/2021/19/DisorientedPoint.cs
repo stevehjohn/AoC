dc4edcbb755e33bc78debade79a3bd06f3258eaf
Stevö <stevehuwjohn@gmail.com>
2021-12-20 00:34:51 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2021/19/DisorientedPoint.cs
@@ -0,0 +1,10 @@
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2021._19;
+
+public class DisorientedPoint : Point
+{
+    public DisorientedPoint(int x, int y, int z) : base(x, y, z)
+    {
+    }
+}
