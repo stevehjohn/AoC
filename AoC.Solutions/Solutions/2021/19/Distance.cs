3fc5574ef5adc971e897e0872844c73deefb4a2e
Stevö <stevehuwjohn@gmail.com>
2021-12-20 21:50:51 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2021/19/Distance.cs
@@ -0,0 +1,19 @@
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2021._19;
+
+public class Distance
+{
+    public Point Delta { get; }
+
+    public Point Beacon1 { get; }
+
+    public Point Beacon2 { get; }
+
+    public Distance(Point delta, Point beacon1, Point beacon2)
+    {
+        Delta = delta;
+        Beacon1 = beacon1;
+        Beacon2 = beacon2;
+    }
+}
