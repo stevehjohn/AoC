920f50f6de29d47856e8327787349c4df490f6cb
Stevö <stevehuwjohn@gmail.com>
2021-12-20 00:02:26 +0000

    WIP

--- AoC.Solutions/Solutions/2021/19/Part1.cs
+++ AoC.Solutions/Solutions/2021/19/Part1.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Common;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2021._19;
 
@@ -11,24 +12,45 @@ public class Part1 : Base
     {
         ParseInput();
 
+        for (var s1 = 0; s1 < _scanners.Count; s1++)
+        {
+            if (_scanners[s1].Position == null)
+            {
+                continue;
+            }
+
+            for (var s2 = 0; s2 < _scanners.Count; s2++)
+            {
+                if (s1 == s2 || _scanners[s2].Position != null)
+                {
+                    continue;
+                }
+            }
+        }
+
         return "TESTING";
     }
 
     private void ParseInput()
     {
-        var scanner = new Scanner(0);
+        var scanner = new Scanner(0)
+                      {
+                          Position = new Point()
+                      };
+
+        _scanners.Add(0, scanner);
 
         var scannerIndex = 0;
 
-        foreach (var line in Input)
+        foreach (var line in Input.Skip(1))
         {
             if (line.StartsWith("---"))
             {
+                scannerIndex++;
+
                 scanner = new Scanner(scannerIndex);
 
                 _scanners.Add(scannerIndex, scanner);
-
-                scannerIndex++;
 
                 continue;
             }
