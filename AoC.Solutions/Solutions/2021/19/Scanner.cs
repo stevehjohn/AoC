b48db754541993d3ca8a2400aa9c31e85b8f65d1
Stevö <stevehuwjohn@gmail.com>
2021-12-20 23:52:56 +0000

    WIP

--- AoC.Solutions/Solutions/2021/19/Scanner.cs
+++ AoC.Solutions/Solutions/2021/19/Scanner.cs
@@ -49,26 +49,21 @@ public class Scanner
         // Then just one matching oriented beacon can be used for position?
         if (matchingBeacons != null)
         {
-            foreach (var beacon in matchingBeacons)
-            {
-                //Console.WriteLine($"{beacon.Origin.Beacon1}                  {beacon.Target.Beacon1}");
-
-                //Console.WriteLine($"    {beacon.Origin.Delta}         {beacon.Target.Delta}");
-
-                //Console.WriteLine($"{beacon.Origin.Beacon2}                  {beacon.Target.Beacon2}");
-
-                //Console.WriteLine();
-            }
+            OrientScanner(matchingBeacons);
         }
+    }
+
+    private void OrientScanner(List<DistancePair> pair)
+    {
     }
 
     private List<DistancePair> GetMatchingDistances(Scanner origin)
     {
-        // TODO: Any more optimisations here? Getting 66 results for 12 matches seems ludicrous.
         var distances = new List<DistancePair>();
 
         var matchCount = 0;
 
+        // TODO: Any more optimisations here? Getting 66 results for 12 matches seems ludicrous.
         var matchesRequired = (RequiredMatchesByScanner - 1) * RequiredMatchesByScanner / 2;
 
         for (var ob = 0; ob < origin.Distances.Count; ob++)
@@ -90,7 +85,7 @@ public class Scanner
                 //    continue;
                 //}
 
-                if (CheckDistancesMatchRegardlessOfOrientation(originDistance.Delta, targetDistance.Delta))
+                if (CheckDistanceMatch(originDistance.Delta, targetDistance.Delta))
                 {
                     if (distances.Count < RequiredMatchesForOrientation)
                     {
@@ -110,16 +105,9 @@ public class Scanner
         return null;
     }
 
-    private static bool CheckDistancesMatchRegardlessOfOrientation(Point left, Point right)
+    private static bool CheckDistanceMatch(Point left, Point right)
     {
-        return left.X == right.X && left.Y == right.Y && left.Z == right.Z
-               || left.X == -right.X && left.Y == right.Y && left.Z == right.Z
-               || left.X == right.X && left.Y == -right.Y && left.Z == right.Z
-               || left.X == -right.X && left.Y == -right.Y && left.Z == right.Z
-               || left.X == right.X && left.Y == right.Y && left.Z == -right.Z
-               || left.X == -right.X && left.Y == right.Y && left.Z == -right.Z
-               || left.X == right.X && left.Y == -right.Y && left.Z == -right.Z
-               || left.X == -right.X && left.Y == -right.Y && left.Z == -right.Z;
+        return left.X == right.X && left.Y == right.Y && left.Z == right.Z;
     }
 
     private void CalculateDistances()
@@ -134,15 +122,9 @@ public class Scanner
 
                 var beacon = Beacons[b];
 
-                var distance = new Point(outerBeacon.X - beacon.X, outerBeacon.Y - beacon.Y, outerBeacon.Z - beacon.Z);
+                var distance = new Point(Math.Abs(outerBeacon.X - beacon.X), Math.Abs(outerBeacon.Y - beacon.Y), Math.Abs(outerBeacon.Z - beacon.Z));
 
                 _distances.Add(new Distance(distance, outerBeacon, beacon));
-
-                //Console.WriteLine(Beacons[ob]);
-
-                //Console.WriteLine($"{Beacons[b]} => {distance}");
-
-                //Console.WriteLine();
             }
         }
     }
