57c88e2a6e0b9e10cb4c67b1789f3f217818bae4
Stevö <stevehuwjohn@gmail.com>
2021-12-20 00:49:16 +0000

    WIP

--- AoC.Solutions/Solutions/2021/19/DisorientedPoint.cs
+++ AoC.Solutions/Solutions/2021/19/DisorientedPoint.cs
@@ -4,7 +4,60 @@ namespace AoC.Solutions.Solutions._2021._19;
 
 public class DisorientedPoint : Point
 {
+    public List<Point> Orientations 
+    {
+        get
+        {
+            if (_orientations == null)
+            {
+                CalculateOrientations();
+            }
+
+            return _orientations;
+        }
+    }
+
+    private List<Point> _orientations;
+
     public DisorientedPoint(int x, int y, int z) : base(x, y, z)
     {
+    }
+
+    public bool Matches(DisorientedPoint other)
+    {
+        return Orientations.FirstOrDefault(o => other.Orientations.Any(oo => o == oo)) != null;
+    }
+
+    private void CalculateOrientations()
+    {
+        _orientations = new List<Point>
+                        {
+                            new(-X, Y, Z),
+                            new(X, Y, Z),
+                            new(-X, -Y, Z),
+                            new(X, -Y, Z),
+                            new(-X, Y, -Z),
+                            new(X, Y, -Z),
+                            new(-X, -Y, -Z),
+                            new(X, -Y, -Z),
+
+                            new(-Y, Z, X),
+                            new(Y, Z, X),
+                            new(-Y, -Z, X),
+                            new(Y, -Z, X),
+                            new(-Y, Z, -X),
+                            new(Y, Z, -X),
+                            new(-Y, -Z, -X),
+                            new(Y, -Z, -X),
+
+                            new(-Z, X, Y),
+                            new(Z, X, Y),
+                            new(-Z, -X, Y),
+                            new(Z, -X, Y),
+                            new(-Z, X, -Y),
+                            new(Z, X, -Y),
+                            new(-Z, -X, -Y),
+                            new(Z, -X, -Y)
+                        };
     }
 }
