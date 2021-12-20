4313805a544c07f90c915f16610aeee18b9276d2
Stevö <stevehuwjohn@gmail.com>
2021-12-20 06:29:37 +0000

    WIP

--- AoC.Solutions/Solutions/2021/19/Scanner.cs
+++ AoC.Solutions/Solutions/2021/19/Scanner.cs
@@ -54,13 +54,19 @@ public class Scanner
             _normalisedBeacons.Add(positiveSpacePoint);
         }
 
+        var xCentre = (NormalisedBeacons.Max(b => b.X) - NormalisedBeacons.Min(b => b.X)) / 2;
+        
+        var yCentre = (NormalisedBeacons.Max(b => b.Y) - NormalisedBeacons.Min(b => b.Y)) / 2;
+        
+        var zCentre = (NormalisedBeacons.Max(b => b.Z) - NormalisedBeacons.Min(b => b.Z)) / 2;
+
         foreach (var beacon in NormalisedBeacons)
         {
-            beacon.X = 500 - beacon.X;
+            beacon.X = 100 - beacon.X;
 
-            beacon.Y = 500 - beacon.Y;
+            beacon.Y = 100 - beacon.Y;
             
-            beacon.Z = 500 - beacon.Z;
+            beacon.Z = 100 - beacon.Z;
         }
     }
 
