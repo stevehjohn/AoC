a92cc7d7bc04d675f5bb896395dfe58b3e30ffea
Stevö <stevehuwjohn@gmail.com>
2021-12-21 01:15:50 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2021/20/Part1.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2021._20;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        Enhance();
+
+        Enhance();
+
+        return "TESTING";
+    }
+}
