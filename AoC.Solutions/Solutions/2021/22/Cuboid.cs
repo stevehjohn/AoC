0dae1528cace2bd22fb702cf03baed4289564226
Steve John <steve.john@just-eat.com>
2021-12-25 22:11:55 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2021/22/Cuboid.cs
@@ -0,0 +1,21 @@
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2021._22;
+
+public class Cuboid
+{
+    public Point A { get; }
+
+    public Point B { get; }
+
+    public Cuboid(Point a, Point b)
+    {
+        A = a;
+        B = b;
+    }
+
+    public Cuboid Intersects(Cuboid other)
+    {
+        return null;
+    }
+}
