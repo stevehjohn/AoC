93f5fb00d00149fd8637d5356ed4c01a5a60bbb6
Stevö <stevehuwjohn@gmail.com>
2021-12-27 21:49:05 +0000

    WIP

--- AoC.Solutions/Solutions/2021/22/Cuboid.cs
+++ AoC.Solutions/Solutions/2021/22/Cuboid.cs
@@ -8,10 +8,7 @@ public class Cuboid
 
     public Point B { get; }
 
-    public int Volume
-    {
-        get { return (B.X - A.X + 1) * (B.Y - A.Y + 1) * (B.Z - A.Z + 1); }
-    }
+    public int Volume => (B.X - A.X + 1) * (B.Y - A.Y + 1) * (B.Z - A.Z + 1);
 
     public Cuboid(Point a, Point b)
     {
@@ -21,17 +18,17 @@ public class Cuboid
 
     public Cuboid Intersects(Cuboid other)
     {
-        var aX = LineIntersect(A.X, B.X, other.A.X);
+        var aX = LineIntersect(A.X, B.X, other.A.X, other.B.X);
 
-        var aY = LineIntersect(A.Y, B.Y, other.A.Y);
+        var aY = LineIntersect(A.Y, B.Y, other.A.Y, other.B.Y);
 
-        var aZ = LineIntersect(A.Z, B.Z, other.A.Z);
+        var aZ = LineIntersect(A.Z, B.Z, other.A.Z, other.B.Z);
 
-        var bX = LineIntersect(other.A.X, other.B.X, B.X);
+        var bX = LineIntersect(other.A.X, other.B.X, A.X, B.X);
 
-        var bY = LineIntersect(other.A.Y, other.B.Y, B.Y);
+        var bY = LineIntersect(other.A.Y, other.B.Y, A.Y, B.Y);
 
-        var bZ = LineIntersect(other.A.Z, other.B.Z, B.Z);
+        var bZ = LineIntersect(other.A.Z, other.B.Z, A.Z, B.Z);
 
         if (aX == null || aY == null || aZ == null || bX == null || bY == null || bZ == null)
         {
@@ -41,11 +38,16 @@ public class Cuboid
         return new Cuboid(new Point(aX.Value, aY.Value, aZ.Value), new Point(bX.Value, bY.Value, bZ.Value));
     }
 
-    private static int? LineIntersect(int aLeft, int aRight, int b)
+    private static int? LineIntersect(int aLeft, int aRight, int bLeft, int bRight)
     {
-        if (b >= aLeft && b <= aRight)
+        if (bLeft >= aLeft && bLeft <= aRight)
         {
-            return b;
+            return bLeft;
+        }
+
+        if (bRight >= aLeft && bRight <= aRight)
+        {
+            return bRight;
         }
 
         return null;
