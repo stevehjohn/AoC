4e2606c4dd10e8eb8e0a1b9d4983a26cf9374c7b
Steve John <steve.john@just-eat.com>
2021-12-27 19:09:56 +0000

    WIP

--- AoC.Solutions/Solutions/2021/22/Cuboid.cs
+++ AoC.Solutions/Solutions/2021/22/Cuboid.cs
@@ -16,6 +16,33 @@ public class Cuboid
 
     public Cuboid Intersects(Cuboid other)
     {
+        var aX = LineIntersect(A.X, B.X, other.A.X);
+
+        var aY = LineIntersect(A.Y, B.Y, other.A.Y);
+
+        var aZ = LineIntersect(A.Z, B.Z, other.A.Z);
+
+        var bX = LineIntersect(other.A.X, other.B.X, B.X);
+
+        var bY = LineIntersect(other.A.Y, other.B.Y, B.Y);
+
+        var bZ = LineIntersect(other.A.Z, other.B.Z, B.Z);
+
+        if (aX == null || aY == null || aZ == null || bX == null || bY == null || bZ == null)
+        {
+            return null;
+        }
+
+        return new Cuboid(new Point(aX.Value, aY.Value, aZ.Value), new Point(bX.Value, bY.Value, bZ.Value));
+    }
+
+    private static int? LineIntersect(int aLeft, int aRight, int b)
+    {
+        if (b >= aLeft || b <= aRight)
+        {
+            return b;
+        }
+
         return null;
     }
 }
