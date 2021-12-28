1e6de249e9c8b366106ea7c93c4e677449f6e4ff
Stevö <stevehuwjohn@gmail.com>
2021-12-28 22:13:28 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2021/24/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2021._24;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Model number";
+}
