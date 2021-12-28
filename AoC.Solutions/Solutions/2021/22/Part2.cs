0fbea3ca471a191f4b12b608548787efa5006790
Stevö <stevehuwjohn@gmail.com>
2021-12-28 03:26:45 +0000

    WIP

--- AoC.Solutions/Solutions/2021/22/Part2.cs
+++ AoC.Solutions/Solutions/2021/22/Part2.cs
@@ -3,13 +3,13 @@
 namespace AoC.Solutions.Solutions._2021._22;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
         ParseInput();
 
-        ProcessInput(20);
+        ProcessInput(420);
 
         return GetVolume().ToString();
     }
