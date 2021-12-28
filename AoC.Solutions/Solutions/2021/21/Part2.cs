7ef21cb6390f89ce7a41187960fc3bdd4d4561c1
Stevö <stevehuwjohn@gmail.com>
2021-12-28 20:04:27 +0000

    WIP

--- AoC.Solutions/Solutions/2021/21/Part2.cs
+++ AoC.Solutions/Solutions/2021/21/Part2.cs
@@ -11,61 +11,73 @@ public class Part2 : Base
     {
         ParseInput();
 
+        var player1Wins = GetPlayerWins(true);
+
+        var player2Wins = GetPlayerWins(false);
+
+        return Math.Max(player1Wins, player2Wins).ToString();
+    }
+
+    private long GetPlayerWins(bool evaluatePlayer1)
+    {
         var scores = (Player1: 0, Player2: 0);
 
         var positions = (Player1: Player1Position, Player2: Player2Position);
 
-        var result = Play(scores, positions, true);
-
-        return Math.Max(result.Player1Universes, result.Player2Universes).ToString();
-    }
-
-    private (long Player1Universes, long Player2Universes) Play((int Player1, int Player2) scores, (int Player1, int Player2) positions, bool player1, int universes = 1)
-    {
-        var result = (0L, 0L);
+        var wins = 0L;
 
         foreach (var probability in _rollProbabilities)
         {
-            if (player1)
-            {
-                positions.Player1 += probability.Roll;
-
-                if (positions.Player1 > 10)
-                {
-                    positions.Player1 -= 10;
-                }
-
-                scores.Player1 += positions.Player1;
-            }
-            else
-            {
-                positions.Player2 += probability.Roll;
-
-                if (positions.Player2 > 10)
-                {
-                    positions.Player2 -= 10;
-                }
-
-                scores.Player2 += positions.Player2;
-            }
-
-            if (scores.Player1 >= 21)
-            {
-                return (universes, 0);
-            }
-
-            if (scores.Player2 >= 21)
-            {
-                return (0, universes);
-            }
-
-            var next = Play(scores, positions, ! player1, universes * probability.Probability);
-
-            result.Item1 += next.Player1Universes;
-
-            result.Item2 += next.Player2Universes;
+            wins += Play(scores, positions, true, probability.Roll, evaluatePlayer1) * probability.Probability;
         }
 
-        return result;
+        return wins;
+    }
+
+    private long Play((int Player1, int Player2) scores, (int Player1, int Player2) positions, bool player1Turn, int roll, bool evaluatePlayer1)
+    {
+        var wins = 0L;
+
+        if (player1Turn)
+        {
+            positions.Player1 += roll;
+
+            if (positions.Player1 > 10)
+            {
+                positions.Player1 -= 10;
+            }
+
+            scores.Player1 += positions.Player1;
+        }
+        else
+        {
+            positions.Player2 += roll;
+
+            if (positions.Player2 > 10)
+            {
+                positions.Player2 -= 10;
+            }
+
+            scores.Player2 += positions.Player2;
+        }
+
+        if (scores.Player1 >= 21 || scores.Player2 >= 21)
+        {
+            if (evaluatePlayer1)
+            {
+                return scores.Player1 >= 21 ? 1 : 0;
+            }
+
+            return scores.Player2 >= 21 ? 1 : 0;
+        }
+
+        player1Turn = ! player1Turn;
+
+        foreach (var probability in _rollProbabilities)
+        {
+            wins += Play(scores, positions, player1Turn, probability.Roll, evaluatePlayer1) * probability.Probability;
+        }
+
+        return wins;
     }
 }
