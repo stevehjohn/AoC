b2f12d37b209e94a49c39c82e1e8bd56909b0b47
Stevö <stevehuwjohn@gmail.com>
2021-12-28 23:10:27 +0000

    WIP

--- AoC.Solutions/Solutions/2021/24/Part1.cs
+++ AoC.Solutions/Solutions/2021/24/Part1.cs
@@ -11,49 +11,4 @@ public class Part1 : Base
 
         return result;
     }
-
-    private string GetModelNumber()
-    {
-        var rules = new Stack<(int target, int offset)>();
-
-        var modelNumber = new char[14];
-
-        var target = 0;
-
-        for (var subroutine = 0; subroutine < Input.Length; subroutine += 18)
-        {
-            var check = GetOperand(subroutine + 5);
-
-            if (check > 0)
-            {
-                rules.Push((target++, GetOperand(subroutine + 15)));
-
-                continue;
-            }
-
-            var rule = rules.Pop();
-
-            var totalOffset = rule.offset + check;
-
-            if (totalOffset > 0)
-            {
-                modelNumber[rule.target] = (char) ('9' - totalOffset);
-
-                modelNumber[target++] = '9';
-            }
-            else
-            {
-                modelNumber[rule.target] = '9';
-
-                modelNumber[target++] = (char) ('9' + totalOffset);
-            }
-        }
-
-        return new string(modelNumber);
-    }
-
-    private int GetOperand(int line)
-    {
-        return int.Parse(Input[line].Split(' ')[2]);
-    }
 }
