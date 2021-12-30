7165669360a862f5604b89bc9d14dee93a14db31
Stevö <stevehuwjohn@gmail.com>
2021-12-30 11:09:49 +0000

    WIP

--- AoC.Solutions/Solutions/2021/23/Base.cs
+++ AoC.Solutions/Solutions/2021/23/Base.cs
@@ -7,334 +7,137 @@ public abstract class Base : Solution
 {
     public override string Description => "Hotel amphipod";
 
-    private (int, int)[,] _initialPositions;
+    private int[] _initialAmphipodState;
 
-    private const int Width = 11;
-
-    private const int Height = 3;
-
-    private int _amphipodCount;
-
+    // Stack for each room?
     protected void ParseInput()
     {
-        _initialPositions = new (int, int)[Width, Height];
+        _initialAmphipodState = new int[8];
+
+        var count = 0;
 
         for (var y = 1; y < 4; y++)
         {
-            for (var x = 1; x < Input[y].Length; x++)
+            for (var x = 1; x < 12; x += 2)
             {
-                if (Input[y][x] == '#' || Input[y][x] == '.' || Input[y][x] == ' ')
+                if (x >= Input[y].Length)
                 {
                     continue;
                 }
 
-                _amphipodCount++;
+                var c = Input[y][x];
 
-                _initialPositions[x - 1, y - 1] = ((Input[y][x] - '@') * 2, _amphipodCount);
+                if (c == '#' || c == '.' || c == ' ')
+                {
+                    continue;
+                }
+
+                _initialAmphipodState[count] = Encode(x - 1, y - 1, (c - '@') * 2, count);
+
+                count++;
             }
         }
 
-#if DUMP && DEBUG
-        Dump(_initialPositions);
+#if DEBUG && DUMP
+        Dump(_initialAmphipodState);
 #endif
     }
 
     protected int Solve()
     {
-        var costs = new List<int>();
-
-        for (var i = 1; i <= _amphipodCount; i++)
-        {
-            Console.WriteLine(i);
-
-            costs.Add(TryMove(_initialPositions, i, Enumerable.Repeat(-1, _amphipodCount).ToArray(), new HashSet<int>()));
-        }
-
-        return costs.Min();
+        return 0;
     }
 
-    private int TryMove((int, int)[,] initialPositions, int index, int[] initialHallwayTargets, HashSet<int> states)
+    private int Encode(int x, int y, int home, int id)
     {
-        var positions = new (int Type, int Id)[Width, Height];
+        var result = ((x & 255) << 24)
+                     | ((y & 255) << 16)
+                     | ((home & 255) << 8)
+                     | (id & 255);
 
-        Array.Copy(initialPositions, positions, Width * Height);
+        return result;
+    }
 
-        var hallwayTargets = new int[initialHallwayTargets.Length];
+    private (int X, int Y, int Home, int Id) Decode(int state)
+    {
+        var x = DecodeX(state);
 
-        Array.Copy(initialHallwayTargets, hallwayTargets, initialHallwayTargets.Length);
+        var y = DecodeY(state);
 
-        var home = 0;
+        var home = DecodeHome(state);
+        
+        var id = state & 255;
 
-        // All home?
-        for (var y = 1; y < Height; y++)
-        {
-            for (var x = 2; x < Width - 2; x += 2)
-            {
-                if (positions[x, y].Type == x)
-                {
-                    home++;
-                }
-                //else
-                //{
-                //    goto notHome;
-                //}
-            }
-        }
+        return (x, y, home, id);
+    }
 
-        if (home == _amphipodCount)
-        {
-            return 0;
-        }
+    private int DecodeX(int state)
+    {
+        return (state >> 24) & 255;
+    }
 
-        notHome:
+    private int DecodeY(int state)
+    {
+        return (state >> 16) & 255;
+    }
 
-        var aX = 0;
-
-        var aY = 0;
-
-        // Find amphipod of index.
-        while (true)
-        {
-            if (positions[aX, aY].Id == index)
-            {
-                break;
-            }
-
-            aX++;
-
-            if (aX == Width)
-            {
-                aX = 0;
-
-                aY++;
-            }
-        }
-
-        var type = positions[aX, aY].Type;
-
-        var cost = 0;
-
-        var moved = false;
-
-        // Is home (and not blocking another in)?
-        if (aX == type)
-        {
-            if (aY == 2 || aY == 1 && positions[aX, 2].Type == type)
-            {
-                goto next;
-            }
-        }
-
-        // In hallway or burrow, can get home?
-        if (positions[type, 2].Type == 0)
-        {
-            cost = CostToGetTo(positions, aX, aY, type, 2);
-
-            if (cost > 0)
-            {
-                MoveTo(positions, aX, aY, type, 2);
-
-                moved = true;
-
-                goto next;
-            }
-        }
-        else if (positions[type, 1].Type == 0 && positions[type, 2].Type == type)
-        {
-            cost = CostToGetTo(positions, aX, aY, type, 1);
-
-            if (cost > 0)
-            {
-                MoveTo(positions, aX, aY, type, 1);
-
-                moved = true;
-
-                goto next;
-            }
-        }
-
-        // In burrow, can get to hallway?
-        var targetX = ++hallwayTargets[index - 1];
-
-        if (targetX >= Width)
-        {
-            return int.MaxValue;
-        }
-
-        if (targetX is 2 or 4 or 6 or 8)
-        {
-            targetX++;
-
-            hallwayTargets[index - 1]++;
-        }
-
-        cost = CostToGetTo(positions, aX, aY, targetX, 0);
-
-        if (cost > 0)
-        {
-            MoveTo(positions, aX, aY, targetX, 0);
-
-            moved = true;
-        }
-
-        next:
+    private int DecodeHome(int state)
+    {
+        return (state >> 8) & 255;
+    }
 
 #if DEBUG && DUMP
-        //Console.ReadKey();
+    private void Dump(int[] state)
+    {
+        Console.WriteLine(" #############");
 
-        //Thread.Sleep(10);
+        Console.Write(" #");
 
-        //Console.CursorVisible = false;
-
-        //Console.CursorTop = 1;
-
-        //Dump(positions);
-#endif
-
-        var hash = 0;
-
-        for (var y = 0; y < Height; y++)
+        for (var x = 0; x < 11; x++)
         {
-            for (var x = 0; x < Width; x++)
-            {
-                hash = HashCode.Combine(hash, positions[x, y].Type);
+            var pod = state.SingleOrDefault(s => DecodeX(s) == x && DecodeY(s) == 0);
 
-                hash = HashCode.Combine(hash, positions[x, y].Id);
+            if (pod == 0)
+            {
+                Console.Write('.');
+
+                continue;
             }
+
+            Console.Write(GetType(pod));
         }
 
-        if (states.Contains(hash))
-        {
-            return int.MaxValue;
-        }
-        else
-        {
-            states.Add(hash);
-        }
+        Console.WriteLine('#');
 
-        //cost *= GetCostMultiplier(type);
-        
-        if (moved)
+        for (var y = 1; y < 3; y++)
         {
-            var costs = new List<int>();
+            Console.Write(y == 1 ? " ###" : "   #");
 
-            for (var i = 1; i <= _amphipodCount; i++)
+            for (var x = 2; x < 10; x += 2)
             {
-                var subCost = TryMove(positions, i, hallwayTargets, states);
+                var pod = state.SingleOrDefault(s => DecodeX(s) == x && DecodeY(s) == y);
 
-                if (subCost < int.MaxValue)
+                if (pod == 0)
                 {
-                    costs.Add(subCost);
-                }
-            }
-
-            if (costs.Any())
-            {
-                cost += costs.Min();
-            }
-            else
-            {
-                return int.MaxValue;
-            }
-        }
-        else
-        {
-            return int.MaxValue;
-        }
-
-        states.Remove(hash);
-
-        return cost;
-    }
-
-    private static void MoveTo((int Type, int Id)[,] positions, int startX, int startY, int endX, int endY)
-    {
-        positions[endX, endY].Type = positions[startX, startY].Type;
-
-        positions[endX, endY].Id = positions[startX, startY].Id;
-
-        positions[startX, startY].Type = 0;
-
-        positions[startX, startY].Id = 0;
-    }
-
-    private static int CostToGetTo((int Type, int)[,] positions, int startX, int startY, int endX, int endY)
-    {
-        var cost = 0;
-
-        while (startY > 0)
-        {
-            startY--;
-
-            if (positions[startX, startY].Type != 0)
-            {
-                return 0;
-            }
-
-            cost++;
-        }
-
-        while (startX != endX)
-        {
-            startX += startX < endX ? 1 : -1;
-
-            if (positions[startX, startY].Type != 0)
-            {
-                return 0;
-            }
-
-            cost++;
-        }
-
-        while (startY < endY)
-        {
-            startY++;
-
-            if (positions[startX, startY].Type != 0)
-            {
-                return 0;
-            }
-
-            cost++;
-        }
-
-        return cost;
-    }
-
-    private static int GetCostMultiplier(int type)
-    {
-        return type switch
-        {
-            4 => 10,
-            6 => 100,
-            8 => 1000,
-            _ => 1
-        };
-    }
-
-#if DUMP && DEBUG
-    private static void Dump((int Type, int Id)[,] positions)
-    {
-        for (var y = 0; y < Height; y++)
-        {
-            Console.Write("..");
-
-            for (var x = 0; x < Width; x++)
-            {
-                if (positions[x, y].Type == 0)
-                {
-                    Console.Write(y > 0 && x != 2 && x != 4 && x != 6 && x != 8 ? ".." : "  ");
+                    Console.Write(".#");
 
                     continue;
                 }
 
-                Console.Write($"{(char) (positions[x, y].Type / 2 + '@')}{positions[x, y].Id}");
+                Console.Write($"{GetType(pod)}#");
             }
 
-            Console.WriteLine("..");
+            Console.WriteLine(y == 1 ? "##" : string.Empty);
         }
 
+        Console.WriteLine("   #########");
+
         Console.WriteLine();
+    }
+
+    private char GetType(int home)
+    {
+        return (char) ('@' + (char) (DecodeHome(home) / 2));
     }
 #endif
 }
