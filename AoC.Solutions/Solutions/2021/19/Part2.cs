0b90b67d301692a7fa948030ddf7155fd7d14056
Stevö <stevehuwjohn@gmail.com>
2022-01-02 11:46:40 +0000

    2021 done!

--- AoC.Solutions/Solutions/2021/19/Part2.cs
+++ AoC.Solutions/Solutions/2021/19/Part2.cs
@@ -39,13 +39,34 @@ public class Part2 : Base
             }
         }
 
+        var highestDistance = 0;
+
+        for (var s1 = 0; s1 < Scanners.Count; s1++)
+        {
+            var originScanner = Scanners[s1];
+
+            for (var s2 = 0; s2 < Scanners.Count; s2++)
+            {
+                if (s1 == s2)
+                {
+                    continue;
+                }
+
+                var distance = originScanner.GetManhattanDistanceFrom(Scanners[s2]);
+
+                if (distance > highestDistance)
+                {
+                    highestDistance = distance;
+                }
+            }
+        }
 #if DEBUG
-        for (var s = 0; s < Scanners.Count; s++)
+                for (var s = 0; s < Scanners.Count; s++)
         {
             Console.WriteLine(Scanners[s].Position);
         }
 #endif
 
-        return "TESTING";
+        return highestDistance.ToString();
     }
 }
