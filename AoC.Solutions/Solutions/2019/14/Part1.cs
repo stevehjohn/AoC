b1a353eba3222fa1b280fb4e886eed7f36ab6f2f
Stevö <stevehuwjohn@gmail.com>
2022-01-05 14:31:31 +0000

    Moving to personal AoC login input

--- AoC.Solutions/Solutions/2019/14/Part1.cs
+++ AoC.Solutions/Solutions/2019/14/Part1.cs
@@ -1,65 +1,65 @@
-﻿using JetBrains.Annotations;
+﻿//using JetBrains.Annotations;
 
-namespace AoC.Solutions.Solutions._2019._14;
+//namespace AoC.Solutions.Solutions._2019._14;
 
-[UsedImplicitly]
-public class Part1 : Base
-{
-    private readonly List<Material> _materials = new();
+//[UsedImplicitly]
+//public class Part1 : Base
+//{
+//    private readonly List<Material> _materials = new();
 
-    private readonly Dictionary<string, int> _stock = new();
+//    private readonly Dictionary<string, int> _stock = new();
 
-    private const string EndMaterial = "FUEL";
+//    private const string EndMaterial = "FUEL";
 
-    private const string BaseMaterial = "ORE";
+//    private const string BaseMaterial = "ORE";
 
-    private int _total;
+//    private int _total;
 
-    public override string GetAnswer()
-    {
-        ParseInput();
+//    public override string GetAnswer()
+//    {
+//        ParseInput();
 
-        var endMaterial = _materials.Single(r => r.Name == EndMaterial);
+//        var endMaterial = _materials.Single(r => r.Name == EndMaterial);
 
-        _total = 0;
+//        _total = 0;
 
-        return _total.ToString();
-    }
+//        return _total.ToString();
+//    }
 
-    private void ParseInput()
-    {
-        foreach (var line in Input)
-        {
-            var reaction = ParseLine(line);
+//    private void ParseInput()
+//    {
+//        foreach (var line in Input)
+//        {
+//            var reaction = ParseLine(line);
 
-            _materials.Add(reaction);
+//            _materials.Add(reaction);
 
-            _stock.Add(reaction.Name, 0);
-        }
-    }
+//            _stock.Add(reaction.Name, 0);
+//        }
+//    }
 
-    private static Material ParseLine(string line)
-    {
-        var io = line.Split("=>", StringSplitOptions.TrimEntries);
+//    private static Material ParseLine(string line)
+//    {
+//        var io = line.Split("=>", StringSplitOptions.TrimEntries);
 
-        var material = ParseComponent(io[1]);
+//        var material = ParseComponent(io[1]);
 
-        var inputs = io[0].Split(',', StringSplitOptions.TrimEntries);
+//        var inputs = io[0].Split(',', StringSplitOptions.TrimEntries);
 
-        foreach (var input in inputs)
-        {
-            var component = ParseComponent(input);
+//        foreach (var input in inputs)
+//        {
+//            var component = ParseComponent(input);
 
-            material.Components.Add(component);
-        }
+//            material.Components.Add(component);
+//        }
 
-        return material;
-    }
+//        return material;
+//    }
 
-    private static Material ParseComponent(string component)
-    {
-        var split = component.Split(' ', StringSplitOptions.TrimEntries);
+//    private static Material ParseComponent(string component)
+//    {
+//        var split = component.Split(' ', StringSplitOptions.TrimEntries);
 
-        return new Material(split[1], int.Parse(split[0]));
-    }
-}
+//        return new Material(split[1], int.Parse(split[0]));
+//    }
+//}
