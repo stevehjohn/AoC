fd776dfe6f2b5259ea6646c11317f45e85a1ccef
Stevö <stevehuwjohn@gmail.com>
2022-01-06 13:25:30 +0000

    WIP

--- AoC.Solutions/Solutions/2019/15/Part1.cs
+++ AoC.Solutions/Solutions/2019/15/Part1.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Solutions.Common;
+﻿#define DUMP
+using AoC.Solutions.Common;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2019._15;
@@ -8,19 +9,23 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
+        GetMap();
+
+#if DEBUG && DUMP
         Console.Clear();
 
         Console.CursorVisible = false;
 
         Console.ForegroundColor = ConsoleColor.DarkGreen;
 
-        GetMap();
-
         Dump();
+#endif
 
         var shortestPath = FindShortestRoute();
-        
+
+#if DEBUG && DUMP
         Console.ForegroundColor = ConsoleColor.Green;
+#endif
 
         return shortestPath.ToString();
     }
@@ -29,7 +34,7 @@ public class Part1 : Base
     {
         var bots = new List<Bot>
                    {
-                       new (new Point(Origin), new Point(Destination), Map)
+                       new(new Point(Origin), new Point(Destination), Map)
                    };
 
         while (true)
@@ -48,16 +53,19 @@ public class Part1 : Base
 
             bots = newBots;
 
+#if DEBUG && DUMP
             Dump(bots);
 
-            Thread.Sleep(50);
+            Thread.Sleep(25);
+#endif
         }
     }
 
+#if DEBUG && DUMP
     private void Dump(List<Bot> bots = null)
     {
         Console.CursorTop = 1;
-        
+
         Console.CursorLeft = 0;
 
         for (var y = 0; y < Width; y++)
@@ -122,4 +130,5 @@ public class Part1 : Base
 
         Console.WriteLine();
     }
+#endif
 }
