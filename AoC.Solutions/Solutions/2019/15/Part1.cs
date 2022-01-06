d24bc470e65b5096cec3ff155c3256a9b67bebf8
Stevö <stevehuwjohn@gmail.com>
2022-01-06 12:20:53 +0000

    WIP

--- AoC.Solutions/Solutions/2019/15/Part1.cs
+++ AoC.Solutions/Solutions/2019/15/Part1.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Common;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2019._15;
 
@@ -11,14 +12,45 @@ public class Part1 : Base
 
         Console.CursorVisible = false;
 
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+
         GetMap();
 
         Dump();
 
-        return "TESTING";
+        var shortestPath = FindShortestRoute();
+        
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        return shortestPath.ToString();
     }
 
-    private void Dump()
+    private int FindShortestRoute()
+    {
+        var bots = new List<Bot>
+                   {
+                       new (new Point(Origin), new Point(Destination), Map)
+                   };
+
+        while (true)
+        {
+            foreach (var bot in bots)
+            {
+                if (bot.IsHome)
+                {
+                    return bot.Steps;
+                }
+
+                bot.Move();
+            }
+
+            Dump(bots);
+
+            Thread.Sleep(50);
+        }
+    }
+
+    private void Dump(List<Bot> bots = null)
     {
         Console.CursorTop = 1;
         
@@ -30,16 +62,50 @@ public class Part1 : Base
 
             for (var x = 0; x < Height; x++)
             {
+                if (bots != null)
+                {
+                    var found = false;
+
+                    foreach (var bot in bots)
+                    {
+                        if (x == bot.Position.X && y == bot.Position.Y)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+
+                            Console.Write('█');
+
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                            found = true;
+
+                            break;
+                        }
+                    }
+
+                    if (found)
+                    {
+                        continue;
+                    }
+                }
+
                 if (x == Origin.X && y == Origin.Y)
                 {
+                    Console.ForegroundColor = ConsoleColor.White;
+
                     Console.Write('S');
+
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
 
                     continue;
                 }
 
                 if (x == Destination.X && y == Destination.Y)
                 {
+                    Console.ForegroundColor = ConsoleColor.White;
+
                     Console.Write('O');
+
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
 
                     continue;
                 }
