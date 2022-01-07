abc1c89737be5d857cdd74b45cbcb1c42ae9327e
Stevö <stevehuwjohn@gmail.com>
2022-01-07 10:33:24 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/14/Component.cs
@@ -0,0 +1,15 @@
+﻿namespace AoC.Solutions.Solutions._2019._14;
+
+public class Component
+{
+    public int QuantityRequired { get; }
+
+    public Material Material { get; }
+
+    public Component(int quantityRequired, Material material)
+    {
+        QuantityRequired = quantityRequired;
+
+        Material = material;
+    }
+}
