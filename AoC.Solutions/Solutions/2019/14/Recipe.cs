6b7fa0061b57b9199444642f242c71ce9fba14d0
Stevö <stevehuwjohn@gmail.com>
2022-01-08 10:20:39 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/14/Recipe.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC.Solutions.Solutions._2019._14;
+
+public class Recipe
+{
+    public string Name { get; }
+
+    public int AmountProduced { get; }
+
+    public List<Ingredient> Ingredients { get; }
+
+    public Recipe(string name, int amountProduced)
+    {
+        Name = name;
+
+        AmountProduced = amountProduced;
+
+        Ingredients = new List<Ingredient>();
+    }
+}
