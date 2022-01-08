3b0f963a840c92a4b66ff150fc9473853821ec19
Stevö <stevehuwjohn@gmail.com>
2022-01-08 14:02:25 +0000

    Quicker

--- AoC.Solutions/Solutions/2019/17/Base.cs
+++ AoC.Solutions/Solutions/2019/17/Base.cs
@@ -1,8 +1,33 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using System.Text;
+using AoC.Solutions.Extensions;
+using AoC.Solutions.Infrastructure;
+using AoC.Solutions.Solutions._2019.Computer;
 
 namespace AoC.Solutions.Solutions._2019._17;
 
 public abstract class Base : Solution
 {
     public override string Description => "Scaffolding vacuum robot";
+
+    protected char[,] Map;
+
+    protected void GetMap()
+    {
+        var cpu = new Cpu();
+
+        cpu.Initialise(65536);
+
+        cpu.LoadProgram(Input);
+
+        cpu.Run();
+
+        var output = new StringBuilder();
+
+        while (cpu.UserOutput.Count > 0)
+        {
+            output.Append((char) cpu.UserOutput.Dequeue());
+        }
+
+        Map = output.ToString().Trim().To2DArray();
+    }
 }
--- AoC.Solutions/Solutions/2019/17/Part1.cs
+++ AoC.Solutions/Solutions/2019/17/Part1.cs
@@ -1,7 +1,4 @@
-﻿using System.Text;
-using AoC.Solutions.Extensions;
-using AoC.Solutions.Solutions._2019.Computer;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2019._17;
 
@@ -10,35 +7,20 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        var cpu = new Cpu();
-
-        cpu.Initialise(65536);
-
-        cpu.LoadProgram(Input);
-
-        cpu.Run();
-
-        var output = new StringBuilder();
-
-        while (cpu.UserOutput.Count > 0)
-        {
-            output.Append((char) cpu.UserOutput.Dequeue());
-        }
-
-        var map = output.ToString().Trim().To2DArray();
+        GetMap();
 
         var alignments = 0;
 
-        for (var y = 1; y < map.GetLength(1) - 1; y++)
+        for (var y = 1; y < Map.GetLength(1) - 1; y++)
         {
-            for (var x = 1; x < map.GetLength(0) - 1; x++)
+            for (var x = 1; x < Map.GetLength(0) - 1; x++)
             {
-                if (map[x, y] != '#')
+                if (Map[x, y] != '#')
                 {
                     continue;
                 }
 
-                if (map[x - 1, y] == '#' && map[x, y - 1] == '#' && map[x + 1, y] == '#' && map[x, y + 1] == '#')
+                if (Map[x - 1, y] == '#' && Map[x, y - 1] == '#' && Map[x + 1, y] == '#' && Map[x, y + 1] == '#')
                 {
                     alignments += x * y;
                 }
