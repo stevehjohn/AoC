91567e0fac7a6777a22ecc9115a65036999a1dfb
Stevö <stevehuwjohn@gmail.com>
2022-01-08 17:28:07 +0000

    WIP

--- AoC.Solutions/Solutions/2019/17/Part2.cs
+++ AoC.Solutions/Solutions/2019/17/Part2.cs
@@ -1,8 +1,8 @@
-﻿using System.Text;
-using AoC.Solutions.Common;
+﻿using AoC.Solutions.Common;
 using AoC.Solutions.Exceptions;
 using JetBrains.Annotations;
-using Microsoft.VisualBasic;
+using System.Text;
+using AoC.Solutions.Solutions._2019.Computer;
 
 namespace AoC.Solutions.Solutions._2019._17;
 
@@ -13,31 +13,39 @@ public class Part2 : Base
     {
         GetMap();
 
-        var newMap = new char[Width + 2, Height + 2];
+        ExpandMapByOne();
 
-        for (var y = 0; y < Height + 2; y++)
+        var steps = GetSteps();
+
+        var cpu = new Cpu();
+
+        cpu.Initialise(65536);
+
+        cpu.LoadProgram(Input);
+
+        cpu.Memory[0] = 2;
+
+        var userInput = "A,B,A,C,B,C,B,C,A,C\n";
+
+        userInput += "R,12,L,6,R,12\n";
+
+        userInput += "L,8,L,6,L,10\n";
+
+        userInput += "R,12,L,10,L,6,R,10\n";
+
+        userInput += "n\n";
+
+        foreach (var c in Encoding.ASCII.GetBytes(userInput))
         {
-            for (var x = 0; x < Width + 2; x++)
-            {
-                if (x == 0 || y == 0 || x == Width + 1 || y == Height + 1)
-                {
-                    newMap[x, y] = '.';
-
-                    continue;
-                }
-
-                newMap[x, y] = Map[x - 1, y - 1];
-            }
+            cpu.UserInput.Enqueue(c);
         }
 
-        Map = newMap;
+        cpu.Run();
 
-        GetSteps();
-
-        return "TESTING";
+        return cpu.UserOutput.Dequeue().ToString();
     }
 
-    private void GetSteps()
+    private string GetSteps()
     {
         var bot = GetBot();
 
@@ -74,6 +82,8 @@ public class Part2 : Base
 
             bot.Position.Y += bot.Direction.Y;
         }
+
+        return commands.ToString()[1..];
     }
 
     private (Point Direction, char Command)? TryTurn((Point Position, Point Direction) bot)
@@ -132,5 +142,27 @@ public class Part2 : Base
         }
 
         throw new PuzzleException("Bot not found.");
+    }
+
+    private void ExpandMapByOne()
+    {
+        var newMap = new char[Width + 2, Height + 2];
+
+        for (var y = 0; y < Height + 2; y++)
+        {
+            for (var x = 0; x < Width + 2; x++)
+            {
+                if (x == 0 || y == 0 || x == Width + 1 || y == Height + 1)
+                {
+                    newMap[x, y] = '.';
+
+                    continue;
+                }
+
+                newMap[x, y] = Map[x - 1, y - 1];
+            }
+        }
+
+        Map = newMap;
     }
 }
