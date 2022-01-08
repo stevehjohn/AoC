58a0fbe4998beef31aec00aac12567befab510a4
Stevö <stevehuwjohn@gmail.com>
2022-01-08 10:43:10 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/14/Part2.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2019._14;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        var fuel = 0;
+
+        var ore = 0L;
+
+        while (true)
+        {
+            ore += CreateFuel();
+
+            if (ore > 1_000_000_000_000)
+            {
+                break;
+            }
+
+            fuel++;
+        }
+
+        return fuel.ToString();
+    }
+}
