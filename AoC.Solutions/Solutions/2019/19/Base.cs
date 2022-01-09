17edd08d27ab1df42370e88de84fc0594317c015
Stevö <stevehuwjohn@gmail.com>
2022-01-09 21:53:33 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/19/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2019._19;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Tractorrr";
+}
