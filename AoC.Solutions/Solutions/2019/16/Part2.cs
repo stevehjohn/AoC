ec09e70b6d1e92ba2401d3329d695a9ed0b4550c
Stevö <stevehuwjohn@gmail.com>
2022-01-09 20:42:19 +0000

    WIP

--- AoC.Solutions/Solutions/2019/16/Part2.cs
+++ AoC.Solutions/Solutions/2019/16/Part2.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2019._16;
 
@@ -8,38 +7,28 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var pattern = new[] { 0, 1, 0, -1 };
-
         var data = string.Concat(Enumerable.Repeat(Input[0], 10_000)).Select(c => byte.Parse(c.ToString())).ToArray();
-        //var data = string.Concat(Enumerable.Repeat("03036732577212944063491565474664", 10_000)).Select(c => byte.Parse(c.ToString())).ToArray();
 
         var offset = int.Parse(Input[0][..7]);
-        //var offset = 303673;
 
         for (var p = 0; p < 100; p++)
         {
-            for (var o = offset; o < data.Length; o++)
+            var sum = 0;
+
+            int i;
+
+            for (i = offset; i < data.Length; i++)
             {
-                var digit = 0;
+                sum += data[i];
+            }
 
-                var patternIndex = 0;
+            for (i = offset; i < data.Length; i++)
+            {
+                var temp = data[i];
 
-                for (var i = o; i < data.Length; i++)
-                {
-                    if ((i + 1) % (o + 1) == 0)
-                    {
-                        patternIndex++;
+                data[i] = (byte) (sum % 10);
 
-                        if (patternIndex > 3)
-                        {
-                            patternIndex = 0;
-                        }
-                    }
-
-                    digit += data[i] * pattern[patternIndex];
-                }
-
-                data[o] = (byte) (Math.Abs(digit) % 10);
+                sum -= temp;
             }
         }
 
