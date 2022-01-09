5a683b317c1f2a2b731657b004e720aac682e986
Stevö <stevehuwjohn@gmail.com>
2022-01-09 21:23:27 +0000

    WIP

--- AoC.Solutions/Solutions/2019/17/Base.cs
+++ AoC.Solutions/Solutions/2019/17/Base.cs
@@ -7,7 +7,7 @@ namespace AoC.Solutions.Solutions._2019._17;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Scaffolding vacuum robot";
+    public override string Description => "Scaffolding vacuum robot (CPU used unmodified)";
 
     protected char[,] Map;
 
