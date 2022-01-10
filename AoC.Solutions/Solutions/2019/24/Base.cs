6ad30780df21e2c4c59ed24022f031174b40ab93
Stevö <stevehuwjohn@gmail.com>
2022-01-10 23:36:57 +0000

    WIP

--- AoC.Solutions/Solutions/2019/24/Base.cs
+++ AoC.Solutions/Solutions/2019/24/Base.cs
@@ -1,53 +1,10 @@
-﻿using AoC.Solutions.Common;
-using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2019._24;
 
 public abstract class Base : Solution
 {
     public override string Description => "Wastl's game of life";
-
-    protected static void PlayRound(bool[,] grid)
-    {
-        var dies = new List<Point>();
-
-        var infests = new List<Point>();
-
-        for (var y = 1; y < 6; y++)
-        {
-            for (var x = 1; x < 6; x++)
-            {
-                var adjacent = AdjacentCount(grid, x, y);
-
-                if (grid[x, y] && adjacent != 1)
-                {
-                    dies.Add(new Point(x, y));
-                }
-
-                if (! grid[x, y] && (adjacent == 1 || adjacent == 2))
-                {
-                    infests.Add(new Point(x, y));
-                }
-            }
-        }
-
-        dies.ForEach(d => grid[d.X, d.Y] = false);
-
-        infests.ForEach(i => grid[i.X, i.Y] = true);
-    }
-
-    private static int AdjacentCount(bool[,] grid, int x, int y)
-    {
-        var count = grid[x - 1, y] ? 1 : 0;
-
-        count += grid[x, y - 1] ? 1 : 0;
-
-        count += grid[x + 1, y] ? 1 : 0;
-
-        count += grid[x, y + 1] ? 1 : 0;
-
-        return count;
-    }
 
     protected bool[,] ParseInput()
     {
--- AoC.Solutions/Solutions/2019/24/Part1.cs
+++ AoC.Solutions/Solutions/2019/24/Part1.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Common;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2019._24;
 
@@ -27,6 +28,48 @@ public class Part1 : Base
 
             previousStates.Add(bioDiversity);
         }
+    }
+
+    protected static void PlayRound(bool[,] grid)
+    {
+        var dies = new List<Point>();
+
+        var infests = new List<Point>();
+
+        for (var y = 1; y < 6; y++)
+        {
+            for (var x = 1; x < 6; x++)
+            {
+                var adjacent = AdjacentCount(grid, x, y);
+
+                if (grid[x, y] && adjacent != 1)
+                {
+                    dies.Add(new Point(x, y));
+                }
+
+                if (! grid[x, y] && (adjacent == 1 || adjacent == 2))
+                {
+                    infests.Add(new Point(x, y));
+                }
+            }
+        }
+
+        dies.ForEach(d => grid[d.X, d.Y] = false);
+
+        infests.ForEach(i => grid[i.X, i.Y] = true);
+    }
+
+    private static int AdjacentCount(bool[,] grid, int x, int y)
+    {
+        var count = grid[x - 1, y] ? 1 : 0;
+
+        count += grid[x, y - 1] ? 1 : 0;
+
+        count += grid[x + 1, y] ? 1 : 0;
+
+        count += grid[x, y + 1] ? 1 : 0;
+
+        return count;
     }
 
     private static int GetBiodiversity(bool[,] grid)
