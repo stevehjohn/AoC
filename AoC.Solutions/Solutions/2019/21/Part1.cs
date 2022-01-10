8e0712f542fbcd6bad2b1e312b8b85bf9457d0e7
Stevö <stevehuwjohn@gmail.com>
2022-01-10 10:22:55 +0000

    2019.21

---
+++ AoC.Solutions/Solutions/2019/21/Part1.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2019._21;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        return RunDroid("NOT B J\nNOT C T\nOR T J\nAND D J\nNOT A T\nOR T J\nWALK\n");
+    }
+}
