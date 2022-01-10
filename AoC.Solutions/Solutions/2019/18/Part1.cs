f6bcbf07266d14748344d822089081f2dc2eae9f
Stevö <stevehuwjohn@gmail.com>
2022-01-10 18:14:14 +0000

    WIP

--- AoC.Solutions/Solutions/2019/18/Part1.cs
+++ AoC.Solutions/Solutions/2019/18/Part1.cs
@@ -1,12 +1,12 @@
-﻿using JetBrains.Annotations;
+﻿//using JetBrains.Annotations;
 
-namespace AoC.Solutions.Solutions._2019._18;
+//namespace AoC.Solutions.Solutions._2019._18;
 
-[UsedImplicitly]
-public class Part1 : Base
-{
-    public override string GetAnswer()
-    {
-        return "TESTING";
-    }
-}
+//[UsedImplicitly]
+//public class Part1 : Base
+//{
+//    public override string GetAnswer()
+//    {
+//        return "TESTING";
+//    }
+//}
