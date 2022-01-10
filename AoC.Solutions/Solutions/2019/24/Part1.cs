f300ea0c35f5136d38f2a57e59ea8ca21e749747
Stevö <stevehuwjohn@gmail.com>
2022-01-10 21:47:39 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/24/Part1.cs
@@ -0,0 +1,49 @@
+﻿using AoC.Solutions.Common;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2019._24;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    private HashSet<int> _previousStates = new();
+
+    private readonly bool[,] _grid = new bool[7, 7];
+
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        PlayRound();
+
+        return "TESTING";
+    }
+
+    private void PlayRound()
+    {
+        var dies = new List<Point>();
+
+        var infests = new List<Point>();
+
+        for (var y = 1; y < 6; y++)
+        {
+            for (var x = 1; x < 6; x++)
+            {
+            }
+        }
+    }
+
+    private void ParseInput()
+    {
+        var y = 1;
+        foreach (var line in Input)
+        {
+            for (var x = 1; x < 6; x++)
+            {
+                _grid[x, y] = line[x - 1] == '#';
+            }
+
+            y++;
+        }
+    }
+}
