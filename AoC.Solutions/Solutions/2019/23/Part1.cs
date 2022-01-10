b6949d8bf65fa83911c0cfabd249612c20001aa3
Stevö <stevehuwjohn@gmail.com>
2022-01-10 12:50:14 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/23/Part1.cs
@@ -0,0 +1,72 @@
+﻿using AoC.Solutions.Solutions._2019.Computer;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2019._23;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var cpus = new Dictionary<int, Cpu>();
+
+        for (var i = 0; i < 50; i++)
+        {
+            var cpu = new Cpu();
+
+            cpu.Initialise(65536);
+
+            cpu.LoadProgram(Input);
+
+            cpu.UserInput.Enqueue(i);
+
+            cpus.Add(i, cpu);
+        }
+
+        while (true)
+        {
+            var packets = new Dictionary<int, (int X, int Y)>();
+
+            for (var i = 0; i < 50; i++)
+            {
+                var cpu = cpus[i];
+
+                cpu.Run();
+
+                while (cpu.UserOutput.Count > 0)
+                {
+                    var address = (int) cpu.UserOutput.Dequeue();
+                    
+                    var x = (int) cpu.UserOutput.Dequeue();
+                    
+                    var y = (int) cpu.UserOutput.Dequeue();
+
+                    if (address == 255)
+                    {
+                        return y.ToString();
+                    }
+
+                    packets.Add(address, (x, y));
+                }
+            }
+
+            for (var i = 0; i < 50; i++)
+            {
+                var cpu = cpus[i];
+
+                if (packets.TryGetValue(i, out var packet))
+                {
+                    cpu.UserInput.Enqueue(packet.X);
+
+                    cpu.UserInput.Enqueue(packet.Y);
+
+                    packets.Remove(i);
+                }
+                else
+                {
+                    cpu.UserInput.Enqueue(-1);
+                }
+            }
+        }
+    }
+}
