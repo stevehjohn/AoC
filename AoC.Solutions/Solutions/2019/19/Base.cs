a7f558311eb60f65c95e8a168aa5f9235dffd4f5
Stevö <stevehuwjohn@gmail.com>
2022-01-10 10:22:55 +0000

    2019.21

--- AoC.Solutions/Solutions/2019/19/Base.cs
+++ AoC.Solutions/Solutions/2019/19/Base.cs
@@ -4,5 +4,6 @@ namespace AoC.Solutions.Solutions._2019._19;
 
 public abstract class Base : Solution
 {
+    // ReSharper disable once StringLiteralTypo
     public override string Description => "Tractorrr";
 }
