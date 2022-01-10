41d7a1cfc89ece19009eeae3352115ccc51cde5b
Stevö <stevehuwjohn@gmail.com>
2022-01-10 22:55:14 +0000

    WIP

--- AoC.Solutions/Solutions/2019/24/Part1.cs
+++ AoC.Solutions/Solutions/2019/24/Part1.cs
@@ -1,5 +1,4 @@
-﻿using AoC.Solutions.Common;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2019._24;
 
@@ -7,8 +6,6 @@ namespace AoC.Solutions.Solutions._2019._24;
 public class Part1 : Base
 {
     private readonly HashSet<int> _previousStates = new();
-
-    private readonly bool[,] _grid = new bool[7, 7];
 
     public override string GetAnswer()
     {
@@ -24,57 +21,11 @@ public class Part1 : Base
 
             if (_previousStates.Contains(bioDiversity))
             {
-                Dump();
-
                 return bioDiversity.ToString();
             }
 
             _previousStates.Add(bioDiversity);
         }
-    }
-
-    private void Dump()
-    {
-        for (var y = 1; y < 6; y++)
-        {
-            for (var x = 1; x < 6; x++)
-            {
-                Console.Write(_grid[x, y] ? '#' : '.');
-            }
-
-            Console.WriteLine();
-        }
-
-        Console.WriteLine();
-    }
-
-    private void PlayRound()
-    {
-        var dies = new List<Point>();
-
-        var infests = new List<Point>();
-
-        for (var y = 1; y < 6; y++)
-        {
-            for (var x = 1; x < 6; x++)
-            {
-                var adjacent = AdjacentCount(x, y);
-
-                if (_grid[x, y] && adjacent != 1)
-                {
-                    dies.Add(new Point(x, y));
-                }
-
-                if (! _grid[x, y] && (adjacent == 1 || adjacent == 2))
-                {
-                    infests.Add(new Point(x, y));
-                }
-            }
-        }
-
-        dies.ForEach(d => _grid[d.X, d.Y] = false);
-
-        infests.ForEach(i => _grid[i.X, i.Y] = true);
     }
 
     private int GetBiodiversity()
@@ -87,7 +38,7 @@ public class Part1 : Base
         {
             for (var x = 1; x < 6; x++)
             {
-                if (_grid[x, y])
+                if (Grid[x, y])
                 {
                     diversity += i;
                 }
@@ -99,19 +50,6 @@ public class Part1 : Base
         return diversity;
     }
 
-    private int AdjacentCount(int x, int y)
-    {
-        var count = _grid[x - 1, y] ? 1 : 0;
-
-        count += _grid[x, y - 1] ? 1 : 0;
-
-        count += _grid[x + 1, y] ? 1 : 0;
-
-        count += _grid[x, y + 1] ? 1 : 0;
-
-        return count;
-    }
-
     private void ParseInput()
     {
         var y = 1;
@@ -119,7 +57,7 @@ public class Part1 : Base
         {
             for (var x = 1; x < 6; x++)
             {
-                _grid[x, y] = line[x - 1] == '#';
+                Grid[x, y] = line[x - 1] == '#';
             }
 
             y++;
