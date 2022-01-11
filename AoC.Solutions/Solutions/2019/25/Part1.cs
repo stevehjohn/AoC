c66c2281019d8451a9ea3c7cd7f7ba385185b7ec
Stevö <stevehuwjohn@gmail.com>
2022-01-11 21:21:07 +0000

    WIP

--- AoC.Solutions/Solutions/2019/25/Part1.cs
+++ AoC.Solutions/Solutions/2019/25/Part1.cs
@@ -17,16 +17,78 @@ public class Part1 : Base
 
         while (true)
         {
-            Console.WriteLine(cpu.Run());
+            cpu.Run();
 
-            Console.WriteLine(ReadString(cpu));
+            var output = ParseOutput(ReadString(cpu));
+
+            Console.WriteLine(output.Room);
+
+            output.Directions.ForEach(Console.WriteLine);
+
+            output.Items.ForEach(Console.WriteLine);
 
             var command = Console.ReadLine();
 
             WriteString(cpu, command);
         }
+    }
 
-        return "TESTING";
+    private static (string Room, List<string> Directions, List<string> Items) ParseOutput(string output)
+    {
+        var lines = output.Split('\n');
+
+        var room = string.Empty;
+
+        var directions = new List<string>();
+
+        var items = new List<string>();
+
+        var mode = 0;
+
+        foreach (var line in lines)
+        {
+            if (line.StartsWith("== "))
+            {
+                room = line;
+
+                continue;
+            }
+
+            if (line == "Doors here lead:")
+            {
+                mode = 1;
+
+                continue;
+            }
+
+            if (line == "Items here:")
+            {
+                mode = 2;
+
+                continue;
+            }
+
+            if (mode > 0 && ! line.StartsWith("- "))
+            {
+                mode = 0;
+
+                continue;
+            }
+
+            if (mode == 1)
+            {
+                directions.Add(line.Substring(2));
+
+                continue;
+            }
+
+            if (mode == 2)
+            {
+                items.Add(line.Substring(2));
+            }
+        }
+
+        return (room, directions, items);
     }
 
     private static string ReadString(Cpu cpu)
