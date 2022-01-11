f4381380708f77b8bbb4be056ee501a059344d08
Stevö <stevehuwjohn@gmail.com>
2022-01-11 14:05:25 +0000

    WIP

--- AoC.Solutions/Solutions/2019/24/Base.cs
+++ AoC.Solutions/Solutions/2019/24/Base.cs
@@ -24,4 +24,130 @@ public abstract class Base : Solution
 
         return result;
     }
+
+    protected static int PlayRound(int grid, int parent = -1, int child = -1)
+    {
+        var dies = 33_554_431;
+
+        var infests = 0;
+
+        var bit = 16_777_216;
+
+        for (var i = 0; i < 25; i++)
+        {
+            int adjacent;
+
+            if (parent >= 0 || child >= 0)
+            {
+                adjacent = AdjacentCount(grid, bit, parent, child);
+            }
+            else
+            {
+                adjacent = AdjacentCount(grid, bit);
+            }
+
+            if ((grid & bit) > 0 && adjacent != 1)
+            {
+                dies ^= bit;
+            }
+
+            if ((grid & bit) == 0 && (adjacent == 1 || adjacent == 2))
+            {
+                infests |= bit;
+            }
+
+            bit >>= 1;
+        }
+
+        grid &= dies;
+
+        grid |= infests;
+
+        return grid;
+    }
+
+    private static int AdjacentCount(int grid, int bit)
+    {
+        var count = 0;
+
+        if ((bit & 17_318_416) == 0)
+        {
+            count += (grid & (bit << 1)) > 0 ? 1 : 0;
+        }
+
+        if ((bit & 1_082_401) == 0)
+        {
+            count += (grid & (bit >> 1)) > 0 ? 1 : 0;
+        }
+
+        count += (grid & (bit << 5)) > 0 ? 1 : 0;
+
+        count += (grid & (bit >> 5)) > 0 ? 1 : 0;
+
+        return count;
+    }
+
+    private static int AdjacentCount(int grid, int bit, int parent, int child)
+    {
+        var count = 0;
+
+        if ((bit & 17_318_416) == 0 && bit != 2_048)
+        {
+            count += (grid & (bit << 1)) > 0 ? 1 : 0;
+        }
+
+        if ((bit & 1_082_401) == 0 && bit != 8_192)
+        {
+            count += (grid & (bit >> 1)) > 0 ? 1 : 0;
+        }
+
+        if (bit != 128)
+        {
+            count += (grid & (bit << 5)) > 0 ? 1 : 0;
+        }
+
+        if (bit != 131_072)
+        {
+            count += (grid & (bit >> 5)) > 0 ? 1 : 0;
+        }
+
+        if (child > 0)
+        {
+            if (bit == 128)
+            {
+                count += CountBits(child & 31);
+            }
+
+            if (bit == 2_048)
+            {
+                count += CountBits(child & 1_082_401);
+            }
+
+            if (bit == 8_192)
+            {
+                count += CountBits(child & 17_318_416);
+            }
+
+            if (bit == 131_072)
+            {
+                count += CountBits(child & 32_505_856);
+            }
+        }
+
+        return count;
+    }
+
+    protected static int CountBits(int value)
+    {
+        var count = 0;
+
+        while (value > 0)
+        {
+            count++;
+
+            value &= value - 1;
+        }
+
+        return count;
+    }
 }
--- AoC.Solutions/Solutions/2019/24/Part2.cs
+++ AoC.Solutions/Solutions/2019/24/Part2.cs
@@ -14,22 +14,45 @@ public class Part2 : Base
 
         for (var i = 0; i < 10; i++)
         {
-            grids = PlayRound(grids);
+            grids = PlayLevels(grids);
         }
+
+        var count = 0;
 
         foreach (var grid in grids)
         {
             Dump(grid);
+
+            count += CountBits(grid);
         }
 
-        return "TESTING";
+        return count.ToString();
     }
 
-    private List<int> PlayRound(List<int> grids)
+    private static List<int> PlayLevels(List<int> grids)
     {
         var newGrids = new List<int>();
 
-        var parent = GetParent(grids.Last());
+        var child = PlayRound(0, grids.First());
+
+        if (child > 0)
+        {
+            newGrids.Add(child);
+        }
+
+        for (var i = 0; i < grids.Count; i++)
+        {
+            var result = PlayRound(grids[i], i < grids.Count - 1 ? grids[i + 1] : 0, child);
+
+            //if (result != 0)
+            {
+                newGrids.Add(result);
+            }
+
+            child = result;
+        }
+
+        var parent = PlayRound(0, 0, grids.Last());
 
         if (parent > 0)
         {
@@ -66,21 +89,7 @@ public class Part2 : Base
         return parent;
     }
 
-    private static int CountBits(int value)
-    {
-        var count = 0;
-
-        while (value > 0)
-        {
-            count++;
-
-            value &= value - 1;
-        }
-
-        return count;
-    }
-
-    private void Dump(int grid)
+    private static void Dump(int grid)
     {
         var bit = 16_777_216;
 
