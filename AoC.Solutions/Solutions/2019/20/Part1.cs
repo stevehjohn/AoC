1c9e3f2e4581d62c3ee2a63a06a839c4addc7bb2
Stevö <stevehuwjohn@gmail.com>
2022-01-12 13:41:27 +0000

    WIP

--- AoC.Solutions/Solutions/2019/20/Part1.cs
+++ AoC.Solutions/Solutions/2019/20/Part1.cs
@@ -1,4 +1,5 @@
-﻿namespace AoC.Solutions.Solutions._2019._20;
+﻿#define DUMP
+namespace AoC.Solutions.Solutions._2019._20;
 
 public class Part1 : Base
 {
@@ -6,6 +7,62 @@ public class Part1 : Base
     {
         ParseInput();
 
+#if DEBUG && DUMP
+        DumpMap();
+
+        Console.CursorLeft = 0;
+
+        Console.CursorTop = Height + 2;
+
+        Console.ForegroundColor = ConsoleColor.Green;
+#endif
+
         return "TESTING";
     }
+
+#if DEBUG && DUMP
+    private void DumpMap()
+    {
+        Console.Clear();
+
+        Console.CursorLeft = 0;
+
+        Console.CursorTop = 1;
+
+        for (var y = 0; y < Height; y++)
+        {
+            Console.Write(' ');
+
+            for (var x = 0; x < Width; x++)
+            {
+                if (Maze[x, y] is -2 or 0)
+                {
+                    Console.Write(' ');
+
+                    continue;
+                }
+
+                if (Maze[x, y] == -1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    Console.Write('█');
+
+                    continue;
+                }
+
+                if (Maze[x, y] > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+
+                    Console.Write('█');
+
+                    continue;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+#endif
 }
