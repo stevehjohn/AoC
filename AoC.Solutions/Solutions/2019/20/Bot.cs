a9e384d2483b562c0d3867a676d30bc4db80f5eb
Stevö <stevehuwjohn@gmail.com>
2022-01-12 21:39:58 +0000

    WIP

--- AoC.Solutions/Solutions/2019/20/Bot.cs
+++ AoC.Solutions/Solutions/2019/20/Bot.cs
@@ -69,7 +69,7 @@ public class Bot
 
         History.Add(new Point(Position.X, Position.Y));
 
-        _level = bot._level
+        _level = bot._level;
 
         Steps++;
     }
@@ -107,7 +107,7 @@ public class Bot
 
                     if (_level < 0)
                     {
-                        return bots;
+                        return bots
                     }
                 }
                 else
