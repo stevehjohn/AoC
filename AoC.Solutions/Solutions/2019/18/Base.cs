4c339749363c7b420b886bd8b6f88acafb2d4713
Stevö <stevehuwjohn@gmail.com>
2022-01-14 10:24:17 +0000

    2019.18 start

--- AoC.Solutions/Solutions/2019/18/Base.cs
+++ AoC.Solutions/Solutions/2019/18/Base.cs
@@ -1,8 +1,77 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿#define DUMP
+using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2019._18;
 
 public abstract class Base : Solution
 {
     public override string Description => "Maze and keys";
+
+    private char[,] _map;
+
+    private int _width;
+
+    private int _height;
+
+    private Point _start;
+
+    private char _target = '\0';
+
+    protected int Solve()
+    {
+#if DUMP && DEBUG
+        Visualiser.DumpMap(_map);
+#endif
+        var bots = new List<Bot>
+                   {
+                       new(_start, _map)
+                   };
+
+        while (bots.Count > 0)
+        {
+#if DUMP && DEBUG
+            Visualiser.DumpBots(bots.Select(b => b.Position).ToList());
+#endif
+            var newBots = new List<Bot>();
+
+            foreach (var bot in bots)
+            {
+                newBots.AddRange(bot.Move());
+            }
+
+            bots = newBots;
+        }
+
+        return 0;
+    }
+
+    protected void ParseInput()
+    {
+        _width = Input[0].Length;
+
+        _height = Input.Length;
+
+        _map = new char[_width, _height];
+
+        for (var y = 0; y < _height; y++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                var c = Input[y][x];
+
+                _map[x, y] = c;
+
+                if (c == '@')
+                {
+                    _start = new Point(x, y);
+                }
+
+                if (c > _target)
+                {
+                    _target = c;
+                }
+            }
+        }
+    }
 }
---
+++ AoC.Solutions/Solutions/2019/18/Visualiser.cs
@@ -0,0 +1,93 @@
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2019._18;
+
+public static class Visualiser
+{
+    private static int _width;
+
+    private static int _height;
+
+    private static List<Point> _previousBots = new();
+
+    public static void DumpBots(List<Point> bots)
+    {
+        foreach (var bot in bots) //.Except(_previousBots))
+        {
+            Console.SetCursorPosition(bot.X + 1, bot.Y + 1);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write('█');
+        }
+
+        foreach (var position in _previousBots)
+        {
+            if (bots.Any(b => b.Equals(position)))
+            {
+                continue;
+            }
+
+            Console.SetCursorPosition(position.X + 1, position.Y + 1);
+
+            Console.Write(' ');
+        }
+
+        _previousBots = bots.ToList();
+
+        Console.SetCursorPosition(0, _height + 3);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Thread.Sleep(1000);
+    }
+
+    public static void DumpMap(char[,] map)
+    {
+        Console.Clear();
+
+        Console.CursorVisible = false;
+
+        Console.SetCursorPosition(0, 1);
+
+        _width = map.GetLength(0);
+
+        _height = map.GetLength(1);
+
+        for (var y = 0; y < _height; y++)
+        {
+            Console.Write(' ');
+
+            for (var x = 0; x < _width; x++)
+            {
+                var c = map[x, y];
+
+                if (c == '#')
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                    Console.Write('█');
+
+                    continue;
+                }
+
+                if (char.IsLetter(c))
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write(c);
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+
+                    continue;
+                }
+
+                Console.Write(' ');
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
