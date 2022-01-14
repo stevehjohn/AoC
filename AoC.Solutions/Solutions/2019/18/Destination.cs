b466e4ce08b13d39f4a0e73c50576b9f16a979c0
Stevö <stevehuwjohn@gmail.com>
2022-01-14 20:48:04 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2019/18/Destination.cs
@@ -0,0 +1,15 @@
+﻿namespace AoC.Solutions.Solutions._2019._18;
+
+public class Destination
+{
+    public char Name { get; }
+
+    public List<Destination> BlockedBy { get; }
+
+    public Destination(char name)
+    {
+        Name = name;
+
+        BlockedBy = new List<Destination>();
+    }
+}
