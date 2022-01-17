24086fa2cfc438d65b53f06247a1d93522aee35c
Stevö <stevehuwjohn@gmail.com>
2022-01-17 18:04:47 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2020/03/Base.cs
@@ -0,0 +1,31 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2020._03;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Tobogganing through the trees";
+
+    protected bool[,] Map;
+
+    protected int Width;
+
+    protected int Height;
+
+    protected void ParseInput()
+    {
+        Width = Input[0].Length;
+
+        Height = Input.Length;
+
+        Map = new bool[Width, Height];
+
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                Map[x, y] = Input[y][x] == '#';
+            }
+        }
+    }
+}
---
+++ AoC.Solutions/Solutions/2020/03/Part1.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._03;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        var trees = 0;
+
+        var x = 0;
+
+        for (var y = 1; y < Height; y++)
+        {
+            x += 3;
+
+            if (x >= Width)
+            {
+                x -= Width;
+            }
+
+            trees += Map[x, y] ? 1 : 0;
+        }
+
+        return trees.ToString();
+    }
+}
