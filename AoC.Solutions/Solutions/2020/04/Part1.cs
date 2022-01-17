afa18bf8d9663183db86e972d13de4cdac4cf741
Stevö <stevehuwjohn@gmail.com>
2022-01-17 18:26:55 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2020/04/Part1.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._04;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var requiredFields = new [] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+
+        var validPassports = 0;
+
+        var matched = 0;
+
+        foreach (var line in Input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                if (matched > 6)
+                {
+                    validPassports++;
+                }
+
+                matched = 0;
+
+                continue;
+            }
+
+            var fields = line.Split(' ');
+
+            foreach (var field in fields)
+            {
+                if (requiredFields.Contains(field[..3]))
+                {
+                    matched++;
+                }
+            }
+        }
+
+        if (matched > 6)
+        {
+            validPassports++;
+        }
+        
+        return validPassports.ToString();
+    }
+}
