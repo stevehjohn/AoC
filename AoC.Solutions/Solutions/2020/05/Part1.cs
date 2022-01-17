f49ef1ba21f28556314d93f57d739088740f027d
Stevö <stevehuwjohn@gmail.com>
2022-01-17 20:08:33 +0000

    WIP

--- AoC.Solutions/Solutions/2020/05/Part1.cs
+++ AoC.Solutions/Solutions/2020/05/Part1.cs
@@ -7,6 +7,50 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "TESTING";
+        var highest = 0;
+
+        foreach (var line in Input)
+        {
+            var seatLocation = GetSeatId(line);
+
+            var seatId = seatLocation.Row * 8 + seatLocation.Column;
+
+            if (seatId > highest)
+            {
+                highest = seatId;
+            }
+        }
+
+        return highest.ToString();
+    }
+
+    private (int Row, int Column) GetSeatId(string input)
+    {
+        var row = ParseBinarySpace(input[..7], 127);
+
+        var column = ParseBinarySpace(input[7..], 7);
+
+        return (Row: row, Column: column);
+    }
+
+    private int ParseBinarySpace(string input, int max)
+    {
+        var min = 0;
+
+        foreach (var c in input)
+        {
+            var halved = (int) Math.Ceiling((max - min) / 2d);
+
+            if (c is 'F' or 'L') // Lower
+            {
+                max -= halved;
+
+                continue;
+            }
+
+            min += halved;
+        }
+        
+        return min;
     }
 }
