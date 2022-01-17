cdad7bbca4e241ed806ebfe468671a8fa6388716
Stevö <stevehuwjohn@gmail.com>
2022-01-17 19:07:26 +0000

    WIP

--- AoC.Solutions/Solutions/2020/04/Part2.cs
+++ AoC.Solutions/Solutions/2020/04/Part2.cs
@@ -8,13 +8,13 @@ public class Part2 : Base
     public override string GetAnswer()
     {
         var validPassports = CountValidPassports();
-        
+
         return validPassports.ToString();
     }
 
     protected override bool IsValidPassport(Dictionary<string, string> data)
     {
-        var requiredFields = new [] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+        var requiredFields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
 
         if (data.Select(kvp => kvp.Key).Intersect(requiredFields).Count() < 7)
         {
@@ -37,6 +37,21 @@ public class Part2 : Base
         }
 
         if (! ValidateHeight(data["hgt"]))
+        {
+            return false;
+        }
+
+        if (! ValidateHairColour(data["hcl"]))
+        {
+            return false;
+        }
+
+        if (! ValidateEyeColour(data["ecl"]))
+        {
+            return false;
+        }
+
+        if (! ValidatePassportId(data["pid"]))
         {
             return false;
         }
@@ -67,5 +82,38 @@ public class Part2 : Base
         }
 
         return height is >= 59 and <= 76;
+    }
+
+    private static bool ValidateHairColour(string value)
+    {
+        if (value.Length != 7 || value[0] != '#')
+        {
+            return false;
+        }
+
+        foreach (var c in value[1..])
+        {
+            if (! char.IsNumber(c) && c is < 'a' or > 'f')
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool ValidateEyeColour(string value)
+    {
+        return new[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" }.Any(c => c == value);
+    }
+
+    private static bool ValidatePassportId(string value)
+    {
+        if (! int.TryParse(value, out var year))
+        {
+            return false;
+        }
+
+        return value.Length == 9;
     }
 }
