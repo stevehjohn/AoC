95ae8070ad361672bb3b46d0339199464538f773
Stevö <stevehuwjohn@gmail.com>
2022-01-18 15:25:15 +0000

    2020.10

---
+++ AoC.Solutions/Solutions/2020/10/Part1.cs
@@ -0,0 +1,44 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._10;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseData();
+
+        var voltage = 0;
+
+        var ones = 0;
+
+        var threes = 1;
+
+        while (true)
+        {
+            var adapters = Data.Where(v => v > voltage && v < voltage + 4).ToList();
+
+            if (adapters.Count == 0)
+            {
+                break;
+            }
+
+            var adapter = (int) adapters.Min();
+
+            if (adapter - voltage == 3)
+            {
+                threes++;
+            }
+
+            if (adapter - voltage == 1)
+            {
+                ones++;
+            }
+
+            voltage = adapter;
+        }
+
+        return (ones * threes).ToString();
+    }
+}
