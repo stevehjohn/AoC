b384fa0d364d1a9aafea7e782a70afe77a8f27ba
Stevö <stevehuwjohn@gmail.com>
2022-01-18 19:32:21 +0000

    WIP

--- AoC.Solutions/Solutions/2020/12/Base.cs
+++ AoC.Solutions/Solutions/2020/12/Base.cs
@@ -1,8 +1,130 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2020._12;
 
 public abstract class Base : Solution
 {
     public override string Description => "Evergreen ship sailing";
+
+    protected int Navigate(bool useWaypoint)
+    {
+        var direction = 90;
+
+        var shipPosition = new Point();
+
+        var waypointPosition = new Point(10, -1);
+
+        foreach (var line in Input)
+        {
+            var value = int.Parse(line[1..]);
+
+            switch (line[0])
+            {
+                case 'N':
+                    shipPosition.Y -= useWaypoint ? 0 : value;
+                    waypointPosition.Y -= useWaypoint ? value : 0;
+
+                    break;
+
+                case 'S':
+                    shipPosition.Y += useWaypoint ? 0 : value;
+                    waypointPosition.Y += useWaypoint ? value : 0;
+
+                    break;
+
+                case 'E':
+                    shipPosition.X += useWaypoint ? 0 : value;
+                    waypointPosition.X += useWaypoint ? value : 0;
+
+                    break;
+
+                case 'W':
+                    shipPosition.X -= useWaypoint ? 0 : value;
+                    waypointPosition.X -= useWaypoint ? value : 0;
+
+                    break;
+
+                case 'L':
+                    if (useWaypoint)
+                    {
+                        var dX = shipPosition.X - waypointPosition.X;
+                        var dY = shipPosition.Y - waypointPosition.Y;
+
+                        waypointPosition.X = shipPosition.X - dY;
+                        waypointPosition.Y = shipPosition.Y + dX;
+                    }
+                    else
+                    {
+                        direction -= value;
+
+                        if (direction < 0)
+                        {
+                            direction += 360;
+                        }
+                    }
+
+                    break;
+
+                case 'R':
+                    if (useWaypoint)
+                    {
+                        var dX = shipPosition.X - waypointPosition.X;
+                        var dY = shipPosition.Y - waypointPosition.Y;
+
+                        waypointPosition.X = shipPosition.X + dY;
+                        waypointPosition.Y = shipPosition.Y - dX;
+                    }
+                    else
+                    {
+                        direction += value;
+
+                        if (direction >= 360)
+                        {
+                            direction -= 360;
+                        }
+                    }
+
+                    break;
+
+                default:
+                    if (useWaypoint)
+                    {
+                        var dX = waypointPosition.X - shipPosition.X;
+                        var dY = waypointPosition.Y - shipPosition.Y;
+
+                        shipPosition.X += dX * value;
+                        shipPosition.Y += dY * value;
+
+                        waypointPosition.X += dX * value;
+                        waypointPosition.Y += dY * value;
+                    }
+                    else
+                    {
+                        switch (direction)
+                        {
+                            case 90:
+                                shipPosition.X += value;
+                                break;
+
+                            case 180:
+                                shipPosition.Y += value;
+                                break;
+
+                            case 270:
+                                shipPosition.X -= value;
+                                break;
+
+                            default:
+                                shipPosition.Y -= value;
+                                break;
+                        }
+                    }
+
+                    break;
+            }
+        }
+
+        return Math.Abs(shipPosition.X) + Math.Abs(shipPosition.Y);
+    }
 }
--- AoC.Solutions/Solutions/2020/12/Part2.cs
+++ AoC.Solutions/Solutions/2020/12/Part2.cs
@@ -3,11 +3,11 @@
 namespace AoC.Solutions.Solutions._2020._12;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var result = Navigate(false);
+        var result = Navigate(true);
 
         return result.ToString();
     }
