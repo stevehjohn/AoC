98101edeaeb26c965b0655313bb52fb9413103a3
Stevö <stevehuwjohn@gmail.com>
2022-01-18 14:06:09 +0000

    2020.9

---
+++ AoC.Solutions/Solutions/2020/09/Base.cs
@@ -0,0 +1,20 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2020._09;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Cypher cracking";
+
+    protected List<long> Data;
+
+    protected void ParseData()
+    {
+        Data = new List<long>();
+
+        foreach (var line in Input)
+        {
+            Data.Add(long.Parse(line));
+        }
+    }
+}
