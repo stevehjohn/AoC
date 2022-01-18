f9e6afd5c7ef8ba4338befc1c3f003c40af9378e
Stevö <stevehuwjohn@gmail.com>
2022-01-18 17:11:28 +0000

    2020.11

---
+++ AoC.Solutions/Solutions/2020/11/Base.cs
@@ -0,0 +1,31 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2020._11;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Conway's game of seating";
+
+    protected int Width;
+
+    protected int Height;
+
+    protected char[,] Map;
+
+    protected void ParseInput()
+    {
+        Width = Input[0].Length;
+
+        Height = Input.Length;
+
+        Map = new char[Width + 2, Height + 2];
+
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                Map[x + 1, y + 1] = Input[y][x];
+            }
+        }
+    }
+}
