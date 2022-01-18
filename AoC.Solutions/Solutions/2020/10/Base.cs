583c9df37df5380ce99a7fca9ca53e49715c6b2c
Stevö <stevehuwjohn@gmail.com>
2022-01-18 15:25:15 +0000

    2020.10

---
+++ AoC.Solutions/Solutions/2020/10/Base.cs
@@ -0,0 +1,21 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2020._10;
+
+public abstract class Base : Solution
+{
+    // ReSharper disable once StringLiteralTypo
+    public override string Description => "Joltage meter";
+
+    protected List<long> Data;
+
+    protected void ParseData()
+    {
+        Data = new List<long>();
+
+        foreach (var line in Input)
+        {
+            Data.Add(long.Parse(line));
+        }
+    }
+}
