c8c6bccad78590b8a826f5bd50b9ee53090da640
Stevö <stevehuwjohn@gmail.com>
2022-01-18 12:31:37 +0000

    2020.8.1

---
+++ AoC.Solutions/Solutions/2020/08/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2020._08;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Console assembly debugging";
+}
