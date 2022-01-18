0806d511b7df6f3842b82f6837ecfb833f32d46a
Stevö <stevehuwjohn@gmail.com>
2022-01-18 14:06:09 +0000

    2020.9

---
+++ AoC.Solutions/Solutions/2020/09/Part1.cs
@@ -0,0 +1,64 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._09;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseData();
+
+        var possibleNumbers = new List<HashSet<long>>();
+
+        for (var i = 1; i < Data.Count; i++)
+        {
+            if (possibleNumbers.Count > 24)
+            {
+                if (! IsInPossibleNumbers(possibleNumbers, Data[i]))
+                {
+                    return Data[i].ToString();
+                }
+            }
+
+            possibleNumbers.Add(CalculateNextPossibleNumbers(i));
+
+            if (possibleNumbers.Count > 25)
+            {
+                possibleNumbers.RemoveAt(0);
+            }
+        }
+
+        return "TESTING";
+    }
+
+    private HashSet<long> CalculateNextPossibleNumbers(int index)
+    {
+        var possibleNumbers = new HashSet<long>();
+
+        for (var i = index - 1; i >= Math.Max(0, index - 24); i--)
+        {
+            if (Data[index] == Data[i])
+            {
+                continue;
+            }
+
+            possibleNumbers.Add(Data[index] + Data[i]);
+        }
+
+        return possibleNumbers;
+    }
+
+    private bool IsInPossibleNumbers(List<HashSet<long>> possibleNumbers, long value)
+    {
+        foreach (var item in possibleNumbers)
+        {
+            if (item.Contains(value))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
