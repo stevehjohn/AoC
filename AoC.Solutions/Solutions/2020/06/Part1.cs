95ac94a2aa0689705dfb23b7a4b995c129f8975e
Stevö <stevehuwjohn@gmail.com>
2022-01-18 09:18:57 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2020/06/Part1.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._06;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var builder = new StringBuilder();
+
+        var total = 0;
+
+        foreach (var line in Input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                total += builder.ToString().Distinct().Count();
+
+                builder.Clear();
+            }
+
+            builder.Append(line);
+        }
+
+        total += builder.ToString().Distinct().Count();
+
+        return total.ToString();
+    }
+}
