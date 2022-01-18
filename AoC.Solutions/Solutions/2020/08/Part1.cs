55e56ab3e14702868c03723101f9d2a3edb0bcad
Stevö <stevehuwjohn@gmail.com>
2022-01-18 13:02:29 +0000

    2020.8

--- AoC.Solutions/Solutions/2020/08/Part1.cs
+++ AoC.Solutions/Solutions/2020/08/Part1.cs
@@ -7,43 +7,8 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        var accumulator = 0;
+        var result = RunProgram();
 
-        var programCounter = 0;
-
-        var executed = new HashSet<int>();
-
-        while (true)
-        {
-            if (executed.Contains(programCounter))
-            {
-                break;
-            }
-
-            var instruction = Input[programCounter];
-
-            executed.Add(programCounter);
-
-            switch (instruction[..3])
-            {
-                case "jmp":
-                    programCounter += int.Parse(instruction[4..]);
-
-                    break;
-
-                case "acc":
-                    accumulator += int.Parse(instruction[4..]);
-
-                    programCounter++;
-
-                    break;
-                default:
-                    programCounter++;
-
-                    break;
-            }
-        }
-
-        return accumulator.ToString();
+        return result.Accumulator.ToString();
     }
 }
