6e48a424d0be8e0628b3694aaf303818602c29d5
Stevö <stevehuwjohn@gmail.com>
2022-01-20 13:23:20 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2020/16/Rule.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC.Solutions.Solutions._2020._16;
+
+public class Rule
+{
+    public string Name { get; }
+
+    public Range Rule1 { get; }
+
+    public Range Rule2 { get; }
+
+    public Rule(string name, Range rule1, Range rule2)
+    {
+        Name = name;
+
+        Rule1 = rule1;
+
+        Rule2 = rule2;
+    }
+}
