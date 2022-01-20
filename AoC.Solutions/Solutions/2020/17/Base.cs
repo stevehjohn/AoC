74bee07ca0c942728eab51dc227d4d6c13b0d0c6
Stevö <stevehuwjohn@gmail.com>
2022-01-20 13:42:53 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2020/17/Base.cs
@@ -0,0 +1,25 @@
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2020._17;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Conway cubes";
+
+    protected List<Point> ActiveCubes;
+
+    protected void ParseInput()
+    {
+        for (var y = 0; y < Input.Length; y++)
+        {
+            for (var x = 0; x < Input[0].Length; x++)
+            {
+                if (Input[y][x] == '#')
+                {
+                    ActiveCubes.Add(new Point(x, y));
+                }
+            }
+        }
+    }
+}
---
+++ AoC.Solutions/Solutions/2020/17/Part1.cs
@@ -0,0 +1,42 @@
+﻿using AoC.Solutions.Common;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._17;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        for (var i = 0; i < 6; i++)
+        {
+            RunCycle();
+        }
+
+        return "TESTING";
+    }
+
+    private void RunCycle()
+    {
+        var flip = new List<Point>();
+
+        foreach (var cube in ActiveCubes)
+        {
+            
+        }
+
+        foreach (var point in flip)
+        {
+            if (ActiveCubes.Contains(point))
+            {
+                ActiveCubes.Remove(point);
+            }
+            else
+            {
+                ActiveCubes.Add(point);
+            }
+        }
+    }
+}
