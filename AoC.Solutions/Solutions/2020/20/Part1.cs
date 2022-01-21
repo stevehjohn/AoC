7744eba9e14489bb7ec2e171e6c6df3a622202c1
Stevö <stevehuwjohn@gmail.com>
2022-01-21 13:52:32 +0000

    2020.20 input

---
+++ AoC.Solutions/Solutions/2020/20/Part1.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._20;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        return "TESTING";
+    }
+}
---
+++ AoC.Solutions/Solutions/2020/20/Tile.cs
@@ -0,0 +1,6 @@
+﻿namespace AoC.Solutions.Solutions._2020._20;
+
+public class Tile
+{
+    
+}
