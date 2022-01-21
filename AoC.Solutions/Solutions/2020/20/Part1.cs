dad896c7ef9e2450532f00442ec2cf72ce33af38
Stevö <stevehuwjohn@gmail.com>
2022-01-21 16:05:04 +0000

    WIP

--- AoC.Solutions/Solutions/2020/20/Part1.cs
+++ AoC.Solutions/Solutions/2020/20/Part1.cs
@@ -1,12 +1,170 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Common;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2020._20;
 
 [UsedImplicitly]
 public class Part1 : Base
 {
+    private readonly List<Tile> _tiles = new();
+
+    private Dictionary<Point, Tile> _jigsaw;
+
     public override string GetAnswer()
     {
+        ParseInput();
+
+        Solve();
+
         return "TESTING";
+    }
+
+    private void Solve()
+    {
+        var tileCount = _tiles.Count;
+
+        _jigsaw = new Dictionary<Point, Tile>
+                  {
+                      { new Point(), _tiles[0] }
+                  };
+
+        _tiles.RemoveAt(0);
+
+        while (_jigsaw.Count < tileCount)
+        {
+            foreach (var tile in _jigsaw)
+            {
+                if (! _jigsaw.ContainsKey(new Point(tile.Key.X, tile.Key.Y - 1)))
+                {
+                    if (FindTileMatch(tile.Value.Top, tile.Key.X, tile.Key.Y - 1))
+                    {
+                        break;
+                    }
+                }
+
+                if (! _jigsaw.ContainsKey(new Point(tile.Key.X + 1, tile.Key.Y)))
+                {
+                    if (FindTileMatch(tile.Value.Right, tile.Key.X + 1, tile.Key.Y))
+                    {
+                        break;
+                    }
+                }
+
+                if (! _jigsaw.ContainsKey(new Point(tile.Key.X, tile.Key.Y + 1)))
+                {
+                    if (FindTileMatch(tile.Value.Bottom, tile.Key.X, tile.Key.Y + 1))
+                    {
+                        break;
+                    }
+                }
+
+                if (! _jigsaw.ContainsKey(new Point(tile.Key.X - 1, tile.Key.Y)))
+                {
+                    if (FindTileMatch(tile.Value.Left, tile.Key.X - 1, tile.Key.Y))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Dump();
+        }
+    }
+
+    private void Dump()
+    {
+        Console.CursorVisible = false;
+
+        Console.Clear();
+
+        var yMin = _jigsaw.Min(t => t.Key.Y);
+
+        var xMin = _jigsaw.Min(t => t.Key.X);
+
+        for (var y = yMin; y <= _jigsaw.Max(t => t.Key.Y); y++)
+        {
+            for (var x = xMin; x <= _jigsaw.Max(t => t.Key.X); x++)
+            {
+                if (_jigsaw.ContainsKey(new Point(x, y)))
+                {
+                    var tile = _jigsaw[new Point(x, y)];
+
+                    Console.SetCursorPosition((x + Math.Abs(xMin)) * 11 + 1, (y + Math.Abs(yMin)) * 11 + 1);
+
+                    Console.Write(tile.TopEdge);
+
+                    for (var ty = 1; ty < tile.LeftEdge.Length - 1; ty++)
+                    {
+                        Console.SetCursorPosition((x + Math.Abs(xMin)) * 11 + 1, (y + Math.Abs(yMin)) * 11 + ty + 1);
+
+                        Console.Write(tile.LeftEdge[ty]);
+
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                        if (ty == 4)
+                        {
+                            Console.Write("..");
+
+                            Console.ForegroundColor = ConsoleColor.Blue;
+
+                            Console.Write(tile.Id);
+
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                            Console.Write("..");
+                        }
+                        else
+                        {
+                            Console.Write("........");
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write(tile.RightEdge[ty]);
+                    }
+
+                    Console.SetCursorPosition((x + Math.Abs(xMin)) * 11 + 1, (y + Math.Abs(yMin)) * 11 + 10);
+
+                    Console.Write(tile.BottomEdge);
+                }
+            }
+        }
+    }
+
+    private bool FindTileMatch(int edge, int x, int y)
+    {
+        var match = _tiles.SingleOrDefault(t => t.Edges.Contains(edge));
+
+        if (match == null)
+        {
+            return false;
+        }
+
+        _jigsaw.Add(new Point(x, y), match);
+
+        _tiles.Remove(match);
+
+        return true;
+    }
+
+    private void ParseInput()
+    {
+        var tileLines = new List<string>();
+
+        foreach (var line in Input)
+        {
+            if (! string.IsNullOrWhiteSpace(line))
+            {
+                tileLines.Add(line);
+
+                continue;
+            }
+
+            _tiles.Add(new Tile(tileLines));
+
+            tileLines.Clear();
+        }
+
+        _tiles.Add(new Tile(tileLines));
     }
 }
--- AoC.Solutions/Solutions/2020/20/Tile.cs
+++ AoC.Solutions/Solutions/2020/20/Tile.cs
@@ -1,6 +1,92 @@
-﻿namespace AoC.Solutions.Solutions._2020._20;
+﻿using System.Collections.Immutable;
+using System.Text;
+
+namespace AoC.Solutions.Solutions._2020._20;
 
 public class Tile
 {
-    
+    public int Id { get; }
+
+    public ImmutableHashSet<int> Edges { get; }
+
+    public int Top { get; private set; }
+
+    public int Right { get; private set; }
+
+    public int Bottom { get; private set; }
+
+    public int Left { get; private set; }
+
+    public string TopEdge { get; private set; }
+
+    public string RightEdge { get; private set; }
+
+    public string BottomEdge { get; private set; }
+
+    public string LeftEdge { get; private set; }
+
+    public Tile(List<string> data)
+    {
+        Id = int.Parse(data[0].Substring(5, 4));
+
+        var left = new StringBuilder();
+
+        var right = new StringBuilder();
+
+        for (var y = 1; y < 11; y++)
+        {
+            left.Append(data[y][0]);
+
+            right.Append(data[y][9]);
+        }
+
+        Top = data[1].GetHashCode();
+
+        Right = right.ToString().GetHashCode();
+
+        Bottom = data[10].GetHashCode();
+
+        Left = left.ToString().GetHashCode();
+
+        TopEdge = data[1].Replace('.', ' ');
+
+        RightEdge = right.ToString().Replace('.', ' ');
+
+        BottomEdge = data[10].Replace('.', ' ');
+
+        LeftEdge = left.ToString().Replace('.', ' ');
+
+        var edges = new HashSet<int>
+                    {
+                        Top,
+                        Right,
+                        Bottom,
+                        Left
+                    };
+
+        Edges = edges.ToImmutableHashSet();
+    }
+
+    public void RotateClockwise()
+    {
+        var temp = Right;
+
+        Right = Top;
+
+        Top = Left;
+
+        Left = Bottom;
+
+        Bottom = temp;
+
+        var sTemp = RightEdge;
+
+        RightEdge = TopEdge;
+
+        TopEdge = LeftEdge;
+
+        LeftEdge = BottomEdge;
+
+        BottomEdge = sTemp;
+    }
 }
