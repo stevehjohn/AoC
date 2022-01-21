53ec5c6432685a8c4bba322fdab7d0e14123686e
Stevö <stevehuwjohn@gmail.com>
2022-01-21 13:36:38 +0000

    2020.19

---
+++ AoC.Solutions/Solutions/2020/19/Part1.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._19;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    private List<string> _messages = new();
+
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        return "TESTING";
+    }
+
+    private void ParseInput()
+    {
+        var i = 0;
+
+        var rules = new Dictionary<int, string>();
+
+        while (! string.IsNullOrWhiteSpace(Input[i]))
+        {
+            var split = Input[i].Split(':');
+
+            rules.Add(int.Parse(split[0]), split[1]);
+
+            i++;
+        }
+
+        i++;
+
+        while (i < Input.Length)
+        {
+            _messages.Add(Input[i]);
+
+            i++;
+        }
+    }
+}
