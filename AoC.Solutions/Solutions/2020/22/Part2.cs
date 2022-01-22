447bfc68958d5ab59f82054b91f6331a25a13f51
Stevö <stevehuwjohn@gmail.com>
2022-01-22 17:30:39 +0000

    2021.1

---
+++ AoC.Solutions/Solutions/2020/22/Part2.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._22;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        Play();
+
+        var score = CalculateScore();
+
+        return score.ToString();
+    }
+
+    private void Play()
+    {
+    }
+}
