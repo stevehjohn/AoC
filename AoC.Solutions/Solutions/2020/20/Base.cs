44f02cf586f6dfbc164e53ade813e6d0e5b729d3
Stevö <stevehuwjohn@gmail.com>
2022-01-22 14:04:08 +0000

    WIP

--- AoC.Solutions/Solutions/2020/20/Base.cs
+++ AoC.Solutions/Solutions/2020/20/Base.cs
@@ -5,4 +5,6 @@ namespace AoC.Solutions.Solutions._2020._20;
 public abstract class Base : Solution
 {
     public override string Description => "Satellite jigsaw";
+
+    protected const string Part1ResultFile = "2020.20.1.result";
 }
---
+++ AoC.Solutions/Solutions/2020/20/Part2.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._20;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        return "TESTING";
+    }
+}
