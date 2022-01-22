6ef11dfc1026c3ba18abcb54dda1eaf749c1a896
Stevö <stevehuwjohn@gmail.com>
2022-01-22 17:27:25 +0000

    2021.1

--- AoC.Solutions/Solutions/2020/22/Base.cs
+++ AoC.Solutions/Solutions/2020/22/Base.cs
@@ -5,4 +5,32 @@ namespace AoC.Solutions.Solutions._2020._22;
 public abstract class Base : Solution
 {
     public override string Description => "Crab cards";
+
+    protected readonly Queue<int> Player1Cards = new();
+
+    protected readonly Queue<int> Player2Cards = new();
+
+    protected void ParseInput()
+    {
+        var player1 = true;
+
+        foreach (var line in Input.Skip(1))
+        {
+            if (string.IsNullOrWhiteSpace(line) || line[0] == 'P')
+            {
+                player1 = false;
+
+                continue;
+            }
+
+            if (player1)
+            {
+                Player1Cards.Enqueue(int.Parse(line));
+            }
+            else
+            {
+                Player2Cards.Enqueue(int.Parse(line));
+            }
+        }
+    }
 }
--- AoC.Solutions/Solutions/2020/22/Part1.cs
+++ AoC.Solutions/Solutions/2020/22/Part1.cs
@@ -5,10 +5,6 @@ namespace AoC.Solutions.Solutions._2020._22;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private readonly Queue<int> _player1Cards = new();
-
-    private readonly Queue<int> _player2Cards = new();
-
     public override string GetAnswer()
     {
         ParseInput();
@@ -22,7 +18,7 @@ public class Part1 : Base
 
     private int CalculateScore()
     {
-        var winnerDeck = (_player1Cards.Count == 0 ? _player2Cards : _player1Cards).Reverse().ToArray();
+        var winnerDeck = (Player1Cards.Count == 0 ? Player2Cards : Player1Cards).Reverse().ToArray();
 
         var score = 0;
 
@@ -36,47 +32,23 @@ public class Part1 : Base
 
     private void Play()
     {
-        while (_player1Cards.Count > 0 && _player2Cards.Count > 0)
+        while (Player1Cards.Count > 0 && Player2Cards.Count > 0)
         {
-            var player1Draw = _player1Cards.Dequeue();
+            var player1Draw = Player1Cards.Dequeue();
             
-            var player2Draw = _player2Cards.Dequeue();
+            var player2Draw = Player2Cards.Dequeue();
 
             if (player1Draw > player2Draw)
             {
-                _player1Cards.Enqueue(player1Draw);
+                Player1Cards.Enqueue(player1Draw);
 
-                _player1Cards.Enqueue(player2Draw);
+                Player1Cards.Enqueue(player2Draw);
             }
             else
             {
-                _player2Cards.Enqueue(player2Draw);
+                Player2Cards.Enqueue(player2Draw);
 
-                _player2Cards.Enqueue(player1Draw);
-            }
-        }
-    }
-
-    private void ParseInput()
-    {
-        var player1 = true;
-
-        foreach (var line in Input.Skip(1))
-        {
-            if (string.IsNullOrWhiteSpace(line) || line[0] == 'P')
-            {
-                player1 = false;
-
-                continue;
-            }
-
-            if (player1)
-            {
-                _player1Cards.Enqueue(int.Parse(line));
-            }
-            else
-            {
-                _player2Cards.Enqueue(int.Parse(line));
+                Player2Cards.Enqueue(player1Draw);
             }
         }
     }
