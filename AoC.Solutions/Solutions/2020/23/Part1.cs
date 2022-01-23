61bcda95a95055d0a014d20ac5445dd7052e0e61
Stevö <stevehuwjohn@gmail.com>
2022-01-23 21:27:00 +0000

    WIP

--- AoC.Solutions/Solutions/2020/23/Part1.cs
+++ AoC.Solutions/Solutions/2020/23/Part1.cs
@@ -1,12 +1,41 @@
-﻿//using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
-//namespace AoC.Solutions.Solutions._2020._23;
+namespace AoC.Solutions.Solutions._2020._23;
 
-//[UsedImplicitly]
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        return "TESTING";
-//    }
-//}
+[UsedImplicitly]
+public class Part1 : Base
+{
+    private readonly byte[] _cupsIndexes = new byte[9];
+    
+    private readonly byte[] _cups = new byte[9];
+
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        for (var i = 0; i < 10; i++)
+        {
+            PerformMove();
+        }
+
+        return string.Concat(_cupsIndexes.Select(c => (char) (c + '0')));
+    }
+
+    private void PerformMove()
+    {
+    }
+
+    private void ParseInput()
+    {
+        var i = 0;
+
+        foreach (var c in Input[0])
+        {
+            _cupsIndexes[(byte) (c - '1')] = (byte) i;
+
+            _cups[i] = (byte) (c - '0');
+
+            i++;
+        }
+    }
+}
