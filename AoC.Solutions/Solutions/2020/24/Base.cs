ec7d0fcb063e34ebb7882661f1dc5746b11a6b98
Stevö <stevehuwjohn@gmail.com>
2022-01-23 13:32:50 +0000

    2020.24 input

---
+++ AoC.Solutions/Solutions/2020/24/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2020._24;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Tile flipping";
+}
