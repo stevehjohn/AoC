6daae09a775e9565ce75589153193ac6bb0e9d5f
Stevö <stevehuwjohn@gmail.com>
2022-01-23 13:18:01 +0000

    2020.21

--- AoC.Solutions/Solutions/2020/21/Part2.cs
+++ AoC.Solutions/Solutions/2020/21/Part2.cs
@@ -9,6 +9,45 @@ public class Part2 : Base
     {
         ParseInput();
 
-        return "TESTING";
+        var ingredientAllergens = IdentifyIngredientAllergens();
+
+        var result = string.Join(',', ingredientAllergens.OrderBy(ia => ia.Allergen).Select(ia => ia.Ingredient));
+
+        return result;
+    }
+
+    private List<(string Ingredient, string Allergen)> IdentifyIngredientAllergens()
+    {
+        var identifiedAllergens = new HashSet<string>();
+
+        while (true)
+        {
+            var allergensToCheck = Allergens.Where(a => identifiedAllergens.All(ia => ia != a.Key));
+
+            foreach (var allergen in allergensToCheck)
+            {
+                if (allergen.Value.Count == 1)
+                {
+                    var allergenIngredient = allergen.Value.Single();
+
+                    identifiedAllergens.Add(allergen.Key);
+
+                    foreach (var otherAllergen in Allergens)
+                    {
+                        if (otherAllergen.Key == allergen.Key)
+                        {
+                            continue;
+                        }
+
+                        otherAllergen.Value.Remove(allergenIngredient);
+                    }
+                }
+            }
+
+            if (Allergens.All(a => a.Value.Count == 1))
+            {
+                return Allergens.Select(a => (Ingredient: a.Value.Single(), Allergen: a.Key)).ToList();
+            }
+        }
     }
 }
