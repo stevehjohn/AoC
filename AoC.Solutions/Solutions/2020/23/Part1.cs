d6c43e1a2cdacc4983bbccb93547bdd259f58c50
Stevö <stevehuwjohn@gmail.com>
2022-01-24 11:42:25 +0000

    WIP

--- AoC.Solutions/Solutions/2020/23/Part1.cs
+++ AoC.Solutions/Solutions/2020/23/Part1.cs
@@ -5,37 +5,75 @@ namespace AoC.Solutions.Solutions._2020._23;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private readonly byte[] _cupsIndexes = new byte[9];
+    private readonly byte[] _cups = new byte[10];
     
-    private readonly byte[] _cups = new byte[9];
+    private int _cupLabel;
 
     public override string GetAnswer()
     {
         ParseInput();
 
-        for (var i = 0; i < 10; i++)
+        //for (var i = 0; i < 10; i++)
         {
             PerformMove();
         }
 
-        return string.Concat(_cupsIndexes.Select(c => (char) (c + '0')));
+        return "TESTING";
     }
 
     private void PerformMove()
     {
+        var targetCup = _cupLabel;
+
+        int i;
+
+        for (i = 0; i < 4; i++)
+        {
+            targetCup = _cups[targetCup];
+        }
+
+        if (targetCup != _cupLabel - 1)
+        {
+            i = 0;
+
+            var highest = targetCup;
+
+            var highestLower = 0;
+
+            while (i < 4)
+            {
+                targetCup = _cups[targetCup];
+
+                if (targetCup > highest)
+                {
+                    highest = targetCup;
+                }
+
+                if (targetCup < _cupLabel && targetCup > highestLower)
+                {
+                    highestLower = targetCup;
+                }
+
+                i++;
+            }
+
+            targetCup = highestLower == 0 ? highest : highestLower;
+        }
+
+        Console.WriteLine(targetCup);
     }
 
     private void ParseInput()
     {
-        var i = 0;
+        var data = Input[0];
 
-        foreach (var c in Input[0])
+        for (var i = 0; i < data.Length - 1; i++)
         {
-            _cupsIndexes[(byte) (c - '1')] = (byte) i;
-
-            _cups[i] = (byte) (c - '0');
-
-            i++;
+            _cups[(byte)(data[i] - '0')] = (byte)(data[i + 1] - '0');
         }
+
+        _cups[(byte)(data[^1] - '0')] = (byte)(data[0] - '0');
+
+        _cupLabel = (byte)(Input[0][0] - '0');
     }
 }
