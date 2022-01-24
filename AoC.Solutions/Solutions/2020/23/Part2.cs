984f6cdab0a218f5c81fb5bfadc335d9ef3d2b80
Stevö <stevehuwjohn@gmail.com>
2022-01-24 16:47:05 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2020/23/Part2.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2020._23;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        for (var i = 0; i < 10_000_000; i++)
+        {
+            PerformMove();
+        }
+
+        return "TESTING";
+    }
+}
