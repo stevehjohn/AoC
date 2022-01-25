a9919ae10bc4370c3b286c0f99377d4db94f2aa4
Stevö <stevehuwjohn@gmail.com>
2022-01-25 10:06:49 +0000

    WIP

--- AoC.Solutions/Solutions/2020/19/Part1.cs
+++ AoC.Solutions/Solutions/2020/19/Part1.cs
@@ -1,41 +1,81 @@
-﻿//using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
-//namespace AoC.Solutions.Solutions._2020._19;
+namespace AoC.Solutions.Solutions._2020._19;
 
-//[UsedImplicitly]
-//public class Part1 : Base
-//{
-//    private List<string> _messages = new();
+[UsedImplicitly]
+public class Part1 : Base
+{
+    private readonly Dictionary<int, Rule> _rules = new();
 
-//    public override string GetAnswer()
-//    {
-//        ParseInput();
+    private readonly List<string> _messages = new();
 
-//        return "TESTING";
-//    }
+    public override string GetAnswer()
+    {
+        ParseInput();
 
-//    private void ParseInput()
-//    {
-//        var i = 0;
+        return "TESTING";
+    }
 
-//        var rules = new Dictionary<int, string>();
+    private void ParseInput()
+    {
+        var i = 0;
 
-//        while (! string.IsNullOrWhiteSpace(Input[i]))
-//        {
-//            var split = Input[i].Split(':');
+        while (!string.IsNullOrWhiteSpace(Input[i]))
+        {
+            var split = Input[i].Split(':', StringSplitOptions.TrimEntries);
 
-//            rules.Add(int.Parse(split[0]), split[1]);
+            var ruleId = int.Parse(split[0]);
 
-//            i++;
-//        }
+            var isLeaf = split[1][0] == '"';
 
-//        i++;
+            if (! _rules.TryGetValue(ruleId, out var rule))
+            {
+                rule = new Rule(ruleId);
 
-//        while (i < Input.Length)
-//        {
-//            _messages.Add(Input[i]);
+                _rules.Add(rule.Id, rule);
+            }
 
-//            i++;
-//        }
-//    }
-//}
+            if (isLeaf)
+            {
+                rule.Character = split[1][1];
+
+                continue;
+            }
+
+            i++;
+        }
+
+        i++;
+
+        while (i < Input.Length)
+        {
+            _messages.Add(Input[i]);
+
+            i++;
+        }
+    }
+}
+
+public class Rule
+{
+    public int Id { get; }
+
+    public char Character { get; set;  }
+
+    public List<Rule> SubRules { get; }
+
+    public List<Rule> LeftRules { get; }
+
+    public List<Rule> RightRules { get; }
+
+    public Rule(int id)
+    {
+        Id = id;
+
+        SubRules = new List<Rule>();
+
+        LeftRules = new List<Rule>();
+
+        RightRules = new List<Rule>();
+    }
+}
