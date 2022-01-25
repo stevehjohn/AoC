76648fac1aba711fc9b8461e65551dbdbcaeb7c4
Stevö <stevehuwjohn@gmail.com>
2022-01-25 13:48:44 +0000

    WIP

--- AoC.Solutions/Solutions/2020/19/Part1.cs
+++ AoC.Solutions/Solutions/2020/19/Part1.cs
@@ -22,6 +22,8 @@ public class Part1 : Base
             validCount += _rootRule.IsValid(message) ? 1 : 0;
         }
 
+        //validCount = _rootRule.IsValid("ababbb") ? 1 : 0;
+
         return validCount.ToString();
     }
 
@@ -106,99 +108,5 @@ public class Part1 : Base
         }
 
         return result;
-    }
-}
-
-public class Rule
-{
-    public int Id { get; }
-
-    public char Character { get; set;  }
-
-    public List<Rule> SubRules { get; set; }
-
-    public List<Rule> LeftRules { get; set; }
-
-    public List<Rule> RightRules { get; set; }
-
-    public Rule(int id)
-    {
-        Id = id;
-    }
-
-    public bool IsValid(string input)
-    {
-        return Validate(input).Length == 0;
-    }
-
-    public string Validate(string input)
-    {
-        if (input.Length == 0)
-        {
-            return input;
-        }
-
-        if (SubRules != null)
-        {
-            return ValidateSubRules(input);
-        }
-
-        if (LeftRules != null && RightRules != null)
-        {
-            return ValidateOrRule(input);
-        }
-
-        return ValidateCharacterRule(input);
-    }
-
-    // Hmm. If no matches, string will never decrease in length.
-    // How to deal with that?
-    private string ValidateCharacterRule(string input)
-    {
-        return input[0] == Character ? input[1..] : input;
-    }
-
-    private string ValidateOrRule(string input)
-    {
-        var inputCopy = input;
-
-        foreach (var leftRule in LeftRules)
-        {
-            input = inputCopy;
-
-            input = leftRule.Validate(input);
-
-            foreach (var rightRule in RightRules)
-            {
-                input = rightRule.Validate(input);
-            }
-        }
-
-        return input;
-    }
-
-    private string ValidateSubRules(string input)
-    {
-        foreach (var rule in SubRules)
-        {
-            input = rule.Validate(input);
-        }
-
-        return input;
-    }
-
-    public override string ToString()
-    {
-        if (Character != '\0')
-        {
-            return $"{Id}: \"{Character}\"";
-        }
-
-        if (SubRules != null)
-        {
-            return $"{Id}: {string.Join(", ", SubRules.Select(r => r.Id))}";
-        }
-
-        return $"{Id}: {string.Join(", ", LeftRules.Select(r => r.Id))} | {string.Join(", ", RightRules.Select(r => r.Id))}";
     }
 }
