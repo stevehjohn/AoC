80e7b196f12c7f63f2bf52e7729c0988e482beb2
Stevö <stevehuwjohn@gmail.com>
2022-01-26 16:35:22 +0000

    2018.5

---
+++ AoC.Solutions/Solutions/2018/05/Base.cs
@@ -0,0 +1,35 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2018._05;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Polymer reactions";
+
+    protected int ReactPolymer(string polymerString)
+    {
+        var polymer = new LinkedList<char>(polymerString.ToCharArray());
+
+        var node = polymer.First;
+
+        while (node != null && node.Next != null)
+        {
+            if (char.ToLower(node.Value) == char.ToLower(node.Next.Value) && node.Value != node.Next.Value)
+            {
+                var previous = node.Previous;
+
+                polymer.Remove(node.Next);
+
+                polymer.Remove(node);
+
+                node = previous ?? polymer.First;
+            }
+            else
+            {
+                node = node.Next;
+            }
+        }
+
+        return polymer.Count;
+    }
+}
---
+++ AoC.Solutions/Solutions/2018/05/Part1.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2018._05;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        return ReactPolymer(Input[0]).ToString();
+    }
+}
