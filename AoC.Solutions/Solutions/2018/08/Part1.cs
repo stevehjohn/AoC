cc54c7bfc4e20268c3f70fb0c36971baec5a06d6
Stevö <stevehuwjohn@gmail.com>
2022-01-26 23:41:26 +0000

    2018.8

---
+++ AoC.Solutions/Solutions/2018/08/Part1.cs
@@ -0,0 +1,45 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2018._08;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var input = ParseInput();
+
+        var result = SumMetadata(input);
+
+        return result.Sum.ToString();
+    }
+
+    private static (int Sum, List<int> Remaining) SumMetadata(List<int> input)
+    {
+        var childCount = input[0];
+
+        var metadataCount = input[1];
+
+        var remaining = input.Skip(2).ToList();
+
+        var total = 0;
+
+        for (var i = 0; i < childCount; i++)
+        {
+            var result = SumMetadata(remaining);
+
+            total += result.Sum;
+
+            remaining = result.Remaining;
+        }
+
+        total += remaining.Take(metadataCount).Sum();
+
+        return (total, remaining.Skip(metadataCount).ToList());
+    }
+
+    private List<int> ParseInput()
+    {
+        return Input[0].Split(' ').Select(int.Parse).ToList();
+    }
+}
