9f6bc5dd78f9801a936586ce49b9f20cfca7e62c
Stevö <stevehuwjohn@gmail.com>
2022-01-26 15:58:19 +0000

    2018.4

---
+++ AoC.Solutions/Solutions/2018/04/Part2.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2018._04;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        Data = ParseInput();
+
+        var guardIds = Data.Select(d => d.GuardId).Distinct();
+
+        var minuteMostAsleep = new List<(int GuardId, int Minute, int Times)>();
+
+        foreach (var guardId in guardIds)
+        {
+            var mostAsleep = GetMinuteMostAsleep(guardId);
+
+            minuteMostAsleep.Add((guardId, mostAsleep.Minute, mostAsleep.Times));
+        }
+
+        var result = minuteMostAsleep.MaxBy(d => d.Times);
+
+        return (result.GuardId * result.Minute).ToString();
+    }
+}
