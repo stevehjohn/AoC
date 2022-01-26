46cb189d2b11bb94866589fe1a0968da7e057da7
Stevö <stevehuwjohn@gmail.com>
2022-01-26 20:08:16 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2018/06/Part2.cs
@@ -0,0 +1,40 @@
+﻿using AoC.Solutions.Common;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2018._06;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        var result = MapClosestPoints();
+
+        return result.ToString();
+    }
+
+    private int MapClosestPoints()
+    {
+        var count = 0;
+
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                var position = new Point(x, y);
+
+                // This could probably be sped up by: https://www.youtube.com/watch?v=BK5x7IUTIyU&t=666s
+                var distances = Points.Select(p => ManhattanDistance(p, position));
+
+                if (distances.Sum() < 10_000)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
