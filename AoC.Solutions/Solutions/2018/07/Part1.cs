d1ef33ca8e5dc83d9bb7d8071865e4b5c5867feb
Stevö <stevehuwjohn@gmail.com>
2022-01-26 21:36:18 +0000

    2018.7

--- AoC.Solutions/Solutions/2018/07/Part1.cs
+++ AoC.Solutions/Solutions/2018/07/Part1.cs
@@ -1,18 +1,83 @@
-﻿using JetBrains.Annotations;
+﻿using System.Text;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2018._07;
 
 [UsedImplicitly]
 public class Part1 : Base
 {
-
+    private readonly Dictionary<char, List<char>> _steps = new();
 
     public override string GetAnswer()
     {
-        return "TESTING";
+        var startSteps = ParseInput();
+
+        var result = Solve(startSteps);
+
+        return result;
     }
 
-    private void ParseInput()
+    private string Solve(IEnumerable<char> startStep)
     {
+        var result = new StringBuilder();
+
+        var queue = new PriorityQueue<char, char>();
+
+        foreach (var item in startStep)
+        {
+            queue.Enqueue(item, item);
+        }
+
+        while (queue.Count > 0)
+        {
+            var step = queue.Dequeue();
+            
+            result.Append(step);
+
+            foreach (var item in _steps.Where(s => s.Value.Count > 0))
+            {
+                item.Value.Remove(step);
+
+                if (item.Value.Count == 0)
+                {
+                    queue.Enqueue(item.Key, item.Key);
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+
+    private IEnumerable<char> ParseInput()
+    {
+        var allSteps = new HashSet<char>();
+        
+        var allRequires = new HashSet<char>();
+
+        foreach (var line in Input)
+        {
+            var step = line[36];
+
+            var requires = line[5];
+
+            if (! _steps.TryGetValue(step, out var requirements))
+            {
+                requirements = new List<char>();
+
+                _steps.Add(step, requirements);
+            }
+            else
+            {
+                requirements = _steps[step];
+            }
+
+            requirements.Add(requires);
+
+            allSteps.Add(step);
+
+            allRequires.Add(requires);
+        }
+
+        return allRequires.Except(allSteps).OrderBy(s => s);
     }
 }
