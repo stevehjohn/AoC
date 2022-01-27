5ea12a46b5c4a7dc055373823eeb30ac14d29634
Stevö <stevehuwjohn@gmail.com>
2022-01-27 13:06:14 +0000

    2018.10

---
+++ AoC.Solutions/Solutions/2018/10/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2018._10;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Writing in the stars";
+}
