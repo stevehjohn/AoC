32f33b5b9f563317cc3af925d7cea37b31ecc0c4
Stevö <stevehuwjohn@gmail.com>
2022-01-27 21:41:35 +0000

    WIP

--- AoC.Visualisations/Visualisations/2018/13/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2018/13/Visualisation.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace AoC.Visualisations.Visualisations._2018._13;
 
@@ -7,21 +9,40 @@ public class Visualisation : Game
 {
     private readonly GraphicsDeviceManager _graphicsDeviceManager;
 
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _sprites;
+
     public Visualisation()
     {
         _graphicsDeviceManager = new GraphicsDeviceManager(this)
                                  {
-                                     PreferredBackBufferWidth = 1920,
+                                     PreferredBackBufferWidth = 1200,
                                      PreferredBackBufferHeight = 1200
                                  };
 
-        Content.RootDirectory = "_Content";
+        // TODO: Make a base class that does this stuff.#
+        // Also, something funky going on with having to add \bin\Windows - investigate.
+        Content.RootDirectory = "_Content\\2020\\13\\bin\\Windows";
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.Blue);
+        GraphicsDevice.Clear(Color.Black);
+
+        _spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
+
+        _spriteBatch.Draw(_sprites, new Vector2(100, 100), new Rectangle(0, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _sprites = Content.Load<Texture2D>("sprites");
     }
 }
