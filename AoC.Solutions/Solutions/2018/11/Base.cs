1372b32c1af32e8f66e370aa6d8a21c267ca31e8
Stevö <stevehuwjohn@gmail.com>
2022-01-27 16:04:03 +0000

    WIP

--- AoC.Solutions/Solutions/2018/11/Base.cs
+++ AoC.Solutions/Solutions/2018/11/Base.cs
@@ -4,5 +4,44 @@ namespace AoC.Solutions.Solutions._2018._11;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Max power... cell";
+
+    protected int[,] Grid;
+
+    protected const int GridSize = 300;
+
+    protected void CalculateCellPowers(int serial)
+    {
+        Grid = new int[GridSize, GridSize];
+
+        for (var y = 0; y < GridSize; y++)
+        {
+            for (var x = 0; x < GridSize; x++)
+            {
+                Grid[x, y] = CalculatePowerLevel(x, y, serial);
+            }
+        }
+    }
+
+    protected int ParseInput()
+    {
+        return int.Parse(Input[0]);
+    }
+
+    private static int CalculatePowerLevel(int x, int y, int serial)
+    {
+        var result = x + 10;
+
+        result *= y;
+
+        result += serial;
+
+        result *= x + 10;
+
+        result = result / 100 % 10;
+
+        result -= 5;
+
+        return result;
+    }
 }
--- AoC.Solutions/Solutions/2018/11/Part1.cs
+++ AoC.Solutions/Solutions/2018/11/Part1.cs
@@ -1,4 +1,6 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Exceptions;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2018._11;
 
@@ -7,6 +9,39 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "TESTING";
+        CalculateCellPowers(ParseInput());
+
+        var result = GetMaxPower();
+
+        return $"{result.X},{result.Y}";
+    }
+
+    private Point GetMaxPower()
+    {
+        var max = 0;
+
+        Point position = null;
+
+        for (var y = 1; y < GridSize - 1; y++)
+        {
+            for (var x = 1; x < GridSize - 1; x++)
+            {
+                var sum = Grid[x - 1, y - 1] + Grid[x, y - 1] + Grid[x + 1, y - 1] + Grid[x - 1, y] + Grid[x, y] + Grid[x + 1, y] + Grid[x - 1, y + 1] + Grid[x, y + 1] + Grid[x + 1, y + 1];
+
+                if (sum > max)
+                {
+                    max = sum;
+
+                    position = new Point(x - 1, y - 1);
+                }
+            }
+        }
+
+        if (position == null)
+        {
+            throw new PuzzleException("Solution not found.");
+        }
+
+        return position;
     }
 }
