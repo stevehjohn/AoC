d24813125e913b716d5284d23afe7dcc3fdfc57f
Stevö <stevehuwjohn@gmail.com>
2022-01-27 18:06:53 +0000

    WIP

--- AoC.Solutions/Solutions/2018/12/Part1.cs
+++ AoC.Solutions/Solutions/2018/12/Part1.cs
@@ -2,8 +2,30 @@
 
 public class Part1 : Base
 {
+    private List<int> _potsWithPlants = new();
+
     public override string GetAnswer()
     {
+        ParseInput();
+
         return "TESTING";
+    }
+
+    private void ParseInput()
+    {
+        var initialState = Input[0][15..];
+
+        for (var i = 0; i < initialState.Length; i++)
+        {
+            if (initialState[i] == '#')
+            {
+                _potsWithPlants.Add(i);
+            }
+        }
+
+        foreach (var line in Input.Skip(2))
+        {
+            
+        }
     }
 }
