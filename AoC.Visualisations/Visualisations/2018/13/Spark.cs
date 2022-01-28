eda2ad296d88178d08c0738dde9376acbd49ae64
Stevö <stevehuwjohn@gmail.com>
2022-01-28 12:42:27 +0000

    WIP

---
+++ AoC.Visualisations/Visualisations/2018/13/Spark.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC.Visualisations.Visualisations._2018._13;
+
+public class Spark
+{
+    public PointFloat Position { get; set; }
+
+    public PointFloat Vector { get; set; }
+
+    public int Ticks { get; set; }
+}
