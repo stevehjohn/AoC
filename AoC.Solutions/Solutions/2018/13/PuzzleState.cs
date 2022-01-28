006716860801825e17a7e39f0ae4c3d87b0c3d16
Stevö <stevehuwjohn@gmail.com>
2022-01-28 01:01:47 +0000

    WIP

--- AoC.Solutions/Solutions/2018/13/PuzzleState.cs
+++ AoC.Solutions/Solutions/2018/13/PuzzleState.cs
@@ -1,6 +1,10 @@
-﻿namespace AoC.Solutions.Solutions._2018._13;
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2018._13;
 
 public class PuzzleState
 {
     public char[,] Map { get; set; }
+
+    public List<Point> Carts { get; set; }
 }
--- AoC.Visualisations/Visualisations/2018/13/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2018/13/Visualisation.cs
@@ -1,8 +1,10 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using System.Diagnostics;
+using AoC.Solutions.Infrastructure;
 using AoC.Solutions.Solutions._2018._13;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Color = Microsoft.Xna.Framework.Color;
+using Point = Microsoft.Xna.Framework.Point;
 using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace AoC.Visualisations.Visualisations._2018._13;
@@ -13,9 +15,15 @@ public class Visualisation : Game, IVisualiser<PuzzleState>
 
     private SpriteBatch _spriteBatch;
 
-    private Texture2D _sprites;
+    private Texture2D _mapTiles;
+
+    private Texture2D _spark;
 
     private readonly Part1 _puzzle;
+
+    private readonly List<Spark> _sparks = new();
+
+    private readonly Random _rng = new();
 
     public Visualisation()
     {
@@ -23,8 +31,8 @@ public class Visualisation : Game, IVisualiser<PuzzleState>
 
         _graphicsDeviceManager = new GraphicsDeviceManager(this)
                                  {
-                                     PreferredBackBufferWidth = 1200,
-                                     PreferredBackBufferHeight = 1200
+                                     PreferredBackBufferWidth = 1050,
+                                     PreferredBackBufferHeight = 1050
                                  };
 
         // TODO: Make a base class that does this stuff.
@@ -39,18 +47,90 @@ public class Visualisation : Game, IVisualiser<PuzzleState>
 
         _spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
 
+        DrawMap(state);
+
+        DrawCarts(state);
+
+        _spriteBatch.End();
+    }
+
+    private void DrawCarts(PuzzleState state)
+    {
+        foreach (var cart in state.Carts)
+        {
+            if (_rng.Next(100) == 0)
+            {
+                _sparks.Add(new Spark { Position = new PointFloat { X = cart.X, Y = cart.Y }, Vector = new PointFloat { X = (-5f + _rng.Next(11)) / 10, Y = (-10f + _rng.Next(21)) / 10 } });
+            }
+        }
+
+        var toRemove = new List<Spark>();
+
+        foreach (var spark in _sparks)
+        {
+            _spriteBatch.Draw(_spark, new Vector2(spark.Position.X * 7, spark.Position.Y * 7), new Rectangle(0, 0, 5, 5), Color.White * (1 - spark.Ticks / 20f), 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 1);
+
+            spark.Ticks++;
+
+            if (spark.Ticks > 20)
+            {
+                toRemove.Add(spark);
+            }
+
+            spark.Position.X += spark.Vector.X;
+
+            spark.Position.Y += spark.Vector.Y;
+
+            spark.Vector.Y += 0.1f;
+        }
+
+        foreach (var spark in toRemove)
+        {
+            _sparks.Remove(spark);
+        }
+
+        Debug.WriteLine(_sparks.Count);
+    }
+
+    private void DrawMap(PuzzleState state)
+    {
         for (var y = 0; y < state.Map.GetLength(1); y++)
         {
             for (var x = 0; x < state.Map.GetLength(0); x++)
             {
-                if (state.Map[x, y] != ' ')
+                switch (state.Map[x, y])
                 {
-                    _spriteBatch.Draw(_sprites, new Vector2(x * 8, y * 8), new Rectangle(0, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+                    case '─':
+                        _spriteBatch.Draw(_mapTiles, new Vector2(x * 7, y * 7), new Rectangle(0, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                        break;
+                    case '│':
+                        _spriteBatch.Draw(_mapTiles, new Vector2(x * 7, y * 7), new Rectangle(7, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                        break;
+                    case '└':
+                        _spriteBatch.Draw(_mapTiles, new Vector2(x * 7, y * 7), new Rectangle(14, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                        break;
+                    case '┌':
+                        _spriteBatch.Draw(_mapTiles, new Vector2(x * 7, y * 7), new Rectangle(21, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                        break;
+                    case '┐':
+                        _spriteBatch.Draw(_mapTiles, new Vector2(x * 7, y * 7), new Rectangle(28, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                        break;
+                    case '┘':
+                        _spriteBatch.Draw(_mapTiles, new Vector2(x * 7, y * 7), new Rectangle(35, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                        break;
+                    case '┼':
+                        _spriteBatch.Draw(_mapTiles, new Vector2(x * 7, y * 7), new Rectangle(42, 0, 7, 7), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                        break;
                 }
             }
         }
-
-        _spriteBatch.End();
     }
 
     protected override void Draw(GameTime gameTime)
@@ -62,6 +142,24 @@ public class Visualisation : Game, IVisualiser<PuzzleState>
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        _sprites = Content.Load<Texture2D>("sprites");
+        _mapTiles = Content.Load<Texture2D>("map-tiles");
+
+        _spark = Content.Load<Texture2D>("spark");
     }
+}
+
+public class Spark
+{
+    public PointFloat Position { get; set; }
+
+    public PointFloat Vector { get; set; }
+
+    public int Ticks { get; set; }
+}
+
+public class PointFloat
+{
+    public float X { get; set; }
+
+    public float Y { get; set; }
 }
