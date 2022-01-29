82c934414ddfe21bb3404d691e0dccafb49833c1
Stevö <stevehuwjohn@gmail.com>
2022-01-29 11:07:49 +0000

    WIP

--- AoC.Visualisations/Visualisations/2018/13/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2018/13/Visualisation.cs
@@ -138,7 +138,7 @@ public class Visualisation : VisualisationBase, IVisualiser<PuzzleState>
             }
             else
             {
-                _collisions.Add(new Collision { Position = _state.CollisionPoint, Ticks = int.MaxValue, SpriteOffset = 5 });
+                _collisions.Add(new Collision { Position = _state.CollisionPoint, Ticks = _puzzle is Part1 ? int.MaxValue : 200, SpriteOffset = 5 });
 
                 _state.CollisionPoint = null;
             }
