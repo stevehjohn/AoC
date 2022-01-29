26166264f167ea8707d86dcda7287fd8f37d2cb8
Stevö <stevehuwjohn@gmail.com>
2022-01-29 10:37:46 +0000

    WIP

---
+++ AoC.Visualisations/Exceptions/VisualisationParameterException.cs
@@ -0,0 +1,12 @@
+﻿namespace AoC.Visualisations.Exceptions;
+
+public class VisualisationParameterException : Exception
+{
+    public VisualisationParameterException()
+    {
+    }
+
+    public VisualisationParameterException(string message) : base(message)
+    {
+    }
+}
--- AoC.Visualisations/Infrastructure/EntryPoint.cs
+++ AoC.Visualisations/Infrastructure/EntryPoint.cs
@@ -1,14 +1,78 @@
-using AoC.Visualisations.Visualisations._2018._13;
+using AoC.Visualisations.Exceptions;
+using Microsoft.Xna.Framework;
 
 namespace AoC.Visualisations.Infrastructure;
 
 public static class EntryPoint
 {
     [STAThread]
-    private static void Main()
+    private static void Main(string[] arguments)
     {
-        using var visualisation = new Visualisation();
-        
-        visualisation.Run();
+        if (arguments.Length != 1)
+        {
+            throw new VisualisationParameterException("Please specify the visualisation to run, format: year.day.part, e.g. 2021.05.2.");
+        }
+
+        string year;
+
+        string day;
+
+        string part;
+
+        try
+        {
+            year = arguments[0][..4];
+
+            day = arguments[0][5..7];
+
+            part = arguments[0][8].ToString();
+
+            if (! int.TryParse(year, out _))
+            {
+                throw new VisualisationParameterException();
+            }
+
+            if (! int.TryParse(day, out _))
+            {
+                throw new VisualisationParameterException();
+            }
+
+            if (! int.TryParse(part, out _))
+            {
+                throw new VisualisationParameterException();
+            }
+        }
+        catch
+        {
+            throw new VisualisationParameterException("Unable to parse required visualisation parameter, format: year.day.part, e.g. 2021.05.2.");
+        }
+
+        var visualisationClass = $"AoC.Visualisations.Visualisations._{year}._{day}.Visualisation";
+
+        Game visualisation;
+
+        try
+        {
+            // ReSharper disable once PossibleNullReferenceException - that's what the try... catch is for.
+            visualisation = (Game) Activator.CreateInstance(null, visualisationClass).Unwrap();
+        }
+        catch
+        {
+            throw new VisualisationParameterException($"Unable to find visualisation for {year}.{day}.{part}.");
+        }
+
+        if (visualisation == null)
+        {
+            throw new VisualisationParameterException($"Unable to find visualisation for {year}.{day}.{part}.");
+        }
+
+        try
+        {
+            visualisation.Run();
+        }
+        finally
+        {
+            visualisation.Dispose();
+        }
     }
 }
