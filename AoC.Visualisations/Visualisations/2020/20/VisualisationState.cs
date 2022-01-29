01c71c2c8b9a963cf49d30a156da5457a820f2a2
Stevö <stevehuwjohn@gmail.com>
2022-01-29 17:43:19 +0000

    WIP

---
+++ AoC.Visualisations/Visualisations/2020/20/VisualisationState.cs
@@ -0,0 +1,21 @@
+﻿using Point = Microsoft.Xna.Framework.Point;
+
+namespace AoC.Visualisations.Visualisations._2020._20;
+
+public class VisualisationState
+{
+    public Mode Mode { get; set; }
+
+    public int Elapsed { get; set; }
+
+    public Point Position { get; set; }
+}
+
+public enum Mode
+{
+    Scanning,
+    HighlightMatch,
+    MoveToCentre,
+    Transform,
+    MoveToJigsaw
+}
