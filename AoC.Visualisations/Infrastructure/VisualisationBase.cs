ae0af4f4add7db2d8370bc01dbab6f53a3c36b0f
Stevö <stevehuwjohn@gmail.com>
2022-01-29 12:16:15 +0000

    WIP

--- AoC.Visualisations/Infrastructure/VisualisationBase.cs
+++ AoC.Visualisations/Infrastructure/VisualisationBase.cs
@@ -5,17 +5,24 @@ namespace AoC.Visualisations.Infrastructure;
 
 public abstract class VisualisationBase<T> : Game, IVisualiser<T>, IMultiPartVisualiser
 {
-    protected readonly Queue<T> StateQueue = new();
+    protected bool HasNextState => _stateQueue.Count > 0;
+
+    private readonly Queue<T> _stateQueue = new();
 
     public abstract void SetPart(int part);
 
     public void PuzzleStateChanged(T state)
     {
-        if (StateQueue.Count > 1000)
+        if (_stateQueue.Count > 1000)
         {
             Thread.Sleep(1000);
         }
 
-        StateQueue.Enqueue(state);
+        _stateQueue.Enqueue(state);
+    }
+
+    protected T GetNextState()
+    {
+        return _stateQueue.Dequeue();
     }
 }
