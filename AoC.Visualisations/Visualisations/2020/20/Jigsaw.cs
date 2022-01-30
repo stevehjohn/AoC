2c47dfa92f297a5846dfda22b7a116b3b8e09616
Stevö <stevehuwjohn@gmail.com>
2022-01-30 16:17:24 +0000

    WIP

--- AoC.Visualisations/Visualisations/2020/20/Jigsaw.cs
+++ AoC.Visualisations/Visualisations/2020/20/Jigsaw.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Color = Microsoft.Xna.Framework.Color;
+using Point = Microsoft.Xna.Framework.Point;
 using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace AoC.Visualisations.Visualisations._2020._20;
@@ -27,10 +28,14 @@ public class Jigsaw
     private const int Left = Top;
 
     private const int CentreY = Top + Constants.TilePadding + Constants.TileSize * (Constants.JigsawSize - 1) / 2;
-    
+
     private const int CentreX = Left + Constants.TilePadding + Constants.TileSize * (Constants.JigsawSize - 1) / 2;
 
     private Tile _currentTile;
+
+    private Point _puzzleSize;
+
+    private Point _puzzleOrigin = new(CentreX, CentreY);
 
     public Jigsaw(List<Tile> imageSegments, Texture2D image, Texture2D mat, Texture2D border)
     {
@@ -53,11 +58,19 @@ public class Jigsaw
         _currentTile = tile;
 
         _jigsaw.Add(tile);
+
+        var width = (_jigsaw.Max(t => t.PositionInPuzzle.X) - _jigsaw.Min(t => t.PositionInPuzzle.X)) * Constants.TileSize;
+
+        var height = (_jigsaw.Max(t => t.PositionInPuzzle.Y) - _jigsaw.Min(t => t.PositionInPuzzle.Y)) * Constants.TileSize;
+
+        _puzzleSize = new Point(width, height);
     }
 
     public void Update()
     {
         _currentTile = null;
+
+        _puzzleOrigin = new Point(CentreX - _puzzleSize.X / 2, CentreY - _puzzleSize.Y / 2);
     }
 
     public void Draw(SpriteBatch spriteBatch)
@@ -66,7 +79,8 @@ public class Jigsaw
 
         DrawPieces(spriteBatch);
 
-        spriteBatch.Draw(_border, new Vector2(Left - Constants.TileSize, Top - Constants.TileSize), new Rectangle(0, 0, MatSize + Constants.TileSize * 2, MatSize + Constants.TileSize * 2), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0.1f);
+        spriteBatch.Draw(_border, new Vector2(Left - Constants.TileSize, Top - Constants.TileSize), new Rectangle(0, 0, MatSize + Constants.TileSize * 2, MatSize + Constants.TileSize * 2), Color.White, 0, Vector2.Zero, Vector2.One,
+                         SpriteEffects.None, 0.1f);
     }
 
 
@@ -74,13 +88,13 @@ public class Jigsaw
     {
         foreach (var tile in _jigsaw)
         {
-            spriteBatch.Draw(_image, 
-                             new Vector2(tile.PositionInPuzzle.X * Constants.TileSize, tile.PositionInPuzzle.Y * Constants.TileSize), 
-                             new Rectangle(tile.PositionInPuzzle.X * Constants.TileSize, tile.PositionInPuzzle.Y * Constants.TileSize, Constants.TileSize, Constants.TileSize), 
-                             Color.White, 
-                             0, 
-                             Vector2.Zero, 
-                             Vector2.One, 
+            spriteBatch.Draw(_image,
+                             new Vector2(_puzzleOrigin.X + tile.PositionInPuzzle.X * Constants.TileSize, _puzzleOrigin.Y + tile.PositionInPuzzle.Y * Constants.TileSize),
+                             new Rectangle(tile.PositionInPuzzle.X * Constants.TileSize, tile.PositionInPuzzle.Y * Constants.TileSize, Constants.TileSize, Constants.TileSize),
+                             Color.White,
+                             0,
+                             Vector2.Zero,
+                             Vector2.One,
                              SpriteEffects.None, 0.05f);
         }
     }
