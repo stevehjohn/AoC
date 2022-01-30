7074589edc29be33b15a03c02896db5a23e0fbc7
Stevö <stevehuwjohn@gmail.com>
2022-01-30 14:19:51 +0000

    WIP

--- AoC.Visualisations/Visualisations/2020/20/TileQueue.cs
+++ AoC.Visualisations/Visualisations/2020/20/TileQueue.cs
@@ -7,11 +7,23 @@ namespace AoC.Visualisations.Visualisations._2020._20;
 
 public class TileQueue
 {
+    public Queue<Tile> MatchedTiles { get; } = new();
+
     private readonly List<Tile> _imageSegments;
 
     private readonly Texture2D _image;
 
     private readonly Texture2D _cell;
+
+    private readonly Texture2D _scanner;
+
+    private readonly Queue<Tile> _scanQueue = new();
+
+    private Tile _scanningFor;
+
+    private int _scannerIndex;
+
+    private int _scannerDirection;
 
     private const int QueueSize = Constants.TileSize * Constants.JigsawSize + Constants.TilePadding * (Constants.JigsawSize + 1);
 
@@ -19,16 +31,81 @@ public class TileQueue
 
     private const int Left = Constants.ScreenWidth - Top - QueueSize;
 
-    public TileQueue(List<Tile> imageSegments, Texture2D image, Texture2D cell)
+    public TileQueue(List<Tile> imageSegments, Texture2D image, Texture2D cell, Texture2D scanner)
     {
         _imageSegments = imageSegments;
 
         _image = image;
 
         _cell = cell;
+
+        _scanner = scanner;
+    }
+
+    public void StartScan(int tileId)
+    {
+        if (tileId == -1)
+        {
+            _scanQueue.Enqueue(_imageSegments[0]);
+        }
+        else
+        {
+            _scanQueue.Enqueue(_imageSegments.Single(s => s.Id == tileId));
+        }
+    }
+
+    public void Update()
+    {
+        if (_scanningFor != null)
+        {
+            if (_imageSegments[_scannerIndex] == _scanningFor)
+            {
+                MatchedTiles.Enqueue(_scanningFor);
+
+                _imageSegments.Remove(_scanningFor);
+
+                _scanningFor = null;
+            }
+            else
+            {
+                _scannerIndex += _scannerDirection;
+            }
+
+            return;
+        }
+
+        if (_scanQueue.Count > 0)
+        {
+            _scanningFor = _scanQueue.Dequeue();
+            
+            var targetIndex = _imageSegments.IndexOf(_scanningFor);
+
+            if (targetIndex > _scannerIndex)
+            {
+                _scannerDirection = 1;
+            }
+            else if (targetIndex < _scannerIndex)
+            {
+                _scannerDirection = -1;
+            }
+
+            if (_scannerIndex == 0)
+            {
+                _scannerIndex = 0;
+            }
+            else if (_scannerIndex >= _imageSegments.Count)
+            {
+                _scannerIndex = _imageSegments.Count - 1;
+            }
+        }
     }
 
     public void Draw(SpriteBatch spriteBatch)
+    {
+        DrawQueue(spriteBatch);
+    }
+
+    private void DrawQueue(SpriteBatch spriteBatch)
     {
         var i = 0;
 
@@ -46,6 +123,11 @@ public class TileQueue
 
                 spriteBatch.Draw(_cell, new Vector2(screenX, screenY), new Rectangle(0, 0, Constants.TileSize + Constants.TilePadding * 2, Constants.TileSize + Constants.TilePadding * 2), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
 
+                if (i >= _imageSegments.Count)
+                {
+                    continue;
+                }
+
                 var tile = _imageSegments[i];
 
                 var spriteEffects = tile.Transform.Contains('H') ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
@@ -57,16 +139,6 @@ public class TileQueue
                 spriteBatch.Draw(_image, new Vector2(screenX + Constants.TilePadding + offset, screenY + Constants.TilePadding + offset), tile.ImageSegment, Color.White, rotation, origin, Vector2.One, spriteEffects, 1);
 
                 i++;
-
-                if (i > _imageSegments.Count)
-                {
-                    break;
-                }
-            }
-
-            if (i > _imageSegments.Count)
-            {
-                break;
             }
         }
     }
