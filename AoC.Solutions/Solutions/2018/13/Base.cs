40bf3695126c0035c229cdfcad49e3c7f5491bb8
Stevö <stevehuwjohn@gmail.com>
2022-01-31 20:11:13 +0000

    WIP

--- AoC.Solutions/Solutions/2018/13/Base.cs
+++ AoC.Solutions/Solutions/2018/13/Base.cs
@@ -38,8 +38,6 @@ public abstract class Base : Solution
     {
         Point collision = null;
 
-        // TODO: Carts.OrderBy(c => c.Position.Y).ThenBy(c => c.Position.X)
-        // Then check for collision here, so 1st collision is reported?
         foreach (var cart in Carts.OrderBy(c => c.Position.Y).ThenBy(c => c.Position.X))
         {
             cart.Position.X += cart.Direction.X;
