e0abeca8f2687788eefa68d8b89b3d5cf064ad52
Stevö <stevehuwjohn@gmail.com>
2022-01-31 20:52:42 +0000

    WIP

--- AoC.Solutions/Solutions/2018/14/Part2.cs
+++ AoC.Solutions/Solutions/2018/14/Part2.cs
@@ -3,11 +3,11 @@
 namespace AoC.Solutions.Solutions._2018._14;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var result = Play(int.Parse(Input[0]));
+        var result = Play(int.Parse(Input[0]), true);
 
         return result;
     }
