7d9b2f113c8f3cf973109caa5f3584c5b599f456
Stevö <stevehuwjohn@gmail.com>
2022-01-31 22:32:59 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2018/16/Cpu.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Solutions.Solutions._2018._16;
+
+public class Cpu
+{
+    private readonly int[] _registers = new int[4];
+
+    private readonly Dictionary<string, Action<int, int, int, int[]>> _operations = new();
+
+    public void SetRegisters(int[] values)
+    {
+        Array.Copy(values, 0, _registers, 0, _registers.Length);
+    }
+
+    public int[] GetRegisters()
+    {
+        var copy = new int[4];
+
+        Array.Copy(_registers, 0, copy, 0, _registers.Length);
+
+        return copy;
+    }
+
+    public void Execute(string opCode, int a, int b, int c)
+    {
+        _operations[opCode].Invoke(a, b, c, _registers);
+    }
+
+    public void Initialise()
+    {
+        // ReSharper disable StringLiteralTypo
+        _operations.Add("addr", (a, b, c, registers) => registers[c] = registers[a] + registers[b]);
+
+        _operations.Add("addi", (a, b, c, registers) => registers[c] = registers[a] + b);
+
+        _operations.Add("mulr", (a, b, c, registers) => registers[c] = registers[a] * registers[b]);
+
+        _operations.Add("muli", (a, b, c, registers) => registers[c] = registers[a] * b);
+
+        _operations.Add("banr", (a, b, c, registers) => registers[c] = registers[a] & registers[b]);
+
+        _operations.Add("bani", (a, b, c, registers) => registers[c] = registers[a] & b);
+
+        _operations.Add("borr", (a, b, c, registers) => registers[c] = registers[a] | registers[b]);
+
+        _operations.Add("bori", (a, b, c, registers) => registers[c] = registers[a] | b);
+
+        _operations.Add("setr", (a, _, c, registers) => registers[c] = registers[a]);
+
+        _operations.Add("seti", (a, _, c, registers) => registers[c] = a);
+
+        _operations.Add("gtir", (a, b, c, registers) => registers[c] = a > registers[b] ? 1 : 0);
+
+        _operations.Add("gtri", (a, b, c, registers) => registers[c] = registers[a] > b ? 1 : 0);
+
+        _operations.Add("gtrr", (a, b, c, registers) => registers[c] = registers[a] > registers[b] ? 1 : 0);
+
+        _operations.Add("eqir", (a, b, c, registers) => registers[c] = a == registers[b] ? 1 : 0);
+
+        _operations.Add("eqri", (a, b, c, registers) => registers[c] = registers[a] == b ? 1 : 0);
+
+        _operations.Add("eqrr", (a, b, c, registers) => registers[c] = registers[a] == registers[b] ? 1 : 0);
+        // ReSharper restore StringLiteralTypo
+    }
+}
---
+++ AoC.Solutions/Solutions/2018/16/Part1.cs
@@ -0,0 +1,62 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2018._16;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    // ReSharper disable StringLiteralTypo
+    private readonly string[] _opCodes = { "addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr" };
+    // ReSharper restore StringLiteralTypo
+    
+    private readonly Cpu _cpu = new();
+
+    public override string GetAnswer()
+    {
+        _cpu.Initialise();
+
+        var total = 0;
+
+        for (var i = 0; i < Input.Length; i += 4)
+        {
+            if (string.IsNullOrWhiteSpace(Input[i]))
+            {
+                break;
+            }
+
+            var parts = Input[i + 1].Split(' ').Select(int.Parse).ToArray();
+
+            var matches = CountMatchingCodes(parts[1], parts[2], parts[3], ParseRegisters(Input[i]), ParseRegisters(Input[i + 2]));
+
+            if (matches >= 3)
+            {
+                total++;
+            }
+        }
+
+        return total.ToString();
+    }
+
+    private int CountMatchingCodes(int a, int b, int c, int[] initial, int[] expected)
+    {
+        var matches = 0;
+
+        for (var i = 0; i < _opCodes.Length; i++)
+        {
+            _cpu.SetRegisters(initial);
+
+            _cpu.Execute(_opCodes[i], a, b, c);
+
+            matches += expected.SequenceEqual(_cpu.GetRegisters()) ? 1 : 0;
+        }
+
+        return matches;
+    }
+
+    private static int[] ParseRegisters(string line)
+    {
+        line = line[9..][..^1];
+
+        return line.Split(',', StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+    }
+}
