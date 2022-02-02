e965df7a363733791e9b6da84f22e47d55ac2d54
Stevö <stevehuwjohn@gmail.com>
2022-02-02 12:41:39 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2018/16/Operation.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC.Solutions.Solutions._2018._16;
+
+public class Operation
+{
+    private string _name;
+
+    private Action<int, int, int, int[]> _operation;
+
+    public Operation(string name, Action<int, int, int, int[]> operation)
+    {
+        _operation = operation;
+    }
+}
