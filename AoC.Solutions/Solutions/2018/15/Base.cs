ab34b4cf72e1e483e927a132813bbd9d1213e562
Stevö <stevehuwjohn@gmail.com>
2022-02-02 12:03:39 +0000

    WIP

--- AoC.Solutions/Solutions/2018/15/Base.cs
+++ AoC.Solutions/Solutions/2018/15/Base.cs
@@ -1,8 +1,77 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2018._15;
 
 public abstract class Base : Solution
 {
     public override string Description => "Elves vs Goblins";
+
+    private int _width;
+
+    private int _height;
+
+    private bool[,] _map;
+
+    private readonly List<Unit> _units = new();
+
+    protected int Play()
+    {
+        var round = 0;
+
+        while (true)
+        {
+            var unitOrder = _units.OrderBy(u => u.Position.Y).ThenBy(u => u.Position.X);
+
+            foreach (var unit in unitOrder)
+            {
+                unit.Play();
+            }
+
+            if (_units.DistinctBy(u => u.Type).Count() == 1)
+            {
+                break;
+            }
+
+            round++;
+        }
+
+        return round * _units.Sum(u => u.Health);
+    }
+
+    protected void ParseInput()
+    {
+        _width = Input[0].Length;
+
+        _height = Input.Length;
+
+        _map = new bool[_width, _height];
+
+        for (var x = 0; x < _width; x++)
+        {
+            for (var y = 0; y < _height; y++)
+            {
+                var c = Input[y][x];
+
+                if (c == '.')
+                {
+                    continue;
+                }
+
+                if (c == '#')
+                {
+                    _map[x, y] = true;
+
+                    continue;
+                }
+
+                _units.Add(new Unit(c == 'E' ? Type.Elf : Type.Goblin, new Point(x, y), _map));
+            }
+        }
+
+        foreach (var unit in _units)
+        {
+            unit.SetUnits(_units);
+        }
+    }
 }
---
+++ AoC.Solutions/Solutions/2018/15/Unit.cs
@@ -0,0 +1,76 @@
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2018._15;
+
+public class Unit
+{
+    public Type Type { get; }
+
+    public Point Position { get; set; }
+
+    public int Health { get; set; } = 200;
+
+    private bool[,] _map;
+
+    private List<Unit> _otherUnits;
+
+    public Unit(Type type, Point position, bool[,] map)
+    {
+        Type = type;
+
+        Position = position;
+
+        _map = map;
+    }
+
+    public void SetUnits(List<Unit> units)
+    {
+        _otherUnits = units.Where(u => u != this).ToList();
+    }
+
+    public void Play()
+    {
+        var targets = _otherUnits.Where(u => u.Type != Type).ToList();
+
+        if (targets.Count == 0)
+        {
+            return;
+        }
+
+        var targetCells = GetTargetCells(targets);
+    }
+
+    private List<Point> GetTargetCells(List<Unit> targets)
+    {
+        var targetCells = new List<Point>();
+
+        foreach (var target in targets)
+        {
+            targetCells.AddRange(CheckNeighborCell(new Point(target.Position.X, target.Position.Y - 1), targets));
+            
+            targetCells.AddRange(CheckNeighborCell(new Point(target.Position.X + 1, target.Position.Y), targets));
+            
+            targetCells.AddRange(CheckNeighborCell(new Point(target.Position.X, target.Position.Y + 1), targets));
+            
+            targetCells.AddRange(CheckNeighborCell(new Point(target.Position.X - 1, target.Position.Y), targets));
+        }
+
+        return targetCells;
+    }
+
+    private IEnumerable<Point> CheckNeighborCell(Point position, List<Unit> targets)
+    {
+        if (_map[position.X, position.Y] || targets.Any(t => t.Position.Equals(position)))
+        {
+            return Enumerable.Empty<Point>();
+        }
+
+        return new List<Point> { position };
+    }
+}
+
+public enum Type
+{
+    Elf,
+    Goblin
+}
