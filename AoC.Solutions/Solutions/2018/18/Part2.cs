2ce4c84114180621fc8ca465e51ba669f5d50509
Stevö <stevehuwjohn@gmail.com>
2022-02-03 02:51:02 +0000

    WIP

--- AoC.Solutions/Solutions/2018/18/Part2.cs
+++ AoC.Solutions/Solutions/2018/18/Part2.cs
@@ -1,25 +1,76 @@
-﻿using JetBrains.Annotations;
+﻿using System.Text;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2018._18;
 
 [UsedImplicitly]
 public class Part2 : Base
 {
+    private readonly Dictionary<string, int> _states = new();
+
     public override string GetAnswer()
     {
         ParseInput();
 
-        // TODO: There will be a repeating pattern here.
-        for (var i = 0; i < 1_000_000_000 ; i++)
+        var cycle = 0;
+
+        while (true)
         {
+            cycle++;
+
             RunCycle();
 
-            if (i % 1_000_000 == 0)
+            var stateHash = HashState();
+
+            if (_states.ContainsKey(stateHash))
             {
-                Console.WriteLine(i);
+                break;
+            }
+
+            _states.Add(stateHash, cycle);
+        }
+
+        var finalState = _states.Single(s => s.Value == 1000000000 % cycle).Key;
+
+        return GetResourceValue(finalState).ToString();
+    }
+
+    private static int GetResourceValue(string state)
+    {
+        var wood = 0;
+
+        var yard = 0;
+
+        for (var i = 0; i < state.Length; i++)
+        {
+            if (state[i] == '|')
+            {
+                wood++;
+
+                continue;
+            }
+
+            if (state[i] == '#')
+            {
+                yard++;
             }
         }
 
-        return GetResourceValue().ToString();
+        return wood * yard;
+    }
+
+    private string HashState()
+    {
+        var builder = new StringBuilder();
+
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                builder.Append(Map[x, y]);
+            }
+        }
+
+        return builder.ToString();
     }
 }
