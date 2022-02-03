19521aa8f0fc9d5eb9326877b29f4672993cfd73
Stevö <stevehuwjohn@gmail.com>
2022-02-03 19:53:46 +0000

    2017

---
+++ AoC.Solutions/Solutions/2017/01/Base.cs
@@ -0,0 +1,29 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2017._01;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Captcha";
+
+    protected int Solve(int offset = 1)
+    {
+        var sum = 0;
+
+        var data = Input[0];
+
+        var length = data.Length;
+
+        for (var i = 0; i < length; i++)
+        {
+            var otherIndex = (i + offset) % length;
+
+            if (data[i] == data[otherIndex])
+            {
+                sum += data[i] - '0';
+            }
+        }
+
+        return sum;
+    }
+}
