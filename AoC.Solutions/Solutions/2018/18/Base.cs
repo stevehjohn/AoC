e47991a39fe0e87898ad2fe5e6b1ff41577cc1e9
Stevö <stevehuwjohn@gmail.com>
2022-02-03 01:21:46 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2018/18/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2018._18;
+
+public abstract class Base : Solution
+{
+    public override string Description => "I'm a lumberjack...";
+}
