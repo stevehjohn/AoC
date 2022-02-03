9511426939f1c9f58146360edf6f8fad77699efe
Stevö <stevehuwjohn@gmail.com>
2022-02-03 20:08:03 +0000

    2017.2

---
+++ AoC.Solutions/Solutions/2017/02/Part1.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._02;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var result = GetChecksum();
+
+        return result.ToString();
+    }
+
+    private int GetChecksum()
+    {
+        var sum = 0;
+
+        foreach (var line in Input)
+        {
+            var cells = line.Split('\t', StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
+
+            sum += cells.Max() - cells.Min();
+        }
+
+        return sum;
+    }
+}
---
+++ AoC.Solutions/Solutions/2017/02/Part2.cs
@@ -0,0 +1,46 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._02;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var result = Solve();
+
+        return result.ToString();
+    }
+
+    private int Solve()
+    {
+        var sum = 0;
+
+        foreach (var line in Input)
+        {
+            var cells = line.Split('\t', StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
+
+            for (var outer = 0; outer < cells.Count; outer++)
+            {
+                for (var inner = outer + 1; inner < cells.Count; inner++)
+                {
+                    var left = cells[outer];
+
+                    var right = cells[inner];
+
+                    if (left == right)
+                    {
+                        continue;
+                    }
+
+                    if (Math.Max(left, right) % Math.Min(left, right) == 0)
+                    {
+                        sum += Math.Max(left, right) / Math.Min(left, right);
+                    }
+                }
+            }
+        }
+
+        return sum;
+    }
+}
