125ecff34dadd459ce4c915f607bc9bd7842f646
Stevö <stevehuwjohn@gmail.com>
2022-02-03 19:53:46 +0000

    2017

---
+++ AoC.Solutions/Solutions/2017/01/Part2.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._01;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        return Solve(Input[0].Length / 2).ToString();
+    }
+}
