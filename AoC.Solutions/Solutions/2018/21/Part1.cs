d808abe182fe197796509f930f5ac88286734078
Stevö <stevehuwjohn@gmail.com>
2022-02-05 01:10:49 +0000

    WIP

--- AoC.Solutions/Solutions/2018/21/Part1.cs
+++ AoC.Solutions/Solutions/2018/21/Part1.cs
@@ -1,12 +1,26 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Solutions._2018.TimeMachine;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2018._21;
 
 [UsedImplicitly]
 public class Part1 : Base
 {
+    // ReSharper disable StringLiteralTypo
     public override string GetAnswer()
     {
-        return "TESTING";
+        var cpu = new Cpu(6);
+
+        cpu.Initialise();
+
+        cpu.LoadProgram(Input);
+
+        cpu.Run(-1, "eqrr");
+
+        var breakOn = Input.Single(i => i.StartsWith("eqrr"));
+
+        var register = int.Parse(breakOn.Split(' ', StringSplitOptions.TrimEntries)[1]);
+        
+        return cpu.GetRegisters()[register].ToString();
     }
 }
