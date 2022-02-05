e6758d9b616ed35030ca150dde017f51cd3a0f85
Stevö <stevehuwjohn@gmail.com>
2022-02-05 23:37:39 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2017/08/Part1.cs
@@ -0,0 +1,79 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._08;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var result = RunProgram();
+
+        return result.ToString();
+    }
+
+    private int RunProgram()
+    {
+        var registers = new Dictionary<string, int>();
+
+        foreach (var line in Input)
+        {
+            var operation = ParseLine(line);
+
+            registers.TryGetValue(operation.ConditionRegister, out var value);
+
+            var conditionMet = false;
+
+            switch (operation.Operator)
+            {
+                case "<":
+                    conditionMet = value < operation.Condition;
+
+                    break;
+                case ">":
+                    conditionMet = value > operation.Condition;
+
+                    break;
+                case "<=":
+                    conditionMet = value <= operation.Condition;
+
+                    break;
+                case ">=":
+                    conditionMet = value >= operation.Condition;
+
+                    break;
+                case "==":
+                    conditionMet = value == operation.Condition;
+
+                    break;
+                case "!=":
+                    conditionMet = value != operation.Condition;
+
+                    break;
+            }
+
+            if (conditionMet)
+            {
+                if (registers.ContainsKey(operation.TargetRegister))
+                {
+                    registers[operation.TargetRegister] += operation.Delta;
+                }
+                else
+                {
+                    registers.Add(operation.TargetRegister, operation.Delta);
+                }
+            }
+        }
+
+        return registers.Max(r => r.Value);
+    }
+
+    private static (string TargetRegister, int Delta, string ConditionRegister, string Operator, int Condition) ParseLine(string line)
+    {
+        var parts = line.Split(' ', StringSplitOptions.TrimEntries);
+
+        var delta = int.Parse(parts[2]) * (parts[1] == "inc" ? 1 : -1);
+
+        return (parts[0], delta, parts[4], parts[5], int.Parse(parts[6]));
+    }
+}
