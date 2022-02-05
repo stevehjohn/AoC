dcbfe0857da394df430d2c069ee7f23b9ec58505
Stevö <stevehuwjohn@gmail.com>
2022-02-05 18:32:49 +0000

    WIP

--- AoC.Solutions/Solutions/2018/25/Part1.cs
+++ AoC.Solutions/Solutions/2018/25/Part1.cs
@@ -14,14 +14,62 @@ public class Part1 : Base
         return result.ToString();
     }
 
-    private int CountConstellations(List<(int X, int Y, int z, int t)> points)
+    private static int CountConstellations(List<(int X, int Y, int Z, int T)> points)
     {
-        return 0;
+        var constellations = new List<HashSet<(int X, int Y, int Z, int T)>>();
+
+        foreach (var outer in points)
+        {
+            var pairs = new HashSet<((int X, int Y, int Z, int T) Left, (int X, int Y, int Z, int T) Right)>();
+
+            foreach (var inner in points)
+            {
+                if (outer == inner)
+                {
+                    continue;
+                }
+
+                if (GetManhattanDistance(outer, inner) < 4)
+                {
+                    pairs.Add((outer, inner));
+                }
+            }
+
+            foreach (var pair in pairs)
+            {
+                if (! constellations.Any(c => c.Contains(pair.Left) || c.Contains(pair.Right)))
+                {
+                    constellations.Add(new HashSet<(int X, int Y, int Z, int T)> { pair.Left, pair.Right });
+
+                    continue;
+                }
+
+                var constellation = constellations.SingleOrDefault(c => c.Contains(pair.Left));
+
+                if (constellation != null)
+                {
+                    constellation.Add(pair.Right);
+
+                    continue;
+                }
+
+                constellation = constellations.Single(c => c.Contains(pair.Right));
+
+                constellation.Add(pair.Left);
+            }
+        }
+
+        return constellations.Count;
     }
 
-    private List<(int X, int Y, int z, int t)> ParseInput()
+    private static int GetManhattanDistance((int X, int Y, int Z, int T) left, (int X, int Y, int Z, int T) right)
     {
-        var result = new List<(int X, int Y, int Z, int t)>();
+        return Math.Abs(left.X - right.X) + Math.Abs(left.Y - right.Y) + Math.Abs(left.Z - right.Z) + Math.Abs(left.T - right.T);
+    }
+
+    private List<(int X, int Y, int Z, int T)> ParseInput()
+    {
+        var result = new List<(int X, int Y, int Z, int T)>();
 
         foreach (var line in Input)
         {
