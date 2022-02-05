caff7c9329c11744a23b4929ffa74a22dbe8bf3e
Stevö <stevehuwjohn@gmail.com>
2022-02-05 00:41:49 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2018/20/Part2.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2018._20;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        BuildMaze();
+
+        var result = Maze.Count(p => p.Distance >= 1_000);
+
+        return result.ToString();
+    }
+}
