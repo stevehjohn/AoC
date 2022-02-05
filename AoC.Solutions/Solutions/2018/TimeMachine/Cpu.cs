a2c5437de69bd9e1a26d93f3afc2475a11642f2b
Stevö <stevehuwjohn@gmail.com>
2022-02-05 15:52:45 +0000

    WIP

--- AoC.Solutions/Solutions/2018/TimeMachine/Cpu.cs
+++ AoC.Solutions/Solutions/2018/TimeMachine/Cpu.cs
@@ -4,9 +4,9 @@ public class Cpu
 {
     private readonly int[] _registers;
 
-    private readonly Dictionary<string, Action<int, int, int, int[]>> _operations = new();
+    private readonly Dictionary<string, Action<int, int, int>> _operations = new();
 
-    private readonly List<string> _program = new();
+    private readonly List<(string OpCode, int A, int B, int C)> _program = new();
 
     private int _instructionPointer;
 
@@ -41,9 +41,9 @@ public class Cpu
                 _registers[_instructionPointerBinding.Value] = _instructionPointer;
             }
 
-            var instruction = ParseLine(_program[_instructionPointer]);
+            var instruction = _program[_instructionPointer];
 
-            _operations[instruction.OpCode].Invoke(instruction.A, instruction.B, instruction.C, _registers);
+            _operations[instruction.OpCode](instruction.A, instruction.B, instruction.C);
 
             if (_breakAt > -1 && _instructionPointer == _breakAt)
             {
@@ -75,7 +75,7 @@ public class Cpu
                 continue;
             }
 
-            _program.Add(line);
+            _program.Add(ParseLine(line));
         }
     }
 
@@ -93,45 +93,50 @@ public class Cpu
         return copy;
     }
 
+    public int GetRegister(int register)
+    {
+        return _registers[register];
+    }
+
     public void Execute(string opCode, int a, int b, int c)
     {
-        _operations[opCode].Invoke(a, b, c, _registers);
+        _operations[opCode](a, b, c);
     }
 
     public void Initialise()
     {
         // ReSharper disable StringLiteralTypo
-        _operations.Add("addr", (a, b, c, registers) => registers[c] = registers[a] + registers[b]);
+        _operations.Add("addr", (a, b, c) => _registers[c] = _registers[a] + _registers[b]);
 
-        _operations.Add("addi", (a, b, c, registers) => registers[c] = registers[a] + b);
+        _operations.Add("addi", (a, b, c) => _registers[c] = _registers[a] + b);
 
-        _operations.Add("mulr", (a, b, c, registers) => registers[c] = registers[a] * registers[b]);
+        _operations.Add("mulr", (a, b, c) => _registers[c] = _registers[a] * _registers[b]);
 
-        _operations.Add("muli", (a, b, c, registers) => registers[c] = registers[a] * b);
+        _operations.Add("muli", (a, b, c) => _registers[c] = _registers[a] * b);
 
-        _operations.Add("banr", (a, b, c, registers) => registers[c] = registers[a] & registers[b]);
+        _operations.Add("banr", (a, b, c) => _registers[c] = _registers[a] & _registers[b]);
 
-        _operations.Add("bani", (a, b, c, registers) => registers[c] = registers[a] & b);
+        _operations.Add("bani", (a, b, c) => _registers[c] = _registers[a] & b);
 
-        _operations.Add("borr", (a, b, c, registers) => registers[c] = registers[a] | registers[b]);
+        _operations.Add("borr", (a, b, c) => _registers[c] = _registers[a] | _registers[b]);
 
-        _operations.Add("bori", (a, b, c, registers) => registers[c] = registers[a] | b);
+        _operations.Add("bori", (a, b, c) => _registers[c] = _registers[a] | b);
 
-        _operations.Add("setr", (a, _, c, registers) => registers[c] = registers[a]);
+        _operations.Add("setr", (a, _, c) => _registers[c] = _registers[a]);
 
-        _operations.Add("seti", (a, _, c, registers) => registers[c] = a);
+        _operations.Add("seti", (a, _, c) => _registers[c] = a);
 
-        _operations.Add("gtir", (a, b, c, registers) => registers[c] = a > registers[b] ? 1 : 0);
+        _operations.Add("gtir", (a, b, c) => _registers[c] = a > _registers[b] ? 1 : 0);
 
-        _operations.Add("gtri", (a, b, c, registers) => registers[c] = registers[a] > b ? 1 : 0);
+        _operations.Add("gtri", (a, b, c) => _registers[c] = _registers[a] > b ? 1 : 0);
 
-        _operations.Add("gtrr", (a, b, c, registers) => registers[c] = registers[a] > registers[b] ? 1 : 0);
+        _operations.Add("gtrr", (a, b, c) => _registers[c] = _registers[a] > _registers[b] ? 1 : 0);
 
-        _operations.Add("eqir", (a, b, c, registers) => registers[c] = a == registers[b] ? 1 : 0);
+        _operations.Add("eqir", (a, b, c) => _registers[c] = a == _registers[b] ? 1 : 0);
 
-        _operations.Add("eqri", (a, b, c, registers) => registers[c] = registers[a] == b ? 1 : 0);
+        _operations.Add("eqri", (a, b, c) => _registers[c] = _registers[a] == b ? 1 : 0);
 
-        _operations.Add("eqrr", (a, b, c, registers) => registers[c] = registers[a] == registers[b] ? 1 : 0);
+        _operations.Add("eqrr", (a, b, c) => _registers[c] = _registers[a] == _registers[b] ? 1 : 0);
         // ReSharper restore StringLiteralTypo
     }
 
