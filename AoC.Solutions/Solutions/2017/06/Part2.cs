69cd3d6bdbf5f6a3ecc4dbdc682bc38020037d0b
Stevö <stevehuwjohn@gmail.com>
2022-02-05 21:21:12 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2017/06/Part2.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._06;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        RunUntilRepeats();
+
+        var result = RunUntilRepeats() - 1;
+
+        return result.ToString();
+    }
+}
