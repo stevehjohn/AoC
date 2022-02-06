ab6c900e59946fb74ac23631fa6981b75c587ff7
Stevö <stevehuwjohn@gmail.com>
2022-02-06 16:46:51 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2017/10/Part1.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._10;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var circle = new CircularLinkedList<int>();
+
+        for (var i = 0; i < 256; i++)
+        {
+            circle.Add(i);
+        }
+
+        return "TESTING";
+    }
+}
