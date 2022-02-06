b48b3676d34dd835ef2b2e5494c294f77196a2e7
Stevö <stevehuwjohn@gmail.com>
2022-02-06 22:41:19 +0000

    WIP

--- AoC.Solutions/Solutions/2017/11/Base.cs
+++ AoC.Solutions/Solutions/2017/11/Base.cs
@@ -1,8 +1,44 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Exceptions;
+using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2017._11;
 
 public abstract class Base : Solution
 {
     public override string Description => "Hex ed";
+
+    protected static (Point Position, int Furthest) WalkPath(string path)
+    {
+        var steps = path.Split(',', StringSplitOptions.TrimEntries);
+
+        var position = new Point();
+
+        foreach (var step in steps)
+        {
+            var move = GetMovement(step);
+
+            position.X += move.X;
+
+            position.Y += move.Y;
+
+            position.Z += move.Z;
+        }
+
+        return (position, 0);
+    }
+
+    private static Point GetMovement(string direction)
+    {
+        return direction switch
+        {
+            "n" => new Point(0, -1, 1),
+            "s" => new Point(0, 1, -1),
+            "ne" => new Point(1, -1),
+            "se" => new Point(1, 0, -1),
+            "nw" => new Point(-1, 0, 1),
+            "sw" => new Point(-1, 1),
+            _ => throw new PuzzleException("Direction not recognised")
+        };
+    }
 }
--- AoC.Solutions/Solutions/2017/11/Part1.cs
+++ AoC.Solutions/Solutions/2017/11/Part1.cs
@@ -1,48 +1,11 @@
-﻿using AoC.Solutions.Common;
-using AoC.Solutions.Exceptions;
-
-namespace AoC.Solutions.Solutions._2017._11;
+﻿namespace AoC.Solutions.Solutions._2017._11;
 
 public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        var childPosition = WalkPath(Input[0]);
+        var childPosition = WalkPath(Input[0]).Position;
 
-        return "TESTING";
-    }
-
-    private Point WalkPath(string path)
-    {
-        var steps = path.Split(',', StringSplitOptions.TrimEntries);
-
-        var position = new Point();
-
-        foreach (var step in steps)
-        {
-            var move = GetMovement(step);
-
-            position.X += move.X;
-
-            position.Y += move.Y;
-
-            position.Z += move.Z;
-        }
-
-        return position;
-    }
-
-    private static Point GetMovement(string direction)
-    {
-        return direction switch
-        {
-            "n" => new Point(0, -1, 1),
-            "s" => new Point(0, 1, -1),
-            "ne" => new Point(1, -1),
-            "se" => new Point(1, 0, -1),
-            "nw" => new Point(-1, 0, 1),
-            "sw" => new Point(-1, 1),
-            _ => throw new PuzzleException("Direction not recognised")
-        };
+        return ((Math.Abs(childPosition.X) + Math.Abs(childPosition.Y) + Math.Abs(childPosition.Z)) / 2).ToString();
     }
 }
