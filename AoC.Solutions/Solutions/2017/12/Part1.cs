61b912f447c41db02151cb4c03964632b2ec3c46
Stevö <stevehuwjohn@gmail.com>
2022-02-06 23:47:54 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2017/12/Part1.cs
@@ -0,0 +1,46 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._12;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    private readonly Dictionary<int, Node> _nodes = new();
+
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        return "TESTING";
+    }
+
+    private void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            var parts = line.Split(" <-> ", StringSplitOptions.TrimEntries);
+
+            var node = new Node(int.Parse(parts[0]));
+
+            _nodes.Add(node.Id, node);
+
+            var connectionsString = parts[1].Split(',', StringSplitOptions.TrimEntries).Select(int.Parse);
+
+            foreach (var connection in connectionsString)
+            {
+                if (_nodes.ContainsKey(connection))
+                {
+                    node.Connections.Add(_nodes[connection]);
+                }
+                else
+                {
+                    var connectionNode = new Node(connection);
+
+                    node.Connections.Add(connectionNode);
+
+                    _nodes.Add(connectionNode.Id, connectionNode);
+                }
+            }
+        }
+    }
+}
