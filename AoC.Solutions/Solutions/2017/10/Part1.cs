536db580dc34acebfbfbd790df141176a2df5cec
Stevö <stevehuwjohn@gmail.com>
2022-02-06 21:39:16 +0000

    WIP

--- AoC.Solutions/Solutions/2017/10/Part1.cs
+++ AoC.Solutions/Solutions/2017/10/Part1.cs
@@ -7,47 +7,14 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        var lengths = Input[0].Split(',', StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
-
-        var data = new int[256];
-
-        for (var i = 0; i < 256; i++)
-        {
-            data[i] = i;
-        }
+        var lengths = Input[0].Split(',', StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
 
         var skipLength = 0;
 
         var position = 0;
 
-        foreach (var length in lengths)
-        {
-            SwapBetween(data, position, length);
-
-            position = (position + length + skipLength) % 256;
-
-            skipLength++;
-        }
+        var data = RunRound(lengths, ref position, ref skipLength);
 
         return (data[0] * data[1]).ToString();
-    }
-
-    private static void SwapBetween(int[] data, int start, int length)
-    {
-        var end = (start + length - 1) % 256;
-
-        for (var i = 0; i < length / 2; i++)
-        {
-            (data[start], data[end]) = (data[end], data[start]);
-
-            start = (start + 1) % 256;
-
-            end--;
-
-            if (end < 0)
-            {
-                end = 255;
-            }
-        }
     }
 }
