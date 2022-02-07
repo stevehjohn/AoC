2c6be4bb52326e9c69322bee03ec997221d315d8
Stevö <stevehuwjohn@gmail.com>
2022-02-07 03:15:45 +0000

    WIP

--- AoC.Solutions/Solutions/2017/14/Part1.cs
+++ AoC.Solutions/Solutions/2017/14/Part1.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Solutions._2017.Common;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2017._14;
 
@@ -7,6 +8,11 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
+        for (var i = 0; i < 128; i++)
+        {
+            var rowHash = KnotHash.MakeHash($"{Input[0]}-i");
+        }
+
         return "TESTING";
     }
 }
