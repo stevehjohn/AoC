8049d2b1ba8bcfe6d07ff909e660c13b8f1bd35b
Stevö <stevehuwjohn@gmail.com>
2022-02-07 08:48:39 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2017/18/Part2.cs
@@ -0,0 +1,20 @@
+﻿//using JetBrains.Annotations;
+
+//namespace AoC.Solutions.Solutions._2017._18;
+
+//[UsedImplicitly]
+//public class Part2 : Base
+//{
+//    public override string GetAnswer()
+//    {
+//        var cpu1 = new Cpu();
+
+//        cpu1.SetRegister('p', 0);
+
+//        var cpu2 = new Cpu();
+
+//        cpu2.SetRegister('p', 1);
+
+//        return "TESTING";
+//    }
+//}
