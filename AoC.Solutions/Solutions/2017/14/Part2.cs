ed6a2f0aabd8932a731df60a8dbb5c0d74e01269
Stevö <stevehuwjohn@gmail.com>
2022-02-07 03:58:06 +0000

    WIP

--- AoC.Solutions/Solutions/2017/14/Part2.cs
+++ AoC.Solutions/Solutions/2017/14/Part2.cs
@@ -6,10 +6,37 @@ namespace AoC.Solutions.Solutions._2017._14;
 [UsedImplicitly]
 public class Part2 : Base
 {
+    private readonly int[,] _disk = new int[128, 128];
+
     public override string GetAnswer()
     {
-        var grid = new int [128, 128];
+        BuildDiskImage();
 
+        var result = CountRegions();
+
+        return result.ToString();
+    }
+
+    private int CountRegions()
+    {
+        var distinct = new HashSet<int>();
+
+        for (var y = 0; y < 128; y++)
+        {
+            for (var x = 0; x < 128; x++)
+            {
+                if (_disk[x, y] != 0)
+                {
+                    distinct.Add(_disk[x, y]);
+                }
+            }
+        }
+
+        return distinct.Count;
+    }
+
+    private void BuildDiskImage()
+    {
         for (var y = 0; y < 128; y++)
         {
             //var rowHash = KnotHash.MakeHash($"{Input[0]}-{y}").ToList();
@@ -27,7 +54,7 @@ public class Part2 : Base
                 {
                     if ((b & bit) > 0)
                     {
-                        grid[x, y] = -1;
+                        _disk[x, y] = -1;
                     }
 
                     bit >>= 1;
@@ -36,7 +63,5 @@ public class Part2 : Base
                 }
             }
         }
-
-        return "TESTING";
     }
 }
