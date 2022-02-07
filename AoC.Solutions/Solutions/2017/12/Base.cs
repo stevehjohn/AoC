48207b7224d7eeb7a2cebaea0cf6eb51331bd9a3
Stevö <stevehuwjohn@gmail.com>
2022-02-07 00:16:48 +0000

    WIP

--- AoC.Solutions/Solutions/2017/12/Base.cs
+++ AoC.Solutions/Solutions/2017/12/Base.cs
@@ -5,4 +5,41 @@ namespace AoC.Solutions.Solutions._2017._12;
 public abstract class Base : Solution
 {
     public override string Description => "Path through the pipes";
+
+    protected readonly Dictionary<int, Node> Nodes = new();
+    
+    protected void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            var parts = line.Split(" <-> ", StringSplitOptions.TrimEntries);
+
+            var id = int.Parse(parts[0]);
+
+            if (! Nodes.TryGetValue(id, out var node))
+            {
+                node = new Node(id);
+
+                Nodes.Add(node.Id, node);
+            }
+
+            var connectionsString = parts[1].Split(',', StringSplitOptions.TrimEntries).Select(int.Parse);
+
+            foreach (var connection in connectionsString)
+            {
+                if (Nodes.ContainsKey(connection))
+                {
+                    node.Connections.Add(Nodes[connection]);
+                }
+                else
+                {
+                    var connectionNode = new Node(connection);
+
+                    node.Connections.Add(connectionNode);
+
+                    Nodes.Add(connectionNode.Id, connectionNode);
+                }
+            }
+        }
+    }
 }
