bb69015ec96e070871b115f50aa93a11dfe64fc9
Stevö <stevehuwjohn@gmail.com>
2022-02-07 01:00:03 +0000

    WIP

--- AoC.Solutions/Solutions/2017/13/Part1.cs
+++ AoC.Solutions/Solutions/2017/13/Part1.cs
@@ -7,6 +7,83 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "TESTING";
+        var firewall = ParseInput();
+
+        var result = GetSeverity(firewall);
+
+        return result.ToString();
+    }
+
+    private static int GetSeverity(Dictionary<int, Layer> firewall)
+    {
+        var position = 0;
+
+        var end = firewall.Max(l => l.Key);
+
+        var severity = 0;
+
+        while (position <= end)
+        {
+            if (! firewall.ContainsKey(position))
+            {
+                position++;
+
+                continue;
+            }
+
+            var layer = firewall[position];
+
+            if (layer.Position == 0)
+            {
+                severity += position * layer.Depth;
+            }
+
+            foreach (var item in firewall)
+            {
+                item.Value.MoveScanner();
+            }
+
+            position++;
+        }
+
+        return severity;
+    }
+
+    private Dictionary<int, Layer> ParseInput()
+    {
+        var firewall = new Dictionary<int, Layer>();
+
+        foreach (var line in Input)
+        {
+            var parts = line.Split(':', StringSplitOptions.TrimEntries);
+
+            firewall.Add(int.Parse(parts[0]), new Layer(int.Parse(parts[1])));
+        }
+
+        return firewall;
+    }
+}
+
+public class Layer
+{
+    public int Depth { get; }
+
+    public int Position { get; private set; }
+
+    private int _direction = 1;
+
+    public Layer(int depth)
+    {
+        Depth = depth;
+    }
+
+    public void MoveScanner()
+    {
+        Position += _direction;
+
+        if (Position == 0 || Position == Depth - 1)
+        {
+            _direction = -_direction;
+        }
     }
 }
