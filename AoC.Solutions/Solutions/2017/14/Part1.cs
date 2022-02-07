7c31cd25f5a7ab57c379ba18c2cc0db44a8424f7
Stevö <stevehuwjohn@gmail.com>
2022-02-07 03:34:00 +0000

    WIP

--- AoC.Solutions/Solutions/2017/14/Part1.cs
+++ AoC.Solutions/Solutions/2017/14/Part1.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Solutions.Solutions._2017.Common;
+﻿using AoC.Solutions.Extensions;
+using AoC.Solutions.Solutions._2017.Common;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2017._14;
@@ -12,23 +13,11 @@ public class Part1 : Base
 
         for (var i = 0; i < 128; i++)
         {
-            var rowHash = KnotHash.MakeHash($"{Input[0]}-i");
+            var rowHash = KnotHash.MakeHash($"{Input[0]}-{i}").ToList();
 
-            bits += CountBits(rowHash);
+            bits += rowHash.Sum(c => "0123456789abcdef".IndexOf(c).CountBits());
         }
 
         return bits.ToString();
-    }
-
-    private static int CountBits(string rowHash)
-    {
-        var bits = 0;
-
-        foreach (var c in rowHash)
-        {
-            
-        }
-
-        return bits;
     }
 }
