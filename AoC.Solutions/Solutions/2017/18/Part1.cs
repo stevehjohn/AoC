b55416e96b2f68ec226bb0a79b5558efba6d8a3b
Stevö <stevehuwjohn@gmail.com>
2022-02-07 08:27:10 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2017/18/Part1.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2017._18;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var cpu = new Cpu();
+
+        var frequency = cpu.RunProgram(Input);
+
+        return frequency.ToString();
+    }
+}
