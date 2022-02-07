7d5938bc768a4d4add6fb4f00f58d06722724ad6
Stevö <stevehuwjohn@gmail.com>
2022-02-07 01:34:09 +0000

    WIP

--- AoC.Solutions/Solutions/2017/13/Part1.cs
+++ AoC.Solutions/Solutions/2017/13/Part1.cs
@@ -9,8 +9,39 @@ public class Part1 : Base
     {
         var firewall = ParseInput();
 
-        var result = GetSeverity(firewall).Severity;
+        var result = GetSeverity(firewall);
 
         return result.ToString();
+    }
+
+    private static int GetSeverity(Dictionary<int, Layer> firewall)
+    {
+        var position = 0;
+
+        var end = firewall.Max(l => l.Key);
+
+        var severity = 0;
+
+        while (position <= end)
+        {
+            if (firewall.ContainsKey(position))
+            {
+                var layer = firewall[position];
+
+                if (layer.Position == 0)
+                {
+                    severity += position * layer.Depth;
+                }
+            }
+
+            foreach (var item in firewall)
+            {
+                item.Value.MoveScanner();
+            }
+
+            position++;
+        }
+
+        return severity;
     }
 }
--- AoC.Solutions/Solutions/2017/13/Part2.cs
+++ AoC.Solutions/Solutions/2017/13/Part2.cs
@@ -1,26 +1,26 @@
-﻿using JetBrains.Annotations;
+﻿//using JetBrains.Annotations;
 
-namespace AoC.Solutions.Solutions._2017._13;
+//namespace AoC.Solutions.Solutions._2017._13;
 
-[UsedImplicitly]
-public class Part2 : Base
-{
-    public override string GetAnswer()
-    {
-        var firewall = ParseInput();
+//[UsedImplicitly]
+//public class Part2 : Base
+//{
+//    public override string GetAnswer()
+//    {
+//        var firewall = ParseInput();
 
-        var delay = 0;
+//        var delay = 0;
 
-        while (GetSeverity(firewall, delay).Caught)
-        {
-            delay++;
+//        while (GetSeverity(firewall, delay).Caught)
+//        {
+//            delay++;
 
-            foreach (var (_, value) in firewall)
-            {
-                value.ResetScanner();
-            }
-        }
+//            foreach (var (_, value) in firewall)
+//            {
+//                value.ResetScanner();
+//            }
+//        }
 
-        return delay.ToString();
-    }
-}
+//        return delay.ToString();
+//    }
+//}
