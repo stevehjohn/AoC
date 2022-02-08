91d0ef05549c89c51930172aaf13a04ea895ee3c
Stevö <stevehuwjohn@gmail.com>
2022-02-08 17:52:43 +0000

    WIP

--- AoC.Solutions/Solutions/2017/21/Part1.cs
+++ AoC.Solutions/Solutions/2017/21/Part1.cs
@@ -7,6 +7,132 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
+        var patterns = ParseInput();
+
         return "TESTING";
+    }
+
+    private Dictionary<int, char[,]> ParseInput()
+    {
+        var patterns = new Dictionary<int, char[,]>();
+
+        foreach (var line in Input)
+        {
+            var parts = line.Split(" => ", StringSplitOptions.TrimEntries);
+
+            var sideLength = 2;
+
+            if (parts[0].Length == 11)
+            {
+                sideLength = 3;
+            }
+
+            var pattern = ParsePattern(parts[0], sideLength);
+
+            var result = ParsePattern(parts[1], sideLength);
+
+            patterns.TryAdd(HashPattern(pattern, sideLength), result);
+
+            pattern = Rotate(pattern, sideLength);
+
+            patterns.TryAdd(HashPattern(pattern, sideLength), result);
+
+            pattern = Rotate(pattern, sideLength);
+
+            patterns.TryAdd(HashPattern(pattern, sideLength), result);
+
+            pattern = Rotate(pattern, sideLength);
+
+            patterns.TryAdd(HashPattern(pattern, sideLength), result);
+
+            pattern = Rotate(pattern, sideLength);
+
+            pattern = FlipVertical(pattern, sideLength);
+
+            patterns.TryAdd(HashPattern(pattern, sideLength), result);
+        
+            pattern = FlipVertical(pattern, sideLength);
+            
+            pattern = FlipHorizontal(pattern, sideLength);
+
+            patterns.TryAdd(HashPattern(pattern, sideLength), result);
+        }
+
+        return patterns;
+    }
+    
+    public char[,] Rotate(char[,] pattern, int sideLength)
+    {
+        var newPattern = new char[sideLength, sideLength];
+
+        for (var y = 0; y < sideLength; y++)
+        {
+            for (var x = 0; x < sideLength; x++)
+            {
+                newPattern[x, y] = pattern[y, sideLength - 1 - x];
+            }
+        }
+
+        return newPattern;
+    }
+
+    public char[,] FlipVertical(char[,] pattern, int sideLength)
+    {
+        var newPattern = new char[sideLength, sideLength];
+
+        for (var y = 0; y < sideLength; y++)
+        {
+            for (var x = 0; x < sideLength; x++)
+            {
+                newPattern[x, y] = pattern[x, sideLength - 1 - y];
+            }
+        }
+
+        return newPattern;
+    }
+
+    public char[,] FlipHorizontal(char[,] pattern, int sideLength)
+    {
+        var newPattern = new char[sideLength, sideLength];
+
+        for (var y = 0; y < sideLength; y++)
+        {
+            for (var x = 0; x < sideLength; x++)
+            {
+                newPattern[x, y] = pattern[sideLength - 1 - x, y];
+            }
+        }
+
+        return newPattern;
+    }
+
+    private static char[,] ParsePattern(string pattern, int sideLength)
+    {
+        var grid = new char[sideLength, sideLength];
+
+        for (var y = 0; y < sideLength; y++)
+        {
+            for (var x = 0; x < sideLength; x++)
+            {
+                grid[x, y] = pattern[y * (sideLength + 1) + x];
+            }
+        }
+
+        return grid;
+    }
+
+    private static int HashPattern(char[,] pattern, int sideLength)
+    {
+        var hash = 0;
+
+        for (var y = 0; y < sideLength; y++)
+        {
+            for (var x = 0; x < sideLength; x++)
+            {
+                hash = HashCode.Combine(hash, pattern[x, y]);
+            }
+        }
+
+        return hash;
     }
 }
