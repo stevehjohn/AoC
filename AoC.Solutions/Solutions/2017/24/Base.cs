ee85bfac9ec581bcb41153946d66693892c53dec
Stevö <stevehuwjohn@gmail.com>
2022-02-09 11:49:32 +0000

    WIP

--- AoC.Solutions/Solutions/2017/24/Base.cs
+++ AoC.Solutions/Solutions/2017/24/Base.cs
@@ -5,4 +5,43 @@ namespace AoC.Solutions.Solutions._2017._24;
 public abstract class Base : Solution
 {
     public override string Description => "Electromagnetic moat";
+
+    private readonly List<(int EdgeA, int EdgeB)> _components = new();
+
+    private int _maxStrength;
+
+    protected int Solve()
+    {
+        var starts = _components.Where(c => c.EdgeA == 0 || c.EdgeB == 0).ToList();
+
+        foreach (var component in starts)
+        {
+            Solve(component.EdgeA == 0 ? component.EdgeB : component.EdgeA, 0, _components.Where(c => c != component).ToList());
+        }
+
+        return _maxStrength;
+    }
+
+    private void Solve(int edge, int strength, List<(int EdgeA, int EdgeB)> remainingEdges)
+    {
+        _maxStrength = Math.Max(strength, _maxStrength);
+
+        foreach (var component in remainingEdges)
+        {
+            if (component.EdgeA == edge || component.EdgeB == edge)
+            {
+                Solve(component.EdgeA == edge ? component.EdgeB : component.EdgeA, strength + component.EdgeA + component.EdgeB, remainingEdges.Where(c => c != component).ToList());
+            }
+        }
+    }
+
+    protected void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            var parts = line.Split('/').Select(int.Parse).ToList();
+
+            _components.Add((parts[0], parts[1]));
+        }
+    }
 }
