b5536ffdf85c89972d6dfbc4aff8128fd575c990
Stevö <stevehuwjohn@gmail.com>
2022-02-11 23:56:05 +0000

    WIP

--- AoC.Solutions/Solutions/2016/08/Part1.cs
+++ AoC.Solutions/Solutions/2016/08/Part1.cs
@@ -5,82 +5,25 @@ namespace AoC.Solutions.Solutions._2016._08;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private readonly bool[,] _screen = new bool[50, 6];
-
     public override string GetAnswer()
     {
-        foreach (var line in Input)
-        {
-            ProcessLine(line);
-        }
+        Solve();
 
+        return CountLitPixels().ToString();
+    }
+
+    private int CountLitPixels()
+    {
         var count = 0;
 
         for (var y = 0; y < 6; y++)
         {
             for (var x = 0; x < 50; x++)
             {
-                count += _screen[x, y] ? 1 : 0;
+                count += Screen[x, y] ? 1 : 0;
             }
         }
 
-        return count.ToString();
-    }
-
-    private void ProcessLine(string line)
-    {
-        if (line.StartsWith("rect"))
-        {
-            var coords = line[5..].Split('x').Select(int.Parse).ToArray();
-
-            for (var y = 0; y < coords[1]; y++)
-            {
-                for (var x = 0; x < coords[0]; x++)
-                {
-                    _screen[x, y] = true;
-                }
-            }
-
-            return;
-        }
-
-        int[] parameters;
-
-        if (line.StartsWith("rotate column"))
-        {
-            line = line[16..];
-
-            parameters = line.Split(" by ", StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
-
-            for (var i = 0; i < parameters[1]; i++)
-            {
-                var temp = _screen[parameters[0], 0];
-
-                for (var y = 0; y < 5; y++)
-                {
-                    _screen[parameters[0], y] = _screen[parameters[0], y + 1];
-                }
-
-                _screen[parameters[0], 5] = temp;
-            }
-
-            return;
-        }
-
-        line = line[13..];
-
-        parameters = line.Split(" by ", StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
-
-        for (var i = 0; i < parameters[1]; i++)
-        {
-            var temp = _screen[0, parameters[0]];
-
-            for (var x = 0; x < 49; x++)
-            {
-                _screen[x, parameters[0]] = _screen[x + 1, parameters[0]];
-            }
-
-            _screen[49, parameters[0]] = temp;
-        }
+        return count;
     }
 }
---
+++ AoC.Solutions/Solutions/2016/08/Part2.cs
@@ -0,0 +1,13 @@
+﻿//using JetBrains.Annotations;
+
+//namespace AoC.Solutions.Solutions._2016._08;
+
+//[UsedImplicitly]
+//public class Part2 : Base
+//{
+//    public override string GetAnswer()
+//    {
+
+//        return count.ToString();
+//    }
+//}
