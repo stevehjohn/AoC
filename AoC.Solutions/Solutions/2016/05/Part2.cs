0b61776f6721a391096d84cf5d424a97d1bbe74b
Stevö <stevehuwjohn@gmail.com>
2022-02-11 20:22:48 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2016/05/Part2.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AoC.Solutions.Solutions._2016._05;
+
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var prefix = Input[0];
+
+        var password = new char[8];
+
+        var suffix = 0;
+
+        while (true)
+        {
+            var hash = MD5.HashData(Encoding.ASCII.GetBytes($"{prefix}{suffix}"));
+
+            var hex = Convert.ToHexString(hash);
+
+            suffix++;
+
+            if (hex.StartsWith("00000"))
+            {
+                var position = hex[5] - '0';
+
+                if (position > 7 || password[position] != '\0')
+                {
+                    continue;
+                }
+
+                password[position] = hex[6];
+
+                if (password.All(c => c != '\0'))
+                {
+                    break;
+                }
+            }
+        }
+
+        return new string(password).ToLower();
+    }
+}
