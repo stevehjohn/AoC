8b22d358616ee87d2ddc9c6e5b1e1d0ec84d439a
Stevö <stevehuwjohn@gmail.com>
2022-02-12 13:52:55 +0000

    WIP

--- AoC.Solutions/Solutions/2016/09/Part2.cs
+++ AoC.Solutions/Solutions/2016/09/Part2.cs
@@ -7,15 +7,33 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var result = GetDecompressedLength(Input[0]);
+        var result = GetDecompressedLength(Input[0], 0, Input[0].Length);
 
         return result.ToString();
     }
 
-    private static long GetDecompressedLength(string input)
+    private static long GetDecompressedLength(string input, int startIndex, int length)
     {
-        var length = 0L;
+        var result = 0L;
 
-        return length;
+        for (var i = startIndex; i < startIndex + length; i++)
+        {
+            if (input[i] == '(')
+            {
+                var parameters = input[(i + 1)..input.IndexOf(')', i)].Split('x').Select(int.Parse).ToArray();
+
+                i = input.IndexOf(')', i) + 1;
+
+                result += GetDecompressedLength(input, i, parameters[0]) * parameters[1];
+
+                i += parameters[0] - 1;
+
+                continue;
+            }
+
+            result++;
+        }
+
+        return result;
     }
 }
