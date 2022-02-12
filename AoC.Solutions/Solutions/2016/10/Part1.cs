627ed05be0d0fbcfe164cba26b61568de17278c7
Stevö <stevehuwjohn@gmail.com>
2022-02-12 14:42:32 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2016/10/Part1.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2016._10;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        InitialiseBots();
+
+        var result = RunBots(false);
+
+        return result.ToString();
+    }
+}
