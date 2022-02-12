53657f3d4c50f223cdb2a8f088fa84cc9a06855e
Stevö <stevehuwjohn@gmail.com>
2022-02-12 01:49:28 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2016/09/Part2.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2016._09;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var result = GetDecompressedLength(Input[0]);
+
+        return result.ToString();
+    }
+
+    private static long GetDecompressedLength(string input)
+    {
+        var length = 0L;
+
+        return length;
+    }
+}
