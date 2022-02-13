f41817242045f84ebfaad4e96bb83a34a1664cb2
Stevö <stevehuwjohn@gmail.com>
2022-02-13 22:52:51 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2016/15/Part2.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2016._15;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var discs = ParseInput();
+
+        discs.Add((7, 11, 0));
+
+        var result = Solve(discs);
+
+        return result.ToString();
+    }
+}
