c547202e93726b2a827de45e8c0b45a332048101
Stevö <stevehuwjohn@gmail.com>
2022-02-13 23:47:48 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2016/16/Part1.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2016._16;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var seed = Input[0];
+
+        var data = GenerateData(seed, 272);
+
+        return GetChecksum(data);
+    }
+}
