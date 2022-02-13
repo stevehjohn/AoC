1a92f1fba13b4b7ac5c3eb948973b676c035d639
Stevö <stevehuwjohn@gmail.com>
2022-02-13 20:12:44 +0000

    WIP

--- AoC.Solutions/Solutions/2016/13/Base.cs
+++ AoC.Solutions/Solutions/2016/13/Base.cs
@@ -1,8 +1,39 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Extensions;
+using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2016._13;
 
 public abstract class Base : Solution
 {
     public override string Description => "Cubicle maze";
+
+    protected bool[,] Maze;
+
+    protected int Width;
+
+    protected int Height;
+
+    protected void BuildMaze()
+    {
+        Maze = new bool[Width, Height];
+
+        var designerNumber = int.Parse(Input[0]);
+
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                var number = x * x + 3 * x + 2 * x * y + y + y * y;
+
+                number += designerNumber;
+
+                var bits = number.CountBits();
+
+                if (bits % 2 == 1)
+                {
+                    Maze[x, y] = true;
+                }
+            }
+        }
+    }
 }
--- AoC.Solutions/Solutions/2016/13/Part1.cs
+++ AoC.Solutions/Solutions/2016/13/Part1.cs
@@ -1,5 +1,4 @@
 ﻿using AoC.Solutions.Common;
-using AoC.Solutions.Extensions;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2016._13;
@@ -7,15 +6,13 @@ namespace AoC.Solutions.Solutions._2016._13;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private bool[,] _maze;
-
-    private int _width;
-
-    private int _height;
-
     public override string GetAnswer()
     {
-        BuildMaze(31, 39);
+        Width = 32;
+
+        Height = 40;
+
+        BuildMaze();
 
         var result = Solve(31, 39);
 
@@ -45,28 +42,28 @@ public class Part1 : Base
                 break;
             }
 
-            if (position.Y > 0 && ! _maze[position.X, position.Y - 1] && ! visited.Contains(new Point(position.X, position.Y - 1)))
+            if (position.Y > 0 && ! Maze[position.X, position.Y - 1] && ! visited.Contains(new Point(position.X, position.Y - 1)))
             {
                 queue.Enqueue((new Point(position.X, position.Y - 1), item.Steps + 1), item.Steps + 1);
 
                 visited.Add(new Point(position.X, position.Y - 1));
             }
 
-            if (position.X < _width - 1 && ! _maze[position.X + 1, position.Y] && ! visited.Contains(new Point(position.X + 1, position.Y)))
+            if (position.X < Width - 1 && ! Maze[position.X + 1, position.Y] && ! visited.Contains(new Point(position.X + 1, position.Y)))
             {
                 queue.Enqueue((new Point(position.X + 1, position.Y), item.Steps + 1), item.Steps + 1);
 
                 visited.Add(new Point(position.X + 1, position.Y));
             }
 
-            if (position.Y < _height - 1 && ! _maze[position.X, position.Y + 1] && ! visited.Contains(new Point(position.X, position.Y + 1)))
+            if (position.Y < Height - 1 && ! Maze[position.X, position.Y + 1] && ! visited.Contains(new Point(position.X, position.Y + 1)))
             {
                 queue.Enqueue((new Point(position.X, position.Y + 1), item.Steps + 1), item.Steps + 1);
 
                 visited.Add(new Point(position.X, position.Y + 1));
             }
 
-            if (position.X > 0 && ! _maze[position.X - 1, position.Y] && ! visited.Contains(new Point(position.X - 1, position.Y)))
+            if (position.X > 0 && ! Maze[position.X - 1, position.Y] && ! visited.Contains(new Point(position.X - 1, position.Y)))
             {
                 queue.Enqueue((new Point(position.X - 1, position.Y), item.Steps + 1), item.Steps + 1);
 
@@ -75,33 +72,5 @@ public class Part1 : Base
         }
 
         return steps;
-    }
-
-    private void BuildMaze(int destinationX, int destinationY)
-    {
-        _width = destinationX + 1;
-
-        _height = destinationY + 1;
-
-        _maze = new bool[_width, _height];
-
-        var designerNumber = int.Parse(Input[0]);
-
-        for (var y = 0; y < _height; y++)
-        {
-            for (var x = 0; x < _width; x++)
-            {
-                var number = x * x + 3 * x + 2 * x * y + y + y * y;
-
-                number += designerNumber;
-
-                var bits = number.CountBits();
-
-                if (bits % 2 == 1)
-                {
-                    _maze[x, y] = true;
-                }
-            }
-        }
     }
 }
