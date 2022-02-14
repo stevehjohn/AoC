700e5dd306f2c99eb387eb08cfd4f0049c34606f
Stevö <stevehuwjohn@gmail.com>
2022-02-14 01:31:49 +0000

    WIP

--- AoC.Solutions/Solutions/2016/17/Part2.cs
+++ AoC.Solutions/Solutions/2016/17/Part2.cs
@@ -3,11 +3,11 @@
 namespace AoC.Solutions.Solutions._2016._17;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var result = Solve(Input[0]);
+        var result = Solve(Input[0], true);
 
         return result;
     }
