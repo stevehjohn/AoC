7d67d2948b120ec2bbffd5f76bbd07c98f2edac8
Stevö <stevehuwjohn@gmail.com>
2022-02-14 01:05:32 +0000

    WIP

--- AoC.Solutions/Solutions/2016/17/Part1.cs
+++ AoC.Solutions/Solutions/2016/17/Part1.cs
@@ -1,4 +1,5 @@
 ﻿using AoC.Solutions.Common;
+using AoC.Solutions.Exceptions;
 using JetBrains.Annotations;
 using System.Security.Cryptography;
 using System.Text;
@@ -12,14 +13,14 @@ public class Part1 : Base
     {
         var result = Solve(Input[0]);
 
-        return result.ToString();
+        return result;
     }
 
-    private static int Solve(string passcode)
+    private static string Solve(string passcode)
     {
-        var queue = new PriorityQueue<(string Passcode, Point Position, int Steps), int>();
+        var queue = new PriorityQueue<(string Passcode, string Directions, Point Position, int Steps), int>();
 
-        queue.Enqueue((passcode, new Point(0, 0), 0), 0);
+        queue.Enqueue((passcode, string.Empty, new Point(0, 0), 0), 0);
 
         var open = new[] { 'B', 'C', 'D', 'E', 'F' };
 
@@ -29,33 +30,34 @@ public class Part1 : Base
 
             if (item.Position.X == 3 && item.Position.Y == 3)
             {
-                Console.WriteLine(item.Passcode);
-                return item.Steps;
+                //Console.WriteLine(item.Passcode);
+                
+                return item.Directions;
             }
 
-            var hash = Convert.ToHexString(MD5.HashData(Encoding.ASCII.GetBytes(item.Passcode)));
+            var hash = Convert.ToHexString(MD5.HashData(Encoding.ASCII.GetBytes($"{item.Passcode}{item.Directions}")));
 
             if (open.Contains(hash[0]) && item.Position.Y > 0)
             {
-                queue.Enqueue(($"{item.Passcode}U", new Point(item.Position.X, item.Position.Y - 1), item.Steps + 1), item.Steps + 1);
+                queue.Enqueue((item.Passcode, $"{item.Directions}U", new Point(item.Position.X, item.Position.Y - 1), item.Steps + 1), item.Steps + 1);
             }
 
             if (open.Contains(hash[1]) && item.Position.Y < 3)
             {
-                queue.Enqueue(($"{item.Passcode}D", new Point(item.Position.X, item.Position.Y + 1), item.Steps + 1), item.Steps + 1);
+                queue.Enqueue((item.Passcode, $"{item.Directions}D", new Point(item.Position.X, item.Position.Y + 1), item.Steps + 1), item.Steps + 1);
             }
 
             if (open.Contains(hash[2]) && item.Position.X > 0)
             {
-                queue.Enqueue(($"{item.Passcode}L", new Point(item.Position.X - 1, item.Position.Y), item.Steps + 1), item.Steps + 1);
+                queue.Enqueue((item.Passcode, $"{item.Directions}L", new Point(item.Position.X - 1, item.Position.Y), item.Steps + 1), item.Steps + 1);
             }
 
             if (open.Contains(hash[3]) && item.Position.X < 3)
             {
-                queue.Enqueue(($"{item.Passcode}R", new Point(item.Position.X + 1, item.Position.Y), item.Steps + 1), item.Steps + 1);
+                queue.Enqueue((item.Passcode, $"{item.Directions}R", new Point(item.Position.X + 1, item.Position.Y), item.Steps + 1), item.Steps + 1);
             }
         }
 
-        return 0;
+        throw new PuzzleException("Solution not found.");
     }
 }
