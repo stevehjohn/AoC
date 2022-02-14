f3378cb1ea03224f3b6f303c23c830ad96d1cde9
Stevö <stevehuwjohn@gmail.com>
2022-02-14 20:49:29 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/04/Part2.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._04;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var result = GetAnswer("000000");
+
+        return result.ToString();
+    }
+}
