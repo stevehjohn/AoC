2d2c09e20304db25619f334f320a58ce214f1e2c
Stevö <stevehuwjohn@gmail.com>
2022-02-14 13:15:54 +0000

    WIP

--- AoC.Solutions/Solutions/2016/21/Part1.cs
+++ AoC.Solutions/Solutions/2016/21/Part1.cs
@@ -15,7 +15,7 @@ public class Part1 : Base
     private string Solve()
     {
         // ReSharper disable once StringLiteralTypo
-        var state = "abcdefgh".ToCharArray();
+        var state = "abcdefgh";
 
         foreach (var line in Input)
         {
@@ -28,28 +28,28 @@ public class Part1 : Base
             switch (parts[0])
             {
                 case "swap":
-                    if (parts[1] == "position")
-                    {
-                        leftIdx = int.Parse(parts[2]);
+                    //if (parts[1] == "position")
+                    //{
+                    //    leftIdx = int.Parse(parts[2]);
 
-                        rightIdx = int.Parse(parts[5]);
+                    //    rightIdx = int.Parse(parts[5]);
 
-                        (state[leftIdx], state[rightIdx]) = (state[rightIdx], state[leftIdx]);
-                    }
-                    else
-                    {
-                        state = new string(state).Replace(parts[2][0], '-').Replace(parts[5][0], parts[2][0]).Replace('-', parts[5][0]).ToCharArray();
-                    }
+                    //    (state[leftIdx], state[rightIdx]) = (state[rightIdx], state[leftIdx]);
+                    //}
+                    //else
+                    //{
+                    //    state = new string(state).Replace(parts[2][0], '-').Replace(parts[5][0], parts[2][0]).Replace('-', parts[5][0]).ToCharArray();
+                    //}
 
                     break;
                 case "rotate":
                     if (parts[1] == "left")
                     {
-
+                        state = RotateLeft(state);
                     }
                     else if (parts[1] == "right")
                     {
-
+                        state = RotateRight(state);
                     }
                     else
                     {
@@ -60,20 +60,42 @@ public class Part1 : Base
                 case "reverse":
                     break;
                 case "move":
-                    var remove = int.Parse(parts[2]);
-
-                    var character = state[remove];
-
-                    var temp = $"{new string(state[..remove])}{new string(state[(remove + 1)..])}";
-
-                    var add = int.Parse(parts[5]);
-
-                    state = $"{temp[..add]}{character}{temp[add..]}".ToCharArray();
+                    state = Move(state, int.Parse(parts[2]), int.Parse(parts[5]));
 
                     break;
             }
         }
 
         return new string(state);
+    }
+
+    //private static string SwapPosition(string state, int a, int b)
+    //{
+    //    var character = state[a];
+
+    //    state = $"{state[..a]}{state[(a + 1)..]}";
+
+
+    //}
+
+    private static string RotateLeft(string state)
+    {
+        return $"{state[1..]}{state[0]}";
+    }
+
+    private static string RotateRight(string state)
+    {
+        return $"{state[..^1]}{state[^1]}";
+    }
+
+    private static string Move(string state, int removeAt, int insertAt)
+    {
+        var character = state[removeAt];
+
+        state = $"{state[..removeAt]}{state[(removeAt + 1)..]}";
+
+        state = $"{state[..insertAt]}{character}{state[insertAt..]}";
+
+        return state;
     }
 }
