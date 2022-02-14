6a3f6f743af4fa9068efa6a6fb617f5dd78f0c76
Stevö <stevehuwjohn@gmail.com>
2022-02-14 01:57:50 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2016/18/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2016._18;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Like a rogue";
+}
