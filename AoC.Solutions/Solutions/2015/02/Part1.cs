f2371db7540a665dc6c51878bccd97cbe2591311
Stevö <stevehuwjohn@gmail.com>
2022-02-14 20:10:09 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/02/Part1.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._02;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var total = 0;
+
+        foreach (var box in Input)
+        {
+            var sides = box.Split('x').Select(int.Parse).OrderBy(s => s).ToList();
+
+            total += 2 * sides[0] * sides[1] + 2 * sides[0] * sides[2] + 2 * sides[1] * sides[2];
+
+            total += sides[0] * sides[1];
+        }
+
+        return total.ToString();
+    }
+}
