b17ef9aca5caeb7f171e31cc961f82d9498b98f2
Stevö <stevehuwjohn@gmail.com>
2022-02-14 16:15:43 +0000

    WIP

--- AoC.Solutions/Solutions/2016/22/Part1.cs
+++ AoC.Solutions/Solutions/2016/22/Part1.cs
@@ -7,37 +7,8 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        var viable = 0;
+        var result = GetViableNodes();
 
-        var data = Input.Skip(2).ToList();
-
-        for (var o = 0; o < data.Count; o++)
-        {
-            for (var i = 0; i < data.Count; i++)
-            {
-                if (o == i)
-                {
-                    continue;
-                }
-
-                var nodeA = ParseLine(data[o]);
-
-                var nodeB = ParseLine(data[i]);
-
-                if (nodeA.Used > 0 && nodeA.Used <= nodeB.Available)
-                {
-                    viable++;
-                }
-            }
-        }
-
-        return viable.ToString();
-    }
-
-    private (int Used, int Available) ParseLine(string line)
-    {
-        var parts = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-
-        return (int.Parse(parts[2][..^1]), int.Parse(parts[3][..^1]));
+        return result.Count.ToString();
     }
 }
---
+++ AoC.Solutions/Solutions/2016/22/Part2.cs
@@ -0,0 +1,62 @@
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Exceptions;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2016._22;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var data = Input.Skip(2).ToList();
+
+        var nodes = new List<(int X, int Y, int Used, int Available)>();
+
+        Point empty = null;
+
+        foreach (var item in data)
+        {
+            var parsed = ParseLine(item);
+
+            nodes.Add(parsed);
+
+            if (parsed.Used == 0)
+            {
+                empty = new Point(parsed.X, parsed.Y);
+            }
+        }
+
+        if (empty == null)
+        {
+            throw new PuzzleException("Solution not found.");
+        }
+
+        var viableNodes = GetViableNodes();
+
+        var wallStart = nodes.Where(n => ! viableNodes.Contains(n)).Min(w => w.X);
+
+        var moves = 0;
+
+        var width = nodes.Max(n => n.X);
+
+        // Move to left of wall
+        moves += empty.X - wallStart + 1;
+
+        empty.X -= moves;
+
+        // Move to top
+        moves += empty.Y;
+
+        // Move to left of target
+        moves += width - empty.X - 1;
+
+        // Move target left, it takes five moves to move the target and then bring the empty tile back to the left of the target
+        moves += (width - 1) * 5;
+
+        // Final move
+        moves++;
+
+        return moves.ToString();
+    }
+}
