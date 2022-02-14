637425935d6cbddd70fea192ff0c4e3ab133003d
Stevö <stevehuwjohn@gmail.com>
2022-02-14 19:58:49 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/01/Part1.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._01;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var floor = 0;
+
+        foreach (var c in Input[0])
+        {
+            floor += c == '(' ? 1 : -1;
+        }
+
+        return floor.ToString();
+    }
+}
