7f373600344ceb4afc85705eed8821139b06a529
Stevö <stevehuwjohn@gmail.com>
2022-02-14 05:02:35 +0000

    WIP

--- AoC.Solutions/Solutions/2016/19/Part2.cs
+++ AoC.Solutions/Solutions/2016/19/Part2.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Exceptions;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2016._19;
 
@@ -7,10 +8,17 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var binary = Convert.ToString(int.Parse(Input[0]), 2);
+        var elves = int.Parse(Input[0]);
 
-        binary = $"{binary[1..]}{binary[0]}";
+        var pow = (int) Math.Floor(Math.Log(elves) / Math.Log(3));
 
-        return Convert.ToInt32(binary, 2).ToString();
+        var b = (int) Math.Pow(3, pow);
+
+        if (elves - b <= b)
+        {
+            return (elves - b).ToString();
+        }
+
+        throw new PuzzleException("Solution not found.");
     }
 }
