1b98b8746abbdb4d653d43db0a22e07744162406
Stevö <stevehuwjohn@gmail.com>
2022-02-14 02:38:08 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2016/19/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2016._19;
+
+public abstract class Base : Solution
+{
+    public override string Description => "The Josephus problem";
+}
