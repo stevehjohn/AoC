6a66d8ac9190bf343fca16c38ae821cf7a5ffdc5
Stevö <stevehuwjohn@gmail.com>
2022-02-15 15:14:15 +0000

    WIP

--- AoC.Visualisations/Visualisations/2018/17/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2018/17/Visualisation.cs
@@ -1,10 +1,14 @@
-﻿using AoC.Solutions.Solutions._2018._17;
+﻿using System.Diagnostics;
+using AoC.Solutions.Solutions._2018._17;
 using AoC.Visualisations.Exceptions;
 using AoC.Visualisations.Infrastructure;
 using JetBrains.Annotations;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Color = Microsoft.Xna.Framework.Color;
+using Mouse = Microsoft.Xna.Framework.Input.Mouse;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace AoC.Visualisations.Visualisations._2018._17;
 
@@ -12,6 +16,14 @@ namespace AoC.Visualisations.Visualisations._2018._17;
 public class Visualisation : VisualisationBase<PuzzleState>
 {
     // ReSharper disable once NotAccessedField.Local
+    private const int ScreenWidth = 1536;
+
+    private const int ScreenHeight = 1152;
+
+    private const int TileSize = 8;
+
+    private const int TileMapWidth = 11;
+
     private readonly GraphicsDeviceManager _graphicsDeviceManager;
 
     private SpriteBatch _spriteBatch;
@@ -22,12 +34,16 @@ public class Visualisation : VisualisationBase<PuzzleState>
 
     private Map _map;
 
+    private int _y;
+
+    private MouseState? _previousMouseState;
+
     public Visualisation()
     {
         _graphicsDeviceManager = new GraphicsDeviceManager(this)
                                  {
-                                     PreferredBackBufferWidth = 1536,
-                                     PreferredBackBufferHeight = 1150
+                                     PreferredBackBufferWidth = ScreenWidth,
+                                     PreferredBackBufferHeight = ScreenHeight
                                  };
 
         // Something funky going on with having to add \bin\Windows - investigate.
@@ -77,13 +93,55 @@ public class Visualisation : VisualisationBase<PuzzleState>
             }
         }
 
+        var mouseState = Mouse.GetState();
+
+        if (_previousMouseState != null)
+        {
+            _y += (_previousMouseState.Value.ScrollWheelValue - mouseState.ScrollWheelValue) / 120;
+
+            if (_y < 0)
+            {
+                _y = 0;
+            }
+
+            Debug.WriteLine(_y);
+        }
+
+        _previousMouseState = mouseState;
+
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.Black);
+        GraphicsDevice.Clear(new Color(9, 10, 17));
+
+        _spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
+
+        DrawMap();
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    private void DrawMap()
+    {
+        for (var y = 0; y < ScreenHeight / TileSize; y ++)
+        {
+            for (var x = 0; x < _map.Width; x++)
+            {
+                var tile = _map[x, y + _y];
+                
+                if (tile > 0)
+                {
+                    var row = (tile - 1) / TileMapWidth;
+                    
+                    var column = (tile - 1) % TileMapWidth;
+
+                    _spriteBatch.Draw(_tiles, new Vector2(x * TileSize, y * TileSize), new Rectangle(column * TileSize, row * TileSize, TileSize, TileSize), Color.White);
+                }
+            }
+        }
     }
 }
