7866c2a2dc768a7166b35cf3e51be3b141021576
Stevö <stevehuwjohn@gmail.com>
2022-02-15 15:14:15 +0000

    WIP

--- AoC.Visualisations/Visualisations/2018/17/Map.cs
+++ AoC.Visualisations/Visualisations/2018/17/Map.cs
@@ -4,28 +4,33 @@ public class Map
 {
     private int[,] _map;
 
-    private int _width;
+    public int Width { get; private set; }
 
     private int _height;
 
     private int _originX;
 
+    private Random Random { get; } = new();
+
     public void CreateMap(char[,] puzzleMap)
     {
-        _width = puzzleMap.GetLength(0);
+        Width = puzzleMap.GetLength(0);
 
         _height = puzzleMap.GetLength(1);
 
-        _map = new int[_width, _height];
+        _map = new int[Width, _height];
 
         FindOrigin(puzzleMap);
 
-        var containers = AddContainers();
+        // Use this to not place torches in containers? Or just have them extinguish?
+        var containers = AddContainers(puzzleMap);
     }
+
+    public int this[int x, int y] => _map[x, y];
 
     private void FindOrigin(char[,] puzzleMap)
     {
-        for (var x = 0; x < _width; x++)
+        for (var x = 0; x < Width; x++)
         {
             if (puzzleMap[x, 0] == '+')
             {
@@ -42,7 +47,7 @@ public class Map
 
         for (var y = 1; y < _height; y++)
         {
-            for (var x = 0; x < _width; x++)
+            for (var x = 0; x < Width; x++)
             {
                 if (puzzleMap[x, y] == '#' && puzzleMap[x, y - 1] == '#' && puzzleMap[x + 1, y] == '#')
                 {
@@ -72,7 +77,7 @@ public class Map
             }
         }
 
-        for (var x = leftX + 1; x < _width; x++)
+        for (var x = leftX + 1; x < Width; x++)
         {
             if (puzzleMap[x, bottomY] != '#')
             {
@@ -92,7 +97,33 @@ public class Map
             }
         }
 
+        AddContainerToMap(leftX, rightX, leftTopY, rightTopY, bottomY);
+
         return new Rectangle(leftX, Math.Min(leftTopY, rightTopY), rightX - leftX + 1, Math.Max(leftTopY, rightTopY) - bottomY + 1);
+    }
+
+    private void AddContainerToMap(int leftX, int rightX, int leftTop, int rightTop, int bottom)
+    {
+        var tileRow = Random.Next(3) * 11 + 1;
+
+        _map[leftX, bottom] = tileRow;
+
+        for (var x = leftX + 1; x < rightX; x++)
+        {
+            _map[x, bottom] = tileRow + 1;
+        }
+
+        _map[rightX, bottom] = tileRow + 2;
+
+        for (var y = bottom - 1; y >= leftTop; y--)
+        {
+            _map[leftX, y] = tileRow + 3;
+        }
+
+        for (var y = bottom - 1; y >= rightTop; y--)
+        {
+            _map[rightX, y] = tileRow + 6;
+        }
     }
 
     private void AddBackground()
