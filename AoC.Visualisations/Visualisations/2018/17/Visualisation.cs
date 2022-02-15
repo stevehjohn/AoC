39967200da2fc1c4a89ebf0e371e0748300a80ad
Stevö <stevehuwjohn@gmail.com>
2022-02-15 11:24:46 +0000

    WIP

---
+++ AoC.Visualisations/Visualisations/2018/17/Visualisation.cs
@@ -0,0 +1,48 @@
+﻿using AoC.Solutions.Solutions._2018._17;
+using AoC.Visualisations.Exceptions;
+using AoC.Visualisations.Infrastructure;
+using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AoC.Visualisations.Visualisations._2018._17;
+
+[UsedImplicitly]
+public class Visualisation : VisualisationBase<PuzzleState>
+{
+    // ReSharper disable once NotAccessedField.Local
+    private readonly GraphicsDeviceManager _graphicsDeviceManager;
+
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _tiles;
+
+    public override void SetPart(int part)
+    {
+        switch (part)
+        {
+            case 1:
+                Puzzle = new Part1(this);
+
+                break;
+            default:
+                throw new VisualisationParameterException();
+        }
+    }
+
+    protected override void Initialize()
+    {
+        IsMouseVisible = true;
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _tiles = Content.Load<Texture2D>("tiles");
+
+        base.LoadContent();
+    }
+}
