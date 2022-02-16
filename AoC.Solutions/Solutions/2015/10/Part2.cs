807c75ac658d3dd602f7f05d8eab5870f1cfd372
Stevö <stevehuwjohn@gmail.com>
2022-02-16 14:36:12 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/10/Part2.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._10;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var input = Input[0];
+
+        for (var i = 0; i < 50; i++)
+        {
+            input = ProcessRound(input);
+        }
+
+        return input.Length.ToString();
+    }
+}
