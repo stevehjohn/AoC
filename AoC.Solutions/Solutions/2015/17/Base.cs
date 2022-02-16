bb3911692c040e2852fb7dc0cb7bdd9aba7b4b97
Stevö <stevehuwjohn@gmail.com>
2022-02-16 22:31:14 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/17/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2015._17;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Too much eggnog";
+}
