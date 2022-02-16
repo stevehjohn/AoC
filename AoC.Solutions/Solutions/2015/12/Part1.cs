a8abe6450f23381da2b1662457e16a9b1a46833d
Stevö <stevehuwjohn@gmail.com>
2022-02-16 17:39:47 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/12/Part1.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._12;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var json = Input[0];
+
+        var sum = 0;
+
+        for (var i = 0; i < json.Length - 1; i++)
+        {
+            if (json[i] is '[' or ',' or ':' && (char.IsNumber(json[i + 1]) || json[i + 1] == '-'))
+            {
+                int n;
+
+                for (n = i + 1; i < json.Length; n++)
+                {
+                    if (! (char.IsNumber(json[n]) || json[n] == '-'))
+                    {
+                        break;
+                    }
+                }
+
+                sum += int.Parse(json[(i + 1)..n]);
+            }
+        }
+
+        return sum.ToString();
+    }
+}
---
+++ AoC.Solutions/Solutions/2015/12/Part2.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AoC.Solutions.Solutions._2015._12;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    private const string Exclude = "red";
+
+    public override string GetAnswer()
+    {
+        dynamic json = JsonConvert.DeserializeObject(Input[0]);
+
+        return GetSum(json).ToString();
+    }
+
+    private long GetSum(JObject jObject)
+    {
+        if (jObject.Properties().Select(a => a.Value).OfType<JValue>().Select(v => v.Value).Contains(Exclude))
+        {
+            return 0;
+        }
+
+        return jObject.Properties().Sum((dynamic a) => (long) GetSum(a.Value));
+    }
+
+    private long GetSum(JArray jArray)
+    {
+        return jArray.Sum((dynamic a) => (long) GetSum(a));
+    }
+
+    private static long GetSum(JValue value)
+    {
+        return value.Type == JTokenType.Integer ? (long) (value.Value ?? 0) : 0;
+    }
+}
