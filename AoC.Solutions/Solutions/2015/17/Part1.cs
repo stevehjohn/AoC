1f45e8e58ae84c68780a34b40bfb3d807a80e710
Stevö <stevehuwjohn@gmail.com>
2022-02-16 22:31:14 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/17/Part1.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._17;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var containers = Input.Select((v, i) => (Index: i, Value: int.Parse(v))).ToDictionary(i => 1 << i.Index, i => i.Value);
+
+        var combinations = 1 << containers.Count;
+
+        var count = 0;
+
+        for (var i = 0; i < combinations; i++)
+        {
+            var total = containers.Where(c => (c.Key & i) > 0).Sum(c => c.Value);
+
+            if (total == 150)
+            {
+                count++;
+            }
+        }
+
+        return count.ToString();
+    }
+}
