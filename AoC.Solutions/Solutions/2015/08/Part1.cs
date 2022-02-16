b4dc9aebc61ecd285eb74baacde846c5b03be712
Stevö <stevehuwjohn@gmail.com>
2022-02-16 13:18:22 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/08/Part1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._08;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var codeLength = 0;
+
+        var memoryUsed = 0;
+
+        foreach (var line in Input)
+        {
+            codeLength += line.Length;
+
+            var data = line[1..^1];
+
+            data = Regex.Unescape(data);
+
+            memoryUsed += data.Length;
+        }
+
+        return (codeLength - memoryUsed).ToString();
+    }
+}
