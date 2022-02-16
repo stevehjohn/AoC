5d6f6ee741080573667638e48da36db475966370
Stevö <stevehuwjohn@gmail.com>
2022-02-16 23:23:16 +0000

    WIP

--- AoC.Solutions/Solutions/2015/18/Part1.cs
+++ AoC.Solutions/Solutions/2015/18/Part1.cs
@@ -1,15 +1,10 @@
-﻿using AoC.Solutions.Common;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2015._18;
 
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private const int GridSize = 100;
-
-    private HashSet<Point> _lights = new();
-
     public override string GetAnswer()
     {
         ParseInput();
@@ -19,73 +14,6 @@ public class Part1 : Base
             RunStep();
         }
 
-        return _lights.Count.ToString();
-    }
-
-    private void RunStep()
-    {
-        var newLights = new HashSet<Point>();
-
-        for (var y = 0; y < GridSize; y++)
-        {
-            for (var x = 0; x < GridSize; x++)
-            {
-                var position = new Point(x, y);
-
-                if (_lights.Contains(position))
-                {
-                    if (CountNeighbors(position) is 2 or 3)
-                    {
-                        newLights.Add(position);
-                    }
-                }
-                else
-                {
-                    if (CountNeighbors(position) == 3)
-                    {
-                        newLights.Add(position);
-                    }
-                }
-            }
-        }
-
-        _lights = newLights;
-    }
-
-    private int CountNeighbors(Point position)
-    {
-        var count = 0;
-
-        count += _lights.Contains(new Point(position.X - 1, position.Y - 1)) ? 1 : 0;
-
-        count += _lights.Contains(new Point(position.X, position.Y - 1)) ? 1 : 0;
-
-        count += _lights.Contains(new Point(position.X + 1, position.Y - 1)) ? 1 : 0;
-
-        count += _lights.Contains(new Point(position.X - 1, position.Y)) ? 1 : 0;
-
-        count += _lights.Contains(new Point(position.X + 1, position.Y)) ? 1 : 0;
-
-        count += _lights.Contains(new Point(position.X - 1, position.Y + 1)) ? 1 : 0;
-
-        count += _lights.Contains(new Point(position.X, position.Y + 1)) ? 1 : 0;
-
-        count += _lights.Contains(new Point(position.X + 1, position.Y + 1)) ? 1 : 0;
-
-        return count;
-    }
-
-    private void ParseInput()
-    {
-        for (var y = 0; y < GridSize; y++)
-        {
-            for (var x = 0; x < GridSize; x++)
-            {
-                if (Input[y][x] == '#')
-                {
-                    _lights.Add(new Point(x, y));
-                }
-            }
-        }
+        return Lights.Count.ToString();
     }
 }
