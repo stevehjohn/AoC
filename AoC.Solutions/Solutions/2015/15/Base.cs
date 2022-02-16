28c4f5499f45d366aafd9b9b134b822a0500ff58
Stevö <stevehuwjohn@gmail.com>
2022-02-16 20:30:16 +0000

    WIP

--- AoC.Solutions/Solutions/2015/15/Base.cs
+++ AoC.Solutions/Solutions/2015/15/Base.cs
@@ -6,11 +6,11 @@ public abstract class Base : Solution
 {
     public override string Description => "Science for hungry people";
 
-    private readonly List<(int Capacity, int Durability, int Flavour, int Texture)> _ingredients = new();
+    private readonly List<(int Capacity, int Durability, int Flavour, int Texture, int Calories)> _ingredients = new();
 
-    protected long Solve()
+    protected int Solve(int calorieTarget = -1)
     {
-        long highest = 0;
+        var highest = 0;
 
         for (var a = 0; a < 101; a++)
         {
@@ -20,13 +20,15 @@ public abstract class Base : Solution
                 {
                     var d = 100 - a - b - c;
 
-                    long capacity = _ingredients[0].Capacity * a + _ingredients[1].Capacity * b + _ingredients[2].Capacity * c + _ingredients[3].Capacity * d;
+                    var capacity = _ingredients[0].Capacity * a + _ingredients[1].Capacity * b + _ingredients[2].Capacity * c + _ingredients[3].Capacity * d;
 
-                    long durability = _ingredients[0].Durability * a + _ingredients[1].Durability * b + _ingredients[2].Durability * c + _ingredients[3].Durability * d;
+                    var durability = _ingredients[0].Durability * a + _ingredients[1].Durability * b + _ingredients[2].Durability * c + _ingredients[3].Durability * d;
                 
-                    long flavour = _ingredients[0].Flavour * a + _ingredients[1].Flavour * b + _ingredients[2].Flavour * c + _ingredients[3].Flavour * d;
+                    var flavour = _ingredients[0].Flavour * a + _ingredients[1].Flavour * b + _ingredients[2].Flavour * c + _ingredients[3].Flavour * d;
                     
-                    long texture = _ingredients[0].Texture * a + _ingredients[1].Texture * b + _ingredients[2].Texture * c + _ingredients[3].Texture * d;
+                    var texture = _ingredients[0].Texture * a + _ingredients[1].Texture * b + _ingredients[2].Texture * c + _ingredients[3].Texture * d;
+
+                    var calories = _ingredients[0].Calories * a + _ingredients[1].Calories * b + _ingredients[2].Calories * c + _ingredients[3].Calories * d;
 
                     if (capacity < 0 || durability < 0 || flavour < 0 || texture < 0)
                     {
@@ -35,7 +37,7 @@ public abstract class Base : Solution
 
                     var score = capacity * durability * flavour * texture;
 
-                    if (score > highest)
+                    if (score > highest && (calorieTarget == -1 || calories == calorieTarget))
                     {
                         highest = score;
                     }
@@ -54,10 +56,10 @@ public abstract class Base : Solution
         }
     }
 
-    private static (int Capacity, int Durability, int Flavour, int Texture) ParseLine(string line)
+    private static (int Capacity, int Durability, int Flavour, int Texture, int Calories) ParseLine(string line)
     {
         var parts = line.Split(new[] { ' ', ',' }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
 
-        return (int.Parse(parts[2]), int.Parse(parts[4]), int.Parse(parts[6]), int.Parse(parts[8]));
+        return (int.Parse(parts[2]), int.Parse(parts[4]), int.Parse(parts[6]), int.Parse(parts[8]), int.Parse(parts[10]));
     }
 }
