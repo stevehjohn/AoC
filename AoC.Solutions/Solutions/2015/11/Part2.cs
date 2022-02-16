295206ecb378fc179ee3cd334a21e2dad46682c8
Stevö <stevehuwjohn@gmail.com>
2022-02-16 15:17:00 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/11/Part2.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._11;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var result = GetNextPassword(Input[0]);
+
+        result = GetNextPassword(result);
+
+        return result;
+    }
+}
