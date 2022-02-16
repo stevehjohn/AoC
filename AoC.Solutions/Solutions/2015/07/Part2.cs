437e20a9798c5d0036f895cdd4851f1fe70b0113
Stevö <stevehuwjohn@gmail.com>
2022-02-16 13:04:52 +0000

    WIP

--- AoC.Solutions/Solutions/2015/07/Part2.cs
+++ AoC.Solutions/Solutions/2015/07/Part2.cs
@@ -3,11 +3,13 @@
 namespace AoC.Solutions.Solutions._2015._07;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
         ParseInput();
+        
+        Values.Add("b", 3176);
 
         var result = FindOutputValue("a");
 
