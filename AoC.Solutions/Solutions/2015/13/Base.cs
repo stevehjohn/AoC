6504de1a1384a2e5c018f0519b142d365d4684af
Stevö <stevehuwjohn@gmail.com>
2022-02-16 17:47:00 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/13/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2015._13;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Knights of the dinner table";
+}
