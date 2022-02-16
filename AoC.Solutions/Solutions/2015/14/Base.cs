acf5f70194b54cbd17cc3efa8cb4d319cc47b234
Stevö <stevehuwjohn@gmail.com>
2022-02-16 19:13:17 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/14/Base.cs
@@ -0,0 +1,58 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2015._14;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Reindeer olympics";
+
+    protected readonly List<Reindeer> Reindeer = new();
+
+    protected void ExecuteSecond()
+    {
+        foreach (var deer in Reindeer)
+        {
+            if (! deer.IsResting)
+            {
+                deer.Distance += deer.Speed;
+            }
+
+            deer.Ticks++;
+
+            if (deer.IsResting && deer.Ticks >= deer.RestTime)
+            {
+                deer.IsResting = false;
+
+                deer.Ticks = 0;
+                
+                continue;
+            }
+
+            if (! deer.IsResting && deer.Ticks >= deer.FlyTime)
+            {
+                deer.IsResting = true;
+
+                deer.Ticks = 0;
+            }
+        }
+
+        var furthest = Reindeer.Max(r => r.Distance);
+
+        Reindeer.Where(r => r.Distance == furthest).ToList().ForEach(r => r.Points++);
+    }
+
+    protected void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            Reindeer.Add(ParseLine(line));
+        }
+    }
+
+    private static Reindeer ParseLine(string line)
+    {
+        var parts = line.Split(' ');
+
+        return new Reindeer(int.Parse(parts[3]), int.Parse(parts[6]), int.Parse(parts[13]));
+    }
+}
