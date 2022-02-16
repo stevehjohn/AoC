d88d4eaab5d83b041b3c81f68e5a1a51df478c34
Stevö <stevehuwjohn@gmail.com>
2022-02-16 18:23:27 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/13/Part2.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._13;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        foreach (var person in People)
+        {
+            Happiness.Add($"{person}Stevo", 0);
+
+            Happiness.Add($"Stevo{person}", 0);
+        }
+
+        People = People.Append("Stevo").ToArray();
+
+        var result = Solve();
+
+        return result.ToString();
+    }
+}
