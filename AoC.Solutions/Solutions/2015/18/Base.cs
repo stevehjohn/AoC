e88d934061895121d0a4d7add8cbe3ee26c3ee1f
Stevö <stevehuwjohn@gmail.com>
2022-02-16 22:52:53 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/18/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2015._18;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Bournville GIF";
+}
