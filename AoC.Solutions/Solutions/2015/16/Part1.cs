3f80b89a0b64375152404e25f34e7df700484153
Stevö <stevehuwjohn@gmail.com>
2022-02-16 21:29:10 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/16/Part1.cs
@@ -0,0 +1,39 @@
+﻿using AoC.Solutions.Exceptions;
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._16;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        // ReSharper disable StringLiteralTypo
+        var imprint = new Dictionary<string, int>
+                      {
+                          { "children", 3 },
+                          { "cats", 7 },
+                          { "samoyeds", 2 },
+                          { "pomeranians", 3 },
+                          { "akitas", 0 },
+                          { "vizslas", 0 },
+                          { "goldfish", 5 },
+                          { "trees", 3 },
+                          { "cars", 2 },
+                          { "perfumes", 1 }
+                      };
+        // ReSharper restore StringLiteralTypo
+
+        foreach (var sue in Input)
+        {
+            var parts = sue.Split(new[] { ' ', ',', ':' }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            if (imprint[parts[2]] == int.Parse(parts[3]) && imprint[parts[4]] == int.Parse(parts[5]) && imprint[parts[6]] == int.Parse(parts[7]))
+            {
+                return parts[1];
+            }
+        }
+
+        throw new PuzzleException("Solution not found.");
+    }
+}
