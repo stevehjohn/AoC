07247d65928726364b96c92bf998ba13dd2bf531
Stevö <stevehuwjohn@gmail.com>
2022-02-17 16:04:51 +0000

    WIP

--- AoC.Solutions/Solutions/2015/20/Part2.cs
+++ AoC.Solutions/Solutions/2015/20/Part2.cs
@@ -11,11 +11,39 @@ public class Part2 : Base
 
         var house = int.Parse(File.ReadAllText("2015.20.1.result")) + 1;
 
-        while (Sigma(house) * 10 < input)
+        //while (Sigma(house) * 10 < input)
+        while (true)
         {
+            var factors = GetFactors(house).TakeLast(50);
+
+            if (factors.Sum() * 11 >= input)
+            {
+                break;
+            }
+
             house++;
         }
 
         return house.ToString();
+    }
+
+    private static List<int> GetFactors(int n)
+    {
+        var result = new List<int>();
+
+        for (var i = 1; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                result.Add(i);
+
+                if (n / i != i)
+                {
+                    result.Add(n / i);
+                }
+            }
+        }
+
+        return result;
     }
 }
