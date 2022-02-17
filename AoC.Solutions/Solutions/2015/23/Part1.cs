fbae974ccf33e78ddbfa98f6e5fe8be5a7ccfde6
Stevö <stevehuwjohn@gmail.com>
2022-02-17 16:41:37 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/23/Part1.cs
@@ -0,0 +1,75 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._23;
+
+[UsedImplicitly]
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var result = RunProgram();
+
+        return result.ToString();
+    }
+
+    private int RunProgram()
+    {
+        var registers = new Dictionary<char, int> { { 'a', 0 }, { 'b', 0 } };
+
+        var programCounter = 0;
+
+        while (true)
+        {
+            if (programCounter >= Input.Length)
+            {
+                break;
+            }
+
+            var line = Input[programCounter];
+
+            var parts = line.Split(new[] { ' ', ',' }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            switch (parts[0])
+            {
+                case "hlf":
+                    registers[parts[1][0]] /= 2;
+
+                    break;
+                case "tpl":
+                    registers[parts[1][0]] *= 3;
+
+                    break;
+                case "inc":
+                    registers[parts[1][0]]++;
+
+                    break;
+                case "jmp":
+                    programCounter += int.Parse(parts[1]);
+
+                    break;
+                case "jie":
+                    if (registers[parts[1][0]] % 2 == 0)
+                    {
+                        programCounter += int.Parse(parts[2]);
+
+                        continue;
+                    }
+
+                    break;
+                case "jio":
+                    if (registers[parts[1][0]] == 1)
+                    {
+                        programCounter += int.Parse(parts[2]);
+
+                        continue;
+                    }
+
+                    break;
+            }
+
+            programCounter++;
+        }
+
+        return registers['a'];
+    }
+}
