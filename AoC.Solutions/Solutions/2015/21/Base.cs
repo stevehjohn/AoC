35afe97ef3b8dc2c6ec3c54d7601598b8a612910
Stevö <stevehuwjohn@gmail.com>
2022-02-17 17:36:37 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/21/Base.cs
@@ -0,0 +1,8 @@
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2015._21;
+
+public abstract class Base : Solution
+{
+    public override string Description => "RPG simulator 2015";
+}
