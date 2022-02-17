d7f3bc42f7dac85a45a1f9c284a57bd6ac448554
Stevö <stevehuwjohn@gmail.com>
2022-02-17 09:21:16 +0000

    WIP

--- AoC.Solutions/Solutions/2015/19/Part1.cs
+++ AoC.Solutions/Solutions/2015/19/Part1.cs
@@ -7,6 +7,35 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "TESTING";
+        var molecule = Input[44];
+
+        var molecules = new HashSet<string>();
+
+        for (var i = 0; i < 43; i++)
+        {
+            var parts = Input[i].Split(" => ", StringSplitOptions.TrimEntries);
+
+            var target = parts[0];
+
+            var replacement = parts[1];
+
+            var index = molecule.IndexOf(target, StringComparison.InvariantCultureIgnoreCase);
+
+            while (index > -1)
+            {
+                molecules.Add($"{molecule[..index]}{replacement}{molecule[(index + 1)..]}");
+
+                index += replacement.Length + 1;
+
+                if (index >= molecule.Length)
+                {
+                    break;
+                }
+
+                index = molecule.IndexOf(target, index, StringComparison.InvariantCultureIgnoreCase);
+            }
+        }
+
+        return molecules.Count.ToString();;
     }
 }
