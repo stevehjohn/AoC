5a658e8c50f86986c6dcee42ffee6da0f734e7a9
Stevö <stevehuwjohn@gmail.com>
2022-02-17 17:05:37 +0000

    WIP

--- AoC.Solutions/Solutions/2015/23/Part2.cs
+++ AoC.Solutions/Solutions/2015/23/Part2.cs
@@ -3,11 +3,11 @@
 namespace AoC.Solutions.Solutions._2015._23;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var result = RunProgram(new Dictionary<char, int> { { 'a', 0 }, { 'b', 0 } });
+        var result = RunProgram(new Dictionary<char, int> { { 'a', 1 }, { 'b', 0 } });
 
         return result.ToString();
     }
