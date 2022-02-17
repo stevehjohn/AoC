16a3dd74add40c046007933caa1ad6bac4da02a2
Stevö <stevehuwjohn@gmail.com>
2022-02-17 15:22:27 +0000

    WIP

--- AoC.Solutions/Solutions/2015/20/Base.cs
+++ AoC.Solutions/Solutions/2015/20/Base.cs
@@ -5,4 +5,24 @@ namespace AoC.Solutions.Solutions._2015._20;
 public abstract class Base : Solution
 {
     public override string Description => "Infinite elves, infinite houses";
+
+    protected static int Sigma(int n)
+    {
+        var sum = 0;
+
+        for (var i = 1; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                sum += i;
+
+                if (n / i != i)
+                {
+                    sum += n / i;
+                }
+            }
+        }
+
+        return sum;
+    }
 }
--- AoC.Solutions/Solutions/2015/20/Part2.cs
+++ AoC.Solutions/Solutions/2015/20/Part2.cs
@@ -3,20 +3,18 @@
 namespace AoC.Solutions.Solutions._2015._20;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
         var input = int.Parse(Input[0]);
 
-        var house = 1;
+        var house = int.Parse(File.ReadAllText("2015.20.1.result")) + 1;
 
         while (Sigma(house) * 10 < input)
         {
             house++;
         }
-
-        File.WriteAllText("2015.20.1.result", $"{house}");
 
         return house.ToString();
     }
