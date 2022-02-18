6f2e08b0015d0ad4b09032c75f10dc6547f5b862
Stevö <stevehuwjohn@gmail.com>
2022-02-18 00:36:32 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/22/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC.Solutions.Solutions._2015._22;
+
+public class Player
+{
+    public int HitPoints { get; set; }
+
+    public int Armour { get; set; }
+
+    public int Mana { get; set; }
+}
