7500215c2aac6721d76df807c2fd3e928fb0672b
Stevö <stevehuwjohn@gmail.com>
2022-02-22 08:59:00 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2015/22/Part2.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2015._22;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        InitialiseSpells();
+
+        var result = GetManaCostToWin(true);
+
+        return result.ToString();
+    }
+}
