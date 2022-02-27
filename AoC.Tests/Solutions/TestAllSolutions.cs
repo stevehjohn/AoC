d9dfc17ba1e35f6a0bdae4f98673947b93a3610d
Stevö <stevehuwjohn@gmail.com>
2022-02-27 11:02:19 +0000

    WIP

--- AoC.Tests/Solutions/TestAllSolutions.cs
+++ AoC.Tests/Solutions/TestAllSolutions.cs
@@ -1,4 +1,6 @@
-﻿using AoC.Tests.Exceptions;
+﻿using AoC.Solutions.Infrastructure;
+using AoC.Solutions.Solutions._2019._01;
+using AoC.Tests.Exceptions;
 using Xunit;
 
 namespace AoC.Tests.Solutions;
@@ -13,8 +15,8 @@ public class TestAllSolutions
     }
 
     [Theory]
-    [ClassData(typeof(PuzzleEnumerator))]
-    public void RunTests(Type solution, string answer)
+    [MemberData(nameof(Solutions))]
+    public void RunTests(Type solution)
     {
         var key = $"{int.Parse(solution.Namespace?.Split('.')[3].Replace("_", string.Empty) ?? "0")}.{int.Parse(solution.Namespace?.Split('.')[4].Replace("_", string.Empty) ?? "0")}.{solution.Name[4]}";
 
@@ -25,11 +27,32 @@ public class TestAllSolutions
             throw new TestException($"Please add the correct answer for {key} to AllAnswers.txt.");
         }
 
+        var instance = Activator.CreateInstance(solution) as Solution;
+
+        var answer = instance?.GetAnswer();
+
         var split = correctAnswerLine.Split(": ");
 
         if (split[1] != answer)
         {
             throw new TestException($"Incorrect answer for {key}. Expected {split[1]}, actual {answer}.");
+        }
+    }
+
+    public static IEnumerable<object[]> Solutions
+    {
+        get
+        {
+            var solutions = typeof(Part1).Assembly
+                                         .GetTypes()
+                                         .Where(t => t.IsSubclassOf(typeof(Solution)) && ! t.IsAbstract)
+                                         .OrderBy(t => t.Namespace)
+                                         .ThenBy(t => t.Name);
+
+            foreach (var solution in solutions)
+            {
+                yield return new object[] { solution };
+            }
         }
     }
 }
