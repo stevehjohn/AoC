3d45853407cfd6bda87a35a204d1059edcb2197b
Stevö <stevehuwjohn@gmail.com>
2022-12-01 05:14:29 +0000

    1

--- AoC.Solutions/Solutions/2022/01/Part2.cs
+++ AoC.Solutions/Solutions/2022/01/Part2.cs
@@ -1,11 +1,11 @@
 ﻿namespace AoC.Solutions.Solutions._2022._01;
 
-public class Part1 : Base
+public class Part2 : Base
 {
     public override string GetAnswer()
     {
         ParseInput();
 
-        return Calories.Max().ToString();
+        return Calories.OrderByDescending(c => c).Take(3).Sum().ToString();
     }
 }
