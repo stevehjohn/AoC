0bf894d67f5572de006ece6cef56a6c25e20a486
Stevö <stevehuwjohn@gmail.com>
2022-12-02 05:50:52 +0000

    Day 2

--- AoC.Solutions/Solutions/2022/02/Part1.cs
+++ AoC.Solutions/Solutions/2022/02/Part1.cs
@@ -4,6 +4,27 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        throw new NotImplementedException();
+        var score = 0;
+
+        foreach (var line in Input)
+        {
+            score += line[2] - 'W';
+
+            switch (line)
+            {
+                case "A X":
+                case "B Y":
+                case "C Z":
+                    score += 3;
+                    break;
+                case "A Y":
+                case "B Z":
+                case "C X":
+                    score += 6;
+                    break;
+            }
+        }
+
+        return score.ToString();
     }
 }
---
+++ AoC.Solutions/Solutions/2022/02/Part2.cs
@@ -0,0 +1,50 @@
+﻿namespace AoC.Solutions.Solutions._2022._02;
+
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var score = 0;
+
+        foreach (var line in Input)
+        {
+            switch (line[2])
+            {
+                case 'X':
+                    switch (line[0])
+                    {
+                        case 'A':
+                            score += 3;
+                            break;
+                        case 'B':
+                            score++;
+                            break;
+                        case 'C':
+                            score += 2;
+                            break;
+                    }
+                    break;
+                case 'Y':
+                    score += line[0] - '@' + 3;
+                    break;
+                case 'Z':
+                    switch (line[0])
+                    {
+                        case 'A':
+                            score += 2;
+                            break;
+                        case 'B':
+                            score += 3;
+                            break;
+                        case 'C':
+                            score++;
+                            break;
+                    }
+                    score += 6;
+                    break;
+            }
+        }
+
+        return score.ToString();
+    }
+}
