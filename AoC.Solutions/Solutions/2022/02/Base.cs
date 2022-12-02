53772524c1f50e91590780bf89201a4c431d11d3
Stevö <stevehuwjohn@gmail.com>
2022-12-02 05:50:52 +0000

    Day 2

--- AoC.Solutions/Solutions/2022/02/Base.cs
+++ AoC.Solutions/Solutions/2022/02/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2022._02;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Rock Paper Scissors";
 }
