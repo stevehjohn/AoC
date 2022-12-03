23976482e660c38f7708ffe25dd2a7c1a1b68ca2
Stevö <stevehuwjohn@gmail.com>
2022-12-03 05:25:10 +0000

    Day 3

---
+++ AoC.Solutions/Solutions/2022/03/Part2.cs
@@ -0,0 +1,25 @@
+﻿namespace AoC.Solutions.Solutions._2022._03;
+
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var sum = 0;
+
+        for (var i = 0; i < Input.Length; i += 3)
+        {
+            var common = Input[i].Where(item => Input[i + 1].Contains(item)).ToArray();
+
+            common = common.Where(item => Input[i + 2].Contains(item)).ToArray();
+
+            sum += common[0] & 0b0001_1111;
+
+            if ((common[0] & 0b0010_0000) == 0)
+            {
+                sum += 26;
+            }
+        }
+
+        return sum.ToString();
+    }
+}
