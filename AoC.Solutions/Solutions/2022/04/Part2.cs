3322a5b54ed83614f9528806b19f617553136319
Stevö <stevehuwjohn@gmail.com>
2022-12-04 05:20:27 +0000

    Cleanupo

--- AoC.Solutions/Solutions/2022/04/Part2.cs
+++ AoC.Solutions/Solutions/2022/04/Part2.cs
@@ -8,11 +8,7 @@ public class Part2 : Base
 
         foreach (var line in Input)
         {
-            var pair = line.Split(',');
-
-            var elf1 = new Range(pair[0]);
-            
-            var elf2 = new Range(pair[1]);
+            var (elf1, elf2) = ParseLine(line);
 
             if ((elf1.Left >= elf2.Left && elf1.Left <= elf2.Right)
                 || (elf1.Right >= elf2.Left && elf1.Right <= elf2.Right)
