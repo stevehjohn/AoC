6771db48fe1aca2b2f54a7afbbd19cf6bb9dbb63
Stevö <stevehuwjohn@gmail.com>
2022-12-04 05:17:58 +0000

    Day 4

--- AoC.Solutions/Solutions/2022/04/Part1.cs
+++ AoC.Solutions/Solutions/2022/04/Part1.cs
@@ -1,9 +1,25 @@
-﻿//namespace AoC.Solutions.Solutions._2022._04;
+﻿namespace AoC.Solutions.Solutions._2022._04;
 
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var contains = 0;
+
+        foreach (var line in Input)
+        {
+            var pair = line.Split(',');
+
+            var elf1 = new Range(pair[0]);
+            
+            var elf2 = new Range(pair[1]);
+
+            if ((elf1.Left <= elf2.Left && elf1.Right >= elf2.Right) || (elf2.Left <= elf1.Left && elf2.Right >= elf1.Right))
+            {
+                contains++;
+            }
+        }
+
+        return contains.ToString();
+    }
+}
--- AoC.Solutions/Solutions/2022/04/Part2.cs
+++ AoC.Solutions/Solutions/2022/04/Part2.cs
@@ -1,9 +1,28 @@
-﻿//namespace AoC.Solutions.Solutions._2022._04;
+﻿namespace AoC.Solutions.Solutions._2022._04;
 
-//public class Part2 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        var overlaps = 0;
+
+        foreach (var line in Input)
+        {
+            var pair = line.Split(',');
+
+            var elf1 = new Range(pair[0]);
+            
+            var elf2 = new Range(pair[1]);
+
+            if ((elf1.Left >= elf2.Left && elf1.Left <= elf2.Right)
+                || (elf1.Right >= elf2.Left && elf1.Right <= elf2.Right)
+                || (elf2.Right >= elf1.Left && elf2.Right <= elf1.Right)
+                || (elf2.Right >= elf1.Left && elf2.Right <= elf1.Right))
+            {
+                overlaps++;
+            }
+        }
+
+        return overlaps.ToString();
+    }
+}
