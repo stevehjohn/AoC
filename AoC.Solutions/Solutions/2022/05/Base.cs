1576ad9b7914f48a3783a7e46d17ee8b69301425
Stevö <stevehuwjohn@gmail.com>
2022-12-05 05:28:57 +0000

    5.1

--- AoC.Solutions/Solutions/2022/05/Base.cs
+++ AoC.Solutions/Solutions/2022/05/Base.cs
@@ -4,5 +4,59 @@ namespace AoC.Solutions.Solutions._2022._05;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Supply Stacks";
+
+    protected Stack<char>[] Stacks;
+
+    protected void ParseStacks()
+    {
+        var index = 0;
+
+        while (! string.IsNullOrWhiteSpace(Input[index]))
+        {
+            index++;
+        }
+
+        index--;
+
+        var count = int.Parse(Input[index].Split(' ', StringSplitOptions.RemoveEmptyEntries).Last());
+
+        var stacks = new Stack<char>[count];
+
+        index--;
+
+        while (index > -1)
+        {
+            for (var x = 0; x < count; x++)
+            {
+                if (stacks[x] == null)
+                {
+                    stacks[x] = new Stack<char>();
+                }
+
+                var crate = Input[index][1 + x * 4];
+
+                if (! char.IsLetter(crate))
+                {
+                    continue;
+                }
+
+                stacks[x].Push(crate);
+            }
+
+            index--;
+        }
+
+        Stacks = stacks;
+    }
+
+    protected void DoMove(string instruction)
+    {
+        var parts = instruction.Split(' ', StringSplitOptions.TrimEntries);
+
+        for (var i = 0; i < int.Parse(parts[1]); i++)
+        {
+            Stacks[int.Parse(parts[5]) - 1].Push(Stacks[int.Parse(parts[3]) - 1].Pop());
+        }
+    }
 }
--- AoC.Solutions/Solutions/2022/05/Part1.cs
+++ AoC.Solutions/Solutions/2022/05/Part1.cs
@@ -1,9 +1,36 @@
-﻿//namespace AoC.Solutions.Solutions._2022._05;
+﻿using System.Text;
 
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+namespace AoC.Solutions.Solutions._2022._05;
+
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseStacks();
+
+        var index = 0;
+
+        while (! string.IsNullOrWhiteSpace(Input[index]))
+        {
+            index++;
+        }
+
+        index++;
+
+        while (index < Input.Length)
+        {
+            DoMove(Input[index]);
+
+            index++;
+        }
+
+        var result = new StringBuilder();
+
+        foreach (var stack in Stacks)
+        {
+            result.Append(stack.Pop());
+        }
+
+        return result.ToString();
+    }
+}
