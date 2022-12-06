30111f9d69d425dda3f1ce234a04e77c15c3188b
Stevö <stevehuwjohn@gmail.com>
2022-12-06 05:28:26 +0000

    Day 6

--- AoC.Solutions/Solutions/2022/06/Base.cs
+++ AoC.Solutions/Solutions/2022/06/Base.cs
@@ -4,5 +4,31 @@ namespace AoC.Solutions.Solutions._2022._06;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Tuning Trouble";
+
+    protected int FindMarker(int length)
+    {
+        var unique = new HashSet<char>();
+
+        int i;
+
+        var data = Input[0];
+
+        for (i = 0; i < data.Length - length + 1; i++)
+        {
+            for (var x = 0; x < length; x++)
+            {
+                unique.Add(data[i + x]);
+            }
+
+            if (unique.Count == length)
+            {
+                break;
+            }
+
+            unique.Clear();
+        }
+
+        return i + length;
+    }
 }
