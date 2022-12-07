364d4a4b2e888b6356707f42c8a6b49e5727110e
Stevö <stevehuwjohn@gmail.com>
2022-12-07 06:08:47 +0000

    WIP

--- AoC.Solutions/Solutions/2022/07/Part1.cs
+++ AoC.Solutions/Solutions/2022/07/Part1.cs
@@ -1,9 +1,17 @@
-﻿//namespace AoC.Solutions.Solutions._2022._07;
+﻿namespace AoC.Solutions.Solutions._2022._07;
 
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ProcessCommands();
+
+        CalculateDirectorySizes();
+
+        SumDirectorySizes();
+
+        var oneHundredK = DirectorySizes.Where(ds => ds.Value <= 100_000);
+
+        return oneHundredK.Sum(ds => ds.Value).ToString();
+    }
+}
