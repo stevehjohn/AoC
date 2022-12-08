7ea3e34495ea1ce2c0a55f2bdd59199a62849005
Stevö <stevehuwjohn@gmail.com>
2022-12-08 06:55:59 +0000

    08: Pt 1

--- AoC.Solutions/Solutions/2022/08/Part1.cs
+++ AoC.Solutions/Solutions/2022/08/Part1.cs
@@ -4,76 +4,8 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        var rows = new List<List<char>>();
-        
-        var cols =new List<List<char>>();
+        var result = ProcessInput();
 
-        foreach (var line in Input)
-        {
-            var row = line.ToCharArray().ToList();
-
-            rows.Add(row);
-
-            for (var x = 0; x < line.Length; x++)
-            {
-                List<char> col;
-
-                if (cols.Count < x + 1)
-                {
-                    col = new List<char>();
-
-                    cols.Add(col);
-                }
-                else
-                {
-                    col = cols[x];
-                }
-
-                col.Add(line[x]);
-            }
-        }
-
-        var visible = Input.Length * 2 + (Input[0].Length - 2) * 2;
-
-        foreach (var row in rows.Skip(1).Take(rows.Count - 2))
-        {
-            visible += CountVisible(row);
-
-            row.Reverse();
-
-            visible += CountVisible(row);
-        }
-
-        foreach (var col in cols.Skip(1).Take(cols.Count - 2))
-        {
-            visible += CountVisible(col);
-
-            col.Reverse();
-
-            visible += CountVisible(col);
-        }
-
-        return visible.ToString();
-    }
-
-    private static int CountVisible(List<char> list)
-    {
-        var visibleCount = 0;
-
-        var max = list[0];
-
-        for (var i = 1; i < list.Count - 2; i++)
-        {
-            var tree = list[i];
-
-            if (tree > max)
-            {
-                visibleCount++;
-
-                max = tree;
-            }
-        }
-
-        return visibleCount;
+        return result.ToString();
     }
 }
