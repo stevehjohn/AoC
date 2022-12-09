88f8c6f15a5cc60a62448f1c7bb960fad049a80b
Stevö <stevehuwjohn@gmail.com>
2022-12-09 06:30:36 +0000

    9.2

--- AoC.Solutions/Solutions/2022/09/Part2.cs
+++ AoC.Solutions/Solutions/2022/09/Part2.cs
@@ -1,9 +1,11 @@
-﻿//namespace AoC.Solutions.Solutions._2022._09;
+﻿namespace AoC.Solutions.Solutions._2022._09;
 
-//public class Part2 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        ProcessInput(10);
+
+        return TailVisited.Count.ToString();
+    }
+}
