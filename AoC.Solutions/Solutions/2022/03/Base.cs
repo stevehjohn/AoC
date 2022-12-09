4b3df3e75a89984f6a96b22dc3cec450ca700d70
Stevö <stevehuwjohn@gmail.com>
2022-12-09 07:59:18 +0000

    Being anal

--- AoC.Solutions/Solutions/2022/03/Base.cs
+++ AoC.Solutions/Solutions/2022/03/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2022._03;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Rucksack Reorganization";
+    public override string Description => "Rucksack reorganization";
 }
--- AoC.Solutions/Solutions/2022/05/Base.cs
+++ AoC.Solutions/Solutions/2022/05/Base.cs
@@ -5,7 +5,7 @@ namespace AoC.Solutions.Solutions._2022._05;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Supply Stacks";
+    public override string Description => "Supply stacks";
 
     protected Stack<char>[] Stacks;
 
