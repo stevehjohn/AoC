cbf16916b2086d43794f06d798c822fb18171f41
Stevö <stevehuwjohn@gmail.com>
2022-12-09 06:20:05 +0000

    WIP

--- AoC.Solutions/Solutions/2022/09/Base.cs
+++ AoC.Solutions/Solutions/2022/09/Base.cs
@@ -1,8 +1,114 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2022._09;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Rope Bridge";
+
+    private const int Width = 10_000;
+
+    private Point[] _knots;
+
+    protected readonly HashSet<int> TailVisited = new();
+
+    protected void ProcessInput()
+    {
+        _knots = new Point[2];
+
+        _knots[0] = new Point(0, 0);
+
+        _knots[1] = new Point(0, 0);
+
+        TailVisited.Add(_knots[1].X + _knots[1].Y * Width);
+
+        foreach (var line in Input)
+        {
+            var distance = int.Parse(line[2..]);
+
+            switch (line[0])
+            {
+                case 'U':
+                    MoveHead(0, distance);
+                    break;
+                case 'R':
+                    MoveHead(distance, 0);
+                    break;
+                case 'D':
+                    MoveHead(0, -distance);
+                    break;
+                case 'L':
+                    MoveHead(-distance, 0);
+                    break;
+            }
+        }
+    }
+
+    private void MoveHead(int x, int y)
+    {
+        while (x != 0 || y != 0)
+        {
+            if (x > 0)
+            {
+                _knots[0].X++;
+
+                x--;
+            }
+            else if (x < 0)
+            {
+                _knots[0].X--;
+
+                x++;
+            }
+
+            if (y > 0)
+            {
+                _knots[0].Y++;
+
+                y--;
+            }
+            else if (y < 0)
+            {
+                _knots[0].Y--;
+
+                y++;
+            }
+
+            MoveTail();
+        }
+    }
+
+    private void MoveTail()
+    {
+        if (Math.Abs(_knots[0].X - _knots[1].X) < 2 && Math.Abs(_knots[0].Y - _knots[1].Y) < 2)
+        {
+            return;
+        }
+
+        var distances = new double[9];
+
+        for (var x = -1; x < 2; x++)
+        {
+            for (var y = -1; y < 2; y++)
+            {
+                distances[x + 1 + (y + 1) * 3] = Math.Sqrt(Math.Pow(Math.Abs(_knots[0].X - (_knots[1].X + x)), 2) + Math.Pow(Math.Abs(_knots[0].Y - (_knots[1].Y + y)), 2));
+            }
+        }
+
+        var closest = distances.Select((d, i) => new { Distance = d, Index = i })
+                               .OrderBy(i => i.Distance)
+                               .First()
+                               .Index;
+
+        var dX = closest % 3 - 1;
+
+        var dY = closest / 3 - 1;
+
+        _knots[1].X += dX;
+
+        _knots[1].Y += dY;
+
+        TailVisited.Add(_knots[1].X + _knots[1].Y * Width);
+    }
 }
