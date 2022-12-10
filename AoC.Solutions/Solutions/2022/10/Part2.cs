802a00aa40adf0d8b02fea6dbaa467e8c99e19cb
Stevö <stevehuwjohn@gmail.com>
2022-12-10 06:47:16 +0000

    WIP

--- AoC.Solutions/Solutions/2022/10/Part2.cs
+++ AoC.Solutions/Solutions/2022/10/Part2.cs
@@ -1,12 +1,19 @@
-﻿namespace AoC.Solutions.Solutions._2022._10;
+﻿using System.Text;
+using AoC.Solutions.Common.Ocr;
+
+namespace AoC.Solutions.Solutions._2022._10;
 
 public class Part2 : Base
 {
+    //public override Variant? OcrOutput => Variant.Small;
+
     public override string GetAnswer()
     {
         var registerX = 1;
 
         var column = 0;
+
+        var answer = new StringBuilder();
 
         foreach (var line in Input)
         {
@@ -20,9 +27,11 @@ public class Part2 : Base
                         column = 0;
 
                         Console.WriteLine();
+                        answer.Append('\0');
                     }
 
-                    Console.Write(Math.Abs(registerX - column) < 2 ? "#" : ".");
+                    Console.Write(Math.Abs(registerX - column) < 2 ? '#' : '.');
+                    answer.Append(Math.Abs(registerX - column) < 2 ? '*' : '.');
 
                     break;
 
@@ -34,9 +43,11 @@ public class Part2 : Base
                         column = 0;
 
                         Console.WriteLine();
+                        answer.Append('\0');
                     }
 
-                    Console.Write(Math.Abs(registerX - column) < 2 ? "#" : ".");
+                    Console.Write(Math.Abs(registerX - column) < 2 ? '#' : '.');
+                    answer.Append(Math.Abs(registerX - column) < 2 ? '*' : '.');
 
                     column++;
 
@@ -45,16 +56,18 @@ public class Part2 : Base
                         column = 0;
 
                         Console.WriteLine();
+                        answer.Append('\0');
                     }
 
                     registerX += int.Parse(line[5..]);
 
-                    Console.Write(Math.Abs(registerX - column) < 2 ? "#" : ".");
+                    Console.Write(Math.Abs(registerX - column) < 2 ? '#' : '.');
+                    answer.Append(Math.Abs(registerX - column) < 2 ? '*' : '.');
 
                     break;
             }
         }
 
-        return "";
+        return answer.ToString();
     }
 }
