064f83604a7c5c95bc9bf62ca91f5b08886807f3
Stevö <stevehuwjohn@gmail.com>
2022-12-10 05:55:34 +0000

    D 10, Pt 1

--- AoC.Solutions/Solutions/2022/10/Part1.cs
+++ AoC.Solutions/Solutions/2022/10/Part1.cs
@@ -1,9 +1,82 @@
-﻿//namespace AoC.Solutions.Solutions._2022._10;
+﻿namespace AoC.Solutions.Solutions._2022._10;
 
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        return "";
-//    }
-//}
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        var registerX = 1;
+
+        var cycle = 1;
+
+        var strength = 0;
+
+        var strengthTimer = 20;
+
+        foreach (var line in Input)
+        {
+            switch (line[..4])
+            {
+                case "noop":
+                    Console.WriteLine($"{cycle}: noop");
+
+                    strengthTimer--;
+
+                    if (strengthTimer == 0)
+                    {
+                        strength += cycle * registerX;
+
+                        strengthTimer = 40;
+                    }
+
+                    cycle++;
+
+                    break;
+
+                case "addx":
+                    Console.WriteLine($"{cycle}: addx");
+
+                    strengthTimer--;
+                    if (strengthTimer == 0)
+                    {
+                        strength += cycle * registerX;
+
+                        strengthTimer = 40;
+                    }
+
+                    cycle++;
+
+                    strengthTimer--;
+                    if (strengthTimer == 0)
+                    {
+                        strength += cycle * registerX;
+
+                        strengthTimer = 40;
+                    }
+
+                    Console.WriteLine($"{cycle}: {line[5..]}");
+
+                    registerX += int.Parse(line[5..]);
+
+                    cycle++;
+
+                    break;
+            }
+        }
+
+        return strength.ToString();
+    }
+}
+
+public class ScheduledOperation
+{
+    public int Cycle { get; }
+
+    public int Value { get; }
+
+    public ScheduledOperation(int cycle, int value)
+    {
+        Cycle = cycle;
+
+        Value = value;
+    }
+}
