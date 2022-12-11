86d676a9b7774563e69d6498b6645ab4d9ecf8ae
Stevö <stevehuwjohn@gmail.com>
2022-12-11 08:25:27 +0000

    Faster, faster!

---
+++ AoC.Solutions/Solutions/2022/11/FastList.cs
@@ -0,0 +1,51 @@
+﻿namespace AoC.Solutions.Solutions._2022._11;
+
+public class FastList<T>
+{
+    private readonly int _capacity;
+
+    private readonly T[] _items;
+
+    private int _start;
+
+    private int _end;
+
+    private int _count;
+
+    public FastList(int capacity)
+    {
+        _capacity = capacity;
+
+        _items = new T[_capacity];
+    }
+
+    public void Add(T item)
+    {
+        _items[_end] = item;
+
+        _end++;
+
+        if (_end >= _capacity)
+        {
+            _end = 0;
+        }
+
+        _count++;
+    }
+
+    public void RemoveFirst()
+    {
+        _start++;
+
+        if (_start >= _capacity)
+        {
+            _start = 0;
+        }
+
+        _count--;
+    }
+
+    public T this[int index] => _items[(_start + index) % _capacity];
+
+    public int Count => _count;
+}
