de951eaec51dfb4b53a3d2b13eeb092a11c78288
Stevö <stevehuwjohn@gmail.com>
2022-12-11 06:23:59 +0000

    D 11, pt 1

---
+++ AoC.Solutions/Solutions/2022/11/Monkey.cs
@@ -0,0 +1,31 @@
+﻿namespace AoC.Solutions.Solutions._2022._11;
+
+public class Monkey
+{
+    public List<long> Items { get; }
+
+    public int DivisorTest { get; }
+
+    public int PassTestMonkey { get; }
+
+    public int FailTestMonkey { get; }
+
+    public char Operator { get; }
+
+    public int Operand { get; }
+
+    public Monkey(int divisorTest, int passTestMonkey, int failTestMonkey, char @operator, int operand)
+    {
+        Items = new();
+
+        DivisorTest = divisorTest;
+
+        PassTestMonkey = passTestMonkey;
+
+        FailTestMonkey = failTestMonkey;
+
+        Operator = @operator;
+
+        Operand = operand;
+    }
+}
