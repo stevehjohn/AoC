32b26de68421c83a40424b4dbb2b76ee6bd1e8a9
Stevö <stevehuwjohn@gmail.com>
2022-12-11 07:24:31 +0000

    Day 11 Pt 2

--- AoC.Solutions/Solutions/2022/11/Part2.cs
+++ AoC.Solutions/Solutions/2022/11/Part2.cs
@@ -1,9 +1,15 @@
-﻿//namespace AoC.Solutions.Solutions._2022._11;
+﻿namespace AoC.Solutions.Solutions._2022._11;
 
-//public class Part2 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        InitialiseMonkeys();
+
+        PlayRounds(10_000, false);
+
+        var mostActive = Inspections.OrderByDescending(i => i).Take(2).ToArray();
+
+        return (mostActive[0] * mostActive[1]).ToString();
+    }
+}
