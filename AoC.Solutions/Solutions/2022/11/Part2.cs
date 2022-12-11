26a132ecbd6aa22451ecd996e721fd72e64e51f8
Stevö <stevehuwjohn@gmail.com>
2022-12-11 08:48:22 +0000

    FARSTER!

--- AoC.Solutions/Solutions/2022/11/Part2.cs
+++ AoC.Solutions/Solutions/2022/11/Part2.cs
@@ -8,8 +8,6 @@ public class Part2 : Base
 
         PlayRounds(10_000, false);
 
-        var mostActive = Inspections.OrderByDescending(i => i).Take(2).ToArray();
-
-        return (mostActive[0] * mostActive[1]).ToString();
+        return GetMonkeyBusiness().ToString();
     }
 }
