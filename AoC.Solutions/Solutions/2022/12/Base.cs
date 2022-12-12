8fe41bd41916a21b604a2b1f254d28ed44b9ee0c
Stevö <stevehuwjohn@gmail.com>
2022-12-12 07:44:33 +0000

    WIP

--- AoC.Solutions/Solutions/2022/12/Base.cs
+++ AoC.Solutions/Solutions/2022/12/Base.cs
@@ -7,11 +7,11 @@ public abstract class Base : Solution
 {
     public override string Description => "Hill climbing algorithm";
 
-    protected int Width;
+    private int _width;
 
-    protected int Height;
+    private int _height;
 
-    protected byte[,] Map;
+    private byte[,] _map;
 
     private Point _start;
 
@@ -19,11 +19,11 @@ public abstract class Base : Solution
 
     protected void ParseInput()
     {
-        Width = Input[0].Length;
+        _width = Input[0].Length;
 
-        Height = Input.Length;
+        _height = Input.Length;
 
-        Map = new byte[Width, Height];
+        _map = new byte[_width, _height];
 
         for (var y = 0; y < Input.Length; y++)
         {
@@ -35,7 +35,7 @@ public abstract class Base : Solution
                 {
                     _start = new Point(x, y);
 
-                    Map[x, y] = 0;
+                    _map[x, y] = 0;
 
                     continue;
                 }
@@ -44,23 +44,30 @@ public abstract class Base : Solution
                 {
                     _end = new Point(x, y);
 
-                    Map[x, y] = 'z' - 'a';
+                    _map[x, y] = 'z' - 'a';
 
                     continue;
                 }
 
-                Map[x, y] = (byte) (character - 'a');
+                _map[x, y] = (byte) (character - 'a');
             }
         }
     }
 
-    protected int FindShortestPath()
+    protected int FindShortestPath(bool startFromEnd = false)
     {
         var visited = new HashSet<Point>();
 
         var queue = new PriorityQueue<(Point Position, int Steps), int>();
 
-        queue.Enqueue((_start, 0), 0);
+        if (startFromEnd)
+        {
+            queue.Enqueue((_end, 0), 0);
+        }
+        else
+        {
+            queue.Enqueue((_start, 0), 0);
+        }
 
         while (queue.Count > 0)
         {
@@ -68,18 +75,33 @@ public abstract class Base : Solution
 
             var position = node.Position;
 
-            if (position.Equals(_end))
+            if (startFromEnd)
             {
-                return node.Steps;
+                if (_map[position.X, position.Y] == 0)
+                {
+                    return node.Steps;
+                }
+            }
+            else
+            {
+                if (position.Equals(_end))
+                {
+                    return node.Steps;
+                }
             }
 
-            var height = Map[position.X, position.Y];
+            var height = _map[position.X, position.Y];
 
             Point newPosition;
 
-            var manhattan = Math.Abs(position.X - _end.X) + Math.Abs(position.Y - _end.Y);
+            var manhattan = 0;
 
-            if (position.X > 0 && Map[position.X - 1, position.Y] <= height + 1)
+            if (! startFromEnd)
+            {
+                manhattan = Math.Abs(position.X - _end.X) + Math.Abs(position.Y - _end.Y);
+            }
+
+            if (position.X > 0 && _map[position.X - 1, position.Y] <= height + 1)
             {
                 newPosition = new Point(position.X - 1, position.Y);
 
@@ -91,7 +113,7 @@ public abstract class Base : Solution
                 }
             }
 
-            if (position.X < Width - 1 && Map[position.X + 1, position.Y] <= height + 1)
+            if (position.X < _width - 1 && _map[position.X + 1, position.Y] <= height + 1)
             {
                 newPosition = new Point(position.X + 1, position.Y);
 
@@ -103,7 +125,7 @@ public abstract class Base : Solution
                 }
             }
 
-            if (position.Y > 0 && Map[position.X, position.Y - 1] <= height + 1)
+            if (position.Y > 0 && _map[position.X, position.Y - 1] <= height + 1)
             {
                 newPosition = new Point(position.X, position.Y - 1);
 
@@ -115,7 +137,7 @@ public abstract class Base : Solution
                 }
             }
 
-            if (position.Y < Height - 1 && Map[position.X, position.Y + 1] <= height + 1)
+            if (position.Y < _height - 1 && _map[position.X, position.Y + 1] <= height + 1)
             {
                 newPosition = new Point(position.X, position.Y + 1);
 
@@ -145,16 +167,16 @@ public abstract class Base : Solution
 
             var position = node.Position;
 
-            if (Map[position.X, position.Y] == 0)
+            if (_map[position.X, position.Y] == 0)
             {
                 return node.Steps;
             }
 
-            var height = Map[position.X, position.Y];
+            var height = _map[position.X, position.Y];
 
             Point newPosition;
 
-            if (position.X > 0 && Map[position.X - 1, position.Y] >= height - 1)
+            if (position.X > 0 && _map[position.X - 1, position.Y] >= height - 1)
             {
                 newPosition = new Point(position.X - 1, position.Y);
 
@@ -166,7 +188,7 @@ public abstract class Base : Solution
                 }
             }
 
-            if (position.X < Width - 1 && Map[position.X + 1, position.Y] >= height - 1)
+            if (position.X < _width - 1 && _map[position.X + 1, position.Y] >= height - 1)
             {
                 newPosition = new Point(position.X + 1, position.Y);
 
@@ -178,7 +200,7 @@ public abstract class Base : Solution
                 }
             }
 
-            if (position.Y > 0 && Map[position.X, position.Y - 1] >= height - 1)
+            if (position.Y > 0 && _map[position.X, position.Y - 1] >= height - 1)
             {
                 newPosition = new Point(position.X, position.Y - 1);
 
@@ -190,7 +212,7 @@ public abstract class Base : Solution
                 }
             }
 
-            if (position.Y < Height - 1 && Map[position.X, position.Y + 1] >= height - 1)
+            if (position.Y < _height - 1 && _map[position.X, position.Y + 1] >= height - 1)
             {
                 newPosition = new Point(position.X, position.Y + 1);
 
--- AoC.Solutions/Solutions/2022/12/Part2.cs
+++ AoC.Solutions/Solutions/2022/12/Part2.cs
@@ -1,6 +1,4 @@
-﻿using AoC.Solutions.Common;
-
-namespace AoC.Solutions.Solutions._2022._12;
+﻿namespace AoC.Solutions.Solutions._2022._12;
 
 public class Part2 : Base
 {
@@ -8,35 +6,8 @@ public class Part2 : Base
     {
         ParseInput();
 
-        var steps = FindShortestPath2();
+        var steps = FindShortestPath(true);
 
         return steps.ToString();
-
-        //var starts = new List<Point>();
-
-        //for (var y = 0; y < Height; y++)
-        //{
-        //    for (var x = 0; x < Width; x++)
-        //    {
-        //        if (Map[x, y] == 0)
-        //        {
-        //            starts.Add(new Point(x, y));
-        //        }
-        //    }
-        //}
-
-        //var min = int.MaxValue;
-
-        //foreach (var start in starts)
-        //{
-        //    var steps = FindShortestPath(start);
-
-        //    if (steps < min)
-        //    {
-        //        min = steps;
-        //    }
-        //}
-
-        //return min.ToString();
     }
 }
