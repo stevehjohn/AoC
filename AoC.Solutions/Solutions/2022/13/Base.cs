8bf3498fb4298e4d83524ce03ce98c05c725fea8
Stevö <stevehuwjohn@gmail.com>
2022-12-13 07:14:21 +0000

    Day 13 pt 1

--- AoC.Solutions/Solutions/2022/13/Base.cs
+++ AoC.Solutions/Solutions/2022/13/Base.cs
@@ -4,5 +4,46 @@ namespace AoC.Solutions.Solutions._2022._13;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Distress signal";
+
+    protected int Solve()
+    {
+        var packetPair = 0;
+
+        var result = 0;
+
+        for (var i = 0; i < Input.Length; i += 3)
+        {
+            packetPair++;
+
+            if (Compare(Input[i].Replace("10", ":"), Input[i + 1].Replace("10", ":")))
+            {
+                result += packetPair;
+            }
+        }
+
+        return result;
+    }
+
+    private bool Compare(string left, string right)
+    {
+        if (! string.IsNullOrWhiteSpace(left) && ! string.IsNullOrWhiteSpace(right))
+        {
+            if (left[0] != right[0])
+            {
+                return (L: left[0], R: right[0]) switch
+                {
+                    { L: ']' } => true,
+                    { R: ']' } => false,
+                    { L: '[' } => Compare(left, $"[{right[0]}]{right[1..]}"),
+                    { R: '[' } => Compare($"[{left[0]}]{left[1..]}", right),
+                    _ => left[0] < right[0]
+                };
+            }
+
+            return Compare(left[1..], right[1..]);
+        }
+
+        return ! string.IsNullOrWhiteSpace(left);
+    }
 }
