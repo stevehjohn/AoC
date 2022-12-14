7a3e772a1901b577ed2a9153506f7de12ca4ee48
Stevö <stevehuwjohn@gmail.com>
2022-12-14 07:26:50 +0000

    ...

--- AoC.Visualisations/Visualisations/2022/14/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2022/14/Visualisation.cs
@@ -152,7 +152,7 @@ public class Visualisation : VisualisationBase<PuzzleState>
                 {
                     if (_state.Map[x, y] == 'o')
                     {
-                        _spriteBatch.Draw(_tiles, new Vector2(x * TileSize, y * TileSize), new Rectangle(0, 4 * TileSize, TileSize, TileSize), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0.5f);
+                        _spriteBatch.Draw(_tiles, new Vector2(x * TileSize, y * TileSize), new Rectangle(0, 5 * TileSize, TileSize, TileSize), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0.5f);
                     }
                 }
             }
