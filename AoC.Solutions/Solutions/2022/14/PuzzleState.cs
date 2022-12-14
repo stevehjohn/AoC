a7aa0e7571c15c60b7dd64f345649be34a82fb53
Stevö <stevehuwjohn@gmail.com>
2022-12-14 09:12:06 +0000

    ...

--- AoC.Solutions/Solutions/2022/14/PuzzleState.cs
+++ AoC.Solutions/Solutions/2022/14/PuzzleState.cs
@@ -1,13 +1,19 @@
-﻿namespace AoC.Solutions.Solutions._2022._14;
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2022._14;
 
 public class PuzzleState
 {
     public char[,] Map { get; }
 
-    public PuzzleState(char[,] map)
+    public Point Position { get; }
+
+    public PuzzleState(char[,] map, Point position)
     {
         Map = new char[map.GetLength(0), map.GetLength(1)];
 
         Buffer.BlockCopy(map, 0, Map, 0, sizeof(char) * map.GetLength(0) * map.GetLength(1));
+
+        Position = new Point(position);
     }
 }
