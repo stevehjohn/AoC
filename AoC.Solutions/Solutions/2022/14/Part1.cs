8710d46c34c301d6829041245d0b537f51b526d2
Stevö <stevehuwjohn@gmail.com>
2022-12-14 07:20:38 +0000

    WIP

--- AoC.Solutions/Solutions/2022/14/Part1.cs
+++ AoC.Solutions/Solutions/2022/14/Part1.cs
@@ -1,7 +1,17 @@
-﻿namespace AoC.Solutions.Solutions._2022._14;
+﻿using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2022._14;
 
 public class Part1 : Base
 {
+    public Part1()
+    {
+    }
+
+    public Part1(IVisualiser<PuzzleState> visualiser) : base(visualiser)
+    {
+    }
+
     public override string GetAnswer()
     {
         CreateCave();
