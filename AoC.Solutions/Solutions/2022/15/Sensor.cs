96a33dafa80e557b3cecbaacae4c5e9b3aa24034
Stevö <stevehuwjohn@gmail.com>
2022-12-15 05:38:52 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2022/15/Sensor.cs
@@ -0,0 +1,19 @@
+﻿using AoC.Solutions.Common;
+
+namespace AoC.Solutions.Solutions._2022._15;
+
+public class Sensor
+{
+    public Point Position { get; }
+
+    public Point ClosestBeacon { get; }
+
+    public int ManhattanRange => Math.Abs(Position.X - ClosestBeacon.X) + Math.Abs(Position.Y - ClosestBeacon.Y);
+
+    public Sensor(Point position, Point closestBeacon)
+    {
+        Position = position;
+
+        ClosestBeacon = closestBeacon;
+    }
+}
