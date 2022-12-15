a574867f6adefc9e3984d18b0023aeefbba9b1e3
Stevö <stevehuwjohn@gmail.com>
2022-12-15 07:29:41 +0000

    15.2

--- AoC.Solutions/Solutions/2022/15/Base.cs
+++ AoC.Solutions/Solutions/2022/15/Base.cs
@@ -1,4 +1,5 @@
 ﻿using AoC.Solutions.Common;
+using AoC.Solutions.Exceptions;
 using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2022._15;
@@ -77,5 +78,112 @@ public abstract class Base : Solution
         }
 
         return covered - dead;
+    }
+
+    protected Point GetDeadZone(int range)
+    {
+        for (var y = 0; y <= range; y++)
+        {
+            var covered = new List<(int L, int R)>();
+
+            foreach (var sensor in _sensors)
+            {
+                var dY = Math.Abs(sensor.Position.Y - y);
+
+                if (dY > sensor.ManhattanRange)
+                {
+                    continue;
+                }
+
+                var lineRange = sensor.ManhattanRange - dY;
+
+                var l = sensor.Position.X - lineRange;
+
+                var r = sensor.Position.X + lineRange;
+
+                covered.Add((l, r));
+            }
+
+            var range1 = covered[0];
+
+            covered.RemoveAt(0);
+
+            var changed = true;
+
+            while (changed)
+            {
+                changed = false;
+
+                var i = 0;
+
+                while (i < covered.Count)
+                {
+                    var item = covered[i];
+
+                    if ((item.L >= range1.L - 1 && item.L <= range1.R + 1)
+                        || (item.R >= range1.L - 1 && item.R <= range1.R + 1)
+                        || (range1.L >= item.L - 1 && range1.L <= item.R + 1) 
+                        || (range1.R >= item.L - 1 && range1.R <= item.R + 1))
+                    {
+                        range1.L = Math.Min(range1.L, item.L);
+                        range1.R = Math.Max(range1.R, item.R);
+
+                        covered.RemoveAt(i);
+
+                        changed = true;
+
+                        continue;
+                    }
+
+                    i++;
+                }
+            }
+
+            if (covered.Count == 0)
+            {
+                continue;
+            }
+
+            var range2 = covered[0];
+
+            covered.RemoveAt(0);
+
+            changed = true;
+
+            while (changed)
+            {
+                changed = false;
+
+                var i = 0;
+
+                while (i < covered.Count)
+                {
+                    var item = covered[i];
+
+                    if ((item.L >= range2.L - 1 && item.L <= range2.R + 1)
+                        || (item.R >= range2.L - 1 && item.R <= range2.R + 1)
+                        || (range2.L >= item.L - 1 && range2.L <= item.R + 1) 
+                        || (range2.R >= item.L - 1 && range2.R <= item.R + 1))
+                    {
+                        range2.L = Math.Min(range2.L, item.L);
+                        range2.R = Math.Max(range2.R, item.R);
+
+                        covered.RemoveAt(i);
+
+                        changed = true;
+
+                        continue;
+                    }
+
+                    i++;
+                }
+            }
+
+            var lBound = Math.Min(range1.R, range2.R);
+
+            return new Point(lBound + 1, y);
+        }
+
+        throw new PuzzleException("Solution not found");
     }
 }
--- AoC.Solutions/Solutions/2022/15/Part2.cs
+++ AoC.Solutions/Solutions/2022/15/Part2.cs
@@ -4,6 +4,11 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "";
+        ParseInput();
+
+        var position = GetDeadZone(4_000_000);
+        // var position = GetDeadZone(20);
+        
+        return (position.X * 4_000_000L + position.Y).ToString();
     }
 }
