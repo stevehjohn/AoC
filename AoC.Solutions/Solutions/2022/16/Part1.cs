e53c4b70e84e96e9b974bfcc1d594d643a2dbaf9
Stevö <stevehuwjohn@gmail.com>
2022-12-16 14:53:25 +0000

    .

--- AoC.Solutions/Solutions/2022/16/Part1.cs
+++ AoC.Solutions/Solutions/2022/16/Part1.cs
@@ -70,12 +70,9 @@ public class Part1 : Base
 
             foreach (var valve in node.Valve.WorkingValves)
             {
-                if (! node.OpenedValves.Contains(valve.Valve.Name))
-                {
-                    var priority = 100 - (float) valve.Valve.FlowRate / valve.Cost;
+                var priority = 100 - (float) valve.Valve.FlowRate / valve.Cost + (node.OpenedValves.Contains(valve.Valve.Name) ? 50 : 0);
 
-                    queue.Enqueue((valve.Valve, node.Time - valve.Cost, node.ReleasedPressure, node.OpenedValves.ToList(), new List<string>(node.History) { valve.Valve.Name }), priority);
-                }
+                queue.Enqueue((valve.Valve, node.Time - valve.Cost, node.ReleasedPressure, node.OpenedValves.ToList(), new List<string>(node.History) { valve.Valve.Name }), priority);
             }
         }
     }
