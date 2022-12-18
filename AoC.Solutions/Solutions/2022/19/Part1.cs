e1b54410801151bb49638404fc862a3f53fea259
Stevö <stevehuwjohn@gmail.com>
2022-12-18 18:33:50 +0000

    D19 prep

--- AoC.Solutions/Solutions/2022/19/Part1.cs
+++ AoC.Solutions/Solutions/2022/19/Part1.cs
@@ -1,9 +1,9 @@
-﻿//namespace AoC.Solutions.Solutions._2022._19;
+﻿namespace AoC.Solutions.Solutions._2022._19;
 
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        return "";
+    }
+}
