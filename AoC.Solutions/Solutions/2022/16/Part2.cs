2b676f72f82a5056c4540e1578e03b4e37509922
Stevö <stevehuwjohn@gmail.com>
2022-12-18 08:28:46 +0000

    WIP

--- AoC.Solutions/Solutions/2022/16/Part2.cs
+++ AoC.Solutions/Solutions/2022/16/Part2.cs
@@ -4,6 +4,8 @@ namespace AoC.Solutions.Solutions._2022._16;
 
 public class Part2 : Base
 {
+    private const int StartMinutes = 26;
+
     public override string GetAnswer()
     {
         ParseInput();
@@ -17,28 +19,30 @@ public class Part2 : Base
     {
         var max = 0;
 
-        var queue = new PriorityQueue<(Valve Valve, int Time, Valve ElephantValve, int ElephantTime, int ReleasedPressure, int OpenedValves), int>();
+        var queue = new TrimablePriorityQueue<(Valve Valve, int Time, Valve ElephantValve, int ElephantTime, int ReleasedPressure, int OpenedValves, int AvailableTotalFlow), int>();
 
-        queue.Enqueue((Start, 26, Start, 26, 0, 0), 0);
+        var availableTotalFlow = Start.WorkingValves.Sum(v => v.Valve.FlowRate);
+
+        queue.Enqueue((Start, StartMinutes, Start, StartMinutes, 0, 0, availableTotalFlow), 0);
 
         while (queue.Count > 0)
         {
+            var node = queue.Dequeue();
+
             if (queue.Count % 100_000 == 0)
             {
                 Console.WriteLine($"{max} ({queue.Count})");
             }
 
-            var node = queue.Dequeue();
+            if (node.ReleasedPressure > max)
+            {
+                max = node.ReleasedPressure;
+
+                Console.WriteLine($"{max} ({queue.Count})");
+            }
 
             if (node.Time <= 0 && node.ElephantTime <= 0)
             {
-                if (node.ReleasedPressure > max)
-                {
-                    max = node.ReleasedPressure;
-
-                    Console.WriteLine(max);
-                }
-
                 continue;
             }
 
@@ -49,6 +53,8 @@ public class Part2 : Base
                 node.OpenedValves |= node.Valve.Designation;
 
                 node.ReleasedPressure += node.Valve.FlowRate * node.Time;
+
+                node.AvailableTotalFlow -= node.Valve.FlowRate;
             }
 
             if (node.ElephantValve.FlowRate > 0 && (node.OpenedValves & node.ElephantValve.Designation) == 0 && node.ElephantTime > 0)
@@ -58,15 +64,19 @@ public class Part2 : Base
                 node.OpenedValves |= node.ElephantValve.Designation;
 
                 node.ReleasedPressure += node.ElephantValve.FlowRate * node.ElephantTime;
+
+                node.AvailableTotalFlow -= node.Valve.FlowRate;
+            }
+
+            if (node.ReleasedPressure > max)
+            {
+                max = node.ReleasedPressure;
+
+                Console.WriteLine($"{max} ({queue.Count})");
             }
 
             if (node.Time <= 0 && node.ElephantTime <= 0)
             {
-                if (node.ReleasedPressure > max)
-                {
-                    max = node.ReleasedPressure;
-                }
-
                 continue;
             }
 
@@ -74,14 +84,14 @@ public class Part2 : Base
             {
                 foreach (var valve in node.Valve.WorkingValves)
                 {
-                    var isOpen = (node.OpenedValves & node.Valve.Designation) > 0;
-
-                    var elephantOpen = (node.OpenedValves & elephantValve.Valve.Designation) > 0;
-
-                    if (node.Time - valve.Cost < 0 && node.ElephantTime - elephantValve.Cost < 0)
+                    if (node.ReleasedPressure + node.AvailableTotalFlow * node.ElephantTime * node.Time < max)
                     {
                         continue;
                     }
+
+                    var isOpen = (node.OpenedValves & node.Valve.Designation) > 0;
+
+                    var elephantOpen = (node.OpenedValves & elephantValve.Valve.Designation) > 0;
 
                     var extraPressure = (node.Time - valve.Cost) * valve.Valve.FlowRate * (isOpen ? 0 : 1);
 
@@ -93,15 +103,15 @@ public class Part2 : Base
 
                     priority -= totalPressure;
 
-                    priority += isOpen || elephantOpen ? 20_000 : 0;
+                    priority += isOpen ? 20_000 : 0;
 
-                    //priority += elephantOpen ? 20_000 : 0;
+                    priority += elephantOpen ? 20_000 : 0;
 
-                    queue.Enqueue((valve.Valve, node.Time - valve.Cost, elephantValve.Valve, node.ElephantTime - elephantValve.Cost, node.ReleasedPressure, node.OpenedValves), priority);
+                    queue.Enqueue((valve.Valve, node.Time - valve.Cost, elephantValve.Valve, node.ElephantTime - elephantValve.Cost, node.ReleasedPressure, node.OpenedValves, node.AvailableTotalFlow), priority);
 
-                    if (queue.Count % 100_000 == 0)
+                    if (queue.Count % 10_000 == 0)
                     {
-                        Console.WriteLine($"{max} ({queue.Count})");
+                        Console.WriteLine($"{max} ({queue.Count}). Trimmed: {queue.Trim(i => i.AvailableTotalFlow < node.AvailableTotalFlow)}");
                     }
                 }
             }
---
+++ AoC.Solutions/Solutions/2022/16/TrimablePriorityQueue.cs
@@ -0,0 +1,85 @@
+﻿using System.Numerics;
+
+namespace AoC.Solutions.Solutions._2022._16;
+
+public class TrimablePriorityQueue<T, TP> where TP : INumber<TP>, IComparable<TP>
+{
+    private readonly List<(TP Priority, T Item)> _items = new();
+
+    public int Count => _items.Count;
+
+    public void Enqueue(T item, TP priority)
+    {
+        if (_items.Count == 0)
+        {
+            _items.Add((priority, item));
+
+            return;
+        }
+
+        var index = 0;
+
+        var endIndex = _items.Count;
+
+        while (endIndex > index)
+        {
+            var windowSize = endIndex - index;
+
+            var middleIndex = index + windowSize / 2;
+
+            var middleValue = _items[middleIndex].Priority;
+
+            var compareResult = middleValue.CompareTo(priority);
+
+            if (compareResult == 0)
+            {
+                _items.Insert(middleIndex, (priority, item));
+
+                return;
+            }
+            
+            if (compareResult < 0)
+            {
+                index = middleIndex + 1;
+
+                continue;
+            }
+            
+            endIndex = middleIndex;
+        }
+
+        _items.Insert(index, (priority, item));
+    }
+
+    public T Dequeue()
+    {
+        var item = _items[0];
+
+        _items.RemoveAt(0);
+
+        return item.Item;
+    }
+
+    public int Trim(Func<T, bool> function)
+    {
+        var count = 0;
+
+        var i = 0;
+
+        while (i < _items.Count)
+        {
+            if (function(_items[i].Item))
+            {
+                _items.RemoveAt(i);
+
+                count++;
+
+                continue;
+            }
+
+            i++;
+        }
+
+        return count;
+    }
+}
