15a101af69fafb5afdcd4dbbeb95de80ed1db047
Stevö <stevehuwjohn@gmail.com>
2022-12-20 09:20:46 +0000

    .

--- AoC.Solutions/Solutions/2022/20/CircularLinkedList.cs
+++ AoC.Solutions/Solutions/2022/20/CircularLinkedList.cs
@@ -1,32 +1,73 @@
-﻿namespace AoC.Solutions.Solutions._2022._20;
+﻿using AoC.Solutions.Exceptions;
+
+namespace AoC.Solutions.Solutions._2022._20;
 
 public class CircularLinkedList<T>
 {
-    private Node<T> _current;
-    
     private Node<T> _start;
 
+    private Node<T> _end;
+    
     public void Add(T item)
     {
         var newItem = new Node<T>(item);
 
-        if (_current == null)
+        if (_start == null)
         {
-            newItem.Previous = newItem;
-
             newItem.Next = newItem;
 
-            _current = newItem;
+            newItem.Previous = newItem;
 
             _start = newItem;
+
+            _end = newItem;
 
             return;
         }
 
+        var tempEnd = _end;
+
+        _end.Next = newItem;
+
+        _end = newItem;
+
+        newItem.Previous = tempEnd;
+
         newItem.Next = _start;
 
-        _current.Next = newItem;
+        _start.Previous = _end;
+    }
 
-        _current = newItem;
+    public Node<T> Get(Func<T, bool> function)
+    {
+        var look = _start;
+
+        do
+        {
+            if (function(look.Value))
+            {
+                return look;
+            }
+
+            look = look.Next;
+
+        } while (look != _start);
+
+        throw new PuzzleException("Not found in list.");
+    }
+
+    public void Swap(Node<T> left, Node<T> right)
+    {
+        var tempPrevious = left.Previous;
+
+        var tempNext = left.Next;
+
+        left.Next = right.Next;
+
+        left.Previous = right.Previous;
+
+        right.Next = tempNext;
+
+        right.Previous = tempPrevious;
     }
 }
