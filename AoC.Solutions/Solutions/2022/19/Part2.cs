a6141edd618d466bb847d6822c04fe7a3816cf3b
Stevö <stevehuwjohn@gmail.com>
2022-12-20 04:41:54 +0000

    Revert "."
    This reverts commit f44638110bc8de617569eb5eb32a46e174bde523.

--- AoC.Solutions/Solutions/2022/19/Part2.cs
+++ AoC.Solutions/Solutions/2022/19/Part2.cs
@@ -1,15 +1,9 @@
-﻿namespace AoC.Solutions.Solutions._2022._19;
+﻿//namespace AoC.Solutions.Solutions._2022._19;
 
-public class Part2 : Base
-{
-    public override string GetAnswer()
-    {
-        Input = Input[..3];
-
-        ParseInput();
-
-        var answer = Simulate(32);
-
-        return (answer[0].Best * answer[1].Best * answer[2].Best).ToString();
-    }
-}
+//public class Part2 : Base
+//{
+//    public override string GetAnswer()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
