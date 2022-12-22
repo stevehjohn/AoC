fdcdbb3f883910659e37d29b97fd948f752b1e82
Stevö <stevehuwjohn@gmail.com>
2022-12-22 10:37:16 +0000

    .

--- AoC.Solutions/Solutions/2022/22/CubeFace.cs
+++ AoC.Solutions/Solutions/2022/22/CubeFace.cs
@@ -2,5 +2,18 @@
 
 public class CubeFace
 {
-    
+    public char[,] Face { get; }
+
+    public CubeFace Right { get; set; }
+
+    public CubeFace Down { get; set; }
+
+    public CubeFace Left { get; set; }
+
+    public CubeFace Up { get; set; }
+
+    public CubeFace(char[,] face)
+    {
+        Face = face;
+    }
 }
