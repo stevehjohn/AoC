6eb8fc3209b038297449671a93ecc35c96e75a23
Stevö <stevehuwjohn@gmail.com>
2022-12-22 10:37:16 +0000

    .

--- AoC.Solutions/Solutions/2022/22/Cube.cs
+++ AoC.Solutions/Solutions/2022/22/Cube.cs
@@ -6,8 +6,22 @@ public class Cube
 {
     private readonly CubeFace[] _faces = new CubeFace[6];
 
+    private int _currentFace;
+
+    private Point _facePosition;
+
     public Cube(List<(Point NetCoordinates, char[,] Face)> faces)
     {
-        
+        for (var i = 0; i < 6; i++)
+        {
+            AddFace(faces, i);
+        }
+    }
+    
+    private void AddFace(List<(Point NetCoordinates, char[,] Face)> faces, int index)
+    {
+        var face = new CubeFace(faces[index].Face);
+
+        _faces[index] = face;
     }
 }
