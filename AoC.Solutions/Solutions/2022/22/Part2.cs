34062905f290b95350a8e556b90c26582c7b9918
Stevö <stevehuwjohn@gmail.com>
2022-12-22 08:54:32 +0000

    WIPpity

--- AoC.Solutions/Solutions/2022/22/Part2.cs
+++ AoC.Solutions/Solutions/2022/22/Part2.cs
@@ -1,9 +1,62 @@
-﻿//namespace AoC.Solutions.Solutions._2022._22;
+﻿namespace AoC.Solutions.Solutions._2022._22;
 
-//public class Part2 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part2 : Base
+{
+    private const int FaceSize = 4;
+
+    public override string GetAnswer()
+    {
+        GetFaces();
+
+        return "";
+    }
+
+    private void GetFaces()
+    {
+        var faces = new List<char[,]>();
+
+        var y = 0;
+
+        while (y < Input.Length)
+        {
+            var line = Input[y];
+
+            var x = 0;
+
+            while (x < line.Length)
+            {
+                if (line[x] == ' ')
+                {
+                    x += FaceSize;
+
+                    continue;
+                }
+
+                var face = GetFace(x, y);
+
+                faces.Add(face);
+
+                x += FaceSize;
+            }
+
+            y += FaceSize;
+        }
+    }
+
+    private char[,] GetFace(int x, int y)
+    {
+        var face = new char[FaceSize, FaceSize];
+
+        for (var iY = 0; iY < FaceSize; iY++)
+        {
+            var line = Input[y + iY];
+
+            for (var iX = 0; iX < FaceSize; iX++)
+            {
+                face[iX, iY] = line[x + iX];
+            }
+        }
+
+        return face;
+    }
+}
