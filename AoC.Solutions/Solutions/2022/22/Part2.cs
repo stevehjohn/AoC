4d549f5dd9a88691843798dabe79663a8bae2cd5
Stevö <stevehuwjohn@gmail.com>
2022-12-23 23:56:14 +0000

    New approach...

--- AoC.Solutions/Solutions/2022/22/Part2.cs
+++ AoC.Solutions/Solutions/2022/22/Part2.cs
@@ -1,5 +1,6 @@
 ﻿#define TEST
 using AoC.Solutions.Common;
+using AoC.Solutions.Exceptions;
 
 namespace AoC.Solutions.Solutions._2022._22;
 
@@ -11,11 +12,15 @@ public class Part2 : Base
     private const int FaceSize = 50;
 #endif
 
+    private const int FaceEndIndex = FaceSize - 1;
+
     private readonly char[,,] _cube = new char[FaceSize, FaceSize, FaceSize];
 
     private string _path;
 
-    private readonly Dictionary<Point, object> _mappings = new();
+    private readonly Dictionary<Point, (Point Start, Func<Point, Point> XChanged, Func<Point, Point> YChanged)> _mappings = new();
+
+    private Point _position;
 
     public override string GetAnswer()
     {
@@ -26,10 +31,11 @@ public class Part2 : Base
         return "";
     }
 
+    // TODO: Would be nice to automatically do this bit.
     private void InitialiseMappings()
     {
 #if TEST
-        _mappings.Add(new Point(2, 0), null);
+        _mappings.Add(new Point(2, 0), (new Point(0, 0, FaceEndIndex), p => new Point(p.X + 1, p.Y, p.Z), p => new Point(p.X - FaceSize, p.Y + 1, p.Z)));
 #else
 #endif
     }
@@ -37,6 +43,10 @@ public class Part2 : Base
     private void ParseInput()
     {
         int y;
+
+        Point cubePosition = null;
+
+        (Point Start, Func<Point, Point> XChanged, Func<Point, Point> YChanged) mapping = (new Point(-1, -1, -1), _ => throw new PuzzleException("Mapping not found."), _ => throw new PuzzleException("Mapping not found."));
 
         for (y = 0; y < Input.Length; y++)
         {
@@ -49,13 +59,31 @@ public class Part2 : Base
 
             for (var x = 0; x < line.Length; x++)
             {
-                if (line[x] == '\0')
+                if (line[x] == ' ')
                 {
                     continue;
                 }
 
-                var mapping = _mappings[new Point(x / FaceSize, y / FaceSize)];
+                if (x % FaceSize == 0 && y % FaceSize == 0)
+                {
+                    mapping = _mappings[new Point(x / FaceSize, y / FaceSize)];
+
+                    cubePosition = new Point(mapping.Start);
+                }
+
+                if (_position == null && line[x] == '.')
+                {
+                    _position = new Point(cubePosition);
+                }
+
+                // ReSharper disable once PossibleNullReferenceException - want this to fail if null.
+                _cube[cubePosition.X, cubePosition.Y, cubePosition.Z] = line[x];
+
+                cubePosition = mapping.XChanged(cubePosition);
             }
+
+            // ReSharper disable once PossibleNullReferenceException - want this to fail if null.
+            cubePosition = mapping.YChanged(cubePosition);
         }
 
         _path = Input[y + 1];
