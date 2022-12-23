cdd8a0ae7c07cbcfc69e53f05543f022d85108fe
Stevö <stevehuwjohn@gmail.com>
2022-12-23 23:13:44 +0000

    .

--- AoC.Solutions/Solutions/2022/22/Part2.cs
+++ AoC.Solutions/Solutions/2022/22/Part2.cs
@@ -1,15 +1,8 @@
 ﻿namespace AoC.Solutions.Solutions._2022._22;
 
-public class Part2 : Base
-{
-    public override string GetAnswer()
-    {
-        IsCube = true;
-
-        ParseInput();
-
-        WalkPath();
-
-        return GetSolution().ToString();
-    }
-}
+//public class Part2 : Base
+//{
+//    public override string GetAnswer()
+//    {
+//    }
+//}
