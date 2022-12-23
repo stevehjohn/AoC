875006c23cf68c14044c43b1864d6764afd38f6e
Stevö <stevehuwjohn@gmail.com>
2022-12-23 10:25:48 +0000

    .

--- AoC.Solutions/Solutions/2022/22/Base.cs
+++ AoC.Solutions/Solutions/2022/22/Base.cs
@@ -210,7 +210,6 @@ public abstract class Base : Solution
             (1, 1, 'U') => (GetPositionAfterTeleport(segmentPosition, new Point(2, 0), 'W'), 'R'),
             (2, 1, 'R') => (GetPositionAfterTeleport(segmentPosition, new Point(3, 2), 'N'), 'D'),
             (2, 2, 'D') => (GetPositionAfterTeleport(segmentPosition, new Point(0, 1), 'S'), 'U'),
-            //(2, 2, 'D') => (new Point(FaceSize - 1 - segmentPosition.X, FaceSize * 2 - 1), 'U'),
             _ => throw new PuzzleException("Unknown map segment.")
         };
 
