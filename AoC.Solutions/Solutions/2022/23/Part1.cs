f3613494642341fd793feb137f4d3fa62345aa4a
Stevö <stevehuwjohn@gmail.com>
2022-12-23 06:03:41 +0000

    .

--- AoC.Solutions/Solutions/2022/23/Part1.cs
+++ AoC.Solutions/Solutions/2022/23/Part1.cs
@@ -1,9 +1,13 @@
-﻿//namespace AoC.Solutions.Solutions._2022._23;
+﻿namespace AoC.Solutions.Solutions._2022._23;
 
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        RunSimulation();
+
+        return "";
+    }
+}
