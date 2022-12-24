288bc57f83f26719cbed4205250b8019b7400cd2
Stevö <stevehuwjohn@gmail.com>
2022-12-24 05:31:11 +0000

    WIP

--- AoC.Solutions/Solutions/2022/24/Base.cs
+++ AoC.Solutions/Solutions/2022/24/Base.cs
@@ -1,8 +1,53 @@
-﻿using AoC.Solutions.Infrastructure;
+﻿using AoC.Solutions.Common;
+using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2022._24;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Blizzard basin";
+
+    private readonly List<(Point Position, char Direction)> _storms = new();
+
+    private int _width;
+
+    private int _height;
+
+    private Point _start;
+
+    private Point _end;
+
+    protected void ParseInput()
+    {
+        _width = Input[0].Length;
+
+        _height = Input.Length;
+
+        for (var y = 0; y < _height; y++)
+        {
+            var line = Input[y];
+
+            for (var x = 0; x < _width; x++)
+            {
+                var c = line[x];
+
+                if (y == 0 && c == '.')
+                {
+                    _start = new Point(x, y);
+                }
+
+                if (y == Input.Length - 1 && c == '.')
+                {
+                    _end = new Point(x, y);
+                }
+
+                if (c == '#' || c == '.')
+                {
+                    continue;
+                }
+
+                _storms.Add((new Point(x, y), c));
+            }
+        }
+    }
 }
--- AoC.Solutions/Solutions/2022/24/Part1.cs
+++ AoC.Solutions/Solutions/2022/24/Part1.cs
@@ -1,9 +1,11 @@
-﻿//namespace AoC.Solutions.Solutions._2022._24;
+﻿namespace AoC.Solutions.Solutions._2022._24;
 
-//public class Part1 : Base
-//{
-//    public override string GetAnswer()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        ParseInput();
+
+        return "";
+    }
+}
