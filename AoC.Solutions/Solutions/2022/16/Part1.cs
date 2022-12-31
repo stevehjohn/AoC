19796a3f5bde270160fcee48b8769d4eb3c725e2
Stevö <stevehuwjohn@gmail.com>
2022-12-31 03:28:21 +0000

    FAST!

--- AoC.Solutions/Solutions/2022/16/Part1.cs
+++ AoC.Solutions/Solutions/2022/16/Part1.cs
@@ -1,6 +1,4 @@
-﻿using AoC.Solutions.Exceptions;
-
-namespace AoC.Solutions.Solutions._2022._16;
+﻿namespace AoC.Solutions.Solutions._2022._16;
 
 public class Part1 : Base
 {
@@ -10,68 +8,8 @@ public class Part1 : Base
 
         OptimiseGraph();
 
-        return Solve().ToString();
-    }
+        Traverse(30);
 
-    private int Solve()
-    {
-        var max = 0;
-
-        var added = new HashSet<(int ValveDesignation, int Time, int OpenedValves)>();
-
-        var queue = new Queue<(Valve Valve, int Time, int ReleasedPressure, int OpenedValves)>();
-
-        foreach (var valve in Start.WorkingValves)
-        {
-            queue.Enqueue((valve.Valve, 30 - valve.Cost, 0, 0));
-        }
-
-        while (queue.Count > 0)
-        {
-            var node = queue.Dequeue();
-
-            if ((node.OpenedValves & node.Valve.Designation) == 0)
-            {
-                node.Time--;
-
-                node.OpenedValves |= node.Valve.Designation;
-
-                node.ReleasedPressure += node.Valve.FlowRate * node.Time;
-            }
-
-            if (node.ReleasedPressure > max)
-            {
-                max = node.ReleasedPressure;
-            }
-
-            if (node.Time <= 0)
-            {
-                continue;
-            }
-
-            foreach (var valve in node.Valve.WorkingValves)
-            {
-                if (node.Time - valve.Cost <= 0 || (valve.Valve.Designation & node.OpenedValves) > 0)
-                {
-                    continue;
-                }
-
-                var newItem = (valve.Valve.Designation, node.Time, node.OpenedValves);
-
-                if (! added.Contains(newItem))
-                {
-                    queue.Enqueue((valve.Valve, node.Time - valve.Cost, node.ReleasedPressure, node.OpenedValves));
-
-                    added.Add(newItem);
-                }
-            }
-        }
-
-        if (max == 0)
-        {
-            throw new PuzzleException("Solution not found");
-        }
-
-        return max;
+        return StateCache.Max(s => s.Value).Flow.ToString();
     }
 }
--- AoC.Solutions/Solutions/2022/16/Part2.cs
+++ AoC.Solutions/Solutions/2022/16/Part2.cs
@@ -2,15 +2,13 @@
 
 public class Part2 : Base
 {
-    private const int StartMinutes = 26;
-
-    private const int ArbitrarySize = 110_000_000;
-
     public override string GetAnswer()
     {
         ParseInput();
 
         OptimiseGraph();
+
+        Traverse(26);
 
         return Solve().ToString();
     }
@@ -19,92 +17,21 @@ public class Part2 : Base
     {
         var max = 0;
 
-        var added = new HashSet<(int ValveDesignation, int Time, int OpenedValves, int ReleasedPressure)>();
+        var cache = StateCache.Where(i => i.Value.OpenedCount > 4).ToList();
 
-        var queue = new Queue<(Valve Valve, int Time, int ReleasedPressure, int OpenedValves, List<(string ValveName, int Time)> History)>();
-
-        var paths = new List<List<(string ValveName, int Flow)>>();
-
-        foreach (var valve in Start.WorkingValves)
+        foreach (var human in cache)
         {
-            queue.Enqueue((valve.Valve, 26 - valve.Cost, 0, 0, new List<(string ValveName, int Time)>()));
-        }
-
-        while (queue.Count > 0)
-        {
-            var node = queue.Dequeue();
-
-            var history = new List<(string ValveName, int Time)>(node.History);
-
-            if ((node.OpenedValves & node.Valve.Designation) == 0)
+            foreach (var elephant in cache)
             {
-                node.Time--;
-
-                node.OpenedValves |= node.Valve.Designation;
-
-                node.ReleasedPressure += node.Valve.FlowRate * node.Time;
-
-                history.Add((node.Valve.Name, node.Time * node.Valve.FlowRate));
-            }
-
-            if (node.ReleasedPressure > max)
-            {
-                max = node.ReleasedPressure;
-            }
-
-            if (node.Time <= 0)
-            {
-                paths.Add(node.History);
-
-                continue;
-            }
-
-            foreach (var valve in node.Valve.WorkingValves)
-            {
-                if (node.Time - valve.Cost <= 0 || (valve.Valve.Designation & node.OpenedValves) > 0)
+                if ((human.Key & elephant.Key) > 0)
                 {
                     continue;
                 }
 
-                var newItem = (valve.Valve.Designation, node.Time - valve.Cost, node.OpenedValves, node.ReleasedPressure);
-
-                if (! added.Contains(newItem))
-                {
-                    queue.Enqueue((valve.Valve, node.Time - valve.Cost, node.ReleasedPressure, node.OpenedValves, history));
-
-                    added.Add(newItem);
-                }
+                max = Math.Max(max, human.Value.Flow + elephant.Value.Flow);
             }
         }
 
-        var ordered = paths.Where(p => p.Count > 5).ToList();
-
-        var m = 0;
-
-        foreach (var path in ordered)
-        {
-            var other = ordered.Where(o => ! o.Any(ov => path.Any(pv => pv.ValveName == ov.ValveName))).ToList();
-
-            if (other.Count > 0)
-            {
-                foreach (var o in other)
-                {
-                    var sum = path.Sum(p => p.Flow) + o.Sum(k => k.Flow);
-
-                    if (sum > m)
-                    {
-                        m = sum;
-
-                        Console.WriteLine(m);
-                    }
-                }
-            }
-        }
-
-        Console.WriteLine();
-        
-        Console.WriteLine(m);
-
-        return 0;
+        return max;
     }
 }
