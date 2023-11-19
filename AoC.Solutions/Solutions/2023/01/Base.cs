5c0bdac8b6300b0dc300a00f7cef67a4e1aa3863
Stevo <stevehuwjohn@gmail.com>
2023-11-19 19:03:36 +0000

    2023 prep

---
+++ AoC.Solutions/Solutions/2023/01/Base.cs
@@ -0,0 +1,13 @@
+using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2023._01;
+
+public class Base : Solution
+{
+    public override string Description => "";
+    
+    public override string GetAnswer()
+    {
+        return "Unknown";
+    }
+}
