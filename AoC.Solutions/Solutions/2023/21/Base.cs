338cbe44066f244343f3ec463299f9681f405080
Stevo John <stevehuwjohn@gmail.com>
2023-11-27 11:27:13 +0000

    Prep.

---
+++ AoC.Solutions/Solutions/2023/21/Base.cs
@@ -0,0 +1,8 @@
+using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2023._21;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Puzzle 1";
+}
