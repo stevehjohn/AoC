89869ac8b91d75137d7b55a2069580cfe00be777
Stevo John <stevehuwjohn@gmail.com>
2023-11-27 11:28:50 +0000

    Prep.

---
+++ AoC.Solutions/Solutions/2023/23/Part1.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2023._23;
+
+[UsedImplicitly]
+public class Part1 : _22.Base
+{
+    public override string GetAnswer()
+    {
+        return "Unknown";
+    }
+}
