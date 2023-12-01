f82bb169258ab51d46419aebe5abcda26d6839a2
Stevo John <stevehuwjohn@gmail.com>
2023-12-01 09:18:41 +0000

    2023.1.1

--- AoC.Solutions/Solutions/2023/01/Part2.cs
+++ AoC.Solutions/Solutions/2023/01/Part2.cs
@@ -5,8 +5,48 @@ namespace AoC.Solutions.Solutions._2023._01;
 [UsedImplicitly]
 public class Part2 : Base
 {
+    private string[] _numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    
     public override string GetAnswer()
     {
-        return "Unknown";
+        var sum = 0;
+        
+        foreach (var line in Input)
+        {
+            sum += GetNumber(line);
+        }
+
+        return sum.ToString();
+    }
+
+    private int GetNumber(string line)
+    {
+        var first = ' ';
+        
+        foreach (var letter in line)
+        {
+            if (char.IsNumber(letter))
+            {
+                first = letter;
+
+                break;
+            }
+        }
+
+        line = new string(line.Reverse().ToArray());
+
+        var last = ' ';
+        
+        foreach (var letter in line)
+        {
+            if (char.IsNumber(letter))
+            {
+                last = letter;
+
+                break;
+            }
+        }
+
+        return int.Parse($"{first}{last}");
     }
 }
