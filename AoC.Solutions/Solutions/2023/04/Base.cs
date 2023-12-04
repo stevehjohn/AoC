dec542ca2f9f1718357b4f3153e53c71fed096ef
Stevo <stevehuwjohn@gmail.com>
2023-12-04 10:07:55 +0000

    2023.4.2

--- AoC.Solutions/Solutions/2023/04/Base.cs
+++ AoC.Solutions/Solutions/2023/04/Base.cs
@@ -5,4 +5,31 @@ namespace AoC.Solutions.Solutions._2023._04;
 public abstract class Base : Solution
 {
     public override string Description => "Scratchcards";
+
+    protected List<int> GetAllPoints()
+    {
+        var points = new List<int>();
+        
+        foreach (var line in Input)
+        {
+            points.Add(GetMatches(line));
+        }
+
+        return points;
+    }
+
+    private static int GetMatches(string line)
+    {
+        line = line.Split(':', StringSplitOptions.TrimEntries)[1];
+
+        var parts = line.Split('|', StringSplitOptions.TrimEntries);
+
+        var winningNumbers = parts[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        var numbers = parts[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        var count = winningNumbers.Intersect(numbers).Count();
+
+        return count;
+    }
 }
--- AoC.Solutions/Solutions/2023/04/Part2.cs
+++ AoC.Solutions/Solutions/2023/04/Part2.cs
@@ -7,6 +7,18 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var points = GetAllPoints().ToArray();
+
+        var counts = points.Select(_ => 1).ToArray();
+
+        for (var i = 0; i < points.Length; i++)
+        {
+            for (var j = 0; j < points[i]; j++)
+            {
+                counts[i + j + 1] += counts[i];
+            }
+        }
+
+        return counts.Sum().ToString();
     }
 }
