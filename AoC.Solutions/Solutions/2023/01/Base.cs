3b065c82c7c032159ee25d89ff35abc3f72215ef
Stevo <stevehuwjohn@gmail.com>
2023-12-04 15:03:44 +0000

    Moar speeed.

--- AoC.Solutions/Solutions/2023/01/Base.cs
+++ AoC.Solutions/Solutions/2023/01/Base.cs
@@ -8,28 +8,28 @@ public abstract class Base : Solution
 
     protected static int GetNumber(string line)
     {
-        var first = ' ';
+        var first = -1;
         
-        var last = ' ';
+        var last = -1;
 
         for (var i = 0; i < line.Length; i++)
         {
-            if (char.IsNumber(line[i]) && first == ' ')
+            if (char.IsNumber(line[i]) && first == -1)
             {
-                first = line[i];
+                first = (line[i] - '0') * 10;
             }
 
-            if (char.IsNumber(line[line.Length - i - 1]) & last == ' ')
+            if (char.IsNumber(line[line.Length - i - 1]) & last == -1)
             {
-                last = line[line.Length - i - 1];
+                last = line[line.Length - i - 1] - '0';
             }
 
-            if (first != ' ' && last != ' ')
+            if (first != -1 && last != -1)
             {
                 break;
             }
         }
 
-        return int.Parse($"{first}{last}");
+        return first + last;
     }
 }
