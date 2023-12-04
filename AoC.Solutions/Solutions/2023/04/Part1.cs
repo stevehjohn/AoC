45f4ef7d56b3996437cc7e33b1e00d27f294eb1f
Stevo <stevehuwjohn@gmail.com>
2023-12-04 09:25:20 +0000

    WIP

--- AoC.Solutions/Solutions/2023/04/Part1.cs
+++ AoC.Solutions/Solutions/2023/04/Part1.cs
@@ -7,6 +7,28 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var points = 0;
+
+        foreach (var line in Input)
+        {
+            points += GetPoints(line);
+        }
+        
+        return points.ToString();
+    }
+
+    private int GetPoints(string line)
+    {
+        line = line.Split(':', StringSplitOptions.TrimEntries)[1];
+
+        var parts = line.Split('|', StringSplitOptions.TrimEntries);
+
+        var winningNumbers = parts[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        var numbers = parts[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        var count = winningNumbers.Intersect(numbers).Count();
+
+        return 1 ^ count;
     }
 }
