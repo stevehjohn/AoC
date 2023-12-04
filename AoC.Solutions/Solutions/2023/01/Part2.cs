b5200138bf9e8af7c7c2195c25f4563f437d741f
Stevo <stevehuwjohn@gmail.com>
2023-12-04 15:06:29 +0000

    Speeeeed

--- AoC.Solutions/Solutions/2023/01/Part2.cs
+++ AoC.Solutions/Solutions/2023/01/Part2.cs
@@ -5,6 +5,18 @@ namespace AoC.Solutions.Solutions._2023._01;
 [UsedImplicitly]
 public class Part2 : Base
 {
+    private readonly Dictionary<string, string> _replacements = new()
+    {
+        { "one", "o1e" },
+        { "two", "t2o" },
+        { "three", "t3e" },
+        { "four", "f4r" },
+        { "five", "f5e" },
+        { "six", "s6x" },
+        { "seven", "s7n" },
+        { "eight", "e8t" },
+        { "nine", "n9e" },
+    };
     public override string GetAnswer()
     {
         var sum = 0;
@@ -17,22 +29,9 @@ public class Part2 : Base
         return sum.ToString();
     }
 
-    private static string ParseNumbers(string line)
+    private string ParseNumbers(string line)
     {
-        var replacements = new Dictionary<string, string>
-        {
-            { "one", "o1e" },
-            { "two", "t2o" },
-            { "three", "t3e" },
-            { "four", "f4r" },
-            { "five", "f5e" },
-            { "six", "s6x" },
-            { "seven", "s7n" },
-            { "eight", "e8t" },
-            { "nine", "n9e" },
-        };
-
-        foreach (var replacement in replacements)
+        foreach (var replacement in _replacements)
         {
             line = line.Replace(replacement.Key, replacement.Value);
         }
