d938911729415fcc86ebb47b930062e3ca8a5469
Stevo <stevehuwjohn@gmail.com>
2023-12-05 09:45:03 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2023/05/Mapping.cs
@@ -0,0 +1,8 @@
+namespace AoC.Solutions.Solutions._2023._05;
+
+public class Mapping
+{
+    public List<Range> SourceRanges { get; set; }
+    
+    public List<Range> DestinationRanges { get; set; }
+}
--- AoC.Solutions/Solutions/2023/05/Part1.cs
+++ AoC.Solutions/Solutions/2023/05/Part1.cs
@@ -5,8 +5,17 @@ namespace AoC.Solutions.Solutions._2023._05;
 [UsedImplicitly]
 public class Part1 : Base
 {
+    private List<int> _seeds;
+    
     public override string GetAnswer()
     {
         return "Unknown";
+    }
+
+    private void ParseInput()
+    {
+        _seeds = Input[0][6..].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        
+        
     }
 }
