ebedb60512e91ee56cea9c758f066c0ed676bbc2
Stevo <stevehuwjohn@gmail.com>
2023-12-06 19:05:19 +0000

    Tidy.

--- AoC.Solutions/Solutions/2023/10/Base.cs
+++ AoC.Solutions/Solutions/2023/10/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2023._10;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Puzzle 1";
+    public override string Description => "";
 }
