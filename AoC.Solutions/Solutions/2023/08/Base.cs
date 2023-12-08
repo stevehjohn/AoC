7e87376c9944ee61fde7fa4fd0d36a2be08ce8df
Stevo <stevehuwjohn@gmail.com>
2023-12-08 11:11:22 +0000

    Refactor

--- AoC.Solutions/Solutions/2023/08/Base.cs
+++ AoC.Solutions/Solutions/2023/08/Base.cs
@@ -19,4 +19,34 @@ public abstract class Base : Solution
             Map.Add(line.Substring(0, 3), (line.Substring(7, 3), line.Substring(12, 3)));
         }
     }
+
+    protected int WalkMap()
+    {
+        var steps = 0;
+
+        var step = 0;
+
+        var node = "AAA";
+        
+        while (true)
+        {
+            steps++;
+            
+            node = Steps[step] == 'L' ? Map[node].Left : Map[node].Right;
+
+            if (node[2] == 'Z')
+            {
+                break;
+            }
+
+            step++;
+
+            if (step == Steps.Length)
+            {
+                step = 0;
+            }
+        }
+
+        return steps;
+    }
 }
