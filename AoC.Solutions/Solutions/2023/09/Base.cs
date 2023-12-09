6ec3171792d344847693f7e79afd4b03d0ccf0a5
Stevo <stevehuwjohn@gmail.com>
2023-12-09 09:41:38 +0000

    2023.09.1

--- AoC.Solutions/Solutions/2023/09/Base.cs
+++ AoC.Solutions/Solutions/2023/09/Base.cs
@@ -4,5 +4,17 @@ namespace AoC.Solutions.Solutions._2023._09;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Mirage maintenance";
+    
+    protected readonly List<List<long>> Sequences = new();
+
+    protected void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            var parts = line.Split(' ', StringSplitOptions.TrimEntries).Select(long.Parse).ToList();
+            
+            Sequences.Add(parts);
+        }
+    }
 }
