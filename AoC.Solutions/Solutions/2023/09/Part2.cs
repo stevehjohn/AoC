5789befae454f07324de17355332994773a411cb
Stevo <stevehuwjohn@gmail.com>
2023-12-09 09:47:49 +0000

    2023.09.2

--- AoC.Solutions/Solutions/2023/09/Part2.cs
+++ AoC.Solutions/Solutions/2023/09/Part2.cs
@@ -7,6 +7,26 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        ParseInput();
+
+        var total = 0L;
+        
+        foreach (var sequence in Sequences)
+        {
+            var sequences = Extrapolate(sequence);
+
+            total += GetHistory(sequences);
+        }
+        
+        return total.ToString();    }
+
+    private long GetHistory(List<List<long>> sequences)
+    {
+        for (var i = sequences.Count - 2; i >= 0; i--)
+        {
+            sequences[i].Insert(0, sequences[i].First() - sequences[i + 1]. First());
+        }
+        
+        return sequences[0].First();
     }
 }
