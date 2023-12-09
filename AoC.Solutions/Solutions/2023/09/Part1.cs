45b997aad2abaa35b8cb836d7edee3bb62fb8df9
Stevo <stevehuwjohn@gmail.com>
2023-12-09 09:41:38 +0000

    2023.09.1

--- AoC.Solutions/Solutions/2023/09/Part1.cs
+++ AoC.Solutions/Solutions/2023/09/Part1.cs
@@ -7,6 +7,57 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        ParseInput();
+
+        var total = 0L;
+
+        foreach (var sequence in Sequences)
+        {
+            total += GetHistory(sequence);
+        }
+        
+        return total.ToString();
+    }
+
+    private long GetHistory(List<long> sequence)
+    {
+        var sequences = new List<List<long>> { sequence };
+
+        while (true)
+        {
+            var newSequence = new List<long>();
+
+            var zero = true; 
+            
+            for (var i = 0; i < sequence.Count - 1; i++)
+            {
+                var delta = sequence[i + 1] - sequence[i];
+
+                if (delta != 0)
+                {
+                    zero = false;
+                }
+                
+                newSequence.Add(delta);
+            }
+            
+            sequences.Add(newSequence);
+            
+            if (zero)
+            {
+                break;
+            }
+
+            sequence = newSequence;
+        }
+
+        for (var i = sequences.Count - 2; i >= 0; i--)
+        {
+            sequences[i].Add(sequences[i].Last() + sequences[i + 1]. Last());
+            //
+            // Console.WriteLine(string.Join(", ", sequences[i]));
+        }
+        
+        return sequences[0].Last();
     }
 }
