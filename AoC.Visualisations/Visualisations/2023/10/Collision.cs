445f2e24668b2588b5d637b1c774509bb0166db1
Stevo <stevehuwjohn@gmail.com>
2023-12-10 13:45:06 +0000

    WIP

---
+++ AoC.Visualisations/Visualisations/2023/10/Collision.cs
@@ -0,0 +1,12 @@
+﻿namespace AoC.Visualisations.Visualisations._2023._10;
+
+public class Collision
+{
+    public Solutions.Common.Point Position { get; set; }
+
+    public int Ticks { get; set; }
+
+    public int SpriteOffset { get; set; }
+
+    public bool IsFinal { get; set; }
+}
---
+++ AoC.Visualisations/Visualisations/2023/10/Spark.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC.Visualisations.Visualisations._2023._10;
+
+public class Spark
+{
+    public PointFloat Position { get; set; }
+
+    public PointFloat Vector { get; set; }
+
+    public int Ticks { get; set; }
+    
+    public int StartTicks { get; set; }
+
+    public int SpriteOffset { get; set; }
+
+    public float YGravity { get; set; } = 0.1f;
+}
