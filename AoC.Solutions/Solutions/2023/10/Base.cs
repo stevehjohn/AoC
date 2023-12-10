c6048f8901a90da999c139e1baf512e6bd3f4986
Stevo <stevehuwjohn@gmail.com>
2023-12-10 11:57:23 +0000

    Tidy

--- AoC.Solutions/Solutions/2023/10/Base.cs
+++ AoC.Solutions/Solutions/2023/10/Base.cs
@@ -5,4 +5,46 @@ namespace AoC.Solutions.Solutions._2023._10;
 public abstract class Base : Solution
 {
     public override string Description => "Pipe maze";
+    
+    protected char[][] Map;
+
+    protected (int X, int Y) ParseInput()
+    {
+        Map = new char[Input.Length + 2][];
+
+        var x = 0;
+
+        var y = 0;
+
+        Map[0] = new char[Input[0].Length + 2];
+        
+        Map[Input.Length + 1] = new char[Input[0].Length + 2];
+        
+        Array.Fill(Map[0], '.');
+        
+        Array.Fill(Map[Input.Length + 1], '.');
+        
+        for (var i = 0; i < Input.Length; i++)
+        {
+            Map[i + 1] = new char[Input[i].Length + 2];
+
+            Map[i + 1][0] = '.';
+            
+            Map[i + 1][Input[i].Length + 1] = '.';
+            
+            for (var j = 0; j < Input[i].Length; j++)
+            {
+                Map[i + 1][j + 1] = Input[i][j];
+
+                if (Input[i][j] == 'S')
+                {
+                    x = j + 1;
+
+                    y = i + 1;
+                }
+            }
+        }
+
+        return (x, y);
+    }
 }
--- AoC.Solutions/Solutions/2023/10/Part1.cs
+++ AoC.Solutions/Solutions/2023/10/Part1.cs
@@ -5,8 +5,6 @@ namespace AoC.Solutions.Solutions._2023._10;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private char[][] _map;
-    
     public override string GetAnswer()
     {
         var (x, y) = ParseInput();
@@ -20,22 +18,22 @@ public class Part1 : Base
     {
         var allSteps = new List<int>();
 
-        if (! new[] { '-', 'L', 'J', '.' }.Contains(_map[y - 1][x]))
+        if (! new[] { '-', 'L', 'J', '.' }.Contains(Map[y - 1][x]))
         {
             allSteps.Add(WalkPipes(x, y - 1, 0, -1));
         }
 
-        if (! new[] { '-', 'L', 'J', '.' }.Contains(_map[y + 1][x]))
+        if (! new[] { '-', 'L', 'J', '.' }.Contains(Map[y + 1][x]))
         {
             allSteps.Add(WalkPipes(x, y + 1, 0, 1));
         }
 
-        if (! new[] { '-', 'L', 'J', '.' }.Contains(_map[y][x - 1]))
+        if (! new[] { '-', 'L', 'J', '.' }.Contains(Map[y][x - 1]))
         {
             allSteps.Add(WalkPipes(x - 1, y, -1, 0));
         }
         
-        if (! new[] { '-', 'L', 'J', '.' }.Contains(_map[y][x + 1]))
+        if (! new[] { '-', 'L', 'J', '.' }.Contains(Map[y][x + 1]))
         {
             allSteps.Add(WalkPipes(x + 1, y, 1, 0));
         }
@@ -51,11 +49,11 @@ public class Part1 : Base
 
         var y = startY;
 
-        while (_map[y][x] != 'S')
+        while (Map[y][x] != 'S')
         {
             steps++;
 
-            switch (_map[y][x], dX, dY)
+            switch (Map[y][x], dX, dY)
             {
                 case ('F', _, -1):
                     dX = 1;
@@ -101,69 +99,8 @@ public class Part1 : Base
             x += dX;
 
             y += dY;
-            
-            // var yy = 0;
-            // foreach (var line in _map)
-            // {
-            //     var xx = 0;
-            //     foreach (var item in line)
-            //     {
-            //         if (y == yy && x == xx)
-            //         {
-            //             Console.Write('*');
-            //         }
-            //         else
-            //         {
-            //             Console.Write(item);
-            //         }
-            //
-            //         xx++;
-            //     }
-            //
-            //     yy++;
-            //     
-            //     Console.WriteLine();
-            // }
-            // Console.WriteLine($"{steps} - {Math.Abs(x - startX + (y - startY))}");
         }
 
         return steps / 2;
-    }
-
-    private (int X, int Y) ParseInput()
-    {
-        _map = new char[Input.Length + 2][];
-
-        var x = 0;
-
-        var y = 0;
-
-        _map[0] = new char[Input[0].Length + 2];
-        _map[Input.Length + 1] = new char[Input[0].Length + 2];
-        
-        Array.Fill(_map[0], '.');
-        Array.Fill(_map[Input.Length + 1], '.');
-        
-        for (var i = 0; i < Input.Length; i++)
-        {
-            _map[i + 1] = new char[Input[i].Length + 2];
-
-            _map[i + 1][0] = '.';
-            _map[i + 1][Input[i].Length + 1] = '.';
-            
-            for (var j = 0; j < Input[i].Length; j++)
-            {
-                _map[i + 1][j + 1] = Input[i][j];
-
-                if (Input[i][j] == 'S')
-                {
-                    x = j + 1;
-
-                    y = i + 1;
-                }
-            }
-        }
-
-        return (x, y);
     }
 }
