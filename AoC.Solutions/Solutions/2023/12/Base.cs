e192e23df9801e9e849202e57eee02a2301ef902
Stevo <stevehuwjohn@gmail.com>
2023-12-12 11:35:13 +0000

    WIP

--- AoC.Solutions/Solutions/2023/12/Base.cs
+++ AoC.Solutions/Solutions/2023/12/Base.cs
@@ -4,5 +4,73 @@ namespace AoC.Solutions.Solutions._2023._12;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Hot springs";
+
+    private readonly Dictionary<string, long> _cache = new();
+    
+    protected static (string Row, int[] Groups) ParseLine(string line)
+    {
+        var parts = line.Split(' ');
+
+        var row = parts[0].Trim('.');
+
+        var groups = parts[1].Split(',').Select(int.Parse).ToArray();
+
+        return (row, groups);
+    }
+
+    protected long GetArrangements(string row, int[] groups)
+    {
+        if (_cache.TryGetValue(row, out var answer))
+        {
+            return answer;
+        }
+
+        answer = CalculateArrangements(row, groups);
+        
+        _cache.Add(row, answer);
+
+        return answer;
+    }
+
+    private long CalculateArrangements(string row, int[] groups)
+    {
+        row = row.TrimStart('.');
+
+        var length = row.Length;
+        
+        if (length == 0)
+        {
+            return groups.Length == 0 ? 1 : 0;
+        }
+
+        if (groups.Length == 0)
+        {
+            return row.Contains('#') ? 0 : 1;
+        }
+
+        var group = groups[0];
+
+        if (row[0] == '#')
+        {
+            if (length < group || row[..group].Contains('.'))
+            {
+                return 0;
+            }
+
+            if (length == group)
+            {
+                return group == 1 ? 1 : 0;
+            }
+
+            if (row[group] == '#')
+            {
+                return 0;
+            }
+            
+            return GetArrangements(row[(group + 1)..], groups[1..]);
+        }
+
+        return GetArrangements($"#{row[1..]}", groups) + GetArrangements(row[1..], groups);
+    }
 }
