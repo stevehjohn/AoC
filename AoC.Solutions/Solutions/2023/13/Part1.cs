292343eca1104bd157019d4d34e0a5424be62951
Stevo <stevehuwjohn@gmail.com>
2023-12-13 09:44:19 +0000

    2023.13 WIP

--- AoC.Solutions/Solutions/2023/13/Part1.cs
+++ AoC.Solutions/Solutions/2023/13/Part1.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2023._13;
@@ -7,6 +8,102 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var pattern = new List<string>();
+
+        var sum = 0;
+        
+        foreach (var line in Input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                sum += AnalysePattern(pattern);
+                
+                pattern.Clear();
+                
+                continue;
+            }
+            
+            pattern.Add(line);
+        }
+
+        if (pattern.Count > 0)
+        {
+            sum += AnalysePattern(pattern);
+        }
+
+        return sum.ToString();
+    }
+
+    private int AnalysePattern(List<string> lines)
+    {
+        var result = GetVerticalReflectionPoint(lines) * 100;
+        
+        if (result == 0)
+        {
+            lines = RotateRight(lines);
+            
+            result = GetVerticalReflectionPoint(lines);
+            
+            return result;
+        }
+
+        return result;
+    }
+
+    private int GetVerticalReflectionPoint(List<string> lines)
+    {
+        var y = lines.Count / 2;
+
+        while (y < lines.Count - 1)
+        {
+            if (IsReflectionPoint(lines, y))
+            {
+                return y + 1;
+            }
+
+            y++;
+        }
+
+        return 0;
+    }
+
+    private bool IsReflectionPoint(List<string> lines, int start)
+    {
+        var top = start;
+
+        var bottom = start + 1;
+
+        while (top >= 0 && bottom < lines.Count)
+        {
+            if (! lines[top].Equals(lines[bottom]))
+            {
+                return false;
+            }
+
+            top--;
+
+            bottom++;
+        }
+
+        return true;
+    }
+
+    private List<string> RotateRight(List<string> lines)
+    {
+        var rotate = new List<string>();
+
+        for (var x = 0; x < lines[0].Length; x++)
+        {
+            var line = new StringBuilder();
+            
+            for (var y = lines.Count - 1; y >= 0; y--)
+            {
+                line.Append(lines[y][x]);
+            }
+
+            rotate.Add(line.ToString());
+        }
+        
+        return rotate;
     }
 }
