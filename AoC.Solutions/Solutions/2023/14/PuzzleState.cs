e3ad6044cff58ff1aa0b86877be12a6168f477f5
Stevo <stevehuwjohn@gmail.com>
2023-12-14 15:35:21 +0000

    WIP

--- AoC.Solutions/Solutions/2023/14/PuzzleState.cs
+++ AoC.Solutions/Solutions/2023/14/PuzzleState.cs
@@ -3,4 +3,8 @@ namespace AoC.Solutions.Solutions._2023._14;
 public class PuzzleState
 {
     public char[,] Map { get; set; }
+
+    public int PatternStart { get; set; }
+    
+    public int PatternEnd { get; set; }
 }
