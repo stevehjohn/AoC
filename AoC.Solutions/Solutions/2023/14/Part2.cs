6be8ba1b97e853911e863f5deff249b90d6f3640
Stevo <stevehuwjohn@gmail.com>
2023-12-14 10:44:04 +0000

    WIP

--- AoC.Solutions/Solutions/2023/14/Part2.cs
+++ AoC.Solutions/Solutions/2023/14/Part2.cs
@@ -22,8 +22,8 @@ public class Part2 : Base
 
         MoveRocks(l => l.OrderBy(r => r.Point.X).ToList(), r => r.X > 0, (rock, other) => other.Y == rock.Y && other.X < rock.X, p => p.X = 0, (rock, other) => rock.X = other.X + 1);
 
-        // MoveRocks(r => r.Y < Rows - 1, (rock, other) => other.X == rock.X && other.Y > rock.Y, p => p.Y = Rows - 1, (rock, other) => rock.Y = other.Y - 1);
-        //
-        // MoveRocks(r => r.X < Columns - 1, (rock, other) => other.Y == rock.Y && other.X < rock.X, p => p.X = Columns - 1, (rock, other) => rock.Y = other.X - 1);
+        MoveRocks(l => l.OrderByDescending(r => r.Point.Y).ToList(), r => r.Y < Rows - 1, (rock, other) => other.X == rock.X && other.Y > rock.Y, p => p.Y = Rows - 1, (rock, other) => rock.Y = other.Y - 1);
+        
+        MoveRocks(l => l.OrderByDescending(r => r.Point.X).ToList(),r => r.X < Columns - 1, (rock, other) => other.Y == rock.Y && other.X > rock.X, p => p.X = Columns - 1, (rock, other) => rock.X = other.X - 1);
     }
 }
