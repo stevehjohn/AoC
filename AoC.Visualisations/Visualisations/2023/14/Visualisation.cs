583badb536ac43e5c7a033ee62cfbb50a9bfb74b
Stevo <stevehuwjohn@gmail.com>
2023-12-14 19:05:08 +0000

    WIP

---
+++ AoC.Visualisations/Visualisations/2023/14/Visualisation.cs
@@ -0,0 +1,103 @@
+﻿using AoC.Solutions.Solutions._2023._14;
+using AoC.Visualisations.Exceptions;
+using AoC.Visualisations.Infrastructure;
+using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Color = Microsoft.Xna.Framework.Color;
+
+namespace AoC.Visualisations.Visualisations._2023._14;
+
+[UsedImplicitly]
+public class Visualisation : VisualisationBase<PuzzleState>
+{
+    private SpriteBatch _spriteBatch;
+    
+    private readonly Queue<char[,]> _maps = new();
+    
+    private PuzzleState _state;
+
+    public Visualisation()
+    {
+        GraphicsDeviceManager = new GraphicsDeviceManager(this)
+                                 {
+                                     PreferredBackBufferWidth = 980,
+                                     PreferredBackBufferHeight = 980
+                                 };
+
+        // Something funky going on with having to add \bin\Windows - investigate.
+        Content.RootDirectory = "./14";
+
+        IgnoreQueueLimit = true;
+    }
+
+    public override void SetPart(int part)
+    {
+        switch (part)
+        {
+            case 2:
+                Puzzle = new Part2(this);
+
+                break;
+            default:
+                throw new VisualisationParameterException();
+        }
+    }
+
+    protected override void Initialize()
+    {
+        IsMouseVisible = true;
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        //_mapTiles = Content.Load<Texture2D>("map-tiles");
+
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (HasNextState)
+        {
+            var state = GetNextState();
+
+            var map = new char[state.Map.GetLength(0), state.Map.GetLength(1)];
+            
+            Array.Copy(state.Map, 0, map, 0, state.Map.GetLength(0) * state.Map.GetLength(1));
+            
+            _maps.Enqueue(map);
+
+            if (state.PatternStart > 0)
+            {
+                _state = state;
+            }
+        }
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        if (_state != null)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
+
+            // Draw
+            
+            _spriteBatch.End();
+        }
+        else
+        {
+            GraphicsDevice.Clear(Color.Black);
+        }
+
+        base.Draw(gameTime);
+    }
+}
