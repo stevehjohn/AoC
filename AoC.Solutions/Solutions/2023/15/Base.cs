a1571526ff271422a43895e11f314ee242813e4d
Stevo <stevehuwjohn@gmail.com>
2023-12-15 12:12:04 +0000

    WIP

--- AoC.Solutions/Solutions/2023/15/Base.cs
+++ AoC.Solutions/Solutions/2023/15/Base.cs
@@ -5,4 +5,11 @@ namespace AoC.Solutions.Solutions._2023._15;
 public abstract class Base : Solution
 {
     public override string Description => "Lens library";
+
+    protected List<string> ParseInput()
+    {
+        var parts = Input[0].Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        return parts.ToList();
+    }
 }
