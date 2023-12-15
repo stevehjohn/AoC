a5056132b5f39d44a98a73ae2aad34cf64f81fca
Stevo <stevehuwjohn@gmail.com>
2023-12-15 12:12:04 +0000

    WIP

--- AoC.Solutions/Solutions/2023/15/Part2.cs
+++ AoC.Solutions/Solutions/2023/15/Part2.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2023._15;
@@ -7,6 +8,48 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
+        var steps = ParseInput();
+
+        var boxes = new List<(string Label, int Lense)>[256];
+
+        for (var i = 0; i < 256; i++)
+        {
+            boxes[i] = new List<(string Label, int Lense)>();
+        }
+
+        foreach (var step in steps)
+        {
+            var task = ParseStep(step);
+        }
+        
         return "Unknown";
+    }
+
+    private (string Label, int FocalLength) ParseStep(string step)
+    {
+        var label = new StringBuilder();
+
+        int i;
+
+        char c = '\0';
+        
+        for (i = 0; i < step.Length; i++)
+        {
+            c = step[i];
+            
+            if (! char.IsLetter(c))
+            {
+                break;
+            }
+
+            label.Append(c);
+        }
+
+        if (c == '-')
+        {
+            return (label.ToString(), 0);
+        }
+
+        return (label.ToString(), step[i + 1] - '0');
     }
 }
