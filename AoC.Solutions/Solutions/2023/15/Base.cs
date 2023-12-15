40a8f311a2399e95202495a9299f1cd368e67ce7
Stevo <stevehuwjohn@gmail.com>
2023-12-15 12:38:48 +0000

    WIP

--- AoC.Solutions/Solutions/2023/15/Base.cs
+++ AoC.Solutions/Solutions/2023/15/Base.cs
@@ -6,6 +6,22 @@ public abstract class Base : Solution
 {
     public override string Description => "Lens library";
 
+    protected static int Hash(string input)
+    {
+        var hash = 0;
+        
+        foreach (var c in input)
+        {
+            hash += (byte) c;
+
+            hash *= 17;
+
+            hash %= 256;
+        }
+
+        return hash;
+    }
+    
     protected List<string> ParseInput()
     {
         var parts = Input[0].Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
