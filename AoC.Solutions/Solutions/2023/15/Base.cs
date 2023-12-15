18c7e5f0272650492b83073953a8fe7cd1a74fa7
Stevo <stevehuwjohn@gmail.com>
2023-12-15 11:49:05 +0000

    2023.15.1

--- AoC.Solutions/Solutions/2023/15/Base.cs
+++ AoC.Solutions/Solutions/2023/15/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2023._15;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Lens library";
 }
--- AoC.Solutions/Solutions/2023/15/Part1.cs
+++ AoC.Solutions/Solutions/2023/15/Part1.cs
@@ -7,6 +7,38 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var steps = ParseInput();
+
+        var sum = 0L;
+
+        foreach (var step in steps)
+        {
+            sum += Hash(step);
+        }
+        
+        return sum.ToString();
+    }
+
+    private static int Hash(string input)
+    {
+        var hash = 0;
+        
+        foreach (var c in input)
+        {
+            hash += (byte) c;
+
+            hash *= 17;
+
+            hash %= 256;
+        }
+
+        return hash;
+    }
+
+    private List<string> ParseInput()
+    {
+        var parts = Input[0].Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        return parts.ToList();
     }
 }
