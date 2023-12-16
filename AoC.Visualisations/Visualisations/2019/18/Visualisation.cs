2d226df53e68f85f8c6813f71b9b4abfbe3034bf
Stevo <stevehuwjohn@gmail.com>
2023-12-16 12:57:34 +0000

    WIP

--- AoC.Visualisations/Visualisations/2019/18/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2019/18/Visualisation.cs
@@ -18,6 +18,23 @@ public class Visualisation : VisualisationBase<PuzzleState>
 
     private PuzzleState _state;
 
+    private readonly Color[] _colors = 
+    {
+        Color.Blue,
+        Color.Red,
+        Color.Magenta,
+        Color.Green,
+        Color.Cyan,
+        Color.Yellow,
+        Color.White
+    };
+
+    private int _frame;
+
+    private int _color;
+
+    private readonly Willy[] _willys = new Willy[4];
+    
     public Visualisation()
     {
         GraphicsDeviceManager = new GraphicsDeviceManager(this)
@@ -33,7 +50,7 @@ public class Visualisation : VisualisationBase<PuzzleState>
     {
         switch (part)
         {
-            case 1:
+            case 2:
                 Puzzle = new Part2(this);
 
                 break;
@@ -70,6 +87,8 @@ public class Visualisation : VisualisationBase<PuzzleState>
 
     protected override void Draw(GameTime gameTime)
     {
+        _frame++;
+        
         GraphicsDevice.Clear(Color.Black);
 
         _spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
@@ -83,15 +102,41 @@ public class Visualisation : VisualisationBase<PuzzleState>
 
     private void DrawMap()
     {
+        if (_frame % 5 == 0)
+        {
+            _color++;
+
+            if (_color == _colors.Length)
+            {
+                _color = 0;
+            }
+        }
+
+        var keyColor = _colors[_color];
+
         for (var y = 0; y < _state.Map.GetLength(1); y++)
         {
             for (var x = 0; x < _state.Map.GetLength(0); x++)
             {
-                switch (_state.Map[x, y])
+                var tile = _state.Map[x, y];
+
+                if (tile == '#')
                 {
-                    case '#':
-                        _spriteBatch.Draw(_tiles, new Vector2(x * 8, y * 8), new Rectangle(0, 0, 8, 8), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
-                        break;
+                    _spriteBatch.Draw(_tiles, new Vector2(x * 8, y * 8), new Rectangle(0, 0, 8, 8), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+                    
+                    continue;
+                }
+
+                if (char.IsLetter(tile))
+                {
+                    if (tile == char.ToLower(tile))
+                    {
+                        _spriteBatch.Draw(_tiles, new Vector2(x * 8, y * 8), new Rectangle(16, 0, 8, 8), keyColor, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+                        
+                        continue;
+                    }
+
+                    _spriteBatch.Draw(_tiles, new Vector2(x * 8, y * 8), new Rectangle(8, 0, 8, 8), Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
                 }
             }
         }
