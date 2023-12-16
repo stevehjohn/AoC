219442efffb79ff0c76bee554043843a33205947
Stevo <stevehuwjohn@gmail.com>
2023-12-16 17:30:07 +0000

    WIP

--- AoC.Solutions/Solutions/2023/16/Part1.cs
+++ AoC.Solutions/Solutions/2023/16/Part1.cs
@@ -1,3 +1,4 @@
+using AoC.Solutions.Infrastructure;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2023._16;
@@ -5,10 +6,20 @@ namespace AoC.Solutions.Solutions._2023._16;
 [UsedImplicitly]
 public class Part1 : Base
 {
+    public Part1()
+    {
+    }
+
+    public Part1(IVisualiser<PuzzleState> visualiser) : base(visualiser)
+    {
+    }
+    
     public override string GetAnswer()
     {
         ParseInput();
 
+        Visualise();
+        
         SimulateBeams(-1, 0, 'E');
         
         return CountEnergised().ToString();
