b2e9209f24f6690a81e9fd0b0a84ac8679cf253d
Stevo <stevehuwjohn@gmail.com>
2023-12-16 12:57:34 +0000

    WIP

---
+++ AoC.Visualisations/Visualisations/2019/18/Willy.cs
@@ -0,0 +1,16 @@
+namespace AoC.Visualisations.Visualisations._2019._18;
+
+public class Willy
+{
+    public int X { get; set; }
+    
+    public int Y { get; set; }
+    
+    public int MapX { get; set; }
+    
+    public int MapY { get; set; }
+    
+    public int Frame { get; set; }
+    
+    public int Direction { get; set; }
+}
