ef33321e3ede8dfec0e99c68525344c1395fa2d3
Stevo <stevehuwjohn@gmail.com>
2023-12-16 11:18:17 +0000

    WIP

--- AoC.Solutions/Solutions/2023/16/Base.cs
+++ AoC.Solutions/Solutions/2023/16/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2023._16;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "The floor will be lava";
 }
