414ee117baaeabe748c12091e1dc78b9e35ff995
Stevo <stevehuwjohn@gmail.com>
2023-12-16 12:23:12 +0000

    WIP

---
+++ AoC.Visualisations/Visualisations/2019/18/Visualisation.cs
@@ -0,0 +1,87 @@
+﻿using AoC.Solutions.Solutions._2019._18;
+using AoC.Visualisations.Exceptions;
+using AoC.Visualisations.Infrastructure;
+using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace AoC.Visualisations.Visualisations._2019._18;
+
+[UsedImplicitly]
+public class Visualisation : VisualisationBase<PuzzleState>
+{
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _tiles;
+
+    private PuzzleState _state;
+
+    public Visualisation()
+    {
+        GraphicsDeviceManager = new GraphicsDeviceManager(this)
+                                 {
+                                     PreferredBackBufferWidth = 648,
+                                     PreferredBackBufferHeight = 656
+                                 };
+
+        Content.RootDirectory = "./17";
+    }
+
+    public override void SetPart(int part)
+    {
+        switch (part)
+        {
+            case 1:
+                Puzzle = new Part2(this);
+
+                break;
+            default:
+                throw new VisualisationParameterException();
+        }
+    }
+
+    protected override void Initialize()
+    {
+        IsMouseVisible = true;
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _tiles = Content.Load<Texture2D>("tiles");
+
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (HasNextState)
+        {
+        }
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(new Color(9, 10, 17));
+
+        _spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
+
+        DrawMap();
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+
+    private void DrawMap()
+    {
+
+    }
+}
