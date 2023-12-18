98c96cb19fc747568caf2ea438f7f0d7c7babe3c
Stevo <stevehuwjohn@gmail.com>
2023-12-18 10:11:29 +0000

    WIP

--- AoC.Solutions/Solutions/2023/18/Part2.cs
+++ AoC.Solutions/Solutions/2023/18/Part2.cs
@@ -7,6 +7,48 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
+        var points = ParseInput();
+        
         return "Unknown";
+    }
+
+    private List<(int X, int Y)> ParseInput()
+    {
+        int x = 0, y = 0;
+
+        var points = new List<(int X, int Y)> { (0, 0) };
+
+        foreach (var line in Input)
+        {
+            var parts = line.Split(' ');
+
+            var length = Convert.ToInt32(parts[2][2..7], 16);
+
+            var direction = parts[2][7];
+
+            switch (direction)
+            {
+                case '0':
+                    x += length;
+                    break;
+                
+                case '1':
+                    y += length;
+                    break;
+                
+                case '2':
+                    x -= length;
+                    break;
+                
+                case '3':
+                    y -= length;
+                    break;
+                
+            }
+            
+            points.Add((x, y));
+        }
+        
+        return points;
     }
 }
