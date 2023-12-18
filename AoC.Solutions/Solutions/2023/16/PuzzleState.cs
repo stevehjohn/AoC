36b45c3922ea8c731b219f02da5b70ef8a51b227
Stevo <stevehuwjohn@gmail.com>
2023-12-18 16:40:22 +0000

    WIP| @

--- AoC.Solutions/Solutions/2023/16/PuzzleState.cs
+++ AoC.Solutions/Solutions/2023/16/PuzzleState.cs
@@ -5,4 +5,10 @@ public class PuzzleState
     public char[,] Map { get; set; }
 
     public List<(int X, int Y, char Direction, int Id, int SourceId)> Beams { get; set; } = new();
+
+    public char StartDirection { get; set; }
+
+    public int LaserX { get; set; }
+    
+    public int LaserY { get; set; }
 }
