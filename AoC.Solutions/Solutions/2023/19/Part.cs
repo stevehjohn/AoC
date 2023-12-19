859c433187ff0be3829521e7faf7af14842f3248
Stevo <stevehuwjohn@gmail.com>
2023-12-19 10:41:02 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2023/19/Part.cs
@@ -0,0 +1,12 @@
+namespace AoC.Solutions.Solutions._2023._19;
+
+public class Part
+{
+    public int X { get; set; }
+    
+    public int M { get; set; }
+    
+    public int A { get; set; }
+
+    public int S { get; set; }
+}
