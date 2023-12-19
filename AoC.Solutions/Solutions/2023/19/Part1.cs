ed7b0ab207c94dd42da647b33b911d214bbd5e57
Stevo <stevehuwjohn@gmail.com>
2023-12-19 11:43:12 +0000

    WIP

--- AoC.Solutions/Solutions/2023/19/Part1.cs
+++ AoC.Solutions/Solutions/2023/19/Part1.cs
@@ -5,17 +5,13 @@ namespace AoC.Solutions.Solutions._2023._19;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private readonly Dictionary<string, List<Rule>> _workflows = new();
-
-    private readonly List<Part> _parts = new();
-    
     public override string GetAnswer()
     {
         ParseInput();
 
         var sum = 0;
         
-        foreach (var part in _parts)
+        foreach (var part in Parts)
         {
             sum += ProcessPart(part);
         }
@@ -29,7 +25,7 @@ public class Part1 : Base
 
         while (name is not ("A" or "R"))
         {
-            var workflow = _workflows[name];
+            var workflow = Workflows[name];
 
             foreach (var rule in workflow)
             {
@@ -69,68 +65,5 @@ public class Part1 : Base
         }
 
         return name == "A" ? part.X + part.M + part.A + part.S : 0;
-    }
-
-    private void ParseInput()
-    {
-        var l = 0;
-
-        while (! string.IsNullOrWhiteSpace(Input[l]))
-        {
-            var parts = Input[l][..^1].Split('{');
-
-            var workflow = parts[0];
-            
-            var rulesString = parts[1].Split(',');
-
-            var rulesList = new List<Rule>();
-
-            foreach (var item in rulesString)
-            {
-                if (! item.Contains(':'))
-                {
-                    rulesList.Add(new Rule { Destination = item});
-                    
-                    continue;
-                }
-
-                var rule = new Rule
-                {
-                    Property = item[0],
-                    Condition = item[1]
-                };
-                
-                var ruleParts = item.Split(':');
-
-                rule.Value = int.Parse(ruleParts[0][2..]);
-
-                rule.Destination = ruleParts[1];
-                
-                rulesList.Add(rule);
-            }
-            
-            _workflows.Add(workflow, rulesList);
-
-            l++;
-        }
-
-        l++;
-
-        while (l < Input.Length)
-        {
-            var parts = Input[l][1..^1].Split(',');
-
-            var part = new Part
-            {
-                X = int.Parse(parts[0][2..]),
-                M = int.Parse(parts[1][2..]),
-                A = int.Parse(parts[2][2..]),
-                S = int.Parse(parts[3][2..])
-            };
-            
-            _parts.Add(part);
-            
-            l++;
-        }
     }
 }
--- AoC.Solutions/Solutions/2023/19/Part2.cs
+++ AoC.Solutions/Solutions/2023/19/Part2.cs
@@ -7,6 +7,26 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        ParseInput(false);
+
+        var acceptWorkflows = Workflows.Where(w => w.Value.Any(r => r.Destination == "A"));
+
+        long sum = 0;
+        
+        foreach (var rule in acceptWorkflows)
+        {
+            var accepted = 0L;
+            
+            CheckRule(rule.Key, rule.Value, 'x');
+
+            sum += accepted;
+        }
+        
+        return sum.ToString();
+    }
+
+    private long CheckRule(string name, List<Rule> rules, char property)
+    {
+        return 0;
     }
 }
