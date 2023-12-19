0896b4fd7b1ef4017118f13ab00c16b432ddf4f0
Stevo <stevehuwjohn@gmail.com>
2023-12-19 10:41:02 +0000

    WIP

--- AoC.Solutions/Solutions/2023/19/Part1.cs
+++ AoC.Solutions/Solutions/2023/19/Part1.cs
@@ -5,8 +5,56 @@ namespace AoC.Solutions.Solutions._2023._19;
 [UsedImplicitly]
 public class Part1 : Base
 {
+    private readonly Dictionary<string, List<Rule>> _workflows = new();
+    
     public override string GetAnswer()
     {
+        ParseInput();
+        
         return "Unknown";
+    }
+
+    private void ParseInput()
+    {
+        var l = 0;
+
+        while (! string.IsNullOrWhiteSpace(Input[l]))
+        {
+            var parts = Input[l][..^1].Split('{');
+
+            var workflow = parts[0];
+            
+            var rulesString = parts[1].Split(',');
+
+            var rulesList = new List<Rule>();
+
+            foreach (var item in rulesString)
+            {
+                if (! item.Contains(':'))
+                {
+                    rulesList.Add(new Rule { Destination = item});
+                    
+                    continue;
+                }
+
+                var rule = new Rule
+                {
+                    Property = item[0],
+                    Condition = item[1]
+                };
+                
+                var ruleParts = item.Split(':');
+
+                rule.Value = int.Parse(ruleParts[0][2..]);
+
+                rule.Destination = ruleParts[1];
+                
+                rulesList.Add(rule);
+            }
+            
+            _workflows.Add(workflow, rulesList);
+
+            l++;
+        }
     }
 }
