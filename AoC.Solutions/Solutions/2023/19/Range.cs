f1c358f0c02afbbb3986ceeabcab9acdcd8ed2f6
Stevo <stevehuwjohn@gmail.com>
2023-12-19 11:57:31 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2023/19/Range.cs
@@ -0,0 +1,15 @@
+namespace AoC.Solutions.Solutions._2023._19;
+
+public class Range
+{
+    public int Start { get; set; }
+    
+    public int End { get; set; }
+
+    public Range(int start, int end)
+    {
+        Start = start;
+        
+        End = end;
+    }
+}
