3e5fe5fbaab12e545b799497ea2c8d54da8199f7
Stevo <stevehuwjohn@gmail.com>
2023-12-20 12:14:34 +0000

    WIP

--- AoC.Solutions/Solutions/2023/20/Base.cs
+++ AoC.Solutions/Solutions/2023/20/Base.cs
@@ -6,7 +6,91 @@ public abstract class Base : Solution
 {
     public override string Description => "Pulse propagation";
 
-    protected readonly Dictionary<string, Module> Modules = new(); 
+    private readonly Dictionary<string, Module> _modules = new(); 
+    
+    protected (int Lows, int Highs) SendPulses(bool part2 = false)
+    {
+        int lows = 0, highs = 0;
+        
+        var queue = new Queue<(string Source, bool Pulse, string Target)>();
+
+        queue.Enqueue(("button", false, "broadcaster"));
+
+        while (queue.TryDequeue(out var pulse))
+        {
+            if (pulse.Pulse)
+            {
+                highs++;
+            }
+            else
+            {
+                lows++;
+            }
+
+            if (! _modules.TryGetValue(pulse.Target, out var module))
+            {
+                continue;
+            }
+
+            switch (module.Type)
+            {
+                case Type.Broadcast:
+                    module.State = pulse.Pulse;
+                    break;
+
+                case Type.FlipFlop:
+                    if (! pulse.Pulse)
+                    {
+                        module.State = ! module.State;
+                    }
+                    break;
+
+                case Type.Conjunction:
+                    module.ReceivedPulses[pulse.Source] = pulse.Pulse;
+                    break;
+            }
+
+            foreach (var target in module.Targets)
+            {
+                if (pulse.Pulse && module.Type == Type.FlipFlop)
+                {
+                    continue;
+                }
+
+                var output = false;
+                
+                switch (module.Type)
+                {
+                    case Type.Broadcast:
+                        output = module.State;
+                        break;
+
+                    case Type.FlipFlop:
+                        if (! pulse.Pulse)
+                        {
+                            output = module.State;
+                        }
+                        break;
+
+                    case Type.Conjunction:
+                        output = ! module.ReceivedPulses.All(r => r.Value);
+                        break;
+                }
+                
+                if (part2)
+                {
+                    if (target == "rx" && ! output)
+                    {
+                        return (0, 0);
+                    }
+                }
+                
+                queue.Enqueue((pulse.Target, output, target));
+            }
+        }
+
+        return (lows, highs);
+    }
     
     protected void ParseInput()
     {
@@ -55,14 +139,14 @@ public abstract class Base : Solution
                 module.ReceivedPulses = new Dictionary<string, bool>();
             }
 
-            Modules.Add(name, module);
+            _modules.Add(name, module);
         }
 
-        foreach (var module in Modules)
+        foreach (var module in _modules)
         {
             foreach (var target in module.Value.Targets)
             {
-                if (Modules.TryGetValue(target, out var targetModule))
+                if (_modules.TryGetValue(target, out var targetModule))
                 {
                     if (targetModule.Type == Type.Conjunction)
                     {
