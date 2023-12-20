64edc6c42c2ddf710db84555765b0c04693df89b
Stevo <stevehuwjohn@gmail.com>
2023-12-20 21:38:46 +0000

    WIP

--- AoC.Visualisations/Visualisations/2023/17/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2023/17/Visualisation.cs
@@ -82,43 +82,48 @@ public class Visualisation : VisualisationBase<PuzzleState>
     {
         GraphicsDevice.Clear(Color.Black);
 
-        for (var i = 1; i < 10; i++)
+        if (HasNextState)
         {
-            if (HasNextState)
+            if (_state == null)
             {
-                if (_state == null)
-                {
-                    _state = GetNextState();
+                _state = GetNextState();
 
-                    InitialiseTerrain();
+                InitialiseTerrain();
+            }
+            else
+            {
+                for (var x = 0; x < _width; x++)
+                {
+                    for (var y = 0; y < _height; y++)
+                    {
+                        var height = _state.Map[x, y];
+
+                        _vertices[x + y * _width].Color = _palette[height];
+                    }
                 }
-                else
+
+                for (var i = 0; i < 10; i++)
                 {
                     _state = GetNextState();
 
                     if (_state.History != null)
                     {
-                        for (var x = 0; x < _width; x++)
-                        {
-                            for (var y = 0; y < _height; y++)
-                            {
-                                var height = _state.Map[x, y];
-
-                                _vertices[x + y * _width].Color = _palette[height];
-                            }
-                        }
-
                         foreach (var point in _state.History)
                         {
                             _vertices[point.X + point.Y * _width].Color = Color.AntiqueWhite;
                         }
                     }
+
+                    if (! HasNextState)
+                    {
+                        break;
+                    }
                 }
             }
-            else if (_state != null)
-            {
-                RenderFinalPath();
-            }
+        }
+        else if (_state != null)
+        {
+            RenderFinalPath();
         }
 
         if (_state != null)
