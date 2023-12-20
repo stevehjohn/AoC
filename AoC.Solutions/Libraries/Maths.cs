1ccd78f1ef5fdc9e6fd93e98fc5c72cf2a82826a
Stevo <stevehuwjohn@gmail.com>
2023-12-20 12:58:10 +0000

    WIP

--- AoC.Solutions/Libraries/Maths.cs
+++ AoC.Solutions/Libraries/Maths.cs
@@ -45,11 +45,6 @@ public static class Maths
         }
     }
 
-    public static long LowestCommonMultiple(long left, long right)
-    {
-        return left * right / GreatestCommonFactor(left, right);
-    }
-
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     private static long GreatestCommonFactor(long left, long right)
     {
