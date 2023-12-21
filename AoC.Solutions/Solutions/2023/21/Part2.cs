40134f095bbf7dc25fc67607126d7f75bbec3fe5
Stevo <stevehuwjohn@gmail.com>
2023-12-21 15:13:41 +0000

    WIP

--- AoC.Solutions/Solutions/2023/21/Part2.cs
+++ AoC.Solutions/Solutions/2023/21/Part2.cs
@@ -7,38 +7,67 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var start = ParseInput();
+
+        var result = Walk(start);
+        
+        return result.ToString();
     }
 
-    private int Walk((int X, int Y) start, int maxSteps)
+    private int Walk((int X, int Y) start)
     {
         var positions = new HashSet<(int X, int Y, int Ux, int Uy)>
         {
             (start.X, start.Y, 0, 0)
         };
 
-        for (var i = 0; i < maxSteps; i++)
+        var universes = new HashSet<(int Ux, int Uy)>();
+
+        var previousUniverses = 1;
+
+        var step = 1;
+        
+        while (true)
         {
             var newPositions = new HashSet<(int X, int Y, int Ux, int Uy)>();
 
             foreach (var position in positions)
             {
-                Move(newPositions, position, -1, 0);
+                Move(newPositions, position, -1, 0, universes);
             
-                Move(newPositions, position, 1, 0);
+                Move(newPositions, position, 1, 0, universes);
             
-                Move(newPositions, position, 0, -1);
+                Move(newPositions, position, 0, -1, universes);
             
-                Move(newPositions, position, 0, 1);
+                Move(newPositions, position, 0, 1, universes);
             }
             
             positions = newPositions;
+
+            if (universes.Count > previousUniverses)
+            {
+                Console.WriteLine($"{step}: {positions.Count}");
+                
+                previousUniverses = universes.Count;
+            }
+
+            if (universes.Count > 12)
+            {
+                break;
+            }
+
+            step++;
+        }
+
+        foreach (var position in positions.GroupBy(p => new { p.Ux, p.Uy }))
+        {
+            Console.WriteLine($"{position.Key.Ux}, {position.Key.Uy}: {position.Count()}");
         }
 
         return positions.Count;
     }
 
-    private void Move(HashSet<(int X, int Y, int Ux, int Uy)> positions, (int X, int Y, int Ux, int Uy) position, int dX, int dY)
+    private void Move(HashSet<(int X, int Y, int Ux, int Uy)> positions, (int X, int Y, int Ux, int Uy) position, int dX, int dY, HashSet<(int Ux, int Uy)> universes)
     {
         position = (position.X + dX, position.Y + dY, position.Ux, position.Uy);
 
@@ -47,7 +76,7 @@ public class Part2 : Base
             position.X = Width - 1;
             position.Ux--;
             
-            return;
+            universes.Add((position.Ux, position.Uy));
         }
 
         if (position.X == Width)
@@ -55,7 +84,7 @@ public class Part2 : Base
             position.X = 0;
             position.Ux++;
             
-            return;
+            universes.Add((position.Ux, position.Uy));
         }
 
         if (position.Y < 0)
@@ -63,7 +92,7 @@ public class Part2 : Base
             position.Y = Height - 1;
             position.Uy--;
             
-            return;
+            universes.Add((position.Ux, position.Uy));
         }
 
         if (position.Y == Height)
@@ -71,14 +100,14 @@ public class Part2 : Base
             position.Y = 0;
             position.Uy++;
             
-            return;
+            universes.Add((position.Ux, position.Uy));
         }
 
         if (Map[position.X, position.Y] == '#')
         {
             return;
         }
-        
+
         positions.Add(position);
     }
 }
