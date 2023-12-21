ab04c72d71bb68e8a39e6984d562e903ee92a72d
Stevo <stevehuwjohn@gmail.com>
2023-12-21 11:29:35 +0000

    WIP

--- AoC.Solutions/Solutions/2023/21/Base.cs
+++ AoC.Solutions/Solutions/2023/21/Base.cs
@@ -5,4 +5,41 @@ namespace AoC.Solutions.Solutions._2023._21;
 public abstract class Base : Solution
 {
     public override string Description => "Step counter";
+
+    protected char[,] Map;
+
+    protected int Width;
+
+    protected int Height;
+
+    protected (int X, int Y) ParseInput()
+    {
+        Width = Input[0].Length;
+
+        Height = Input.Length;
+        
+        Map = new char[Width, Height];
+
+        var y = 0;
+
+        (int X, int Y) start = (0, 0);
+        
+        foreach (var line in Input)
+        {
+            for (var x = 0; x < line.Length; x++)
+            {
+                Map[x, y] = line[x];
+
+                if (line[x] == 'S')
+                {
+                    start = (x, y);
+                    Map[x, y] = '.';
+                }
+            }
+
+            y++;
+        }
+
+        return start;
+    }
 }
--- AoC.Solutions/Solutions/2023/21/Part1.cs
+++ AoC.Solutions/Solutions/2023/21/Part1.cs
@@ -5,12 +5,6 @@ namespace AoC.Solutions.Solutions._2023._21;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private char[,] _map;
-
-    private int _width;
-
-    private int _height;
-
     public override string GetAnswer()
     {
         var start = ParseInput();
@@ -33,22 +27,22 @@ public class Part1 : Base
 
             foreach (var position in positions)
             {
-                if (position.X > 0 && _map[position.X - 1, position.Y] == '.' && ! newPositions.Contains((position.X - 1, position.Y)))
+                if (position.X > 0 && Map[position.X - 1, position.Y] == '.' && ! newPositions.Contains((position.X - 1, position.Y)))
                 {
                     newPositions.Add((position.X - 1, position.Y));
                 }
 
-                if (position.X < _width - 1 && _map[position.X + 1, position.Y] == '.' && ! newPositions.Contains((position.X + 1, position.Y)))
+                if (position.X < Width - 1 && Map[position.X + 1, position.Y] == '.' && ! newPositions.Contains((position.X + 1, position.Y)))
                 {
                     newPositions.Add((position.X + 1, position.Y));
                 }
 
-                if (position.Y > 0 && _map[position.X, position.Y - 1] == '.' && ! newPositions.Contains((position.X, position.Y - 1)))
+                if (position.Y > 0 && Map[position.X, position.Y - 1] == '.' && ! newPositions.Contains((position.X, position.Y - 1)))
                 {
                     newPositions.Add((position.X, position.Y - 1));
                 }
 
-                if (position.Y < _height - 1 && _map[position.X, position.Y + 1] == '.' && ! newPositions.Contains((position.X, position.Y + 1)))
+                if (position.Y < Height - 1 && Map[position.X, position.Y + 1] == '.' && ! newPositions.Contains((position.X, position.Y + 1)))
                 {
                     newPositions.Add((position.X, position.Y + 1));
                 }
@@ -58,36 +52,5 @@ public class Part1 : Base
         }
 
         return positions.Count;
-    }
-
-    private (int X, int Y) ParseInput()
-    {
-        _width = Input[0].Length;
-
-        _height = Input.Length;
-        
-        _map = new char[_width, _height];
-
-        var y = 0;
-
-        (int X, int Y) start = (0, 0);
-        
-        foreach (var line in Input)
-        {
-            for (var x = 0; x < line.Length; x++)
-            {
-                _map[x, y] = line[x];
-
-                if (line[x] == 'S')
-                {
-                    start = (x, y);
-                    _map[x, y] = '.';
-                }
-            }
-
-            y++;
-        }
-
-        return start;
     }
 }
