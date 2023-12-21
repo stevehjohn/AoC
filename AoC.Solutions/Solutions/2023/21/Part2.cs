2811540453e69cd8327e92121d4722296e35b595
Stevo <stevehuwjohn@gmail.com>
2023-12-21 14:22:32 +0000

    WIP

--- AoC.Solutions/Solutions/2023/21/Part2.cs
+++ AoC.Solutions/Solutions/2023/21/Part2.cs
@@ -7,15 +7,6 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        var start = ParseInput();
-
-        var result = Solve(start, 5000);
-        
-        return result.ToString();
-    }
-
-    private long Solve((int X, int Y) start, int maxSteps)
-    {
-        return 0;
+        return "Unknown";
     }
 }
