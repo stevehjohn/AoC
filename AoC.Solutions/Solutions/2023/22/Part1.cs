6f620dc7bfa6ce936c56b4a4d540742d871473d8
Stevo <stevehuwjohn@gmail.com>
2023-12-22 11:02:20 +0000

    WIP

--- AoC.Solutions/Solutions/2023/22/Part1.cs
+++ AoC.Solutions/Solutions/2023/22/Part1.cs
@@ -19,6 +19,39 @@ public class Part1 : Base
 
     private void SettleBricks()
     {
+        var moved = false;
+
+        do
+        {
+            foreach (var brick in _bricks)
+            {
+                if (brick.Start.Z == 1)
+                {
+                    continue;
+                }
+
+                if (! Resting(brick))
+                {
+                    brick.Start.Z--;
+
+                    brick.End.Z--;
+
+                    moved = true;
+                }
+            }
+        } while (moved);
+    }
+
+    private bool Resting((Point Start, Point End) brick)
+    {
+        foreach (var item in _bricks)
+        {
+            if (item.Start.Z == brick.Start.Z - 1 || item.End.Z == brick.Start.Z - 1)
+            {
+            }
+        }
+
+        return false;
     }
 
     private void ParseInput()
---
+++ AoC.Solutions/Solutions/2023/22/Type.cs
@@ -0,0 +1,8 @@
+namespace AoC.Solutions.Solutions._2023._22;
+
+public enum Type
+{
+    Vertical,
+    Horizontal,
+    Deep
+}
