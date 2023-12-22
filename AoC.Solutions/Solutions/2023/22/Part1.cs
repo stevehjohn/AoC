6d2e754043f740c89eede7951798dc059242cae4
Stevo <stevehuwjohn@gmail.com>
2023-12-22 10:51:40 +0000

    WIP

--- AoC.Solutions/Solutions/2023/22/Part1.cs
+++ AoC.Solutions/Solutions/2023/22/Part1.cs
@@ -1,3 +1,4 @@
+using AoC.Solutions.Common;
 using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2023._22;
@@ -5,8 +6,39 @@ namespace AoC.Solutions.Solutions._2023._22;
 [UsedImplicitly]
 public class Part1 : Base
 {
+    private List<(Point Start, Point End)> _bricks = new();
+    
     public override string GetAnswer()
     {
+        ParseInput();
+     
+        SettleBricks();
+        
         return "Unknown";
+    }
+
+    private void SettleBricks()
+    {
+    }
+
+    private void ParseInput()
+    {
+        foreach (var line in Input)
+        {
+            var parts = line.Split('~');
+
+            var start = Point.Parse(parts[0]);
+
+            var end = Point.Parse(parts[1]);
+
+            if (end.Z < start.Z)
+            {
+                (start, end) = (end, start);
+            }
+
+            _bricks.Add((start, end));
+        }
+
+        _bricks = _bricks.OrderBy(b => b.Start.Z).ToList();
     }
 }
