c9cbea3fc0cb119921817387d9dd21eceb382f77
Stevo <stevehuwjohn@gmail.com>
2023-12-23 13:17:52 +0000

    WIP

--- AoC.Solutions/Solutions/2023/23/Base.cs
+++ AoC.Solutions/Solutions/2023/23/Base.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using AoC.Solutions.Extensions;
 using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2023._23;
@@ -5,4 +7,142 @@ namespace AoC.Solutions.Solutions._2023._23;
 public abstract class Base : Solution
 {
     public override string Description => "A long walk";
+    
+    private char[,] _map;
+
+    private int _width;
+
+    private int _height;
+
+    private int _pathTiles;
+    
+    private static readonly (int, int) North = (0, -1);
+    
+    private static readonly (int, int) East = (1, 0);
+    
+    private static readonly (int, int) South = (0, 1);
+    
+    private static readonly (int, int) West = (-1, 0);
+    
+    protected int Solve(bool isPart2 = false)
+    {
+        var queue = new Queue<(int X, int Y, (int Dx, int Dy) Direction, int Steps, HashSet<(int X, int Y)> Visited)>();
+        
+        queue.Enqueue((1, 0, South, 0, new HashSet<(int X, int Y)>()));
+
+        var stepCounts = new List<int>();
+
+        var sw = Stopwatch.StartNew();
+
+        var prev = 0;
+        
+        while (queue.TryDequeue(out var position))
+        {
+            if (position.Steps > _pathTiles)
+            {
+                continue;
+            }
+
+            if (position.X == _width - 2 && position.Y == _height - 1)
+            {
+                stepCounts.Add(position.Steps);
+
+                if (position.Steps != prev)
+                {
+                    Console.WriteLine($"{position.Steps}: {sw.Elapsed}");
+                    
+                    sw.Restart();
+
+                    prev = position.Steps;
+                }
+
+                continue;
+            }
+
+            var tile = _map[position.X, position.Y];
+
+            if (! isPart2)
+            {
+                if (tile == '>')
+                {
+                    AddNewPosition(queue, position, position.Direction, East);
+
+                    continue;
+                }
+
+                if (tile == 'v')
+                {
+                    AddNewPosition(queue, position, position.Direction, South);
+
+                    continue;
+                }
+            }
+
+            if (position.Direction != North)
+            {
+                AddNewPosition(queue, position, position.Direction, South);
+            }
+
+            if (position.Direction != East)
+            {
+                AddNewPosition(queue, position, position.Direction, West);
+            }
+
+            if (position.Direction != South)
+            {
+                AddNewPosition(queue, position, position.Direction, North);
+            }
+
+            if (position.Direction != West)
+            {
+                AddNewPosition(queue, position, position.Direction, East);
+            }
+        }
+        
+        return stepCounts.Max();
+    }
+
+    private void AddNewPosition(
+        Queue<(int X, int Y, (int Dx, int Dy) Direction, int Steps, HashSet<(int X, int Y)>)> queue,
+        (int X, int Y, (int Dx, int Dy) Direction, int Steps, HashSet<(int X, int Y)> Visited) position, 
+        (int Dx, int Dy) direction,
+        (int Dx, int Dy) newDirection)
+    {
+        if (_map[position.X + newDirection.Dx, position.Y + newDirection.Dy] != '#')
+        {
+            if (position.Visited.Add((position.X + newDirection.Dx, position.Y + newDirection.Dy)))
+            {
+                if (direction != newDirection)
+                {
+                    queue.Enqueue((position.X + newDirection.Dx, position.Y + newDirection.Dy, newDirection, position.Steps + 1,
+                        new HashSet<(int X, int Y)>(position.Visited)));
+                }
+                else
+                {
+                    queue.Enqueue((position.X + newDirection.Dx, position.Y + newDirection.Dy, newDirection, position.Steps + 1,
+                        position.Visited));
+                }
+            }
+        }
+    }
+
+    protected void ParseInput()
+    {
+        _map = Input.To2DArray();
+
+        _width = _map.GetLength(0);
+
+        _height = _map.GetLength(1);
+
+        for (var y = 0; y < _height; y++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                if (_map[x, y] != '#')
+                {
+                    _pathTiles++;
+                }
+            }
+        }
+    }
 }
