cd3e9adb71464f47767214f7eaf3a888315f725c
Stevo <stevehuwjohn@gmail.com>
2023-12-23 11:55:18 +0000

    WIP

--- AoC.Solutions/Solutions/2023/23/Part2.cs
+++ AoC.Solutions/Solutions/2023/23/Part2.cs
@@ -7,6 +7,23 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        ParseInput();
+        
+        var result = Solve();
+        
+        return result.ToString();
+    }
+
+    private void Dump()
+    {
+        for (var x = 1; x < Width - 1; x++)
+        {
+            for (var y = 1; y < Height - 1; y++)
+            {
+                Console.Write(Map[x, y]);
+            }
+            
+            Console.WriteLine();
+        }
     }
 }
