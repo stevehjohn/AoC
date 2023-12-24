0cb27d3a45682cd83f591dec89cafae8da75851a
Stevo <stevehuwjohn@gmail.com>
2023-12-24 14:06:29 +0000

    WIP

--- AoC.Solutions/Solutions/2023/24/Part2.cs
+++ AoC.Solutions/Solutions/2023/24/Part2.cs
@@ -8,71 +8,33 @@ public class Part2 : Base
     public override string GetAnswer()
     {
         ParseInput();
-     
-        FindVelocityForCollision();
         
-        return "Unknown";
-    }
-
-    private void FindVelocityForCollision()
-    {
-        const int area = 1_000;
-        
-        for (var x = 0; x < area + 1; x++)
+        for (var left = 0; left < Hail.Count - 1; left++)
         {
-            for (var y = 0; y < area + 1; y++)
+            for (var right = left + 1; right < Hail.Count; right++)
             {
-                var intersection = IntersectionOfHail2D(x, y);
-                
-                if (intersection != null)
+                if (CheckIfParallel(Hail[left], Hail[right]))
                 {
-                    var z = Hail[0].Position.Z + intersection.Value.Time * 2 * Hail[0].Velocity.Z;
-                    
-                    Console.WriteLine($"v: {x}, {y}    p: {intersection.Value.X}, {intersection.Value.Y}, {z}    t: {intersection.Value.Time}");
-
-                    var leftHail = Hail[0];
-
-                    for (var i = 1; i < Hail.Count; i++)
-                    {
-                        var rightHail = Hail[i];
-                    }
-
-                    return;
+                    Console.WriteLine("Parallel");
                 }
             }
         }
+
+        return "Unknown";
     }
 
-    private (double X, double Y, long Time)? IntersectionOfHail2D(int rockVelocityX, int rockVelocityY)
+    private static bool CheckIfParallel((DoublePoint Position, DoublePoint Velocity) left, (DoublePoint Position, DoublePoint Velocity) right)
     {
-        (double X, double Y, long Time)? commonCollision = null;
+        var a1 = left.Velocity.Y / left.Velocity.X;
+        var b1 = left.Position.Y - a1 * left.Position.X;
+        var a2 = right.Velocity.Y / right.Velocity.X;
+        var b2 = right.Position.Y - a2 * right.Position.X;
 
-        for (var right = 1; right < Hail.Count; right++)
+        if (EqualsWithinTolerance(a1, a2))
         {
-            var leftHail = (Hail[0].Position, new DoublePoint(Hail[0].Velocity.X + rockVelocityX, Hail[0].Velocity.Y + rockVelocityY));
-            
-            var rightHail = (Hail[right].Position, new DoublePoint(Hail[right].Velocity.X + rockVelocityX, Hail[right].Velocity.Y + rockVelocityY));
-
-            var collision = CollidesInFuture(leftHail, rightHail);
-
-            if (collision == null)
-            {
-                return null;
-            }
-
-            if (commonCollision == null)
-            {
-                commonCollision = collision;
-                
-                continue;
-            }
-
-            if (commonCollision != collision)
-            {
-                return null;
-            }
+            return ! EqualsWithinTolerance(b1, b2);
         }
 
-        return commonCollision;
+        return false;
     }
 }
