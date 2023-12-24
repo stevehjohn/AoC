1e555874e9b343daef449e0f5a4d32667e2cb5c7
Stevo <stevehuwjohn@gmail.com>
2023-12-24 23:38:31 +0000

    WIP

--- AoC.Solutions/Solutions/2023/24/Part2.cs
+++ AoC.Solutions/Solutions/2023/24/Part2.cs
@@ -7,6 +7,23 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
+        ParseInput();
+
+        var nonIntersecting = FindNonIntersectingStones();
+
+        var h1 = nonIntersecting[0];
+
+        var h2 = nonIntersecting[1];
+        
+        var y = h1.Position.Y;
+
+        var vY = h1.Velocity.Y;
+
         return "Unknown";
+    }
+    
+    private List<(DoublePoint Position, DoublePoint Velocity)> FindNonIntersectingStones()
+    {
+        return Hail.GroupBy(h => new { pY = h.Position.Y, vY = h.Velocity.Y }).Single(g => g.Count() > 1).ToList();
     }
 }
