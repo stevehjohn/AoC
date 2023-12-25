7d912598104c0f9c4e3df2cb7a659ba7fcf72298
Stevo <stevehuwjohn@gmail.com>
2023-12-25 13:20:23 +0000

    WIP

--- AoC.Solutions/Solutions/2023/25/Part1.cs
+++ AoC.Solutions/Solutions/2023/25/Part1.cs
@@ -6,17 +6,42 @@ namespace AoC.Solutions.Solutions._2023._25;
 public class Part1 : Base
 {
     private readonly List<(string L, string R)> _nodes = new();
+
+    private readonly HashSet<string> _distinct = new();
     
     public override string GetAnswer()
     {
         ParseInput();
-        
-        Console.WriteLine(Walk());
+
+        Solve();
         
         return "Unknown";
     }
 
-    private int Walk()
+    private int Solve()
+    {
+        for (var i = 0; i < _nodes.Count; i++)
+        {
+            for (var j = i + 1; j < _nodes.Count; j++)
+            {
+                for (var k = j + 1; k < _nodes.Count; k++)
+                {
+                    var count = Walk(new List<(string L, string R)> { _nodes[i], _nodes[j], _nodes[k] }); 
+                    
+                    if (count < _distinct.Count)
+                    {
+                        Console.WriteLine($"YO: {_nodes[i]}, {_nodes[j]}, {_nodes[k]}");
+
+                        return count;
+                    }
+                }
+            }
+        }
+
+        return _distinct.Count;
+    }
+
+    private int Walk(List<(string L, string R)> ignore)
     {
         var queue = new Queue<string>();
     
@@ -33,6 +58,11 @@ public class Part1 : Base
 
             foreach (var connection in _nodes.Where(n => n.L == node).ToList())
             {
+                if (ignore.Contains((node, connection.R)))
+                {
+                    continue;
+                }
+                
                 queue.Enqueue(connection.R);
             }
         }
@@ -46,6 +76,8 @@ public class Part1 : Base
         {
             var parts = line.Split(':', StringSplitOptions.TrimEntries);
 
+            _distinct.Add(parts[0]);
+            
             var connections = parts[1].Split(' ', StringSplitOptions.TrimEntries);
 
             foreach (var connection in connections)
@@ -53,6 +85,8 @@ public class Part1 : Base
                 _nodes.Add((parts[0], connection));
 
                 _nodes.Add((connection, parts[0]));
+
+                _distinct.Add(connection);
             }
         }
     }
