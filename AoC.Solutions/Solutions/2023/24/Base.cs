76f5dc05e182249b1187580cd87b5ba71207035f
Stevo <stevehuwjohn@gmail.com>
2023-12-25 18:58:19 +0000

    WIP

--- AoC.Solutions/Solutions/2023/24/Base.cs
+++ AoC.Solutions/Solutions/2023/24/Base.cs
@@ -5,16 +5,4 @@ namespace AoC.Solutions.Solutions._2023._24;
 public abstract class Base : Solution
 {
     public override string Description => "Never tell me the odds";
-
-    protected readonly List<(DoublePoint Position, DoublePoint Velocity)> Hail = new();
-
-    protected void ParseInput()
-    {
-        foreach (var line in Input)
-        {
-            var parts = line.Split('@', StringSplitOptions.TrimEntries);
-
-            Hail.Add((DoublePoint.Parse(parts[0]), DoublePoint.Parse(parts[1])));
-        }
-    }
 }
