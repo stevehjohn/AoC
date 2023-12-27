e8471a1d842e25bfa9decafe2db8d1684e942f9b
Stevo <stevehuwjohn@gmail.com>
2023-12-27 14:21:29 +0000

    WIP

--- AoC.Solutions/Solutions/2023/23/Base.cs
+++ AoC.Solutions/Solutions/2023/23/Base.cs
@@ -7,18 +7,164 @@ public abstract class Base : Solution
 {
     public override string Description => "A long walk";
 
-    protected char[,] Map;
+    protected readonly List<(int X, int Y)> Intersections = new();
 
-    protected int Width;
+    protected readonly List<int> Counts = new();
 
-    protected int Height;
+    private char[,] _map;
+
+    private int _width;
+
+    private int _height;
+
+    private readonly Dictionary<int, ((int X, int Y) Start, (int X, int Y) End, int Steps)> _edges = new();
+
+    private readonly Dictionary<(int X, int Y), List<int>> _startIndexes = new();
+    
+    private readonly HashSet<(int, int)> _history = new();
+
+    protected void FindLongestPath((int X, int Y) position, int steps)
+    {
+        if (position == Intersections[^1])
+        {
+            Counts.Add(steps);
+            
+            return;
+        }
+
+        var reaches = _startIndexes[position];
+
+        foreach (var id in reaches)
+        {
+            var item = _edges[id];
+            
+            if (_history.Add(item.End))
+            {
+                FindLongestPath(item.End, steps + item.Steps);
+
+                _history.Remove(item.End);
+            }
+        }
+    }
+
+    protected void CreateEdges(bool isPart2 = false)
+    {
+        FindIntersections();
+
+        var id = 0;
+        
+        foreach (var intersection in Intersections)
+        {
+            foreach (var other in Intersections)
+            {
+                if (other == intersection)
+                {
+                    continue;
+                }
+        
+                var distance = CanReach(intersection, other, isPart2);
+        
+                if (distance > 0)
+                {
+                    _edges.Add(id, (intersection, other, distance));
+
+                    if (! _startIndexes.ContainsKey(intersection))
+                    {
+                        _startIndexes[intersection] = new List<int> { id };
+                    }
+                    else
+                    {
+                        _startIndexes[intersection].Add(id);
+                    }
+
+                    id++;
+                }
+            }
+        }
+    }
+
+    private int CanReach((int X, int Y) start, (int X, int Y) end, bool isPart2)
+    {
+        var queue = new Queue<((int X, int Y) Position, int Steps)>();
+        
+        queue.Enqueue((start, 0));
+
+        var visited = new HashSet<(int, int)>();
+
+        while (queue.TryDequeue(out var node))
+        {
+            var (position, steps) = node;
+            
+            if (! visited.Add(position))
+            {
+                continue;
+            }
+            
+            if (position == end)
+            {
+                return steps;
+            }
+
+            if (position != start && Intersections.Contains(position))
+            {
+                continue;
+            }
+
+            if (position.X > 0 && _map[position.X - 1, position.Y] != '#' && (_map[position.X - 1, position.Y] != '>' || isPart2))
+            {
+                queue.Enqueue(((position.X - 1, position.Y), steps + 1));
+            }
+
+            if (position.X < _width - 1 && _map[position.X + 1, position.Y] != '#')
+            {
+                queue.Enqueue(((position.X + 1, position.Y), steps + 1));
+            }
+
+            if (position.Y > 0 && _map[position.X, position.Y - 1] != '#' && (_map[position.X, position.Y - 1] != 'v' || isPart2))
+            {
+                queue.Enqueue(((position.X, position.Y - 1), steps + 1));
+            }
+
+            if (position.Y < _height - 1 && _map[position.X, position.Y + 1] != '#')
+            {
+                queue.Enqueue(((position.X, position.Y + 1), steps + 1));
+            }
+        }
+
+        return 0;
+    }
+
+    private void FindIntersections()
+    {
+        Intersections.Add((1, 0));
+        
+        for (var x = 1; x < _width - 1; x++)
+        {
+            for (var y = 1; y < _height - 1; y++)
+            {
+                var count = 0;
+
+                count += _map[x - 1, y] == '#' ? 1 : 0;
+                count += _map[x + 1, y] == '#' ? 1 : 0;
+                count += _map[x, y - 1] == '#' ? 1 : 0;
+                count += _map[x, y + 1] == '#' ? 1 : 0;
+
+                if (count < 2 && _map[x, y] != '#')
+                {
+                    Intersections.Add((x, y));
+                }
+            }
+        }
+        
+        Intersections.Add((_width - 2, _height - 1));
+    }
     
     protected void ParseInput()
     {
-        Map = Input.To2DArray();
+        _map = Input.To2DArray();
 
-        Width = Map.GetLength(0);
+        _width = _map.GetLength(0);
 
-        Height = Map.GetLength(1);
+        _height = _map.GetLength(1);
     }
 }
--- AoC.Solutions/Solutions/2023/23/Part1.cs
+++ AoC.Solutions/Solutions/2023/23/Part1.cs
@@ -5,92 +5,14 @@ namespace AoC.Solutions.Solutions._2023._23;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private static readonly (int, int) North = (0, -1);
-
-    private static readonly (int, int) East = (1, 0);
-
-    private static readonly (int, int) South = (0, 1);
-
-    private static readonly (int, int) West = (-1, 0);
-
     public override string GetAnswer()
     {
         ParseInput();
-
-        var result = Solve();
         
-        return result.ToString();
-    }
-    
-    private int Solve()
-    {
-        var queue = new Queue<(int X, int Y, (int Dx, int Dy) Direction, int Steps, HashSet<(int X, int Y)> Visited)>();
+        CreateEdges();
+
+        FindLongestPath(Intersections[0], 0);
         
-        queue.Enqueue((1, 0, South, 0, new HashSet<(int X, int Y)>()));
-
-        var stepCounts = new List<int>();
-        
-        while (queue.TryDequeue(out var position))
-        {
-            if (position.X == Width - 2 && position.Y == Height - 1)
-            {
-                stepCounts.Add(position.Steps);
-
-                continue;
-            }
-
-            var tile = Map[position.X, position.Y];
-            
-            if (tile == '>')
-            {
-                AddNewPosition(queue, position, East);
-            
-                continue;
-            }
-            
-            if (tile == 'v')
-            {
-                AddNewPosition(queue, position, South);
-            
-                continue;
-            }
-
-            if (position.Direction != North)
-            {
-                AddNewPosition(queue, position, South);
-            }
-
-            if (position.Direction != East)
-            {
-                AddNewPosition(queue, position, West);
-            }
-
-            if (position.Direction != South)
-            {
-                AddNewPosition(queue, position, North);
-            }
-
-            if (position.Direction != West)
-            {
-                AddNewPosition(queue, position, East);
-            }
-        }
-        
-        return stepCounts.Max();
-    }
-
-    private void AddNewPosition(
-        Queue<(int X, int Y, (int Dx, int Dy) Direction, int Steps, HashSet<(int X, int Y)>)> queue,
-        (int X, int Y, (int Dx, int Dy) Direction, int Steps, HashSet<(int X, int Y)> Visited) position, 
-        (int Dx, int Dy) direction)
-    {
-        if (Map[position.X + direction.Dx, position.Y + direction.Dy] != '#')
-        {
-            if (position.Visited.Add((position.X + direction.Dx, position.Y + direction.Dy)))
-            {
-                queue.Enqueue((position.X + direction.Dx, position.Y + direction.Dy, direction, position.Steps + 1,
-                    new HashSet<(int X, int Y)>(position.Visited)));
-            }
-        }
+        return Counts.Max().ToString();
     }
 }
