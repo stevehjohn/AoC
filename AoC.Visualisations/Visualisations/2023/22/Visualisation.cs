40d8239ffb807c6ca87fbe32f030866955fb0881
Stevo <stevehuwjohn@gmail.com>
2023-12-30 16:52:11 +0000

    WIP

--- AoC.Visualisations/Visualisations/2023/22/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2023/22/Visualisation.cs
@@ -1,6 +1,29 @@
+using AoC.Solutions.Solutions._2023._22;
+using AoC.Visualisations.Exceptions;
+using AoC.Visualisations.Infrastructure;
+using JetBrains.Annotations;
+
 namespace AoC.Visualisations.Visualisations._2023._22;
 
-public class Visualisation
+[UsedImplicitly]
+public class Visualisation : VisualisationBase<PuzzleState>
 {
-    
+    public override void SetPart(int part)
+    {
+        switch (part)
+        {
+            case 1:
+                Puzzle = new Part1(this);
+
+                break;
+
+            case 2:
+                Puzzle = new Part2(this);
+
+                break;
+
+            default:
+                throw new VisualisationParameterException();
+        }
+    }
 }
