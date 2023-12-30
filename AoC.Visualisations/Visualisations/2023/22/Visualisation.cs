67485de456ce06f1a0a3b027f441b49464097035
Stevo <stevehuwjohn@gmail.com>
2023-12-30 17:00:25 +0000

    WIP

--- AoC.Visualisations/Visualisations/2023/22/Visualisation.cs
+++ AoC.Visualisations/Visualisations/2023/22/Visualisation.cs
@@ -2,12 +2,24 @@ using AoC.Solutions.Solutions._2023._22;
 using AoC.Visualisations.Exceptions;
 using AoC.Visualisations.Infrastructure;
 using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
 
 namespace AoC.Visualisations.Visualisations._2023._22;
 
 [UsedImplicitly]
 public class Visualisation : VisualisationBase<PuzzleState>
 {
+    public Visualisation()
+    {
+        GraphicsDeviceManager = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = 100,
+            PreferredBackBufferHeight = 980
+        };
+
+        Content.RootDirectory = "./22";
+    }
+
     public override void SetPart(int part)
     {
         switch (part)
