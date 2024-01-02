4b7377edf04871916bb81a13079ded1b2d43f8b9
Stevo John <stevehuwjohn@gmail.com>
2024-01-02 13:36:01 +0000

    Tidy.

--- AoC.Solutions/Solutions/2023/22/Part1.cs
+++ AoC.Solutions/Solutions/2023/22/Part1.cs
@@ -32,50 +32,50 @@ public class Part1 : Base
         Parallel.For(1, Count + 1,
             () => 0,
             (id, _, c) =>
-        {
-            var copy = new int[MaxHeight, 10, 10];
-
-            Array.Copy(Map, copy, MaxHeight * 100);
-
-            var removed = false;
-
-            for (var z = 1; z < HighestZ; z++)
             {
-                for (var x = 0; x < 10; x++)
-                {
-                    for (var y = 0; y < 10; y++)
-                    {
-                        // ReSharper disable once AccessToModifiedClosure
-                        if (copy[z, x, y] == id)
-                        {
-                            for (var zD = 0; zD < 5; zD++)
-                            {
-                                if (copy[z + zD, x, y] == id)
-                                {
-                                    copy[z + zD, x, y] = -1;
-                                }
-                            }
+                var copy = new int[MaxHeight, 10, 10];
 
-                            removed = true;
+                Array.Copy(Map, copy, MaxHeight * 100);
+
+                var removed = false;
+
+                for (var z = 1; z < HighestZ; z++)
+                {
+                    for (var x = 0; x < 10; x++)
+                    {
+                        for (var y = 0; y < 10; y++)
+                        {
+                            // ReSharper disable once AccessToModifiedClosure
+                            if (copy[z, x, y] == id)
+                            {
+                                for (var zD = 0; zD < 5; zD++)
+                                {
+                                    if (copy[z + zD, x, y] == id)
+                                    {
+                                        copy[z + zD, x, y] = -1;
+                                    }
+                                }
+
+                                removed = true;
+                            }
                         }
+                    }
+
+                    if (removed)
+                    {
+                        break;
                     }
                 }
 
-                if (removed)
+                var count = SettleBricks(copy, false);
+
+                if (count == 0)
                 {
-                    break;
+                    Visualise(false, id);
                 }
-            }
 
-            var count = SettleBricks(copy, false);
-
-            if (count == 0)
-            {
-                Visualise(false, id);
-            }
-
-            return c + (1 - count);
-        }, c => Interlocked.Add(ref result, c));
+                return c + (1 - count);
+            }, c => Interlocked.Add(ref result, c));
 
         return result;
     }
