13027b08a25b740469d44e43c378abb8a165201a
Stevo John <stevehuwjohn@gmail.com>
2024-01-02 15:47:30 +0000

    WIP

--- AoC.Solutions/Program.cs
+++ AoC.Solutions/Program.cs
@@ -2,6 +2,7 @@
 using AoC.Solutions.Infrastructure;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Reflection;
 
 namespace AoC.Solutions;
@@ -12,10 +13,10 @@ public static class Program
     public static void Main(string[] arguments)
     {
         var solutions = Assembly.GetExecutingAssembly()
-                                .GetTypes()
-                                .Where(t => t.IsSubclassOf(typeof(Solution)) && ! t.IsAbstract)
-                                .OrderBy(t => t.Namespace)
-                                .ThenBy(t => t.Name);
+            .GetTypes()
+            .Where(t => t.IsSubclassOf(typeof(Solution)) && !t.IsAbstract)
+            .OrderBy(t => t.Namespace)
+            .ThenBy(t => t.Name);
 
         var yearMs = 0L;
 
@@ -26,7 +27,7 @@ public static class Program
         string[] answers = null;
 
         var results = new Dictionary<(int Yeat, int Day, int Part), (double Microseconds, string Summary)>();
-        
+
         try
         {
             answers = File.ReadAllLines($"Solutions{Path.DirectorySeparatorChar}AllAnswers.txt");
@@ -56,7 +57,8 @@ public static class Program
 
             if (arguments.Length > 0 && arguments[0].ToLower() != "true")
             {
-                var solutionKey = $"{int.Parse(solution.Namespace?.Split('.')[3].Replace("_", string.Empty) ?? "0")}.{int.Parse(solution.Namespace?.Split('.')[4].Replace("_", string.Empty) ?? "0"):D2}.{solution.Name[4]}";
+                var solutionKey =
+                    $"{int.Parse(solution.Namespace?.Split('.')[3].Replace("_", string.Empty) ?? "0")}.{int.Parse(solution.Namespace?.Split('.')[4].Replace("_", string.Empty) ?? "0"):D2}.{solution.Name[4]}";
 
                 if (solutionKey.Substring(0, arguments[0].Length) != arguments[0])
                 {
@@ -72,21 +74,21 @@ public static class Program
             }
 
             var firstTime = double.MaxValue;
-            
+
             var stopwatch = new Stopwatch();
 
             var answer = string.Empty;
-            
+
             if (arguments.Length != 1 || (arguments.Length == 1 && arguments[0].Length < 5))
             {
                 stopwatch.Start();
-                
+
                 answer = instance.GetAnswer();
-                
+
                 stopwatch.Stop();
 
                 firstTime = stopwatch.Elapsed.TotalMicroseconds;
-                
+
                 instance = Activator.CreateInstance(solution) as Solution;
 
                 if (instance == null)
@@ -101,7 +103,7 @@ public static class Program
             if (firstTime == double.MaxValue || firstTime < TimeSpan.FromSeconds(10).TotalMicroseconds)
             {
                 stopwatch.Reset();
-                
+
                 stopwatch.Start();
 
                 answer = instance.GetAnswer();
@@ -123,21 +125,21 @@ public static class Program
             else if (year != previousYear)
             {
                 CleanUp();
-                
+
                 WriteYearSummary();
 
                 previousYear = year;
 
                 yearMs = 0;
-                
+
                 GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive, true, true);
             }
-            
+
             CheckAnswer(instance.GetType(), answer);
 
             var displayAnswer = answer.Length < 26
-                                    ? answer
-                                    : $"{answer[..26]}...";
+                ? answer
+                : $"{answer[..26]}...";
 
             var description = string.Empty;
 
@@ -151,18 +153,18 @@ public static class Program
             var microseconds = Math.Min(stopwatch.Elapsed.TotalMicroseconds, firstTime);
 
             var part = solution.Name[4] - '0';
-            
+
             var summary = $" {year} {day,2}.{part}: {displayAnswer,-30} {$"{microseconds:N0}μs",-12}  {description}";
-            
+
             results.Add((year, day, part), (microseconds, summary));
-            
+
             Console.WriteLine(summary);
 
             yearMs += (long) microseconds;
         }
 
         CleanUp();
-        
+
         WriteYearSummary();
 
         if (arguments.Length > 0)
@@ -172,17 +174,57 @@ public static class Program
                 UpdateResults();
             }
         }
-        
+
         return;
 
         void UpdateResults()
         {
-            if (!File.Exists("../results.md"))
+            const string resultsFileName = "./results.md";
+
+            if (!File.Exists(resultsFileName))
             {
-                Console.WriteLine("results.md not found, will not update.\n");
+                Console.WriteLine(" results.md not found, will not update.\n");
             }
+
+            var file = File.ReadAllLines(resultsFileName).ToList();
+
+            var updated = false;
             
-            
+            foreach (var result in results)
+            {
+                var (year, day, part) = result.Key;
+
+                var index = file.FindIndex(l => l.StartsWith($" {year} {day,2}.{part}:"));
+
+                if (index < 0)
+                {
+                    Console.WriteLine($" {year} {day,2}.{part} not found.");
+                    
+                    // TODO: Add to end?
+                    
+                    continue;
+                }
+                
+                var line = file[index];
+
+                var time = int.Parse(line[43..].Split(' ', StringSplitOptions.TrimEntries)[0][..^2], NumberStyles.AllowThousands);
+
+                if (result.Value.Microseconds < time)
+                {
+                    file[index] = result.Value.Summary;
+
+                    updated = true;
+                }
+            }
+
+            if (!updated)
+            {
+                Console.WriteLine(" No times were quicker, will not update results.md.\n");
+            }
+            else
+            {
+                File.WriteAllLines(resultsFileName, file);
+            }
         }
 
         void CheckAnswer(Type solution, string answer)
@@ -200,7 +242,8 @@ public static class Program
                 return;
             }
 
-            var key = $"{int.Parse(solution.Namespace?.Split('.')[3].Replace("_", string.Empty) ?? "0")}.{int.Parse(solution.Namespace?.Split('.')[4].Replace("_", string.Empty) ?? "0")}.{solution.Name[4]}";
+            var key =
+                $"{int.Parse(solution.Namespace?.Split('.')[3].Replace("_", string.Empty) ?? "0")}.{int.Parse(solution.Namespace?.Split('.')[4].Replace("_", string.Empty) ?? "0")}.{solution.Name[4]}";
 
             var correctAnswerLine = answers.FirstOrDefault(a => a.StartsWith(key));
 
@@ -233,7 +276,7 @@ public static class Program
                 }
 
                 Console.WriteLine(".");
-                
+
                 Console.ForegroundColor = temp;
             }
         }
@@ -244,11 +287,11 @@ public static class Program
 
             if (yearMs < 1_000_000)
             {
-                Console.WriteLine($"{new string(' ', 43)}{$"{yearMs / 1_000d:N3}ms", -13}");
+                Console.WriteLine($"{new string(' ', 43)}{$"{yearMs / 1_000d:N3}ms",-13}");
             }
             else
             {
-                Console.WriteLine($"{new string(' ', 43)}{$"{yearMs / 1_000_000d:N3}s", -13}");
+                Console.WriteLine($"{new string(' ', 43)}{$"{yearMs / 1_000_000d:N3}s",-13}");
             }
 
             Console.WriteLine();
