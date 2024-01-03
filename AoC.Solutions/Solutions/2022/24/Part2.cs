c73e9e7cbdcd8afca953bbc86f06ff25bdfe1a57
Stevo <stevehuwjohn@gmail.com>
2024-01-03 21:21:37 +0000

    WIP

--- AoC.Solutions/Solutions/2022/24/Part2.cs
+++ AoC.Solutions/Solutions/2022/24/Part2.cs
@@ -1,19 +1,10 @@
-﻿using AoC.Solutions.Infrastructure;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2022._24;
 
 [UsedImplicitly]
 public class Part2 : Base
 {
-    public Part2()
-    {
-    }
-
-    public Part2(IVisualiser<PuzzleState> visualiser) : base(visualiser)
-    {
-    }
-    
     public override string GetAnswer()
     {
         ParseInput();
