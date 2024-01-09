f2145e05c05cd8ad30625d2d111f0b62d2bb07c9
Stevo <stevehuwjohn@gmail.com>
2024-01-09 13:47:40 +0000

    WIP

--- publish-games.sh
+++ publish-games.sh
@@ -4,18 +4,21 @@ dotnet publish ./AoC.Games/AoC.Games.csproj -c Release -r osx-arm64 /p:PublishRe
 dotnet publish ./AoC.Games/AoC.Games.csproj -c Release -r win-x64 /p:PublishReadyToRun=true /p:TieredCompilation=false --self-contained
 cd ./AoC.Games/bin/Release/net8.0/osx-x64/publish
 pwd
+rm *.zip
 zip -r AoC.Games.macOS.Intel.zip *
 cd -
 mv ./AoC.Games/bin/Release/net8.0/osx-x64/publish/AoC.Games.macOS.Intel.zip .
 
 cd ./AoC.Games/bin/Release/net8.0/osx-arm64/publish
 pwd
+rm *.zip
 zip -r AoC.Games.macOS.Apple.zip *
 cd -
-mv ./AoC.Games/bin/Release/net8.0/osx-x64/publish/AoC.Games.macOS.Apple.zip .
+mv ./AoC.Games/bin/Release/net8.0/osx-arm64/publish/AoC.Games.macOS.Apple.zip .
 
 cd ./AoC.Games/bin/Release/net8.0/win-x64/publish
 pwd
+rm *.zip
 zip -r AoC.Games.Windows.Intel.zip *
 cd -
-mv ./AoC.Games/bin/Release/net8.0/osx-x64/publish/AoC.Games.Windows.Intel.zip .
+mv ./AoC.Games/bin/Release/net8.0/win-x64/publish/AoC.Games.Windows.Intel.zip .
