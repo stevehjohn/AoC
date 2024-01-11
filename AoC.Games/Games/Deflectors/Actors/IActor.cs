4eaed33e2a0579aa534fd868b45525994e7bc715
Stevo <stevehuwjohn@gmail.com>
2024-01-11 15:40:32 +0000

    WIP

--- AoC.Games/Games/Deflectors/Actors/IActor.cs
+++ AoC.Games/Games/Deflectors/Actors/IActor.cs
@@ -1,7 +1,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace AoC.Games.Games.Deflectors;
+namespace AoC.Games.Games.Deflectors.Actors;
 
 public interface IActor
 {
