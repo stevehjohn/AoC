23248d0f2f9cc751c907d2b2f8a8c300e8be2ffc
Stevo <stevehuwjohn@gmail.com>
2024-01-15 17:05:12 +0000

    Wip

--- AoC.Games/Games/Mazes/Constants.cs
+++ AoC.Games/Games/Mazes/Constants.cs
@@ -1,10 +1,10 @@
 namespace AoC.Games.Games.Mazes;
 
-public class Constants
+public static class Constants
 {
-    public const int Width = 201;
+    public const int Width = 121;
 
-    public const int Height = 141;
+    public const int Height = 81;
 
-    public const int TileSize = 5;
+    public const int TileSize = 8;
 }
