37f53971747fd01201fb98644928c3ab1ad6f3f9
Stevo John <stevehuwjohn@gmail.com>
2024-01-16 10:07:22 +0000

    Wip

--- AoC.Games/Infrastructure/EntryPoint.cs
+++ AoC.Games/Infrastructure/EntryPoint.cs
@@ -1,13 +1,27 @@
-using AoC.Games.Games.Mazes;
+using Microsoft.Xna.Framework;
 
 namespace AoC.Games.Infrastructure;
 
 public static class EntryPoint
 {
     [STAThread]
-    private static void Main()
+    private static void Main(string[] arguments)
     {
-        var game = new Game();
+        var gameName = arguments.Length > 0 ? arguments[0] : string.Empty;
+
+        Game game;        
+        
+        switch (gameName)
+        {
+            case "mirrors":
+                game = new Games.Deflectors.Game();
+                break;
+            
+            default:
+                game = new Games.Mazes.Game();
+                
+                break;
+        }
         
         game.Run();
     }
