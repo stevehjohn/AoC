5541dd26e634b58574d2b79eca9ba2a11cd1ee54
Stevo John <stevehuwjohn@gmail.com>
2024-12-02 09:52:39 +0000

    Part 1

--- AoC.Solutions/Solutions/2024/02/Part1.cs
+++ AoC.Solutions/Solutions/2024/02/Part1.cs
@@ -7,6 +7,48 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var result = 0;
+        
+        for (var i = 0; i < Input.Length; i++)
+        {
+            var parts = Input[i].Split(' ');
+
+            var sign = 0;
+
+            var safe = true;
+            
+            for (var j = 1; j < parts.Length; j++)
+            {
+                var difference = int.Parse(parts[j - 1]) - int.Parse(parts[j]);
+
+                if (Math.Abs(difference) is < 1 or > 3)
+                {
+                    safe = false;
+                    
+                    break;
+                }
+
+                if (sign == 0)
+                {
+                    sign = difference < 0 ? -1 : 1;
+                }
+                else
+                {
+                    if ((difference < 0 ? -1 : 1) != sign)
+                    {
+                        safe = false;
+                    
+                        break;
+                    }
+                }
+            }
+
+            if (safe)
+            {
+                result++;
+            }
+        }
+
+        return result.ToString();
     }
 }
