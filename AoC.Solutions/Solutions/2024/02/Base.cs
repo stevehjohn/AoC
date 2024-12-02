9f26cd7ee48f50dd6d0e724cc30cc09b7495f1c2
Stevo John <stevehuwjohn@gmail.com>
2024-12-02 10:40:49 +0000

    Refactoring

--- AoC.Solutions/Solutions/2024/02/Base.cs
+++ AoC.Solutions/Solutions/2024/02/Base.cs
@@ -5,4 +5,39 @@ namespace AoC.Solutions.Solutions._2024._02;
 public abstract class Base : Solution
 {
     public override string Description => "Red-Nosed Reports";
+
+    protected static bool IsSafe(string[] levels)
+    {
+        var sign = 0;
+
+        var safe = true;
+        
+        for (var j = 1; j < levels.Length; j++)
+        {
+            var difference = int.Parse(levels[j - 1]) - int.Parse(levels[j]);
+
+            if (Math.Abs(difference) is < 1 or > 3)
+            {
+                safe = false;
+                
+                break;
+            }
+
+            if (sign == 0)
+            {
+                sign = difference < 0 ? -1 : 1;
+            }
+            else
+            {
+                if ((difference < 0 ? -1 : 1) != sign)
+                {
+                    safe = false;
+                
+                    break;
+                }
+            }
+        }
+
+        return safe;
+    }
 }
--- AoC.Solutions/Solutions/2024/02/Part1.cs
+++ AoC.Solutions/Solutions/2024/02/Part1.cs
@@ -11,39 +11,9 @@ public class Part1 : Base
     
         for (var i = 0; i < Input.Length; i++)
         {
-            var parts = Input[i].Split(' ');
-
-            var sign = 0;
-
-            var safe = true;
-        
-            for (var j = 1; j < parts.Length; j++)
-            {
-                var difference = int.Parse(parts[j - 1]) - int.Parse(parts[j]);
-
-                if (Math.Abs(difference) is < 1 or > 3)
-                {
-                    safe = false;
-                
-                    break;
-                }
-
-                if (sign == 0)
-                {
-                    sign = difference < 0 ? -1 : 1;
-                }
-                else
-                {
-                    if ((difference < 0 ? -1 : 1) != sign)
-                    {
-                        safe = false;
-                
-                        break;
-                    }
-                }
-            }
-
-            if (safe)
+            var levels = Input[i].Split(' ');
+            
+            if (IsSafe(levels))
             {
                 result++;
             }
