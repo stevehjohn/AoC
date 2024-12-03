1f9a05e62a5d49149b7d4533bb7997532005a052
Stevo John <stevehuwjohn@gmail.com>
2024-12-03 10:51:21 +0000

    Refactor

--- AoC.Solutions/Solutions/2024/03/Base.cs
+++ AoC.Solutions/Solutions/2024/03/Base.cs
@@ -5,4 +5,37 @@ namespace AoC.Solutions.Solutions._2024._03;
 public abstract class Base : Solution
 {
     public override string Description => "Mull it over";
+
+    protected static (int Index, string Instruction) FindNextMulInstruction(string line, int index)
+    {
+        index = line.IndexOf("mul(", index, StringComparison.InvariantCultureIgnoreCase);
+
+        if (index == -1)
+        {
+            return (-1, null);
+        }
+
+        var end = line.IndexOf(")", index + 1, StringComparison.InvariantCultureIgnoreCase);
+
+        return (index, line[index..(end + 1)]);
+    }
+
+    protected static int ParseMulInstruction(string instruction)
+    {
+        instruction = instruction[4..^1];
+
+        var parts = instruction.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+        if (parts.Length != 2)
+        {
+            return int.MinValue;
+        }
+
+        if (! int.TryParse(parts[0], out var left) || ! int.TryParse(parts[1], out var right))
+        {
+            return int.MinValue;
+        }
+
+        return left * right;
+    }
 }
--- AoC.Solutions/Solutions/2024/03/Part1.cs
+++ AoC.Solutions/Solutions/2024/03/Part1.cs
@@ -9,67 +9,33 @@ public class Part1 : Base
     {
         var answer = 0L;
 
-        for (var i = 0; i < Input.Length; i++)
+        var program = string.Join(string.Empty, Input);
+        
+        var index = 0;
+
+        while (index < program.Length)
         {
-            var index = 0;
+            var result = FindNextMulInstruction(program, index);
 
-            while (index < Input[i].Length)
+            if (result.Index == -1)
             {
-                var result = FindNextMulInstruction(Input[i], index);
-
-                if (result.Index == -1)
-                {
-                    break;
-                }
-
-                var returnValue = ParseMulInstruction(result.Instruction);
-
-                if (returnValue == int.MinValue)
-                {
-                    index += 1;
-
-                    continue;
-                }
-
-                index = result.Index + 1;
-
-                answer += returnValue;
+                break;
             }
+
+            var returnValue = ParseMulInstruction(result.Instruction);
+
+            if (returnValue == int.MinValue)
+            {
+                index += 1;
+
+                continue;
+            }
+
+            index = result.Index + 1;
+
+            answer += returnValue;
         }
 
         return answer.ToString();
-    }
-
-    private static (int Index, string Instruction) FindNextMulInstruction(string line, int index)
-    {
-        index = line.IndexOf("mul(", index, StringComparison.InvariantCultureIgnoreCase);
-
-        if (index == -1)
-        {
-            return (-1, null);
-        }
-
-        var end = line.IndexOf(")", index + 1, StringComparison.InvariantCultureIgnoreCase);
-
-        return (index, line[index..(end + 1)]);
-    }
-
-    private static int ParseMulInstruction(string instruction)
-    {
-        instruction = instruction[4..^1];
-
-        var parts = instruction.Split(',', StringSplitOptions.RemoveEmptyEntries);
-
-        if (parts.Length != 2)
-        {
-            return int.MinValue;
-        }
-
-        if (! int.TryParse(parts[0], out var left) || ! int.TryParse(parts[1], out var right))
-        {
-            return int.MinValue;
-        }
-
-        return left * right;
     }
 }
