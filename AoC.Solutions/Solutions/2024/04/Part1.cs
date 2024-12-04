40fb15fa1425143af4f6118c0f07e408aca377fb
Stevo John <stevehuwjohn@gmail.com>
2024-12-04 10:35:12 +0000

    2024.04 Pt 2 WIP

--- AoC.Solutions/Solutions/2024/04/Part1.cs
+++ AoC.Solutions/Solutions/2024/04/Part1.cs
@@ -5,10 +5,90 @@ namespace AoC.Solutions.Solutions._2024._04;
 [UsedImplicitly]
 public class Part1 : Base
 {
+
+    private readonly (int Left, int Up)[] _directions = [(-1, 0), (0, -1), (1, 0), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)];
+
+    private int _width;
+
+    private int _height;
+    
     public override string GetAnswer()
     {
         var result = ScanPuzzle();
         
         return result.ToString();
+    }
+    
+    private int ScanPuzzle()
+    {
+        _width = Input[0].Length;
+
+        _height = Input.Length;
+        
+        var count = 0;
+        
+        for (var y = 0; y < _height; y++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                count += CheckCell(x, y);
+            }
+        }
+
+        return count;
+    }
+
+    private int CheckCell(int x, int y)
+    {
+        const string word = "XMAS";
+        
+        if (Input[y][x] != word[0])
+        {
+            return 0;
+        }
+
+        var count = 0;
+
+        for (var d = 0; d < _directions.Length; d++)
+        {
+            var direction = _directions[d];
+
+            var found = true;
+            
+            for (var i = 1; i < word.Length; i++)
+            {
+                var checkX = x + direction.Left * i;
+
+                if (checkX < 0 || checkX >= _width)
+                {
+                    found = false;
+                    
+                    break;
+                }
+
+                var checkY = y + direction.Up * i;
+
+                if (checkY < 0 || checkY >= _height)
+                {
+                    found = false;
+                    
+                    break;
+                }
+
+                if (Input[checkY][checkX] != word[i])
+                {
+                    found = false;
+                    
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
--- AoC.Solutions/Solutions/2024/04/Part2.cs
+++ AoC.Solutions/Solutions/2024/04/Part2.cs
@@ -5,8 +5,58 @@ namespace AoC.Solutions.Solutions._2024._04;
 [UsedImplicitly]
 public class Part2 : Base
 {
+    private int _width;
+
+    private int _height;
+
     public override string GetAnswer()
     {
-        return "Unknown";
+        var result = ScanPuzzle();
+        
+        return result.ToString();
+    }
+    
+    private int ScanPuzzle()
+    {
+        _width = Input[0].Length;
+
+        _height = Input.Length;
+        
+        var count = 0;
+        
+        for (var y = 1; y < _height - 1; y++)
+        {
+            for (var x = 1; x < _width - 1; x++)
+            {
+                count += CheckCell(x, y);
+            }
+        }
+
+        return count;
+    }
+    
+    private int CheckCell(int x, int y)
+    {
+        if (Input[y][x] != 'A')
+        {
+            return 0;
+        }
+
+        var count = 0;
+
+        count += CheckOpposingCells(x, y, -1, -1) ? 1 : 0;
+
+        count += CheckOpposingCells(x, y, 1, -1) ? 1 : 0;
+
+        count += CheckOpposingCells(x, y, -1, 1) ? 1 : 0;
+
+        count += CheckOpposingCells(x, y, -1, 1) ? 1 : 0;
+        
+        return count;
+    }
+
+    private bool CheckOpposingCells(int x, int y, int dX, int dY)
+    {
+        return Input[y - dY][x - dX] == 'M' && Input[y + dY][x + dX] == 'S';
     }
 }
