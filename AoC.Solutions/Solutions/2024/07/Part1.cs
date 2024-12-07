cca9c408df5d71b15d1550116d76242238b69049
Stevo John <stevehuwjohn@gmail.com>
2024-12-07 05:14:50 +0000

    2024.07 Pt 1 WIP

--- AoC.Solutions/Solutions/2024/07/Part1.cs
+++ AoC.Solutions/Solutions/2024/07/Part1.cs
@@ -7,6 +7,51 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var result = 0L;
+
+        foreach (var line in Input)
+        {
+            var total = ProcessLine(line);
+
+            if (total > 0)
+            {
+                result += total;
+            }
+        }
+        
+        return result.ToString();
+    }
+
+    private static long ProcessLine(string line)
+    {
+        var parts = line.Split(':', StringSplitOptions.TrimEntries);
+
+        var total = long.Parse(parts[0]);
+
+        var components = parts[1].Split(' ').Select(long.Parse).ToList();
+
+        for (var i = 0; i < components.Count * components.Count; i++)
+        {
+            var test = 0L;
+            
+            for (var j = 0; j < components.Count; j++)
+            {
+                if (((1 << j) & i) == 0)
+                {
+                    test += components[j];
+                }
+                else
+                {
+                    test *= components[j];
+                }
+            }
+
+            if (test == total)
+            {
+                return total;
+            }
+        }
+
+        return 0;
     }
 }
