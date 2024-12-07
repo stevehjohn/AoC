dac4a3d3b1e04b2109f343084cf4b48b62a9747d
Stevo John <stevehuwjohn@gmail.com>
2024-12-07 07:06:20 +0000

    Faster

--- AoC.Solutions/Solutions/2024/07/Base.cs
+++ AoC.Solutions/Solutions/2024/07/Base.cs
@@ -18,7 +18,9 @@ public abstract class Base : Solution
 
             var components = parts[1].Split(' ').Select(long.Parse).ToArray();
 
-            var total = isPart2 ? ProcessLineComplex(expected, components, 1, components[0]) : ProcessLineSimple(expected, components);
+            var total = isPart2 
+                ? ProcessLineComplex(expected, components, 1, components[0]) 
+                : ProcessLineSimple(expected, components, 1, components[0]);
 
             if (total > 0)
             {
@@ -28,39 +30,31 @@ public abstract class Base : Solution
         
         return result.ToString();
     }
-    
-    private static long ProcessLineSimple(long expected, long[] components)
+        
+    private static long ProcessLineSimple(long expected, long[] components, int index, long currentTotal)
     {
-        for (var i = 0; i < Math.Pow(2, components.Length); i++)
+        if (currentTotal > expected)
         {
-            var total = 0L;
-            
-            for (var j = 0; j < components.Length; j++)
-            {
-                if (((1 << j) & i) == 0)
-                {
-                    total += components[j];
-                }
-                else
-                {
-                    total *= components[j];
-                }
-
-                if (total > expected)
-                {
-                    break;
-                }
-            }
-
-            if (total == expected)
-            {
-                return total;
-            }
+            return 0;
         }
 
-        return 0;
+        if (index == components.Length)
+        {
+            return currentTotal == expected ? currentTotal : 0;
+        }
+
+        var result = ProcessLineSimple(expected, components, index + 1, currentTotal + components[index]);
+            
+        if (result > 0)
+        {
+            return result;
+        }
+
+        result = ProcessLineSimple(expected, components, index + 1, currentTotal * components[index]);
+
+        return result;
     }
-    
+
     private static long ProcessLineComplex(long expected, long[] components, int index, long currentTotal)
     {
         if (currentTotal > expected)
