c85add0de028bab7ded51459770089f1fb4bbd9c
Stevo John <stevehuwjohn@gmail.com>
2024-12-08 09:28:04 +0000

    204.08 Pt 1

--- AoC.Solutions/Solutions/2024/08/Base.cs
+++ AoC.Solutions/Solutions/2024/08/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2024._08;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Puzzle 08";
+    public override string Description => "Resonant collinearity";
 }
