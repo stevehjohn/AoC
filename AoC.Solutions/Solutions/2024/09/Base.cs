594130b445f9918e1e1091620ad00c101d9d4e81
Stevo John <stevehuwjohn@gmail.com>
2024-12-08 10:10:45 +0000

    Day 9 prep

---
+++ AoC.Solutions/Solutions/2024/09/Base.cs
@@ -0,0 +1,8 @@
+using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2024._09;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Puzzle 09";
+}
