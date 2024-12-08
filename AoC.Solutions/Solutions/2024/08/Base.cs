f501b31f09435693158131075d8df3940f3b556d
Stevo John <stevehuwjohn@gmail.com>
2024-12-08 09:31:12 +0000

    Refactor

--- AoC.Solutions/Solutions/2024/08/Base.cs
+++ AoC.Solutions/Solutions/2024/08/Base.cs
@@ -5,4 +5,69 @@ namespace AoC.Solutions.Solutions._2024._08;
 public abstract class Base : Solution
 {
     public override string Description => "Resonant collinearity";
+    
+    protected readonly HashSet<(int X, int Y)> AntiNodes = [];
+
+    private readonly List<(int X, int Y, char Frequency)> _antennas = [];
+
+    private int _width;
+
+    private int _height;
+    
+    protected void LocateNodes()
+    {
+        _width = Input[0].Length;
+
+        _height = Input.Length;
+        
+        for (var y = 0; y < _height; y++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                if (Input[y][x] != '.')
+                {
+                    _antennas.Add((x, y, Input[y][x]));
+                }
+            }
+        }
+    }
+
+    protected void CalculateAntiNodes()
+    {
+        foreach (var left in _antennas)
+        {
+            foreach (var right in _antennas)
+            {
+                if (left.Frequency != right.Frequency || left == right)
+                {
+                    continue;
+                }
+
+                var dX = right.X - left.X;
+
+                var dY = right.Y - left.Y;
+
+                AddAntiNode(left, dX, dY);
+            }
+        }
+    }
+
+    private void AddAntiNode((int X, int Y, char Frequency) antenna, int dX, int dY)
+    {
+        var x = antenna.X - dX;
+
+        if (x < 0 || x >= _width)
+        {
+            return;
+        }
+
+        var y = antenna.Y - dY;
+
+        if (y < 0 || y >= _height)
+        {
+            return;
+        }
+        
+        AntiNodes.Add((x, y));
+    }
 }
--- AoC.Solutions/Solutions/2024/08/Part1.cs
+++ AoC.Solutions/Solutions/2024/08/Part1.cs
@@ -5,77 +5,12 @@ namespace AoC.Solutions.Solutions._2024._08;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private readonly List<(int X, int Y, char Frequency)> _antennas = [];
-
-    private readonly HashSet<(int X, int Y)> _antiNodes = [];
-
-    private int _width;
-
-    private int _height;
-    
     public override string GetAnswer()
     {
         LocateNodes();
 
         CalculateAntiNodes();
 
-        return _antiNodes.Count.ToString();
-    }
-
-    private void CalculateAntiNodes()
-    {
-        foreach (var left in _antennas)
-        {
-            foreach (var right in _antennas)
-            {
-                if (left.Frequency != right.Frequency || left == right)
-                {
-                    continue;
-                }
-
-                var dX = right.X - left.X;
-
-                var dY = right.Y - left.Y;
-
-                AddAntiNode(left, dX, dY);
-            }
-        }
-    }
-
-    private void AddAntiNode((int X, int Y, char Frequency) antenna, int dX, int dY)
-    {
-        var x = antenna.X - dX;
-
-        if (x < 0 || x >= _width)
-        {
-            return;
-        }
-
-        var y = antenna.Y - dY;
-
-        if (y < 0 || y >= _height)
-        {
-            return;
-        }
-        
-        _antiNodes.Add((x, y));
-    }
-
-    private void LocateNodes()
-    {
-        _width = Input[0].Length;
-
-        _height = Input.Length;
-        
-        for (var y = 0; y < _height; y++)
-        {
-            for (var x = 0; x < _width; x++)
-            {
-                if (Input[y][x] != '.')
-                {
-                    _antennas.Add((x, y, Input[y][x]));
-                }
-            }
-        }
+        return AntiNodes.Count.ToString();
     }
 }
