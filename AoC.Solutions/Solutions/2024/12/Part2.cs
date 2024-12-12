db63eb38dbaba631416b2cc6d0442bd67a95de60
Stevo John <stevehuwjohn@gmail.com>
2024-12-12 10:52:42 +0000

    2024.12 WIP

--- AoC.Solutions/Solutions/2024/12/Part2.cs
+++ AoC.Solutions/Solutions/2024/12/Part2.cs
@@ -25,6 +25,46 @@ public class Part2 : Base
 
     private int CountEdges((char Plant, List<(int X, int Y)> Cells) region)
     {
-        return 1;
+        var count = 0;
+
+        count += WalkEdge(region.Plant, region.Cells[0].X, region.Cells[0].Y);
+        
+        return count;
+    }
+
+    private int WalkEdge(char plant, int startX, int startY)
+    {
+        var turns = 0;
+        
+        var x = startX;
+
+        var y = startY;
+
+        var dX = 1;
+
+        var dY = 0;
+        
+        do
+        {
+            x += dX;
+
+            y += dY;
+
+            if (x < 0 || x >= Width || y < 0 || y >= Height || Map[x, y] != plant)
+            {
+                x -= dX;
+
+                y -= dY;
+
+                dX = -dY;
+
+                dY = dX;
+
+                turns++;
+            }
+
+        } while (x != startX && y != startY);
+
+        return turns;
     }
 }
