bbff67d65625ddceae91eb309e737ba8ed0eea30
Stevo John <stevehuwjohn@gmail.com>
2024-12-12 10:39:44 +0000

    Refactoring

--- AoC.Solutions/Solutions/2024/12/Base.cs
+++ AoC.Solutions/Solutions/2024/12/Base.cs
@@ -1,3 +1,4 @@
+using AoC.Solutions.Extensions;
 using AoC.Solutions.Infrastructure;
 
 namespace AoC.Solutions.Solutions._2024._12;
@@ -5,4 +6,109 @@ namespace AoC.Solutions.Solutions._2024._12;
 public abstract class Base : Solution
 {
     public override string Description => "Garden groups";
+
+    protected readonly List<(char Plant, List<(int X, int Y)> Cells)> Regions = [];
+
+    private readonly HashSet<(int X, int Y)> _visited = [];
+
+    private readonly Queue<(int X, int Y)> _queue = [];
+    
+    private char[,] _map;
+
+    private int _width;
+
+    private int _height;
+    
+    protected void ParseInput()
+    {
+        _map = Input.To2DArray();
+
+        _width = _map.GetLength(0);
+
+        _height = _map.GetLength(1);
+    }
+
+    protected void FindRegions()
+    {
+        for (var y = 0; y < _height; y++)
+        {
+            for (var x = 0; x < _width; x++)
+            {
+                if (! _visited.Contains((x, y)))
+                {
+                    MapRegion(x, y);
+                }
+            }
+        }
+    }
+
+    protected int GetPerimeter((char Plant, List<(int X, int Y)> Cells) region)
+    {
+        var perimeter = 0;
+        
+        for (var i = 0; i < region.Cells.Count; i++)
+        {
+            var cell = region.Cells[i];
+
+            perimeter += IsEdge(region.Plant, cell.X - 1, cell.Y) ? 1 : 0;
+            perimeter += IsEdge(region.Plant, cell.X + 1, cell.Y) ? 1 : 0;
+            perimeter += IsEdge(region.Plant, cell.X, cell.Y - 1) ? 1 : 0;
+            perimeter += IsEdge(region.Plant, cell.X, cell.Y + 1) ? 1 : 0;
+        }
+
+        return perimeter;
+    }
+    
+    private bool IsEdge(char plant, int x, int y)
+    {
+        if (x < 0 || x >= _width || y < 0 || y >= _height)
+        {
+            return true;
+        }
+
+        return _map[x, y] != plant;
+    }
+
+    private void MapRegion(int x, int y)
+    {
+        var region = new List<(int X, int Y)>();
+
+        var plant = _map[x, y];
+        
+        Regions.Add((plant, region));
+        
+        _queue.Enqueue((x, y));
+
+        while (_queue.Count > 0)
+        {
+            var cell = _queue.Dequeue();
+
+            if (! _visited.Add((cell.X, cell.Y)))
+            {
+                continue;
+            }
+
+            region.Add((cell.X, cell.Y));
+            
+            SafeEnqueue(plant, cell.X + 1, cell.Y);
+            SafeEnqueue(plant, cell.X - 1, cell.Y);
+            SafeEnqueue(plant, cell.X, cell.Y + 1);
+            SafeEnqueue(plant, cell.X, cell.Y - 1);
+        }
+    }
+
+    private void SafeEnqueue(char plant, int x, int y)
+    {
+        if (x < 0 || x >= _width || y < 0 || y >= _height)
+        {
+            return;
+        }
+
+        if (_map[x, y] != plant || _visited.Contains((x, y)))
+        {
+            return;
+        }
+
+        _queue.Enqueue((x, y));
+    }
 }
--- AoC.Solutions/Solutions/2024/12/Part1.cs
+++ AoC.Solutions/Solutions/2024/12/Part1.cs
@@ -6,18 +6,6 @@ namespace AoC.Solutions.Solutions._2024._12;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private readonly HashSet<(int X, int Y)> _visited = [];
-
-    private readonly List<(char Plant, List<(int X, int Y)> Cells)> _regions = [];
-
-    private readonly Queue<(int X, int Y)> _queue = [];
-    
-    private char[,] _map;
-
-    private int _width;
-
-    private int _height;
-    
     public override string GetAnswer()
     {
         ParseInput();
@@ -26,9 +14,9 @@ public class Part1 : Base
 
         var cost = 0;
 
-        for (var i = 0; i < _regions.Count; i++)
+        for (var i = 0; i < Regions.Count; i++)
         {
-            var region = _regions[i];
+            var region = Regions[i];
 
             cost += region.Cells.Count * GetPerimeter(region);
         }
@@ -36,96 +24,4 @@ public class Part1 : Base
         return cost.ToString();
     }
 
-    private int GetPerimeter((char Plant, List<(int X, int Y)> Cells) region)
-    {
-        var perimeter = 0;
-        
-        for (var i = 0; i < region.Cells.Count; i++)
-        {
-            var cell = region.Cells[i];
-
-            perimeter += IsEdge(region.Plant, cell.X - 1, cell.Y) ? 1 : 0;
-            perimeter += IsEdge(region.Plant, cell.X + 1, cell.Y) ? 1 : 0;
-            perimeter += IsEdge(region.Plant, cell.X, cell.Y - 1) ? 1 : 0;
-            perimeter += IsEdge(region.Plant, cell.X, cell.Y + 1) ? 1 : 0;
-        }
-
-        return perimeter;
-    }
-
-    private bool IsEdge(char plant, int x, int y)
-    {
-        if (x < 0 || x >= _width || y < 0 || y >= _height)
-        {
-            return true;
-        }
-
-        return _map[x, y] != plant;
-    }
-
-    private void FindRegions()
-    {
-        for (var y = 0; y < _height; y++)
-        {
-            for (var x = 0; x < _width; x++)
-            {
-                if (! _visited.Contains((x, y)))
-                {
-                    MapRegion(x, y);
-                }
-            }
-        }
-    }
-
-    private void MapRegion(int x, int y)
-    {
-        var region = new List<(int X, int Y)>();
-
-        var plant = _map[x, y];
-        
-        _regions.Add((plant, region));
-        
-        _queue.Enqueue((x, y));
-
-        while (_queue.Count > 0)
-        {
-            var cell = _queue.Dequeue();
-
-            if (! _visited.Add((cell.X, cell.Y)))
-            {
-                continue;
-            }
-
-            region.Add((cell.X, cell.Y));
-            
-            SafeEnqueue(plant, cell.X + 1, cell.Y);
-            SafeEnqueue(plant, cell.X - 1, cell.Y);
-            SafeEnqueue(plant, cell.X, cell.Y + 1);
-            SafeEnqueue(plant, cell.X, cell.Y - 1);
-        }
-    }
-
-    private void SafeEnqueue(char plant, int x, int y)
-    {
-        if (x < 0 || x >= _width || y < 0 || y >= _height)
-        {
-            return;
-        }
-
-        if (_map[x, y] != plant || _visited.Contains((x, y)))
-        {
-            return;
-        }
-
-        _queue.Enqueue((x, y));
-    }
-
-    private void ParseInput()
-    {
-        _map = Input.To2DArray();
-
-        _width = _map.GetLength(0);
-
-        _height = _map.GetLength(1);
-    }
 }
