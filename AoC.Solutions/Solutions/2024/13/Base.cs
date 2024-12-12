30d4b4118e9e6c7e7dcd23ef27cfabd49815d317
Stevo John <stevehuwjohn@gmail.com>
2024-12-12 19:13:40 +0000

    Day 13 prep

---
+++ AoC.Solutions/Solutions/2024/13/Base.cs
@@ -0,0 +1,8 @@
+using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2024._13;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Puzzle 13";
+}
