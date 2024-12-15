ee4a6749913f3e58d13f4763bcb918d667bd6bda
Stevo John <stevehuwjohn@gmail.com>
2024-12-15 13:41:24 +0000

    2024.15.1

--- AoC.Solutions/Solutions/2024/15/Base.cs
+++ AoC.Solutions/Solutions/2024/15/Base.cs
@@ -1,4 +1,3 @@
-using System.Drawing;
 using System.Text;
 using AoC.Solutions.Extensions;
 using AoC.Solutions.Infrastructure;
@@ -13,39 +12,24 @@ public abstract class Base : Solution
     
     private char[,] _map;
 
-    private char[] _pushMask;
-
     private int _width;
 
     private int _height;
 
     private string _directions;
 
-    private Point _robot;
+    private int _robotX;
+
+    private int _robotY;
 
     protected void RunRobot()
     {
-        Console.Clear();
-        
         for (var i = 0; i < _directions.Length; i++)
         {
-            Console.CursorLeft = 0;
-
-            Console.CursorTop = 0;
-            
-            DumpMap();
-            
-            Console.WriteLine();
-            
-            Console.WriteLine($"{i}: {_directions[i]}");
-
-            if (i > 500)
-            {
-                Console.ReadKey();
-            }
-
             MakeMove(_directions[i]);
         }
+            
+        DumpMap();
     }
 
     private void MakeMove(char direction)
@@ -58,36 +42,28 @@ public abstract class Base : Solution
             _ => (1, 0)
         };
 
-        var x = _robot.X + dX;
+        var x = _robotX + dX;
 
-        var y = _robot.Y + dY;
+        var y = _robotY + dY;
 
         var cell = _map[x, y];
-        
-        if (cell == '#')
-        {
-            return;
-        }
 
-        if (cell == '.')
+        switch (cell)
         {
-            _robot.X = x;
+            case '#':
+                return;
 
-            _robot.Y = y;
-            
-            return;
-        }
+            case '.':
+                _robotX = x;
 
-        if (dY != 0 && IsPart2)
-        {
-            TryPushVertically(x, y, dY);
-            
-            return;
+                _robotY = y;
+
+                return;
         }
 
         var i = 1;
-        
-        while (_map[x + i * dX, y + i * dY] is ('O' or '[' or ']') and not '#')
+
+        while (_map[x + i * dX, y + i * dY] is 'O' and not '#')
         {
             i++;
         }
@@ -104,153 +80,9 @@ public abstract class Base : Solution
             _map[x + (i - 1) * dX, y + (i - 1) * dY] = '.';
         }
 
-        _robot.X = x;
+        _robotX = x;
 
-        _robot.Y = y;
-    }
-
-    private void TryPushVertically(int x, int y, int dY)
-    {
-        Array.Fill(_pushMask, '.');
-
-        _pushMask[x] = 'O';
-
-        if (_map[x, y] == '[')
-        {
-            _pushMask[x + 1] = 'O';
-        }
-        else
-        {
-            _pushMask[x - 1] = 'O';
-        }
-
-        var cY = y;
-        
-        var clear = true;
-
-        while (cY > 0 && cY < _height - 1)
-        {
-            cY += dY;
-
-            var pushed = false;
-            
-            for (var i = 2; i < _width - 2; i++)
-            {
-                if (_pushMask[i] == 'O' && _map[i, cY] == '#')
-                {
-                    clear = false;
-                    
-                    break;
-                }
-
-                if (_map[i, cY] is '.' or '#')
-                {
-                    continue;
-                }
-
-                if (_pushMask[i] != 'O')
-                {
-                    continue;
-                }
-
-                _pushMask[i] = 'O';
-                
-                if (_map[i, cY] == '[')
-                {
-                    _pushMask[i + 1] = 'O';
-                }
-                else
-                {
-                    _pushMask[i - 1] = 'O';
-                }
-
-                pushed = true;
-            }
-
-            if (clear && ! pushed)
-            {
-                break;
-            }
-        }
-
-        if (clear)
-        {
-            Console.WriteLine();
-            
-            for (var i = 0; i < _width; i++)
-            {
-                Console.Write(_pushMask[i]);
-            }
-
-            Console.WriteLine($" {cY}");
-            
-            PushVertically(y, dY, cY);
-
-            _robot.X = x;
-
-            _robot.Y = y;
-        }
-    }
-
-    private void PushVertically(int y, int dY, int cY)
-    {
-        while (cY != y)
-        {
-            for (var i = 2; i < _width - 2; i++)
-            {
-                if (_pushMask[i] == 'O' && _map[i, cY] == '.')
-                {
-                    _map[i, cY] = _map[i, cY - dY];
-
-                    _map[i, cY - dY] = '.';
-                }
-            }
-
-            // Console.CursorLeft = 0;
-            //
-            // Console.CursorTop = 0;
-
-            cY -= dY;
-
-            for (var i = 2; i < _width - 2; i++)
-            {
-                if (_pushMask[i] != '.')
-                {
-                    if (_map[i, cY - dY] is ']' or '#')
-                    {
-                        _pushMask[i] = '.';
-                    }
-
-                    break;
-                }
-            }
-            
-            for (var i = _width - 2; i > 2; i--)
-            {
-                if (_pushMask[i] != '.')
-                {
-                    if (_map[i, cY - dY] is '[' or '#')
-                    {
-                        _pushMask[i] = '.';
-                    }
-
-                    break;
-                }
-            }
-            
-            // DumpMap();
-            //
-            // Console.WriteLine();
-            //
-            // for (var i = 0; i < _width; i++)
-            // {
-            //     Console.Write(_pushMask[i]);
-            // }
-            //
-            // Console.WriteLine($" {cY}");
-            //
-            // Console.ReadKey();
-        }
+        _robotY = y;
     }
 
     protected long SumCoordinates()
@@ -277,7 +109,7 @@ public abstract class Base : Solution
         {
             for (var x = 0; x < _width; x++)
             {
-                if (x == _robot.X && y == _robot.Y)
+                if (x == _robotX && y == _robotY)
                 {
                     Console.Write('@');
                     
@@ -303,13 +135,15 @@ public abstract class Base : Solution
         {
             var line = Input[y];
 
-            if (_robot == default)
+            if (_robotX == 0)
             {
                 for (var x = 0; x < _width; x++)
                 {
                     if (line[x] == '@')
                     {
-                        _robot = new Point(x, y);
+                        _robotX = x;
+
+                        _robotY = y;
                         
                         break;
                     }
@@ -324,51 +158,9 @@ public abstract class Base : Solution
 
         _height = y;
 
-        if (IsPart2)
-        {
-            _robot.X *= 2;
+        _map = Input[..y].To2DArray();
 
-            _map = new char[_width * 2, _height];
-
-            for (y = 0; y < _height; y++)
-            {
-                var line = Input[y];
-
-                for (var x = 0; x < _width; x++)
-                {
-                    switch (line[x])
-                    {
-                        case '@':
-                        case '.':
-                            _map[x * 2, y] = '.';
-                            _map[x * 2 + 1, y] = '.';
-                            break;
-
-                        case '#':
-                            _map[x * 2, y] = '#';
-                            _map[x * 2 + 1, y] = '#';
-                            break;
-                        
-                        case 'O':
-                            _map[x * 2, y] = '[';
-                            _map[x * 2 + 1, y] = ']';
-                            break;
-                    }
-                }
-            }
-            
-            _width *= 2;
-
-            _pushMask = new char[_width];
-            
-            DumpMap();
-        }
-        else
-        {
-            _map = Input[..y].To2DArray();
-
-            _map[_robot.X, _robot.Y] = '.';
-        }
+        _map[(int) _robotX, _robotY] = '.';
         
         y++;
 
