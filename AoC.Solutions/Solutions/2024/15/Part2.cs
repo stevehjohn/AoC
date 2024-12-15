ed124e008636311a32605840aaa67ea24540ff9f
Stevo John <stevehuwjohn@gmail.com>
2024-12-15 10:42:32 +0000

    Pt 2 prep

--- AoC.Solutions/Solutions/2024/15/Part2.cs
+++ AoC.Solutions/Solutions/2024/15/Part2.cs
@@ -1,9 +1,18 @@
+using JetBrains.Annotations;
+
 namespace AoC.Solutions.Solutions._2024._15;
 
+[UsedImplicitly]
 public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        ParseInput();
+
+        RunRobot();
+
+        var result = SumCoordinates();
+        
+        return result.ToString();
     }
 }
