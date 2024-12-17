88452475c1d35e66155d9b35fad1e65a147fdedc
Stevo John <stevehuwjohn@gmail.com>
2024-12-17 23:40:29 +0000

    WIP

--- AoC.Solutions/Solutions/2024/16/Part2.cs
+++ AoC.Solutions/Solutions/2024/16/Part2.cs
@@ -10,8 +10,7 @@ public class Part2 : Base
     {
         var graph = new Graph(Input.To2DArray());
 
-        var result = graph.WalkToEnd((start, end, direction, vertex) =>
-            start == end ? 1_000 :
+        var result = graph.WalkToEnd((direction, vertex) =>
             direction == vertex.Heading ? vertex.Distance : 1_000 + vertex.Distance);
 
         return result.ToString();
