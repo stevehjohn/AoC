130c3fa3251fec758a7a98392f231e8235f3128d
Stevo John <stevehuwjohn@gmail.com>
2024-12-18 09:11:31 +0000

    2024.18 WIP

--- AoC.Solutions/Solutions/2024/18/Base.cs
+++ AoC.Solutions/Solutions/2024/18/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2024._18;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Puzzle 17";
+    public override string Description => "RAM run";
 }
