784aabf20d22b9a027d8bece81335bf1e2cf378a
Stevo John <stevehuwjohn@gmail.com>
2024-12-19 20:48:27 +0000

    Puzzle names

--- AoC.Solutions/Solutions/2024/22/Base.cs
+++ AoC.Solutions/Solutions/2024/22/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2024._22;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Puzzle 20";
+    public override string Description => "Puzzle 22";
 }
