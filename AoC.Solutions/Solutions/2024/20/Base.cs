98889ef9362d4f9aad74490c0943ebc30186734b
Stevo John <stevehuwjohn@gmail.com>
2024-12-19 12:49:01 +0000

    WIP

---
+++ AoC.Solutions/Solutions/2024/20/Base.cs
@@ -0,0 +1,8 @@
+using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2024._20;
+
+public abstract class Base : Solution
+{
+    public override string Description => "Puzzle 20";
+}
