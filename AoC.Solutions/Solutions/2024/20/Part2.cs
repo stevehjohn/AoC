442d903d2af4d273221a4e905defef2bb6c01767
Stevo John <stevehuwjohn@gmail.com>
2024-12-20 11:50:11 +0000

    WIP

--- AoC.Solutions/Solutions/2024/20/Part2.cs
+++ AoC.Solutions/Solutions/2024/20/Part2.cs
@@ -9,10 +9,52 @@ public class Part2 : Base
     {
         ParseInput();
 
-        var baseTime = Race();
+        var state = Race();
 
         var count = 0;
+
+        var track = new State[state.Steps];
+
+        var i = 0;
         
+        while (state != null)
+        {
+            track[i] = state;
+            
+            state = state.Previous;
+
+            i++;
+        }
+
+        var cheats = new Dictionary<int, int>();
+
+        for (i = 0; i < track.Length - 1; i++)
+        {
+            var left = track[i];
+
+            for (var j = i + 1; j < track.Length; j++)
+            {
+                var right = track[j];
+                
+                if (left.Position.ManhattanDistance(right.Position) == 2)
+                {
+                    var saving = left.Steps - right.Steps - 2;
+
+                    if (saving < 100)
+                    {
+                        continue;
+                    }
+
+                    if (! cheats.TryAdd(saving, 1))
+                    {
+                        cheats[saving]++;
+                    }
+
+                    count++;
+                }
+            }
+        }
+
         return count.ToString();
     }
 }
