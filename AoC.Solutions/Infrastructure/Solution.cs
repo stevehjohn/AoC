7a50e8bdfd752361401a0f0c7d2e8941b9d13d58
Stevo John <stevehuwjohn@gmail.com>
2024-12-20 16:36:15 +0000

    Only download input if available

--- AoC.Solutions/Infrastructure/Solution.cs
+++ AoC.Solutions/Infrastructure/Solution.cs
@@ -14,7 +14,12 @@ public abstract class Solution
     {
         var nameSpace = GetType().Namespace ?? string.Empty;
 
-        Input = InputProvider.GetInput(nameSpace);
+        var unlock = DateTime.UtcNow.AddHours(-5);
+
+        if (unlock.Day >= int.Parse(GetType().FullName!.Split('.')[^2][1..]))
+        {
+            Input = InputProvider.GetInput(nameSpace);
+        }
     }
 
     public abstract string GetAnswer();
