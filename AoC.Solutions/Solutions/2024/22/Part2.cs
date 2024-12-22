923747dfc899057b3b1c734a67cd529df3830e6b
Stevo John <stevehuwjohn@gmail.com>
2024-12-22 10:36:13 +0000

    2024.22 WIP

--- AoC.Solutions/Solutions/2024/22/Part2.cs
+++ AoC.Solutions/Solutions/2024/22/Part2.cs
@@ -7,6 +7,31 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
+        foreach (var line in Input)
+        {
+            var sequence = GetSequence(long.Parse(line));
+        }
+        
         return "Unknown";
+    }
+    
+    private static List<(long Price, long Delta)> GetSequence(long seed)
+    {
+        var previous = seed % 10;
+        
+        var sequence = new List<(long Price, long Delta)>();
+
+        for (var i = 0; i < 1_999; i++)
+        {
+            seed = SimulateRound(seed);
+
+            var price = seed % 10;
+            
+            sequence.Add((price, price - previous));
+
+            previous = price;
+        }
+
+        return sequence;
     }
 }
