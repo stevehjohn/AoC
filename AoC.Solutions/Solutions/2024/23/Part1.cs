cb2f198410ea17787717aa62ad4711b5c2bd14a4
Stevo John <stevehuwjohn@gmail.com>
2024-12-23 11:12:03 +0000

    2024.23 WIP

--- AoC.Solutions/Solutions/2024/23/Part1.cs
+++ AoC.Solutions/Solutions/2024/23/Part1.cs
@@ -5,13 +5,31 @@ namespace AoC.Solutions.Solutions._2024._23;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private List<(string Left, string Right)> _connections = [];
+    private readonly List<(string Left, string Right)> _connections = [];
     
     public override string GetAnswer()
     {
         ParseInput();
+
+        var result = 0;
+
+        foreach (var connection in _connections)
+        {
+            var leftConnections = _connections.Where(c => c.Right == connection.Left).ToList();
+
+            var rightConnections = _connections.Where(c => c.Left == connection.Right).ToList();
+
+            var union = leftConnections.Union(rightConnections).ToList();
+
+            if (union.Count > 3)// && union.Any(c => c.Left[0] == 't' || c.Right[0] == 't'))
+            {
+                result++;
+            
+                Console.WriteLine(string.Join(',', union));
+            }
+        }
         
-        return "Unknown";
+        return result.ToString();
     }
 
     private void ParseInput()
