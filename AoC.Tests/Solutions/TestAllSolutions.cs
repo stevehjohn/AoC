42e3f239d7dd7414a4adb949729a0c0ad3ab280d
Stevo John <stevehuwjohn@gmail.com>
2025-11-01 14:13:25 +0000

    Tweak

--- AoC.Tests/Solutions/TestAllSolutions.cs
+++ AoC.Tests/Solutions/TestAllSolutions.cs
@@ -25,11 +25,15 @@ public class TestAllSolutions
         {
             path = "./Aoc.Solutions/Solutions/";
             
+            Console.WriteLine($"Loading: {path}");
+            
             _answers = CryptoFileProvider.LoadFile(path, "AllAnswers.clear");
         }
         else
         {
             path = "./Solutions/";
+            
+            Console.WriteLine($"Loading: {path}");
             
             _answers = CryptoFileProvider.LoadFile(path, "AllAnswers.clear");
         }
