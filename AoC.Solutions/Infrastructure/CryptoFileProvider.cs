901475c66652d16ebb841539570b68cc28dfbbc5
Stevo John <stevehuwjohn@gmail.com>
2025-11-01 14:51:01 +0000

    Fix

--- AoC.Solutions/Infrastructure/CryptoFileProvider.cs
+++ AoC.Solutions/Infrastructure/CryptoFileProvider.cs
@@ -45,9 +45,9 @@ public static class CryptoFileProvider
 
             return File.ReadAllLines(clearPath);
         }
-        
-        Console.WriteLine($"Exists: {File.Exists(encryptedPath)}");
 
+        Console.WriteLine(Environment.CurrentDirectory);
+        
         if (File.Exists(encryptedPath) && ! File.Exists(clearPath))
         {
             Decrypt(encryptedPath, clearPath);
