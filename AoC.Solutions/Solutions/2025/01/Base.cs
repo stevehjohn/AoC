c03336acd9172f81b0cd9dbac1c1d29a5302adff
Stevo John <stevehuwjohn@gmail.com>
2025-11-27 16:31:14 +0000

    WIP

--- AoC.Solutions/Solutions/2025/01/Base.cs
+++ AoC.Solutions/Solutions/2025/01/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2025._01;
 
 public abstract class Base : Solution
 {
-    public override string Description => "";
+    public override string Description => "Puzzle 01";
 }
