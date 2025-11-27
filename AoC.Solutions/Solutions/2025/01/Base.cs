a6ac7f803b31ac2caccca687df28d4a40ccd5adf
Stevo John <stevehuwjohn@gmail.com>
2025-11-27 16:28:21 +0000

    2025 prep

---
+++ AoC.Solutions/Solutions/2025/01/Base.cs
@@ -0,0 +1,8 @@
+using AoC.Solutions.Infrastructure;
+
+namespace AoC.Solutions.Solutions._2025._01;
+
+public abstract class Base : Solution
+{
+    public override string Description => "";
+}
---
+++ AoC.Solutions/Solutions/2025/01/Part1.cs
@@ -0,0 +1,9 @@
+namespace AoC.Solutions.Solutions._2025._01;
+
+public class Part1 : Base
+{
+    public override string GetAnswer()
+    {
+        return "Unknown";
+    }
+}
