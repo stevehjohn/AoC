d70e5e9a43fbb91083244f796d54159ecf2f6480
Stevo John <stevehuwjohn@gmail.com>
2025-11-30 11:57:34 +0000

    Pt. 2 prep

--- AoC.Solutions/Solutions/2025/02/Part1.cs
+++ AoC.Solutions/Solutions/2025/02/Part1.cs
@@ -1,9 +1,9 @@
 using JetBrains.Annotations;
 
-namespace AoC.Solutions.Solutions._2025._01;
+namespace AoC.Solutions.Solutions._2025._02;
 
 [UsedImplicitly]
-public class Part1 : Base
+public class Part1 : _01.Base
 {
     public override string GetAnswer()
     {
