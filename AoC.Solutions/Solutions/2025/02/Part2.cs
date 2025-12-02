7f275778ac4944b8b43c375851913ca602bc5bc8
Stevo John <stevehuwjohn@gmail.com>
2025-12-02 09:44:40 +0000

    2025.2.2

--- AoC.Solutions/Solutions/2025/02/Part2.cs
+++ AoC.Solutions/Solutions/2025/02/Part2.cs
@@ -7,6 +7,60 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var ranges = Input[0].Split(',');
+
+        var sum = 0L;
+
+        foreach (var range in ranges)
+        {
+            var ids = range.Split('-');
+
+            var start = long.Parse(ids[0]);
+
+            var end = long.Parse(ids[1]);
+
+            for (var i = start; i <= end; i++)
+            {
+                sum += SumInvalidIPatterns(i);
+            }
+        }
+
+        return sum.ToString();
+    }
+
+    private static long SumInvalidIPatterns(long id)
+    {
+        var text = id.ToString();
+
+        for (var patternLength = 1; patternLength <= text.Length / 2; patternLength++)
+        {
+            if (text.Length % patternLength != 0)
+            {
+                continue;
+            }
+
+            var pattern = text[..patternLength];
+            
+            var isRepeating = true;
+
+            for (var k = patternLength; k < text.Length; k += patternLength)
+            {
+                var segment = text[k..(k + patternLength)];
+            
+                if (segment != pattern)
+                {
+                    isRepeating = false;
+                    
+                    break;
+                }
+            }
+
+            if (isRepeating)
+            {
+                return id;
+            }
+        }
+
+        return 0;
     }
 }
