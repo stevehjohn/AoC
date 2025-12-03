a00fb89f1abc8850355698539e3869f3de739d81
Stevo John <stevehuwjohn@gmail.com>
2025-12-03 10:30:43 +0000

    2025.3.2

--- AoC.Solutions/Solutions/2025/03/Part2.cs
+++ AoC.Solutions/Solutions/2025/03/Part2.cs
@@ -7,6 +7,44 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var joltage = 0L;
+        
+        foreach (var line in Input)
+        {
+            var result = new char[12];
+            
+            var startPosition = 0;
+
+            for (var i = 0; i < 12; i++)
+            {
+                var required = 12 - i - 1;
+                
+                var searchEnd = line.Length - required;
+
+                var highDigit = '0';
+                
+                var highPosition = startPosition;
+
+                for (var j = startPosition; j < searchEnd; j++)
+                {
+                    if (line[j] > highDigit)
+                    {
+                        highDigit = line[j];
+                        
+                        highPosition = j;
+                    }
+                }
+
+                result[i] = highDigit;
+                
+                startPosition = highPosition + 1;
+            }
+
+            var high = long.Parse(new string(result));
+            
+            joltage += high;
+        }
+
+        return joltage.ToString();
     }
 }
