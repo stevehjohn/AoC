5eea0e990a5a0e25db600ffc81c046d9ddf7378e
Stevo John <stevehuwjohn@gmail.com>
2025-12-05 09:35:07 +0000

    2025.05.2

--- AoC.Solutions/Solutions/2025/05/Part2.cs
+++ AoC.Solutions/Solutions/2025/05/Part2.cs
@@ -7,6 +7,37 @@ public class Part2 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        ParseRanges();
+        
+        Ranges.Sort();
+
+        var i = 0;
+
+        while (i < Ranges.Count - 1)
+        {
+            var left = Ranges[i];
+
+            var right = Ranges[i + 1];
+
+            if (left.End >= right.Start)
+            {
+                Ranges[i] = (left.Start, Math.Max(left.End, right.End));
+                
+                Ranges.RemoveAt(i + 1);
+                
+                continue;
+            }
+
+            i++;
+        }
+
+        var fresh = 0L;
+
+        foreach (var range in Ranges)
+        {
+            fresh += range.End - range.Start + 1;
+        }
+
+        return fresh.ToString();
     }
 }
