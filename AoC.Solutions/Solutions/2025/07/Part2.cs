c01488e910aa3f0fd2121ad2e8e62b4c267b7137
Stevo John <stevehuwjohn@gmail.com>
2025-12-06 14:18:57 +0000

    Stuff

---
+++ AoC.Solutions/Solutions/2025/07/Part2.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+
+namespace AoC.Solutions.Solutions._2025._07;
+
+[UsedImplicitly]
+public class Part2 : Base
+{
+    public override string GetAnswer()
+    {
+        return "Unknown";
+    }
+}
