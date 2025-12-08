f5f654848331351669081c3172a14c70c470a823
Stevo John <stevehuwjohn@gmail.com>
2025-12-08 18:18:29 +0000

    Tweaks

--- AoC.Solutions/Solutions/2018/TimeMachine/Cpu.cs
+++ AoC.Solutions/Solutions/2018/TimeMachine/Cpu.cs
@@ -7,15 +7,15 @@ public sealed class Cpu
     private readonly int[] _registers;
 
     private Instruction[] _program = Array.Empty<Instruction>();
-
+    
     private int _programLength;
 
     private int _instructionPointer;
-
+    
     private int _instructionPointerBinding = -1;
 
     private int _breakAt;
-
+    
     private OpCode? _breakOn;
 
     public Cpu(int registerCount)
@@ -26,14 +26,13 @@ public sealed class Cpu
     public void Run(int breakAt = -1, OpCode? breakOn = null)
     {
         _instructionPointer = 0;
-
+        
         _breakAt = breakAt;
-
+        
         _breakOn = breakOn;
 
         Continue();
     }
-
     public void Execute(OpCode opCode, int a, int b, int c)
     {
         _registers[c] = opCode switch
@@ -57,45 +56,45 @@ public sealed class Cpu
             _ => _registers[c]
         };
     }
-
+    
     public void Continue()
     {
         var registers = _registers;
-
+        
         var program = _program;
-
+        
         var length = _programLength;
-
+        
         var instructionPointerBinding = _instructionPointerBinding;
 
-        while ((uint) _instructionPointer < (uint) length)
+        while ((uint)_instructionPointer < (uint)length)
         {
             if (instructionPointerBinding >= 0)
             {
                 registers[instructionPointerBinding] = _instructionPointer;
             }
 
-            ref readonly var instruction = ref program[_instructionPointer];
+            ref readonly var instr = ref program[_instructionPointer];
 
-            registers[instruction.C] = instruction.OpCode switch
+            registers[instr.C] = instr.OpCode switch
             {
-                OpCode.Addr => registers[instruction.A] + registers[instruction.B],
-                OpCode.Addi => registers[instruction.A] + instruction.B,
-                OpCode.Mulr => registers[instruction.A] * registers[instruction.B],
-                OpCode.Muli => registers[instruction.A] * instruction.B,
-                OpCode.Banr => registers[instruction.A] & registers[instruction.B],
-                OpCode.Bani => registers[instruction.A] & instruction.B,
-                OpCode.Borr => registers[instruction.A] | registers[instruction.B],
-                OpCode.Bori => registers[instruction.A] | instruction.B,
-                OpCode.Setr => registers[instruction.A],
-                OpCode.Seti => instruction.A,
-                OpCode.Gtir => instruction.A > registers[instruction.B] ? 1 : 0,
-                OpCode.Gtri => registers[instruction.A] > instruction.B ? 1 : 0,
-                OpCode.Gtrr => registers[instruction.A] > registers[instruction.B] ? 1 : 0,
-                OpCode.Eqir => instruction.A == registers[instruction.B] ? 1 : 0,
-                OpCode.Eqri => registers[instruction.A] == instruction.B ? 1 : 0,
-                OpCode.Eqrr => registers[instruction.A] == registers[instruction.B] ? 1 : 0,
-                _ => registers[instruction.C]
+                OpCode.Addr => registers[instr.A] + registers[instr.B],
+                OpCode.Addi => registers[instr.A] + instr.B,
+                OpCode.Mulr => registers[instr.A] * registers[instr.B],
+                OpCode.Muli => registers[instr.A] * instr.B,
+                OpCode.Banr => registers[instr.A] & registers[instr.B],
+                OpCode.Bani => registers[instr.A] & instr.B,
+                OpCode.Borr => registers[instr.A] | registers[instr.B],
+                OpCode.Bori => registers[instr.A] | instr.B,
+                OpCode.Setr => registers[instr.A],
+                OpCode.Seti => instr.A,
+                OpCode.Gtir => instr.A > registers[instr.B] ? 1 : 0,
+                OpCode.Gtri => registers[instr.A] > instr.B ? 1 : 0,
+                OpCode.Gtrr => registers[instr.A] > registers[instr.B] ? 1 : 0,
+                OpCode.Eqir => instr.A == registers[instr.B] ? 1 : 0,
+                OpCode.Eqri => registers[instr.A] == instr.B ? 1 : 0,
+                OpCode.Eqrr => registers[instr.A] == registers[instr.B] ? 1 : 0,
+                _ => registers[instr.C]
             };
 
             if (_breakAt > -1 && _instructionPointer == _breakAt)
@@ -112,7 +111,7 @@ public sealed class Cpu
 
             _instructionPointer++;
 
-            if (_breakOn.HasValue && instruction.OpCode == _breakOn.Value)
+            if (_breakOn.HasValue && instr.OpCode == _breakOn.Value)
             {
                 _registers.AsSpan().CopyTo(registers);
                 
@@ -132,7 +131,7 @@ public sealed class Cpu
             if (line.StartsWith('#'))
             {
                 _instructionPointerBinding = line[4] - '0';
-
+                
                 continue;
             }
 
@@ -140,7 +139,7 @@ public sealed class Cpu
         }
 
         _program = list.ToArray();
-
+        
         _programLength = _program.Length;
     }
 
@@ -152,9 +151,9 @@ public sealed class Cpu
     public int[] GetRegisters()
     {
         var copy = new int[_registers.Length];
-
+        
         Array.Copy(_registers, 0, copy, 0, _registers.Length);
-
+        
         return copy;
     }
 
@@ -164,14 +163,13 @@ public sealed class Cpu
     private static Instruction ParseLine(string line)
     {
         var parts = line.Split(' ', StringSplitOptions.TrimEntries);
-
         return new Instruction(ParseOpCode(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]), int.Parse(parts[3]));
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static OpCode ParseOpCode(string op) => op switch
     {
-        // ReSharper disable once StringLiteralTypo
+        // ReSharper disable StringLiteralTypo
         "addr" => OpCode.Addr,
         "addi" => OpCode.Addi,
         "mulr" => OpCode.Mulr,
@@ -188,7 +186,7 @@ public sealed class Cpu
         "eqir" => OpCode.Eqir,
         "eqri" => OpCode.Eqri,
         "eqrr" => OpCode.Eqrr,
-        // ReSharper restore once StringLiteralTypo
+        // ReSharper restore StringLiteralTypo
         _ => throw new ArgumentOutOfRangeException(nameof(op), op, "Unknown opcode")
     };
 }
