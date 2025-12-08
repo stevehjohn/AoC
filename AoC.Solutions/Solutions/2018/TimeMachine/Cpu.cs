87c1a2ad323a9c2672dcab75242b8631718bfd80
Stevo John <stevehuwjohn@gmail.com>
2025-12-08 18:27:16 +0000

    Neater

--- AoC.Solutions/Solutions/2018/TimeMachine/Cpu.cs
+++ AoC.Solutions/Solutions/2018/TimeMachine/Cpu.cs
@@ -36,26 +36,25 @@ public sealed class Cpu
 
     public void Execute(OpCode opCode, int a, int b, int c)
     {
-        var instruction = new Instruction(opCode, a, b, c);
-
-        switch (instruction.OpCode)
+        switch (opCode)
         {
-            case OpCode.Addr: _registers[instruction.C] = _registers[instruction.A] + _registers[instruction.B]; break;
-            case OpCode.Addi: _registers[instruction.C] = _registers[instruction.A] + instruction.B; break;
-            case OpCode.Mulr: _registers[instruction.C] = _registers[instruction.A] * _registers[instruction.B]; break;
-            case OpCode.Muli: _registers[instruction.C] = _registers[instruction.A] * instruction.B; break;
-            case OpCode.Banr: _registers[instruction.C] = _registers[instruction.A] & _registers[instruction.B]; break;
-            case OpCode.Bani: _registers[instruction.C] = _registers[instruction.A] & instruction.B; break;
-            case OpCode.Borr: _registers[instruction.C] = _registers[instruction.A] | _registers[instruction.B]; break;
-            case OpCode.Bori: _registers[instruction.C] = _registers[instruction.A] | instruction.B; break;
-            case OpCode.Setr: _registers[instruction.C] = _registers[instruction.A]; break;
-            case OpCode.Seti: _registers[instruction.C] = instruction.A; break;
-            case OpCode.Gtir: _registers[instruction.C] = instruction.A > _registers[instruction.B] ? 1 : 0; break;
-            case OpCode.Gtri: _registers[instruction.C] = _registers[instruction.A] > instruction.B ? 1 : 0; break;
-            case OpCode.Gtrr: _registers[instruction.C] = _registers[instruction.A] > _registers[instruction.B] ? 1 : 0; break;
-            case OpCode.Eqir: _registers[instruction.C] = instruction.A == _registers[instruction.B] ? 1 : 0; break;
-            case OpCode.Eqri: _registers[instruction.C] = _registers[instruction.A] == instruction.B ? 1 : 0; break;
-            case OpCode.Eqrr: _registers[instruction.C] = _registers[instruction.A] == _registers[instruction.B] ? 1 : 0; break;
+            case OpCode.Addr: _registers[c] = _registers[a] + _registers[b]; break;
+            case OpCode.Addi: _registers[c] = _registers[a] + b; break;
+            case OpCode.Mulr: _registers[c] = _registers[a] * _registers[b]; break;
+            case OpCode.Muli: _registers[c] = _registers[a] * b; break;
+            case OpCode.Banr: _registers[c] = _registers[a] & _registers[b]; break;
+            case OpCode.Bani: _registers[c] = _registers[a] & b; break;
+            case OpCode.Borr: _registers[c] = _registers[a] | _registers[b]; break;
+            case OpCode.Bori: _registers[c] = _registers[a] | b; break;
+            case OpCode.Setr: _registers[c] = _registers[a]; break;
+            case OpCode.Seti: _registers[c] = a; break;
+            case OpCode.Gtir: _registers[c] = a > _registers[b] ? 1 : 0; break;
+            case OpCode.Gtri: _registers[c] = _registers[a] > b ? 1 : 0; break;
+            case OpCode.Gtrr: _registers[c] = _registers[a] > _registers[b] ? 1 : 0; break;
+            case OpCode.Eqir: _registers[c] = a == _registers[b] ? 1 : 0; break;
+            case OpCode.Eqri: _registers[c] = _registers[a] == b ? 1 : 0; break;
+            case OpCode.Eqrr: _registers[c] = _registers[a] == _registers[b] ? 1 : 0; break;
+            default: _registers[c] = _registers[c]; break;
         }
     }
 
@@ -76,26 +75,26 @@ public sealed class Cpu
                 registers[instructionPointerBinding] = _instructionPointer;
             }
 
-            ref readonly var instr = ref program[_instructionPointer];
+            ref readonly var instruction = ref program[_instructionPointer];
 
-            switch (instr.OpCode)
+            switch (instruction.OpCode)
             {
-                case OpCode.Addr: registers[instr.C] = registers[instr.A] + registers[instr.B]; break;
-                case OpCode.Addi: registers[instr.C] = registers[instr.A] + instr.B; break;
-                case OpCode.Mulr: registers[instr.C] = registers[instr.A] * registers[instr.B]; break;
-                case OpCode.Muli: registers[instr.C] = registers[instr.A] * instr.B; break;
-                case OpCode.Banr: registers[instr.C] = registers[instr.A] & registers[instr.B]; break;
-                case OpCode.Bani: registers[instr.C] = registers[instr.A] & instr.B; break;
-                case OpCode.Borr: registers[instr.C] = registers[instr.A] | registers[instr.B]; break;
-                case OpCode.Bori: registers[instr.C] = registers[instr.A] | instr.B; break;
-                case OpCode.Setr: registers[instr.C] = registers[instr.A]; break;
-                case OpCode.Seti: registers[instr.C] = instr.A; break;
-                case OpCode.Gtir: registers[instr.C] = instr.A > registers[instr.B] ? 1 : 0; break;
-                case OpCode.Gtri: registers[instr.C] = registers[instr.A] > instr.B ? 1 : 0; break;
-                case OpCode.Gtrr: registers[instr.C] = registers[instr.A] > registers[instr.B] ? 1 : 0; break;
-                case OpCode.Eqir: registers[instr.C] = instr.A == registers[instr.B] ? 1 : 0; break;
-                case OpCode.Eqri: registers[instr.C] = registers[instr.A] == instr.B ? 1 : 0; break;
-                case OpCode.Eqrr: registers[instr.C] = registers[instr.A] == registers[instr.B] ? 1 : 0; break;
+                case OpCode.Addr: registers[instruction.C] = registers[instruction.A] + registers[instruction.B]; break;
+                case OpCode.Addi: registers[instruction.C] = registers[instruction.A] + instruction.B; break;
+                case OpCode.Mulr: registers[instruction.C] = registers[instruction.A] * registers[instruction.B]; break;
+                case OpCode.Muli: registers[instruction.C] = registers[instruction.A] * instruction.B; break;
+                case OpCode.Banr: registers[instruction.C] = registers[instruction.A] & registers[instruction.B]; break;
+                case OpCode.Bani: registers[instruction.C] = registers[instruction.A] & instruction.B; break;
+                case OpCode.Borr: registers[instruction.C] = registers[instruction.A] | registers[instruction.B]; break;
+                case OpCode.Bori: registers[instruction.C] = registers[instruction.A] | instruction.B; break;
+                case OpCode.Setr: registers[instruction.C] = registers[instruction.A]; break;
+                case OpCode.Seti: registers[instruction.C] = instruction.A; break;
+                case OpCode.Gtir: registers[instruction.C] = instruction.A > registers[instruction.B] ? 1 : 0; break;
+                case OpCode.Gtri: registers[instruction.C] = registers[instruction.A] > instruction.B ? 1 : 0; break;
+                case OpCode.Gtrr: registers[instruction.C] = registers[instruction.A] > registers[instruction.B] ? 1 : 0; break;
+                case OpCode.Eqir: registers[instruction.C] = instruction.A == registers[instruction.B] ? 1 : 0; break;
+                case OpCode.Eqri: registers[instruction.C] = registers[instruction.A] == instruction.B ? 1 : 0; break;
+                case OpCode.Eqrr: registers[instruction.C] = registers[instruction.A] == registers[instruction.B] ? 1 : 0; break;
             }
 
             if (_breakAt > -1 && _instructionPointer == _breakAt)
@@ -112,7 +111,7 @@ public sealed class Cpu
 
             _instructionPointer++;
 
-            if (_breakOn.HasValue && instr.OpCode == _breakOn.Value)
+            if (_breakOn.HasValue && instruction.OpCode == _breakOn.Value)
             {
                 _registers.AsSpan().CopyTo(registers);
 
