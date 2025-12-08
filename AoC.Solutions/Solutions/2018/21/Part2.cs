26f55fd94c8066d39645d038df877f22b2023248
Stevo John <stevehuwjohn@gmail.com>
2025-12-08 17:19:47 +0000

    Revert

--- AoC.Solutions/Solutions/2018/21/Part2.cs
+++ AoC.Solutions/Solutions/2018/21/Part2.cs
@@ -1,49 +1,42 @@
-﻿using JetBrains.Annotations;
+﻿using AoC.Solutions.Solutions._2018.TimeMachine;
+using JetBrains.Annotations;
 
 namespace AoC.Solutions.Solutions._2018._21;
 
 [UsedImplicitly]
 public class Part2 : Base
 {
+    // ReSharper disable StringLiteralTypo
     public override string GetAnswer()
     {
-        var seen = new HashSet<int>();
-        
-        var last = 0;
+        var cpu = new Cpu(6);
 
-        var r5 = 0;
+        cpu.LoadProgram(Input);
+
+        var previousValues = new HashSet<int>();
+
+        var breakOn = Input.Single(i => i.StartsWith("eqrr"));
+
+        var register = int.Parse(breakOn.Split(' ', StringSplitOptions.TrimEntries)[1]);
+
+        cpu.Run(-1, "eqrr");
+
+        var last = 0;
 
         while (true)
         {
-            var r3 = r5 | 65536;
-            r5 = 7586220;
+            cpu.Continue();
 
-            while (true)
+            var registerValue = cpu.GetRegister(register);
+
+            if (! previousValues.Add(registerValue))
             {
-                var r1 = r3 & 255;
-                unchecked
-                {
-                    r5 = (r5 + r1) & 0xFFFFFF;
-                    r5 = (r5 * 65899) & 0xFFFFFF;
-                }
-
-                if (r3 < 256)
-                {
-                    break;
-                }
-
-                r3 /= 256;
+                break;
             }
 
-            // At this point, r5 is exactly the value compared against r0 at `eqrr 5 0 1`.
-
-            if (!seen.Add(r5))
-            {
-                // First repeat; last is the final unique value that gives the longest run
-                return last.ToString();
-            }
-
-            last = r5;
+            last = registerValue;
         }
+
+        return last.ToString();
     }
 }
