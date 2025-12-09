0ae5a517b01abc73ba490aae2850f7756780ff49
Stevo John <stevehuwjohn@gmail.com>
2025-12-09 06:44:14 +0000

    2025.09.1 refactor

--- AoC.Solutions/Solutions/2025/09/Base.cs
+++ AoC.Solutions/Solutions/2025/09/Base.cs
@@ -5,4 +5,18 @@ namespace AoC.Solutions.Solutions._2025._09;
 public abstract class Base : Solution
 {
     public override string Description => "Movie theater";
+
+    protected Coordinate[] Coordinates;
+    
+    protected void ParseInput()
+    {
+        Coordinates = new Coordinate[Input.Length];
+
+        var i = 0;
+        
+        foreach (var line in Input)
+        {
+            Coordinates[i++] = Coordinate.Parse(line);
+        }
+    }
 }
--- AoC.Solutions/Solutions/2025/09/Part1.cs
+++ AoC.Solutions/Solutions/2025/09/Part1.cs
@@ -1,4 +1,3 @@
-using AoC.Solutions.Infrastructure;
 using AoC.Solutions.Libraries;
 using JetBrains.Annotations;
 
@@ -7,21 +6,19 @@ namespace AoC.Solutions.Solutions._2025._09;
 [UsedImplicitly]
 public class Part1 : Base
 {
-    private Coordinate[] _coordinates;
-    
     public override string GetAnswer()
     {
         ParseInput();
 
         var largest = 0L;
 
-        for (var l = 0; l < _coordinates.Length - 1; l++)
+        for (var l = 0; l < Coordinates.Length - 1; l++)
         {
-            for (var r = l + 1; r < _coordinates.Length; r++)
+            for (var r = l + 1; r < Coordinates.Length; r++)
             {
-                var left = _coordinates[l];
+                var left = Coordinates[l];
 
-                var right = _coordinates[r];
+                var right = Coordinates[r];
 
                 var area = Measurement.AreaInCells(left, right);
 
@@ -33,17 +30,5 @@ public class Part1 : Base
         }
 
         return largest.ToString();
-    }
-
-    private void ParseInput()
-    {
-        _coordinates = new Coordinate[Input.Length];
-
-        var i = 0;
-        
-        foreach (var line in Input)
-        {
-            _coordinates[i++] = Coordinate.Parse(line);
-        }
     }
 }
