ab82c73cc8b3d423ff8d13a31191d934bbea6c4a
Stevo John <stevehuwjohn@gmail.com>
2025-12-12 10:11:53 +0000

    2025.12

--- AoC.Solutions/Solutions/2025/12/Base.cs
+++ AoC.Solutions/Solutions/2025/12/Base.cs
@@ -4,5 +4,5 @@ namespace AoC.Solutions.Solutions._2025._12;
 
 public abstract class Base : Solution
 {
-    public override string Description => "Puzzle 12";
+    public override string Description => "Christmas tree farm";
 }
--- AoC.Solutions/Solutions/2025/12/Part1.cs
+++ AoC.Solutions/Solutions/2025/12/Part1.cs
@@ -7,6 +7,52 @@ public class Part1 : Base
 {
     public override string GetAnswer()
     {
-        return "Unknown";
+        var areas = new Dictionary<int, int>();
+
+        var i = 0;
+        
+        for (;; i += 5)
+        {
+            if (Input[i][1] != ':')
+            {
+                break;
+            }
+
+            var area = 0;
+            
+            for (var y = 1; y < 4; y++)
+            {
+                area += Input[i + y].Count(c => c == '#');
+            }
+            
+            areas.Add(i / 5, area);
+        }
+
+        var result = 0;
+
+        for (; i < Input.Length; i++)
+        {
+            var line = Input[i];
+
+            var area = int.Parse(line[..2]) * int.Parse(line[3..5]);
+
+            var presentCounts = line[7..].Split(' ');
+
+            var used = 0;
+
+            for (var x = 0; x < presentCounts.Length; x++)
+            {
+                var required = int.Parse(presentCounts[x]);
+
+                used += required * areas[x];
+            }
+
+            if (used < area)
+            {
+                result++;
+            }
+        }
+
+        return result.ToString();
     }
 }
