7e6f40cf292bd6b9472ff488f0d6757e97c225cc
Stevo John <stevehuwjohn@gmail.com>
2025-12-17 15:19:15 +0000

    Optimised

--- AoC.Solutions/Solutions/2020/05/Base.cs
+++ AoC.Solutions/Solutions/2020/05/Base.cs
@@ -8,14 +8,14 @@ public abstract class Base : Solution
 
     protected static (int Row, int Column) GetSeatId(string input)
     {
-        var row = ParseBinarySpace(input[..7]);
+        var row = ParseBinarySpace(input[..7], 64);
 
-        var column = ParseBinarySpace(input[7..]);
+        var column = ParseBinarySpace(input[7..], 4);
 
         return (Row: row, Column: column);
     }
 
-    private static int ParseBinarySpace(string input)
+    private static int ParseBinarySpace(string input, int max)
     {
         var value = 0;
 
@@ -23,7 +23,7 @@ public abstract class Base : Solution
         {
             if (input[i] is 'B' or 'R')
             {
-                value |= 1 << (input.Length - i - 1);
+                value |= max >> i;
             }
         }
         
