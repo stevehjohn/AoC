731f90d99d2dd8f929da883afd3678a94683435c
Stevo John <stevehuwjohn@gmail.com>
2025-12-17 16:56:11 +0000

    Whoop

--- AoC.Solutions/Solutions/2021/03/Part1.cs
+++ AoC.Solutions/Solutions/2021/03/Part1.cs
@@ -9,26 +9,33 @@ public class Part1 : Base
     {
         var length = Input[0].Length;
 
+        var numbers = new int[Input.Length];
+
+        for (var i = 0; i < Input.Length; i++)
+        {
+            numbers[i] = ParseBinary(Input[i]);
+        }
+
         var bit = 1 << (length - 1);
 
         var value = 0;
 
         var half = Input.Length / 2;
-        
+
         for (var i = 0; i < length; i++)
         {
             var ones = 0;
 
             for (var l = 0; l < Input.Length; l++)
             {
-                if (Input[l][i] == '1')
+                if ((numbers[l] & bit) > 0)
                 {
                     ones++;
 
                     if (ones > half)
                     {
                         value += bit;
-                        
+
                         break;
                     }
                 }
@@ -40,5 +47,17 @@ public class Part1 : Base
         var mask = (1 << length) - 1;
 
         return (value * (value ^ mask)).ToString();
+    }
+
+    private static int ParseBinary(string line)
+    {
+        var value = 0;
+
+        for (var i = 0; i < line.Length; i++)
+        {
+            value = (value << 1) | (line[i] == '1' ? 1 : 0);
+        }
+
+        return value;
     }
 }
